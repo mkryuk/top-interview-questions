8b8a8ec0bb39f63142138ef4bfb7bc4467c81f1d
Maxim Kryuk <mkryuk@gmail.com>
2021-11-10 17:57:57 +0200

    Array: Rotate Image rotate solution with explanations

--- src/easy-collection/array/rotate-image/solution.ts
+++ src/easy-collection/array/rotate-image/solution.ts
@@ -2,17 +2,21 @@
  Do not return anything, modify matrix in-place instead.
  */
 export function rotate(matrix: number[][]): void {
-  const matrixLength = matrix.length;
-  const border = matrixLength - 1;
-  const layersCount = Math.ceil(matrixLength / 2);
+  const n = matrix.length;
+  const layersCount = Math.ceil(n / 2);
 
-  for (let layer = 0; layer < layersCount; layer++) {
-    for (let index = layer; index < border - layer; index++) {
-      const temp = matrix[layer][index];
-      matrix[layer][index] = matrix[border - index][layer];
-      matrix[border - index][layer] = matrix[border - layer][border - index];
-      matrix[border - layer][border - index] = matrix[index][border - layer];
-      matrix[index][border - layer] = temp;
+  for (let i = 0; i < layersCount; i++) {
+    for (let j = i; j < n - 1 - i; j++) {
+      // store element from the top of the matrix to the temp
+      const temp = matrix[i][j];
+      // put element from the left to the top
+      matrix[i][j] = matrix[n - 1 - j][i];
+      // put element from the bottom to the left
+      matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+      // put element from the right to the bottom
+      matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+      // put element from the temp (top) to the right
+      matrix[j][n - 1 - i] = temp;
     }
   }
 }

379100bef0f071e25918be7c33268f079a42c04f
Maxim Kryuk <mkryuk@gmail.com>
2021-11-12 14:48:31 +0200

    Strings: First Unique Character in a String init

---
+++ src/easy-collection/strings/first-unique-character-in-a-string/test.spec.ts
@@ -0,0 +1,19 @@
+import { firstUniqChar } from "./solution";
+
+it('firstUniqChar should return 0 for s = "leetcode"', () => {
+  const s = "leetcode";
+  const result = firstUniqChar(s);
+  expect(result).toBe(0);
+});
+
+it('firstUniqChar should return 2 for s = "loveleetcode"', () => {
+  const s = "loveleetcode";
+  const result = firstUniqChar(s);
+  expect(result).toBe(2);
+});
+
+it('firstUniqChar should return -1 for s = "aabb"', () => {
+  const s = "aabb";
+  const result = firstUniqChar(s);
+  expect(result).toBe(-1);
+});

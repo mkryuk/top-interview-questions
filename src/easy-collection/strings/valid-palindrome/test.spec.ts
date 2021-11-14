e73206b70640787348faa697ef55432ee3b72c44
Maxim Kryuk <mkryuk@gmail.com>
2021-11-14 18:21:56 +0200

    String: Valid Palindrome init

---
+++ src/easy-collection/strings/valid-palindrome/test.spec.ts
@@ -0,0 +1,19 @@
+import { isPalindrome } from "./solution";
+
+it('isPalindrome should return true for s = "A man, a plan, a canal: Panama"', () => {
+  const s = "A man, a plan, a canal: Panama";
+  const result = isPalindrome(s);
+  expect(result).toBe(true);
+});
+
+it('isPalindrome should return false for s = "race a car"', () => {
+  const s = "race a car";
+  const result = isPalindrome(s);
+  expect(result).toBe(false);
+});
+
+it('isPalindrome should return true for s = " "', () => {
+  const s = " ";
+  const result = isPalindrome(s);
+  expect(result).toBe(true);
+});

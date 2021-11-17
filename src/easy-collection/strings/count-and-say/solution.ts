823f3a0bcdf53e8bd251385a1d69d641c9e0202f
Maxim Kryuk <mkryuk@gmail.com>
2021-11-17 13:20:38 +0200

    String: Count and Say solution

--- src/easy-collection/strings/count-and-say/solution.ts
+++ src/easy-collection/strings/count-and-say/solution.ts
@@ -1,3 +1,19 @@
 export function countAndSay(n: number): string {
-  return "";
+  if (n === 1) {
+    return "1";
+  }
+  const number = countAndSay(n - 1);
+  let result = "";
+  let count = 0;
+
+  for (let k = 0, i = 0; k < number.length; ) {
+    while (number[i] === number[k]) {
+      count++;
+      i++;
+    }
+    result += `${count}${number[k]}`;
+    k = i;
+    count = 0;
+  }
+  return result;
 }
--- src/easy-collection/strings/count-and-say/test.spec.ts
+++ src/easy-collection/strings/count-and-say/test.spec.ts
@@ -11,3 +11,9 @@ it('countAndSay should return "1211" for n = 4', function () {
   const result = countAndSay(n);
   expect(result).toEqual("1211");
 });
+
+it('countAndSay should return "13211311123113112211" for n = 10', function () {
+  const n = 10;
+  const result = countAndSay(n);
+  expect(result).toEqual("13211311123113112211");
+});

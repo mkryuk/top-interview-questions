d2b986ed224ee0e14e56d63488ceb55598f8455f
Maxim Kryuk <mkryuk@gmail.com>
2021-11-17 14:12:48 +0200

    String: Longest Common Prefix solution

--- src/easy-collection/strings/longest-common-prefix/solution.ts
+++ src/easy-collection/strings/longest-common-prefix/solution.ts
@@ -1,3 +1,25 @@
 export function longestCommonPrefix(strs: string[]): string {
-  return "";
+  let shortestStr = strs[0].length;
+  for (let i = 1; i < strs.length; i++) {
+    if (strs[i].length < shortestStr) {
+      shortestStr = strs[i].length;
+    }
+  }
+
+  let result = "";
+  let temp = "";
+  let mismatch = false;
+  for (let i = 0; i < shortestStr; i++) {
+    temp = strs[0][i];
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[j][i] !== temp) {
+        mismatch = true;
+        break;
+      }
+    }
+    if (!mismatch) {
+      result += temp;
+    }
+  }
+  return result;
 }

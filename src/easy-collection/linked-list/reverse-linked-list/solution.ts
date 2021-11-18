ec91bce88bdf92f0542f329f88c5192390e2ef64
Maxim Kryuk <mkryuk@gmail.com>
2021-11-18 16:52:14 +0200

    Linked List: Reverse Linked List init

---
+++ src/easy-collection/linked-list/reverse-linked-list/solution.ts
@@ -0,0 +1,5 @@
+import { ListNode } from "../common";
+
+export function reverseList(head: ListNode | null): ListNode | null {
+  return null;
+}
---
+++ src/easy-collection/linked-list/reverse-linked-list/test.spec.ts
@@ -0,0 +1,26 @@
+import { arrayToList, listToArray } from "../common";
+import { reverseList } from "./solution";
+
+it("reverseList should return [5,4,3,2,1] for head = [1,2,3,4,5]", function () {
+  const head = [1, 2, 3, 4, 5];
+  const list = arrayToList(head);
+  const result = reverseList(list);
+  const resultArray = listToArray(result);
+  expect(resultArray).toEqual([5, 4, 3, 2, 1]);
+});
+
+it("reverseList should return [2,1] for head = [1,2]", function () {
+  const head = [1, 2];
+  const list = arrayToList(head);
+  const result = reverseList(list);
+  const resultArray = listToArray(result);
+  expect(resultArray).toEqual([2, 1]);
+});
+
+it("reverseList should return [] for head = []", function () {
+  const head: number[] = [];
+  const list = arrayToList(head);
+  const result = reverseList(list);
+  const resultArray = listToArray(result);
+  expect(resultArray).toEqual([]);
+});

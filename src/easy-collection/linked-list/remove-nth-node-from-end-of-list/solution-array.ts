1717ce8cb5efca1103fa4bdf7926a24074396da1
Maxim Kryuk <mkryuk@gmail.com>
2021-11-18 13:23:03 +0200

    Linked List: Remove Nth Node From End of List array solution

--- src/easy-collection/linked-list/remove-nth-node-from-end-of-list/solution-array.ts
+++ src/easy-collection/linked-list/remove-nth-node-from-end-of-list/solution-array.ts
@@ -4,5 +4,19 @@ export function removeNthFromEnd(
   head: ListNode | null,
   n: number
 ): ListNode | null {
-  return null;
+  const listArray: ListNode[] = [];
+  while (head) {
+    listArray.push(head);
+    head = head.next;
+  }
+  if (listArray.length === 1) {
+    return null;
+  }
+  // we remove node from the head
+  if (listArray.length - 1 - n === -1) {
+    return listArray[1];
+  }
+  listArray[listArray.length - 1 - n].next =
+    listArray[listArray.length - n].next;
+  return listArray[0];
 }

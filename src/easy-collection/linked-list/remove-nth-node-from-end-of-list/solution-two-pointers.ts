50b5441c452885fcf5791b2ceb4de9c737a367c3
Maxim Kryuk <mkryuk@gmail.com>
2021-11-18 13:23:20 +0200

    Linked List: Remove Nth Node From End of List two pointers solution

---
+++ src/easy-collection/linked-list/remove-nth-node-from-end-of-list/solution-two-pointers.ts
@@ -0,0 +1,24 @@
+import { ListNode } from "../common";
+
+export function removeNthFromEnd(
+  head: ListNode | null,
+  n: number
+): ListNode | null {
+  let headBehind = new ListNode(0);
+  headBehind.next = head;
+  let second: ListNode | null = headBehind;
+  let first = head;
+
+  for (let i = 0; i < n; i++) {
+    first = first?.next || null;
+  }
+
+  while (first != null) {
+    second = second?.next || null;
+    first = first.next;
+  }
+  if (second && second.next) {
+    second.next = second.next.next;
+  }
+  return headBehind.next;
+}

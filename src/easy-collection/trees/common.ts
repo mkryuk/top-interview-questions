a8b8b5c37a2cf8481d548d69108341e2b585e46d
Maxim Kryuk <mkryuk@gmail.com>
2021-11-24 18:03:42 +0200

    Trees: Maximum Depth of Binary Tree init

---
+++ src/easy-collection/trees/common.ts
@@ -0,0 +1,20 @@
+// Definition for a binary tree node.
+export class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+export function arrayToTreeNode(nodes: (number | null)[]): TreeNode {
+  return new TreeNode();
+}
+
+export function treeNodeToArray(tree: TreeNode): (number | null)[] {
+  return [];
+}

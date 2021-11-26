38d18a3941b7e94fa23791685215a3f6c9be323f
Maxim Kryuk <mkryuk@gmail.com>
2021-11-26 14:26:11 +0200

    Trees: Maximum Depth of Binary Tree solution

--- src/easy-collection/trees/maximum-depth-of-binary-tree/solution.ts
+++ src/easy-collection/trees/maximum-depth-of-binary-tree/solution.ts
@@ -1,5 +1,17 @@
 import { TreeNode } from "../common";
 
 export function maxDepth(root: TreeNode | null): number {
-  return 0;
+  if (!root) {
+    return 0;
+  }
+
+  return getLevel(root, 1);
+}
+
+function getLevel(root: TreeNode | null, level: number): number {
+  let leftLevel = root?.left ? getLevel(root?.left || null, level + 1) : level;
+  let rightLevel = root?.right
+    ? getLevel(root?.right || null, level + 1)
+    : level;
+  return leftLevel > rightLevel ? leftLevel : rightLevel;
 }

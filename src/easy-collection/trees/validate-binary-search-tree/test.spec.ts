00be89449dc10d08a7eed2240fe614d82dfc8f2d
Maxim Kryuk <mkryuk@gmail.com>
2021-11-28 12:55:42 +0200

    Trees: Validate Binary Search Tree solution

--- src/easy-collection/trees/validate-binary-search-tree/test.spec.ts
+++ src/easy-collection/trees/validate-binary-search-tree/test.spec.ts
@@ -14,3 +14,24 @@ it("isValidBST should return false for root = [5,1,4,null,null,3,6]", function (
   const result = isValidBST(tree);
   expect(result).toBeFalse();
 });
+
+it("isValidBST should return false for root = [2,2,2]", function () {
+  const root = [2, 2, 2];
+  const tree = arrayToTreeNode(root);
+  const result = isValidBST(tree);
+  expect(result).toBeFalse();
+});
+
+it("isValidBST should return false for root = [5,4,6,null,null,3,7]", function () {
+  const root = [5, 4, 6, null, null, 3, 7];
+  const tree = arrayToTreeNode(root);
+  const result = isValidBST(tree);
+  expect(result).toBeFalse();
+});
+
+it("isValidBST should return false for root = [0,null,-1]", function () {
+  const root = [0, null, -1];
+  const tree = arrayToTreeNode(root);
+  const result = isValidBST(tree);
+  expect(result).toBeFalse();
+});

12da6a77ed4fedd0bd71e84d10224006bdf9eac0
Maxim Kryuk <mkryuk@gmail.com>
2021-11-29 23:56:39 +0200

    Trees: Symmetric Tree recursive solution

--- src/easy-collection/trees/symmetric-tree/solution.ts
+++ src/easy-collection/trees/symmetric-tree/solution.ts
@@ -1,5 +1,19 @@
 import { TreeNode } from "../common";
 
 export function isSymmetric(root: TreeNode | null): boolean {
-  return false;
+  return isSymmetricNode(root?.left || null, root?.right || null);
+}
+
+function isSymmetricNode(
+  left: TreeNode | null,
+  right: TreeNode | null
+): boolean {
+  if (left === null && right === null) {
+    return true;
+  }
+  return (
+    left?.val === right?.val &&
+    isSymmetricNode(left?.left || null, right?.right || null) &&
+    isSymmetricNode(left?.right || null, right?.left || null)
+  );
 }
--- src/easy-collection/trees/symmetric-tree/test.spec.ts
+++ src/easy-collection/trees/symmetric-tree/test.spec.ts
@@ -14,3 +14,17 @@ it("isSymmetric should return false for root = [1,2,2,null,3,null,3]", function 
   const result = isSymmetric(tree);
   expect(result).toBeFalse();
 });
+
+it("isSymmetric should return false for root = [1,2,2,3,null,3,null]", function () {
+  const root = [1, 2, 2, 3, null, 3, null];
+  const tree = arrayToTreeNode(root);
+  const result = isSymmetric(tree);
+  expect(result).toBeFalse();
+});
+
+it("isSymmetric should return true for root = [1,2,2,3,null,null,3]", function () {
+  const root = [1, 2, 2, 3, null, null, 3];
+  const tree = arrayToTreeNode(root);
+  const result = isSymmetric(tree);
+  expect(result).toBeTrue();
+});

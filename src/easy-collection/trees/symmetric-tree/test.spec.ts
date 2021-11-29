41370e045821793e1477e4d297feb3deda821348
Maxim Kryuk <mkryuk@gmail.com>
2021-11-29 18:36:20 +0200

    Trees: Symmetric Tree init

---
+++ src/easy-collection/trees/symmetric-tree/test.spec.ts
@@ -0,0 +1,16 @@
+import { arrayToTreeNode } from "../common";
+import { isSymmetric } from "./solution";
+
+it("isSymmetric should return true for root = [1,2,2,3,4,4,3]", function () {
+  const root = [1, 2, 2, 3, 4, 4, 3];
+  const tree = arrayToTreeNode(root);
+  const result = isSymmetric(tree);
+  expect(result).toBeTrue();
+});
+
+it("isSymmetric should return false for root = [1,2,2,null,3,null,3]", function () {
+  const root = [1, 2, 2, null, 3, null, 3];
+  const tree = arrayToTreeNode(root);
+  const result = isSymmetric(tree);
+  expect(result).toBeFalse();
+});

b6673b1b0ec367a1de125b499c7fd5885837a534
Maxim Kryuk <mkryuk@gmail.com>
2021-12-04 16:27:08 +0200

    Trees: Binary Tree Level Order Traversal iterative solution

--- src/easy-collection/trees/binary-tree-level-order-traversal/solution-iterative.ts
+++ src/easy-collection/trees/binary-tree-level-order-traversal/solution-iterative.ts
@@ -1,5 +1,30 @@
 import { TreeNode } from "../common";
 
 export function levelOrder(root: TreeNode | null): number[][] {
-  return [];
+  if (!root) {
+    return [];
+  }
+  let queue: (TreeNode | null)[] = [root];
+  let level = 0;
+  let result: number[][] = [];
+  while (queue.length) {
+    let levelLength = queue.length;
+    for (let i = 0; i < levelLength; i++) {
+      if (result.length === level) {
+        result[level] = [];
+      }
+      let node = queue.shift();
+      if (node) {
+        result[level].push(node.val);
+      }
+      if (node?.left) {
+        queue.push(node.left);
+      }
+      if (node?.right) {
+        queue.push(node.right);
+      }
+    }
+    level++;
+  }
+  return result;
 }

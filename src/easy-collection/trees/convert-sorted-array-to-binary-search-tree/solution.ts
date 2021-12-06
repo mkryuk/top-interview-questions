9aeaba7f23fb878bf2d69f218aa280dcf32903c1
Maxim Kryuk <mkryuk@gmail.com>
2021-12-06 19:01:42 +0200

    Trees: Convert Sorted Array to Binary Search Tree solution

--- src/easy-collection/trees/convert-sorted-array-to-binary-search-tree/solution.ts
+++ src/easy-collection/trees/convert-sorted-array-to-binary-search-tree/solution.ts
@@ -1,5 +1,25 @@
 import { TreeNode } from "../common";
 
 export function sortedArrayToBST(nums: number[]): TreeNode | null {
-  return null;
+  let root: TreeNode | null = new TreeNode();
+  root = addNode(root, nums, 0, nums.length - 1);
+  return root;
+}
+
+function addNode(
+  root: TreeNode | null,
+  nums: number[],
+  left: number,
+  right: number
+): TreeNode | null {
+  if (left > right) {
+    return null;
+  }
+  let temp = new TreeNode();
+  let index = Math.ceil((right - left) / 2) + left;
+  temp.val = nums[index];
+  root = temp;
+  root.left = addNode(root.left, nums, left, index - 1);
+  root.right = addNode(root.right, nums, index + 1, right);
+  return root;
 }
--- src/easy-collection/trees/convert-sorted-array-to-binary-search-tree/test.spec.ts
+++ src/easy-collection/trees/convert-sorted-array-to-binary-search-tree/test.spec.ts
@@ -8,6 +8,20 @@ it("sortedArrayToBST should return [0,-3,9,-10,null,5] for nums = [-10,-3,0,5,9]
   expect(resultArray).toEqual([0, -3, 9, -10, null, 5, null]);
 });
 
+it("sortedArrayToBST should return [0,-3,9,-10,null,5,10] for nums = [-10,-3,0,5,9,10]", function () {
+  const nums = [-10, -3, 0, 5, 9, 10];
+  const result = sortedArrayToBST(nums);
+  const resultArray = treeNodeToArray(result);
+  expect(resultArray).toEqual([5, -3, 10, -10, 0, 9, null]);
+});
+
+it("sortedArrayToBST should return [-10,-3,0,5,9,10,12] for nums = [5,-3,10,-10,0,9,12]", function () {
+  const nums = [-10, -3, 0, 5, 9, 10, 12];
+  const result = sortedArrayToBST(nums);
+  const resultArray = treeNodeToArray(result);
+  expect(resultArray).toEqual([5, -3, 10, -10, 0, 9, 12]);
+});
+
 it("sortedArrayToBST should return [3,1] for nums = [1,3]", function () {
   const nums = [1, 3];
   const result = sortedArrayToBST(nums);

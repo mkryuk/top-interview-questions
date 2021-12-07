8510f321aa56356a15a57fe0fb7c76d4073de808
Maxim Kryuk <mkryuk@gmail.com>
2021-12-07 20:44:19 +0200

    Sorting And Searching: Merge Sorted Array solution

--- src/easy-collection/sorting-and-searching/merge-sorted-array/solution.ts
+++ src/easy-collection/sorting-and-searching/merge-sorted-array/solution.ts
@@ -6,4 +6,17 @@ export function merge(
   m: number,
   nums2: number[],
   n: number
-): void {}
+): void {
+  let p1 = m - 1;
+  let p2 = n - 1;
+  for (let i = m + n - 1; i >= 0; i--) {
+    if (p2 < 0) {
+      break;
+    }
+    if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+      nums1[i] = nums1[p1--];
+    } else {
+      nums1[i] = nums2[p2--];
+    }
+  }
+}
--- src/easy-collection/sorting-and-searching/merge-sorted-array/test.spec.ts
+++ src/easy-collection/sorting-and-searching/merge-sorted-array/test.spec.ts
@@ -9,6 +9,15 @@ it("merge should return [1,2,2,3,5,6] for nums1 = [1,2,3,0,0,0], m = 3, nums2 = 
   expect(nums1).toEqual([1, 2, 2, 3, 5, 6]);
 });
 
+it("merge should return [1,2,2,3,4,5,6] for nums1 = [1,2,3,4,0,0,0], m = 3, nums2 = [2,5,6], n = 3", function () {
+  const nums1 = [1, 2, 3, 4, 0, 0, 0],
+    m = 4,
+    nums2 = [2, 5, 6],
+    n = 3;
+  merge(nums1, m, nums2, n);
+  expect(nums1).toEqual([1, 2, 2, 3, 4, 5, 6]);
+});
+
 it("merge should return [1] for nums1 = [1], m = 1, nums2 = [], n = 0", function () {
   const nums1 = [1],
     m = 1,
@@ -25,4 +34,22 @@ it("merge should return [1] for nums1 = [0], m = 0, nums2 = [1], n = 1", functio
     n = 1;
   merge(nums1, m, nums2, n);
   expect(nums1).toEqual([1]);
+});
+
+it("merge should return [1,2] for nums1 = [2,0], m = 1, nums2 = [1], n = 1", function () {
+  const nums1 = [2, 0],
+    m = 1,
+    nums2 = [1],
+    n = 1;
+  merge(nums1, m, nums2, n);
+  expect(nums1).toEqual([1, 2]);
+});
+
+it("merge should return [1,2,3] for nums1 = [2,0,0], m = 1, nums2 = [1,3], n = 2", function () {
+  const nums1 = [2, 0, 0],
+    m = 1,
+    nums2 = [1, 3],
+    n = 2;
+  merge(nums1, m, nums2, n);
+  expect(nums1).toEqual([1, 2, 3]);
 });

e783de01184a6f022371b0a3a9617270f6b6378a
Maxim Kryuk <mkryuk@gmail.com>
2021-12-08 15:43:43 +0200

    Sorting And Searching: First Bad Version solution

--- src/easy-collection/sorting-and-searching/first-bad-version/solution.ts
+++ src/easy-collection/sorting-and-searching/first-bad-version/solution.ts
@@ -7,6 +7,16 @@
 
 export var solution = function (isBadVersion: any) {
   return function (n: number): number {
-    return 0;
+    let left = 1,
+      right = n;
+    while (left <= right) {
+      let index = Math.floor((right - left) / 2) + left;
+      if (isBadVersion(index)) {
+        right = index - 1;
+      } else {
+        left = index + 1;
+      }
+    }
+    return left;
   };
 };

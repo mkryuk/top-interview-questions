afaea4a87f378e4c91c7d1cdf0559ae4e0f87b1b
Maxim Kryuk <mkryuk@gmail.com>
2021-12-15 18:45:54 +0200

    Design: Min Stack using built in array solution

--- src/easy-collection/design/min-stack/solution.ts
+++ src/easy-collection/design/min-stack/solution.ts
@@ -1,15 +1,20 @@
 export class MinStack {
-  constructor() {}
+  constructor(private stack: { val: number; min: number }[] = []) {}
 
-  push(val: number): void {}
+  push(val: number): void {
+    const min = Math.min(this.getMin() ?? Infinity, val);
+    this.stack.push({ val, min });
+  }
 
-  pop(): void {}
+  pop(): void {
+    this.stack.pop();
+  }
 
   top(): number {
-    return 0;
+    return this.stack[this.stack.length - 1].val;
   }
 
   getMin(): number {
-    return 0;
+    return this.stack[this.stack.length - 1]?.min;
   }
 }
--- src/easy-collection/design/min-stack/test.spec.ts
+++ src/easy-collection/design/min-stack/test.spec.ts
@@ -1,5 +1,11 @@
 import { MinStack } from "./solution";
 
+function createStack(nums: number[]): MinStack {
+  const stack = new MinStack();
+  nums.forEach((n) => stack.push(n));
+  return stack;
+}
+
 it("push should push the element to the stack", () => {
   const stack = new MinStack();
   const val = 42;
@@ -8,7 +14,7 @@ it("push should push the element to the stack", () => {
   expect(top).toEqual(val);
 });
 
-it("pop should remove top element fom the stack", () => {
+it("pop should remove top element fom the stack [1,2]", () => {
   const stack = new MinStack();
   const [val1, val2] = [1, 2];
   stack.push(val1);
@@ -20,12 +26,51 @@ it("pop should remove top element fom the stack", () => {
   expect(top2).toEqual(val2);
 });
 
-it("getMin should return min element fom the stack", () => {
-  const stack = new MinStack();
-  const [val1, valMin, val3] = [2, -1, 3];
-  stack.push(val1);
-  stack.push(valMin);
-  stack.push(val3);
+it("getMin should return min element fom the [2,-1,3]", () => {
+  const nums = [2, -1, 3];
+  const stack = createStack(nums);
   const min = stack.getMin();
-  expect(min).toEqual(valMin);
+  expect(min).toEqual(-1);
+});
+
+it("getMin should return first min element fom the [2,-1,3,-5,4]", () => {
+  const nums = [2, -1, 3, -5, 4];
+  const stack = createStack(nums);
+  const min = stack.getMin();
+  expect(min).toEqual(-5);
+});
+
+it("getMin should return second min element fom the [2,-1,3,-5,4]", () => {
+  const nums = [2, -1, 3, -5, 4];
+  const stack = createStack(nums);
+  stack.pop();
+  stack.pop();
+  const min = stack.getMin();
+  expect(min).toEqual(-1);
+});
+
+it("should pass the sequence", () => {
+  const nums = [2147483646, 2147483646, 2147483647];
+  const stack = createStack(nums);
+  let top = stack.top();
+  expect(top).toEqual(2147483647);
+  stack.pop();
+  let min = stack.getMin();
+  expect(min).toEqual(2147483646);
+  stack.pop();
+  min = stack.getMin();
+  expect(min).toEqual(2147483646);
+  stack.pop();
+  stack.push(2147483647);
+  top = stack.top();
+  expect(top).toEqual(2147483647);
+  min = stack.getMin();
+  expect(top).toEqual(2147483647);
+  stack.push(-2147483648);
+  stack.top();
+  min = stack.getMin();
+  expect(min).toEqual(-2147483648);
+  stack.pop();
+  min = stack.getMin();
+  expect(min).toEqual(2147483647);
 });

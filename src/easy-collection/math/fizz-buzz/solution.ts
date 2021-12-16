f21de4c2aeb51757e910a5611888871990f4fe7d
Maxim Kryuk <mkryuk@gmail.com>
2021-12-16 17:35:44 +0200

    Math: Fizz Buzz solution

--- src/easy-collection/math/fizz-buzz/solution.ts
+++ src/easy-collection/math/fizz-buzz/solution.ts
@@ -1,3 +1,17 @@
 export function fizzBuzz(n: number): string[] {
-  return [];
+  const result: string[] = [];
+  for (let i = 1; i <= n; i++) {
+    const divBy3 = i % 3 === 0;
+    const divBy5 = i % 5 === 0;
+    if (divBy3 && divBy5) {
+      result.push("FizzBuzz");
+    } else if (divBy3) {
+      result.push("Fizz");
+    } else if (divBy5) {
+      result.push("Buzz");
+    } else {
+      result.push(i.toString());
+    }
+  }
+  return result;
 }

52d362f8e4540c4c751f04e72a8f11c41986d0b9
Maxim Kryuk <mkryuk@gmail.com>
2021-12-17 19:55:49 +0200

    Math: Count Primes solution

--- src/easy-collection/math/count-primes/solution.ts
+++ src/easy-collection/math/count-primes/solution.ts
@@ -1,3 +1,15 @@
 export function countPrimes(n: number): number {
-  return 0;
+  if (n < 2) {
+    return 0;
+  }
+  let isPrime = new Array(n).fill(1, 2);
+  for (let i = 2; i * i <= n; i++) {
+    if (!isPrime[i]) {
+      continue;
+    }
+    for (let j = i * i; j < n; j += i) {
+      isPrime[j] = 0;
+    }
+  }
+  return isPrime.filter((n) => n === 1).length;
 }
--- src/easy-collection/math/count-primes/test.spec.ts
+++ src/easy-collection/math/count-primes/test.spec.ts
@@ -6,6 +6,12 @@ it("countPrimes should return 4 for n = 10", () => {
   expect(result).toEqual(4);
 });
 
+it("countPrimes should return 10 for n = 29", () => {
+  const n = 29;
+  const result = countPrimes(n);
+  expect(result).toEqual(9);
+});
+
 it("countPrimes should return 0 for n = 0", () => {
   const n = 0;
   const result = countPrimes(n);
@@ -20,6 +26,12 @@ it("countPrimes should return 0 for n = 1", () => {
 
 it("countPrimes should return 1 for n = 2", () => {
   const n = 2;
+  const result = countPrimes(n);
+  expect(result).toEqual(0);
+});
+
+it("countPrimes should return 1 for n = 3", () => {
+  const n = 3;
   const result = countPrimes(n);
   expect(result).toEqual(1);
 });

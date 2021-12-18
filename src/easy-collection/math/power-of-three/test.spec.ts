da855002b0015738d444cccc0d53a60784e55cc4
Maxim Kryuk <mkryuk@gmail.com>
2021-12-18 22:28:53 +0200

    Math: Power of Three init

---
+++ src/easy-collection/math/power-of-three/test.spec.ts
@@ -0,0 +1,25 @@
+import { isPowerOfThree } from "./solution";
+
+it("isPowerOfThree should return true for n = 27", function () {
+  const n = 27;
+  const result = isPowerOfThree(n);
+  expect(result).toEqual(true);
+});
+
+it("isPowerOfThree should return false for n = 0", function () {
+  const n = 0;
+  const result = isPowerOfThree(n);
+  expect(result).toEqual(false);
+});
+
+it("isPowerOfThree should return true for n = 9", function () {
+  const n = 9;
+  const result = isPowerOfThree(n);
+  expect(result).toEqual(true);
+});
+
+it("isPowerOfThree should return true for n = 45", function () {
+  const n = 45;
+  const result = isPowerOfThree(n);
+  expect(result).toEqual(false);
+});

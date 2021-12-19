fd71d86be020969548cd0841efd882a98a100347
Maxim Kryuk <mkryuk@gmail.com>
2021-12-19 12:06:56 +0200

    Math: Power of Three math solution

---
+++ src/easy-collection/math/power-of-three/solution-math.ts
@@ -0,0 +1,3 @@
+export function isPowerOfThree(n: number): boolean {
+  return (Math.log10(n) / Math.log10(3)) % 1 === 0;
+}
---
+++ src/easy-collection/math/power-of-three/test-math.spec.ts
@@ -0,0 +1,31 @@
+import { isPowerOfThree } from "./solution-math";
+
+it("isPowerOfThree should return true for n = 27", function () {
+  const n = 27;
+  const result = isPowerOfThree(n);
+  expect(result).toEqual(true);
+});
+
+it("isPowerOfThree should return false for n = 0", function () {
+  const n = 0;
+  const result = isPowerOfThree(n);
+  expect(result).toEqual(false);
+});
+
+it("isPowerOfThree should return true for n = 1", function () {
+  const n = 1;
+  const result = isPowerOfThree(n);
+  expect(result).toEqual(true);
+});
+
+it("isPowerOfThree should return true for n = 9", function () {
+  const n = 9;
+  const result = isPowerOfThree(n);
+  expect(result).toEqual(true);
+});
+
+it("isPowerOfThree should return true for n = 45", function () {
+  const n = 45;
+  const result = isPowerOfThree(n);
+  expect(result).toEqual(false);
+});

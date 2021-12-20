5391e4a0990aaf0233e63efd00749bb7edcb2327
Maxim Kryuk <mkryuk@gmail.com>
2021-12-20 15:12:51 +0200

    Math: Roman to integer solution

--- src/easy-collection/math/roman-to-integer/solution.ts
+++ src/easy-collection/math/roman-to-integer/solution.ts
@@ -1,3 +1,29 @@
 export function romanToInt(s: string): number {
-  return 0;
+  const romans = new Map<string, number>([
+    ["I", 1],
+    ["V", 5],
+    ["X", 10],
+    ["L", 50],
+    ["C", 100],
+    ["D", 500],
+    ["M", 1000],
+  ]);
+  const exceptions = new Map<string, string[]>([
+    ["I", ["V", "X"]],
+    ["X", ["L", "C"]],
+    ["C", ["D", "M"]],
+  ]);
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (
+      exceptions.has(s[i]) &&
+      i < s.length - 1 &&
+      exceptions.get(s[i])?.includes(s[i + 1])
+    ) {
+      result -= romans.get(s[i]) ?? 0;
+    } else {
+      result += romans.get(s[i]) ?? 0;
+    }
+  }
+  return result;
 }
--- src/easy-collection/math/roman-to-integer/test.spec.ts
+++ src/easy-collection/math/roman-to-integer/test.spec.ts
@@ -9,7 +9,7 @@ it('romanToInt should return 3 for s = "III"', function () {
 it('romanToInt should return 4 for s = "IV"', function () {
   const s = "IV";
   const result = romanToInt(s);
-  expect(result).toEqual(3);
+  expect(result).toEqual(4);
 });
 
 it('romanToInt should return 58 for s = "LVIII"', function () {
@@ -22,4 +22,16 @@ it('romanToInt should return 1994 for s = "MCMXCIV"', function () {
   const s = "MCMXCIV";
   const result = romanToInt(s);
   expect(result).toEqual(1994);
+});
+
+it('romanToInt should return 1999 for s = "MCMXCIX"', function () {
+  const s = "MCMXCIX";
+  const result = romanToInt(s);
+  expect(result).toEqual(1999);
+});
+
+it('romanToInt should return 1444 for s = "MCDXLIV"', function () {
+  const s = "MCDXLIV";
+  const result = romanToInt(s);
+  expect(result).toEqual(1444);
 });

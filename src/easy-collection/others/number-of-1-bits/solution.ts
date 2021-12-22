5dca024b471f94a4a7185d59632f34ae289ab3d4
Maxim Kryuk <mkryuk@gmail.com>
2021-12-22 13:16:10 +0200

    Other: Number of 1 Bits solution

--- src/easy-collection/others/number-of-1-bits/solution.ts
+++ src/easy-collection/others/number-of-1-bits/solution.ts
@@ -1,3 +1,11 @@
 export function hammingWeight(n: number): number {
-  return 0;
+  let result = 0;
+  let mask = 1;
+  for (let i = 0; i < 32; i++) {
+    if (n & mask) {
+      result++;
+    }
+    mask <<= 1;
+  }
+  return result;
 }

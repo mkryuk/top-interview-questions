9196ead0e5a3cb110271873ff44c3dbf544c595e
Maxim Kryuk <mkryuk@gmail.com>
2021-12-22 16:28:43 +0200

    Other: Hamming Distance solution

--- src/easy-collection/others/hamming-distance/solution.ts
+++ src/easy-collection/others/hamming-distance/solution.ts
@@ -1,3 +1,9 @@
 export function hammingDistance(x: number, y: number): number {
-  return 0;
+  let result = 0;
+  let xor = x ^ y;
+  while (xor !== 0) {
+    xor &= xor - 1;
+    result++;
+  }
+  return result;
 }

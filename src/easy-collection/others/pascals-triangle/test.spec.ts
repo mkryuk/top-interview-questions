c5bd7c4f2905ff67cdc484b075e779e83b5a89b6
Maxim Kryuk <mkryuk@gmail.com>
2021-12-24 12:33:05 +0200

    Others: Pascal's Triangle init

---
+++ src/easy-collection/others/pascals-triangle/test.spec.ts
@@ -0,0 +1,19 @@
+import { generate } from "./solution";
+
+it("generate should return [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]] for numRows = 5", function () {
+  const numRows = 5;
+  const result = generate(numRows);
+  expect(result).toEqual([
+    [1],
+    [1, 1],
+    [1, 2, 1],
+    [1, 3, 3, 1],
+    [1, 4, 6, 4, 1],
+  ]);
+});
+
+it("generate should return [[1]] for numRows = 1", function () {
+  const numRows = 1;
+  const result = generate(numRows);
+  expect(result).toEqual([[1]]);
+});

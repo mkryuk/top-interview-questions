19da21d7896741c96890676b0fb9f1d2ba7ba949
Maxim Kryuk <mkryuk@gmail.com>
2021-12-26 19:55:42 +0200

    Others: Pascal's Triangle solution

--- src/easy-collection/others/pascals-triangle/solution.ts
+++ src/easy-collection/others/pascals-triangle/solution.ts
@@ -1,3 +1,16 @@
 export function generate(numRows: number): number[][] {
-  return [];
+  let result: number[][] = [];
+  for (let i = 0; i < numRows; i++) {
+    let row: number[] = [];
+    for (let j = 0; j < i + 1; j++) {
+      if (j === 0 || j === i) {
+        row.push(1);
+      } else {
+        let value = result[i - 1][j - 1] + result[i - 1][j];
+        row.push(value);
+      }
+    }
+    result.push(row);
+  }
+  return result;
 }
--- src/easy-collection/others/pascals-triangle/test.spec.ts
+++ src/easy-collection/others/pascals-triangle/test.spec.ts
@@ -12,6 +12,20 @@ it("generate should return [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]] for numRows
   ]);
 });
 
+it("generate should return [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1],[1,5,10,10,5,1],[1,6,15,20,15,6,1],] for numRows = 7", function () {
+  const numRows = 7;
+  const result = generate(numRows);
+  expect(result).toEqual([
+    [1],
+    [1, 1],
+    [1, 2, 1],
+    [1, 3, 3, 1],
+    [1, 4, 6, 4, 1],
+    [1, 5, 10, 10, 5, 1],
+    [1, 6, 15, 20, 15, 6, 1],
+  ]);
+});
+
 it("generate should return [[1]] for numRows = 1", function () {
   const numRows = 1;
   const result = generate(numRows);

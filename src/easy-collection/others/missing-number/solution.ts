9acb083f1d2b54027b08db149802eff9c2f5191c
Maxim Kryuk <mkryuk@gmail.com>
2021-12-27 22:20:33 +0200

    Others: Missing Number solution

--- src/easy-collection/others/missing-number/solution.ts
+++ src/easy-collection/others/missing-number/solution.ts
@@ -1,3 +1,10 @@
 export function missingNumber(nums: number[]): number {
-  return 0;
+  const numSet = new Set<number>([]);
+  for (let i = 0; i <= nums.length; i++) {
+    numSet.add(i);
+  }
+  for (let i = 0; i < nums.length; i++) {
+    numSet.delete(nums[i]);
+  }
+  return numSet.values().next().value;
 }

195e017c829ff0e4355f5e009c3ec95f5b96a41e
Maxim Kryuk <mkryuk@gmail.com>
2021-12-29 18:14:52 +0200

    Array and Strings: 3Sum hash map solution

--- src/medium-collection/array-and-strings/3sum/solution.ts
+++ src/medium-collection/array-and-strings/3sum/solution.ts
@@ -1,3 +1,26 @@
 export function threeSum(nums: number[]): number[][] {
-  return [];
+  nums.sort();
+  let result: number[][] = [];
+  for (let i = 0; i < nums.length && nums[i] <= 0; i++) {
+    // skip equal numbers to avoid duplicate triples
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+    twoSum(i, nums, result);
+  }
+  return result;
+}
+
+function twoSum(i: number, nums: number[], result: number[][]) {
+  let hashMap = new Map();
+  for (let j = i + 1; j < nums.length; j++) {
+    if (hashMap.has(nums[j])) {
+      result.push([nums[i], nums[hashMap.get(nums[j])], nums[j]]);
+      // skip equal numbers to avoid duplicate triples
+      while (j + 1 < nums.length && nums[j] === nums[j + 1]) {
+        j++;
+      }
+    }
+    hashMap.set(0 - nums[i] - nums[j], j);
+  }
 }
--- src/medium-collection/array-and-strings/3sum/test.spec.ts
+++ src/medium-collection/array-and-strings/3sum/test.spec.ts
@@ -4,8 +4,8 @@ it("threeSum should return [[-1,-1,2],[-1,0,1]] for nums = [-1,0,1,2,-1,-4]", fu
   const nums = [-1, 0, 1, 2, -1, -4];
   const result = threeSum(nums);
   expect(result).toEqual([
-    [-1, -1, 2],
     [-1, 0, 1],
+    [-1, -1, 2],
   ]);
 });
 

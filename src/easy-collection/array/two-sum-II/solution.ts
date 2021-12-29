280eb81d6e84583e7ac6fdd9e8d1abdfd1479fee
Maxim Kryuk <mkryuk@gmail.com>
2021-12-29 19:04:48 +0200

    Array and Strings: Two Sum II - Input Array Is Sorted solution

--- src/easy-collection/array/two-sum-II/solution.ts
+++ src/easy-collection/array/two-sum-II/solution.ts
@@ -1,3 +1,15 @@
 export function twoSum(numbers: number[], target: number): number[] {
-  return [];
+  let low = 0,
+    high = numbers.length - 1;
+  while (low < high) {
+    const sum = numbers[low] + numbers[high];
+    if (sum < target) {
+      low++;
+    } else if (sum > target) {
+      high--;
+    } else {
+      return [low + 1, high + 1];
+    }
+  }
+  return [-1, -1];
 }
--- src/easy-collection/array/two-sum-II/test.spec.ts
+++ src/easy-collection/array/two-sum-II/test.spec.ts
@@ -7,6 +7,13 @@ it("twoSum should return [1,2] for numbers = [2,7,11,15], target = 9", function 
   expect(result).toEqual([1, 2]);
 });
 
+it("twoSum should return [1,2] for numbers = [2,7,11,15], target = 9", function () {
+  const numbers = [-5, -1, 0, 2, 3, 5, 7, 11, 15],
+    target = 9;
+  const result = twoSum(numbers, target);
+  expect(result).toEqual([4, 7]);
+});
+
 it("twoSum should return [1,3] for numbers = [2,3,4], target = 6", function () {
   const numbers = [2, 3, 4],
     target = 6;

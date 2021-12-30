696b7380dde54389013a789f4a8a39ce3b7b8b46
Maxim Kryuk <mkryuk@gmail.com>
2021-12-30 13:05:08 +0200

    Array and Strings: Set Matrix Zeroes init

---
+++ src/medium-collection/array-and-strings/set-matrix-zeroes/solution.ts
@@ -0,0 +1,4 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function setZeroes(matrix: number[][]): void {}
---
+++ src/medium-collection/array-and-strings/set-matrix-zeroes/test.spec.ts
@@ -0,0 +1,27 @@
+it("setZeroes should modify matrix = [[1,1,1],[1,0,1],[1,1,1]] to [[1,0,1],[0,0,0],[1,0,1]]", function () {
+  const matrix = [
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+  ];
+  setZeroes(matrix);
+  expect(matrix).toEqual([
+    [1, 0, 1],
+    [0, 0, 0],
+    [1, 0, 1],
+  ]);
+});
+
+it("setZeroes should modify matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]] to [[0,0,0,0],[0,4,5,0],[0,3,1,0]]", function () {
+  const matrix = [
+    [0, 1, 2, 0],
+    [3, 4, 5, 2],
+    [1, 3, 1, 5],
+  ];
+  setZeroes(matrix);
+  expect(matrix).toEqual([
+    [0, 0, 0, 0],
+    [0, 4, 5, 0],
+    [0, 3, 1, 0],
+  ]);
+});

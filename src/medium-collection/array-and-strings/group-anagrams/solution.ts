1a9c4fc4a6edaeb51ef58996cab0f1b69a5ce3b4
Maxim Kryuk <mkryuk@gmail.com>
2021-12-31 18:29:04 +0200

    Array and Strings: Group Anagrams solution

--- src/medium-collection/array-and-strings/group-anagrams/solution.ts
+++ src/medium-collection/array-and-strings/group-anagrams/solution.ts
@@ -1,3 +1,46 @@
 export function groupAnagrams(strs: string[]): string[][] {
-  return [];
+  const strMap = new Map<number, string[]>();
+  const result: string[][] = [];
+  strs.forEach((s) => {
+    const length = s.length;
+    if (strMap.has(length)) {
+      const temp = strMap.get(length) ?? [];
+      temp.push(s);
+      strMap.set(length, temp);
+    } else {
+      strMap.set(length, [s]);
+    }
+  });
+
+  strMap.forEach((strings) => {
+    let temp = strings;
+    while (temp.length) {
+      const first = temp.pop() ?? "";
+      let tempResult = [];
+      tempResult.push(first);
+      temp = temp.filter((s) => {
+        if (areAnagrams(first, s)) {
+          tempResult.push(s);
+          return false;
+        }
+        return true;
+      });
+      result.push(tempResult);
+    }
+  });
+  return result;
+}
+
+function areAnagrams(str1: string, str2: string) {
+  const arr1 = str1.split("").sort();
+  const arr2 = str2.split("").sort();
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 }
--- src/medium-collection/array-and-strings/group-anagrams/test.spec.ts
+++ src/medium-collection/array-and-strings/group-anagrams/test.spec.ts
@@ -6,6 +6,17 @@ it('groupAnagrams should return [["bat"],["nat","tan"],["ate","eat","tea"]] for 
   expect(result).toEqual([["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]);
 });
 
+it('groupAnagrams should return [["squad","quads"],["fats","fast"],["bat"],["tea","ate"]] for strs = ["quads","fast","fats","squad","ate","tea","bat"]', function () {
+  const strs = ["quads", "fast", "fats", "squad", "ate", "tea", "bat"];
+  const result = groupAnagrams(strs);
+  expect(result).toEqual([
+    ["squad", "quads"],
+    ["fats", "fast"],
+    ["bat"],
+    ["tea", "ate"],
+  ]);
+});
+
 it('groupAnagrams should return [[""]] for strs = [""]', function () {
   const strs = [""];
   const result = groupAnagrams(strs);

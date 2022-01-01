0894de92f613448af4b6d35d5bade33865827581
Maxim Kryuk <mkryuk@gmail.com>
2022-01-02 00:27:33 +0200

    Array and Strings: Longest Substring Without Repeating Characters solution

--- src/medium-collection/array-and-strings/longest-substring/solution.ts
+++ src/medium-collection/array-and-strings/longest-substring/solution.ts
@@ -1,3 +1,34 @@
 export function lengthOfLongestSubstring(s: string): number {
-  return 0;
+  let jumpMap = new Map<string, number>();
+  let jumpTable = new Array(s.length).fill(0);
+  // we create map where to jump when we found duplicate character
+  // for instance for "dvdfa" we will have [0, 0, 1, 0, 0]
+  // that means that after we get second d
+  // we should start to check from 1 index
+  for (let i = 0; i < s.length; i++) {
+    if (jumpMap.has(s[i])) {
+      const index = jumpMap.get(s[i]) ?? 0;
+      jumpTable[i] = index + 1;
+    }
+    jumpMap.set(s[i], i);
+  }
+  let strMap = new Set<string>();
+  let longest = 0;
+  let currentLength = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (strMap.has(s[i])) {
+      strMap = new Set<string>();
+      currentLength = 1;
+      if (jumpTable[i] !== 0) {
+        let ti = i;
+        i = jumpTable[i];
+        jumpTable[ti] = 0;
+      }
+    } else {
+      currentLength++;
+    }
+    strMap.add(s[i]);
+    longest = currentLength > longest ? currentLength : longest;
+  }
+  return longest;
 }
--- src/medium-collection/array-and-strings/longest-substring/test.spec.ts
+++ src/medium-collection/array-and-strings/longest-substring/test.spec.ts
@@ -17,3 +17,15 @@ it('lengthOfLongestSubstring should return 3 for s = "pwwkew"', function () {
   const result = lengthOfLongestSubstring(s);
   expect(result).toEqual(3);
 });
+
+it('lengthOfLongestSubstring should return 5 for s = "pwwkexwabcd"', function () {
+  const s = "pwwkexwabcd";
+  const result = lengthOfLongestSubstring(s);
+  expect(result).toEqual(8);
+});
+
+it('lengthOfLongestSubstring should return 3 for s = "dvdf"', function () {
+  const s = "dvdf";
+  const result = lengthOfLongestSubstring(s);
+  expect(result).toEqual(3);
+});

304945b8cedaab8d2e376d07f4a239df742e0ca4
Maxim Kryuk <mkryuk@gmail.com>
2022-01-01 21:18:03 +0200

    Array and Strings: Longest Substring Without Repeating Characters init

---
+++ src/medium-collection/array-and-strings/longest-substring/test.spec.ts
@@ -0,0 +1,19 @@
+import { lengthOfLongestSubstring } from "./solution";
+
+it('lengthOfLongestSubstring should return 3 for s = "abcabcbb"', function () {
+  const s = "abcabcbb";
+  const result = lengthOfLongestSubstring(s);
+  expect(result).toEqual(3);
+});
+
+it('lengthOfLongestSubstring should return 1 for s = "bbbbb"', function () {
+  const s = "bbbbb";
+  const result = lengthOfLongestSubstring(s);
+  expect(result).toEqual(1);
+});
+
+it('lengthOfLongestSubstring should return 3 for s = "pwwkew"', function () {
+  const s = "pwwkew";
+  const result = lengthOfLongestSubstring(s);
+  expect(result).toEqual(3);
+});

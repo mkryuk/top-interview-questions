2cc4b1a059867da724dc1a4b7d643374fb1749c0
Maxim Kryuk <mkryuk@gmail.com>
2022-01-07 20:54:48 +0200

    Array and Strings: Missing Ranges solution

--- src/medium-collection/array-and-strings/missing-ranges/solution.ts
+++ src/medium-collection/array-and-strings/missing-ranges/solution.ts
@@ -3,5 +3,35 @@ export function findMissingRanges(
   lower: number,
   upper: number
 ): string[] {
-  return [];
+  let result: string[] = [];
+  if (nums.length === 0) {
+    const diff = upper - lower;
+    return diff > 1
+      ? [`${lower}->${upper}`]
+      : diff === 1
+      ? [`${lower}`, `${upper}`]
+      : [`${lower}`];
+  }
+  if (lower < nums[0]) {
+    let record = nums[0] - lower > 1 ? `${lower}->${nums[0] - 1}` : `${lower}`;
+    result.push(record);
+  }
+  const lastIndex = nums.length - 1;
+  for (let i = 0; i < lastIndex; i++) {
+    const diff = nums[i + 1] - nums[i];
+    if (diff > 1) {
+      let record =
+        diff === 2 ? `${nums[i] + 1}` : `${nums[i] + 1}->${nums[i + 1] - 1}`;
+      result.push(record);
+    }
+  }
+
+  if (upper > nums[lastIndex]) {
+    let record =
+      upper - nums[lastIndex] > 1
+        ? `${nums[lastIndex] + 1}->${upper}`
+        : `${upper}`;
+    result.push(record);
+  }
+  return result;
 }

bbf9291f20b3ecb32e2b9e05b18cc45d4c4f0588
Maxim Kryuk <mkryuk@gmail.com>
2022-01-10 17:54:14 +0200

    Linked List: Add Two Numbers solution

--- src/medium-collection/linked-list/add-two-numbers/solution.ts
+++ src/medium-collection/linked-list/add-two-numbers/solution.ts
@@ -4,5 +4,32 @@ export function addTwoNumbers(
   l1: ListNode | null,
   l2: ListNode | null
 ): ListNode | null {
-  return null;
+  let result: ListNode | null = null;
+  let currentNode: ListNode | null = null;
+  let overflow = 0;
+  while (l1 || l2 || overflow !== 0) {
+    let left = l1?.val ?? 0;
+    let right = l2?.val ?? 0;
+    let sum = left + right + overflow;
+    let val = sum % 10;
+    let tempNode = new ListNode();
+    tempNode.val = val;
+    if (result === null) {
+      result = tempNode;
+      currentNode = result;
+    } else {
+      if (currentNode) {
+        currentNode.next = tempNode;
+        currentNode = currentNode.next;
+      }
+    }
+    overflow = Math.trunc(sum / 10);
+    if (l1) {
+      l1 = l1.next;
+    }
+    if (l2) {
+      l2 = l2.next;
+    }
+  }
+  return result;
 }

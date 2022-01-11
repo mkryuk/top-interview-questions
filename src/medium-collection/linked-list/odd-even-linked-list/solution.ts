57cc470f4b93f1423ec4eeb9eeb6a37702d99191
Maxim Kryuk <mkryuk@gmail.com>
2022-01-11 19:37:57 +0200

    Linked List: Odd Even Linked List solution

--- src/medium-collection/linked-list/odd-even-linked-list/solution.ts
+++ src/medium-collection/linked-list/odd-even-linked-list/solution.ts
@@ -1,5 +1,18 @@
 import { ListNode } from "../common";
 
 export function oddEvenList(head: ListNode | null): ListNode | null {
-  return null;
+  if (head === null) {
+    return null;
+  }
+  let oddTail = head;
+  let evenHead = oddTail?.next ?? null;
+  let evenTail = evenHead;
+  while (oddTail && evenTail && evenTail?.next) {
+    oddTail.next = evenTail.next;
+    oddTail = oddTail.next;
+    evenTail.next = oddTail.next;
+    evenTail = evenTail.next;
+  }
+  oddTail.next = evenHead;
+  return head;
 }
--- src/medium-collection/linked-list/odd-even-linked-list/test.spec.ts
+++ src/medium-collection/linked-list/odd-even-linked-list/test.spec.ts
@@ -16,3 +16,43 @@ it("oddEvenList should return [2,3,6,7,1,5,4] for head = [2,1,3,5,6,4,7]", funct
   const resultArray = listToArray(result);
   expect(resultArray).toEqual([2, 3, 6, 7, 1, 5, 4]);
 });
+
+it("oddEvenList should return [1,3,2,4] for head = [1,2,3,4]", function () {
+  const head = [1, 2, 3, 4];
+  const headList = arrayToList(head);
+  const result = oddEvenList(headList);
+  const resultArray = listToArray(result);
+  expect(resultArray).toEqual([1, 3, 2, 4]);
+});
+
+it("oddEvenList should return [1,3,2] for head = [1,2,3]", function () {
+  const head = [1, 2, 3];
+  const headList = arrayToList(head);
+  const result = oddEvenList(headList);
+  const resultArray = listToArray(result);
+  expect(resultArray).toEqual([1, 3, 2]);
+});
+
+it("oddEvenList should return [1,2] for head = [1,2]", function () {
+  const head = [1, 2];
+  const headList = arrayToList(head);
+  const result = oddEvenList(headList);
+  const resultArray = listToArray(result);
+  expect(resultArray).toEqual([1, 2]);
+});
+
+it("oddEvenList should return [1] for head = [1]", function () {
+  const head = [1];
+  const headList = arrayToList(head);
+  const result = oddEvenList(headList);
+  const resultArray = listToArray(result);
+  expect(resultArray).toEqual([1]);
+});
+
+it("oddEvenList should return [] for head = []", function () {
+  const head: number[] = [];
+  const headList = arrayToList(head);
+  const result = oddEvenList(headList);
+  const resultArray = listToArray(result);
+  expect(resultArray).toEqual([]);
+});

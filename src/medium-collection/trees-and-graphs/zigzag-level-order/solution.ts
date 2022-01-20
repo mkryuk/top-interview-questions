a454a3b99f7dc846f2fb8b5e3efc6d704dfe0aac
Maxim Kryuk <mkryuk@gmail.com>
2022-01-20 14:00:23 +0200

    Trees And Graphs: Binary Tree Zigzag Level Order Traversal solution

--- src/medium-collection/trees-and-graphs/zigzag-level-order/solution.ts
+++ src/medium-collection/trees-and-graphs/zigzag-level-order/solution.ts
@@ -1,5 +1,32 @@
 import { TreeNode } from "../common";
 
 export function zigzagLevelOrder(root: TreeNode | null): number[][] {
-  return [];
+  if (!root) {
+    return [];
+  }
+  let result: number[][] = [];
+  let queue = [root];
+  let level = 0;
+  while (queue.length) {
+    const queueLength = queue.length;
+    for (let i = 0; i < queueLength; i++) {
+      if (result.length === level) {
+        result[level] = [];
+      }
+      const node = queue.shift();
+      if (level % 2 === 0 && node) {
+        result[level].push(node.val);
+      } else if (level % 2 !== 0 && node) {
+        result[level].unshift(node.val);
+      }
+      if (node?.left) {
+        queue.push(node.left);
+      }
+      if (node?.right) {
+        queue.push(node.right);
+      }
+    }
+    level++;
+  }
+  return result;
 }

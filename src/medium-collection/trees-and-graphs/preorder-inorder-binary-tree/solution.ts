a49b07bd1c9e1227a7b9e226078c2517a2264912
Maxim Kryuk <mkryuk@gmail.com>
2022-01-22 14:41:38 +0200

    Trees And Graphs: Construct Binary Tree from Preorder and Inorder Traversal solution

--- src/medium-collection/trees-and-graphs/preorder-inorder-binary-tree/solution.ts
+++ src/medium-collection/trees-and-graphs/preorder-inorder-binary-tree/solution.ts
@@ -1,8 +1,36 @@
 import { TreeNode } from "../common";
 
+let preorderIndex: number;
+let inorderMap: Map<number, number>;
+
 export function buildTree(
   preorder: number[],
   inorder: number[]
 ): TreeNode | null {
-  return null;
+  preorderIndex = 0;
+  // create an inorder map to find index by value in O(1)
+  // for (let i = 0; i < inorder.length; i++) {
+  //   inorderMap.set(inorder[i], i);
+  // }
+  inorderMap = new Map<number, number>(
+    inorder.map((value, index) => [value, index])
+  );
+
+  return arrayToTree(preorder, 0, preorder.length - 1);
+}
+
+function arrayToTree(
+  preorder: number[],
+  left: number,
+  right: number
+): TreeNode | null {
+  if (left > right) {
+    return null;
+  }
+  let rootValue = preorder[preorderIndex++];
+  let root = new TreeNode(rootValue);
+  let inorderIndex = inorderMap.get(rootValue) ?? 0;
+  root.left = arrayToTree(preorder, left, inorderIndex - 1);
+  root.right = arrayToTree(preorder, inorderIndex + 1, right);
+  return root;
 }

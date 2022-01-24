884f5a426a711a58be7072695a116bca2ceeef0d
Maxim Kryuk <mkryuk@gmail.com>
2022-01-24 19:45:03 +0200

    Trees And Graphs: Populating Next Right Pointers in Each Node solution iterative

--- src/medium-collection/trees-and-graphs/next-right-pointer/solution.ts
+++ src/medium-collection/trees-and-graphs/next-right-pointer/solution.ts
@@ -1,5 +1,24 @@
 import { Node } from "../common";
 
 export function connect(root: Node | null): Node | null {
+  let queue = [root];
+  while (queue.length) {
+    const queueLength = queue.length;
+    for (let i = 0; i < queueLength; i++) {
+      const node = queue.shift();
+      if (node && i < queueLength - 1) {
+        // we have right node
+        // of the same level
+        // on top of the queue
+        node.next = queue[0];
+      }
+      if (node?.left) {
+        queue.push(node.left);
+      }
+      if (node?.right) {
+        queue.push(node.right);
+      }
+    }
+  }
   return root;
 }

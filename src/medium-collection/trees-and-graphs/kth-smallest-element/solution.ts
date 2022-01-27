bce2b3300cb4f3a04768536f5ed8699f19af6e6f
Maxim Kryuk <mkryuk@gmail.com>
2022-01-27 16:30:32 +0200

    Trees And Graphs: Kth Smallest Element in a BST solution

--- src/medium-collection/trees-and-graphs/kth-smallest-element/solution.ts
+++ src/medium-collection/trees-and-graphs/kth-smallest-element/solution.ts
@@ -1,5 +1,32 @@
 import { TreeNode } from "../common";
 
 export function kthSmallest(root: TreeNode | null, k: number): number {
+  if (!root) {
+    return 0;
+  }
+
+  let count = 1;
+  let stack: TreeNode[] = [root];
+  let visited = new Set<TreeNode>();
+  while (stack.length) {
+    let current: TreeNode | null = stack[0];
+    if (current.left && !visited.has(current.left)) {
+      current = current.left;
+      stack.unshift(current);
+      continue;
+    }
+    if (count === k) {
+      return current.val;
+    }
+    count++;
+    visited.add(current);
+    stack.shift();
+    if (current.right && !visited.has(current.right)) {
+      current = current.right;
+      stack.unshift(current);
+      continue;
+    }
+  }
+
   return 0;
 }
--- src/medium-collection/trees-and-graphs/kth-smallest-element/test.spec.ts
+++ src/medium-collection/trees-and-graphs/kth-smallest-element/test.spec.ts
@@ -9,10 +9,18 @@ it("kthSmallest should return 1 for root = [3,1,4,null,2], k = 1", () => {
   expect(result).toBe(1);
 });
 
-it("kthSmallest should return 3for root = [5,3,6,2,4,null,null,1], k = 3", () => {
+it("kthSmallest should return 3 for root = [5,3,6,2,4,null,null,1], k = 3", () => {
   const root = [5, 3, 6, 2, 4, null, null, 1],
     k = 3;
   const rootTreeNode = arrayToTreeNode(root);
   const result = kthSmallest(rootTreeNode, k);
   expect(result).toBe(3);
+});
+
+it("kthSmallest should return 5 for root = [4,2,6,1,3,5,7], k = 5", () => {
+  const root = [4, 2, 6, 1, 3, 5, 7],
+    k = 5;
+  const rootTreeNode = arrayToTreeNode(root);
+  const result = kthSmallest(rootTreeNode, k);
+  expect(result).toBe(5);
 });

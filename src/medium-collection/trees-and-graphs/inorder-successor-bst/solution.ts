1e338494d29b06fe780936fd48b6e412d33f7339
Maxim Kryuk <mkryuk@gmail.com>
2022-01-29 16:43:53 +0200

    Trees And Graphs: Inorder Successor in BST solution

--- src/medium-collection/trees-and-graphs/inorder-successor-bst/solution.ts
+++ src/medium-collection/trees-and-graphs/inorder-successor-bst/solution.ts
@@ -4,5 +4,21 @@ export function inorderSuccessor(
   root: TreeNode | null,
   p: TreeNode | null
 ): TreeNode | null {
+  if (!root || !p) {
+    return null;
+  }
+  const stack = [root];
+  let node = root.left;
+  while (stack.length || node) {
+    while (node) {
+      stack.push(node);
+      node = node.left;
+    }
+    node = stack.pop() ?? null;
+    if (node && p && node.val > p.val) {
+      return node;
+    }
+    node = node?.right ?? null;
+  }
   return null;
 }
--- src/medium-collection/trees-and-graphs/inorder-successor-bst/test.spec.ts
+++ src/medium-collection/trees-and-graphs/inorder-successor-bst/test.spec.ts
@@ -1,14 +1,13 @@
 import { inorderSuccessor } from "./solution";
-import { arrayToTreeNode, treeNodeToArray } from "../common";
+import { arrayToTreeNode } from "../common";
 
-it("inorderSuccessor should return [2] for root = [2,1,3], p = [1]", function () {
+it("inorderSuccessor should return 2 for root = [2,1,3], p = [1]", function () {
   const root = [2, 1, 3],
     p = [1];
   const rootNode = arrayToTreeNode(root);
   const pNode = arrayToTreeNode(p);
   const result = inorderSuccessor(rootNode, pNode);
-  const resultArray = treeNodeToArray(result);
-  expect(resultArray).toEqual([2]);
+  expect(result?.val).toEqual(2);
 });
 
 it("inorderSuccessor should return null for root = [5,3,6,2,4,null,null,1], p = [6]", function () {
@@ -18,4 +17,31 @@ it("inorderSuccessor should return null for root = [5,3,6,2,4,null,null,1], p = 
   const pNode = arrayToTreeNode(p);
   const result = inorderSuccessor(rootNode, pNode);
   expect(result).toEqual(null);
+});
+
+it("inorderSuccessor should return 4 for root = [4,2,6,1,3,5,7], p = [3]", function () {
+  const root = [4, 2, 6, 1, 3, 5, 7],
+    p = [3];
+  const rootNode = arrayToTreeNode(root);
+  const pNode = arrayToTreeNode(p);
+  const result = inorderSuccessor(rootNode, pNode);
+  expect(result?.val).toEqual(4);
+});
+
+it("inorderSuccessor should return 7 for root = [4,2,6,1,3,5,7], p = [6]", function () {
+  const root = [4, 2, 6, 1, 3, 5, 7],
+    p = [6];
+  const rootNode = arrayToTreeNode(root);
+  const pNode = arrayToTreeNode(p);
+  const result = inorderSuccessor(rootNode, pNode);
+  expect(result?.val).toEqual(7);
+});
+
+it("inorderSuccessor should return 5 for root = [4,2,6,1,3,5,7], p = [4]", function () {
+  const root = [4, 2, 6, 1, 3, 5, 7],
+    p = [4];
+  const rootNode = arrayToTreeNode(root);
+  const pNode = arrayToTreeNode(p);
+  const result = inorderSuccessor(rootNode, pNode);
+  expect(result?.val).toEqual(5);
 });

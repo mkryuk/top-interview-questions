4e8531fa6b648cc938c7c0f4d8741217407aa2f4
Maxim Kryuk <mkryuk@gmail.com>
2022-01-31 13:40:43 +0200

    Trees And Graphs: Number of Islands solution

--- src/medium-collection/trees-and-graphs/number-of-islands/solution.ts
+++ src/medium-collection/trees-and-graphs/number-of-islands/solution.ts
@@ -1,3 +1,31 @@
 export function numIslands(grid: string[][]): number {
-  return 0;
+  if (grid === null || grid.length === 0) {
+    return 0;
+  }
+  let result = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let k = 0; k < grid[0].length; k++) {
+      if (grid[i][k] === "1") {
+        result++;
+        dfs(grid, i, k);
+      }
+    }
+  }
+  return result;
+}
+
+// Treat the 2d grid map as an undirected graph
+// and there is an edge between two horizontally
+// or vertically adjacent nodes of value '1'.
+function dfs(grid: string[][], r: number, c: number) {
+  const h = grid.length;
+  const w = grid[0].length;
+  if (r < 0 || r >= h || c < 0 || c >= w || grid[r][c] === "0") {
+    return;
+  }
+  grid[r][c] = "0";
+  dfs(grid, r + 1, c);
+  dfs(grid, r - 1, c);
+  dfs(grid, r, c + 1);
+  dfs(grid, r, c - 1);
 }

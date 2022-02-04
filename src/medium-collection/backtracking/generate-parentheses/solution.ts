e659f490291c917ede709b16af35b1e2bcae6dc2
Maxim Kryuk <mkryuk@gmail.com>
2022-02-04 16:44:45 +0200

    Backtracking: Generate Parentheses solution

--- src/medium-collection/backtracking/generate-parentheses/solution.ts
+++ src/medium-collection/backtracking/generate-parentheses/solution.ts
@@ -1,3 +1,24 @@
 export function generateParenthesis(n: number): string[] {
-  return [];
+  let result: string[] = [];
+  backtrack("", n, 0, 0, result);
+  return result;
+}
+
+function backtrack(
+  parenthesis: string,
+  n: number,
+  open: number,
+  close: number,
+  result: string[]
+) {
+  if (parenthesis.length === n * 2) {
+    result.push(parenthesis);
+    return;
+  }
+  if (open < n) {
+    backtrack(parenthesis + "(", n, open + 1, close, result);
+  }
+  if (close < open) {
+    backtrack(parenthesis + ")", n, open, close + 1, result);
+  }
 }

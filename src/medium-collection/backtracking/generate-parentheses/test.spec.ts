470362d9e2d0b9b9190991074381336248e0e2cd
Maxim Kryuk <mkryuk@gmail.com>
2022-02-04 15:40:50 +0200

    Backtracking: Generate Parentheses added tests

--- src/medium-collection/backtracking/generate-parentheses/test.spec.ts
+++ src/medium-collection/backtracking/generate-parentheses/test.spec.ts
@@ -1,13 +1,52 @@
 import { generateParenthesis } from "./solution";
 
-it('generateParenthesis should return ["((()))","(()())","(())()","()(())","()()()"] for n = 3', () => {
-  const n = 3;
-  const result = generateParenthesis(n);
-  expect(result).toEqual(["((()))", "(()())", "(())()", "()(())", "()()()"]);
+it("generateParenthesis should return correct result for n = 1", () => {
+  const n = 1;
+  const parenthesis = generateParenthesis(n);
+  expect(parenthesis).toEqual(["()"]);
 });
 
-it('generateParenthesis should return ["()"] for n = 1', () => {
-  const n = 1;
-  const result = generateParenthesis(n);
-  expect(result).toEqual(["()"]);
+it("generateParenthesis should return correct result for n = 2", () => {
+  const n = 2;
+  const parenthesis = generateParenthesis(n);
+  let expectedResult = ["(())", "()()"];
+  expectedResult.forEach((item) => {
+    const result = parenthesis.includes(item);
+    expect(result).toBeTrue();
+  });
+});
+
+it("generateParenthesis should return correct result for n = 3", () => {
+  const n = 3;
+  const parenthesis = generateParenthesis(n);
+  let expectedResult = ["((()))", "(())()", "()(())", "(()())", "()()()"];
+  expectedResult.forEach((item) => {
+    const result = parenthesis.includes(item);
+    expect(result).toBeTrue();
+  });
+});
+
+it("generateParenthesis should return correct result for n = 4", () => {
+  const n = 4;
+  const parenthesis = generateParenthesis(n);
+  let expectedResult = [
+    "(((())))",
+    "((()()))",
+    "((())())",
+    "((()))()",
+    "(()(()))",
+    "(()()())",
+    "(()())()",
+    "(())(())",
+    "(())()()",
+    "()((()))",
+    "()(()())",
+    "()(())()",
+    "()()(())",
+    "()()()()",
+  ];
+  expectedResult.forEach((item) => {
+    const result = parenthesis.includes(item);
+    expect(result).toBeTrue();
+  });
 });

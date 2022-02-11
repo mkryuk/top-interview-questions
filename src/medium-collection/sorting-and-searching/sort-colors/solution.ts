269a4b8dd76541e265863dc43efcca028551bd5e
Maxim Kryuk <mkryuk@gmail.com>
2022-02-11 15:34:20 +0200

    Sorting and Searching: Sort Colors solution

--- src/medium-collection/sorting-and-searching/sort-colors/solution.ts
+++ src/medium-collection/sorting-and-searching/sort-colors/solution.ts
@@ -1,4 +1,29 @@
 /**
  Do not return anything, modify nums in-place instead.
  */
-export function sortColors(nums: number[]): void {}
+export function sortColors(nums: number[]): void {
+  const colorCounts = [0, 0, 0];
+  for (let i = 0; i < nums.length; i++) {
+    const color = nums[i];
+    const index = getIndex(color, colorCounts);
+    move(i, index, nums);
+    colorCounts[color]++;
+  }
+}
+
+function getIndex(color: number, colorCounts: number[]) {
+  let result = 0;
+  for (let i = 0; i <= color; i++) {
+    result += colorCounts[i];
+  }
+  return result;
+}
+
+function move(from: number, to: number, nums: number[]) {
+  if (from === to) {
+    return;
+  }
+  var element = nums[from];
+  nums.splice(from, 1);
+  nums.splice(to, 0, element);
+}

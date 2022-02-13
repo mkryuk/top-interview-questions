afa4707221651943a3d012d389a6d08ee8d7c9f1
Maxim Kryuk <mkryuk@gmail.com>
2022-02-13 20:00:56 +0200

    Sorting and Searching: Top K Frequent Elements solution

--- src/medium-collection/sorting-and-searching/top-k-elements/solution.ts
+++ src/medium-collection/sorting-and-searching/top-k-elements/solution.ts
@@ -1,3 +1,12 @@
 export function topKFrequent(nums: number[], k: number): number[] {
-  return [];
+  let map = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    let count = map.get(nums[i]) ?? 0;
+    map.set(nums[i], count + 1);
+  }
+  const items = Array.from(map, ([num, cnt]) => ({ num, cnt }));
+  return items
+    .sort((a, b) => b.cnt - a.cnt)
+    .splice(0, k)
+    .map((v) => v.num);
 }
--- src/medium-collection/sorting-and-searching/top-k-elements/test.spec.ts
+++ src/medium-collection/sorting-and-searching/top-k-elements/test.spec.ts
@@ -7,6 +7,34 @@ it("topKFrequent should return [1,2] fort nums = [1,1,1,2,2,3], k = 2", function
   expect(result).toEqual([1, 2]);
 });
 
+it("topKFrequent should return [0,2,1] fort nums = [2,1,1,2,2,3,0,0,0,0], k = 3", function () {
+  const nums = [2, 1, 1, 2, 2, 3, 0, 0, 0, 0],
+    k = 3;
+  const result = topKFrequent(nums, k);
+  expect(result).toEqual([0, 2, 1]);
+});
+
+it("topKFrequent should return [0,2,1] fort nums = [20,1,1,20,20,30,0,0,0,20,0,0,0,0], k = 3", function () {
+  const nums = [20, 1, 1, 20, 20, 30, 0, 0, 0, 20, 0, 0, 0, 0],
+    k = 3;
+  const result = topKFrequent(nums, k);
+  expect(result).toEqual([0, 20, 1]);
+});
+
+it("topKFrequent should return [-1] fort nums = [-1,-1], k = 1", function () {
+  const nums = [-1, -1],
+    k = 1;
+  const result = topKFrequent(nums, k);
+  expect(result).toEqual([-1]);
+});
+
+it("topKFrequent should return [1,2] fort nums = [1,2], k = 2", function () {
+  const nums = [1, 2],
+    k = 2;
+  const result = topKFrequent(nums, k);
+  expect(result).toEqual([1, 2]);
+});
+
 it("topKFrequent should return [1] fort nums = [1], k = 1", function () {
   const nums = [1],
     k = 1;

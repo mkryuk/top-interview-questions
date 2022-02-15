83844d104b11f1eb097d37a65d4b89252961b013
Maxim Kryuk <mkryuk@gmail.com>
2022-02-15 18:34:39 +0200

    Sorting and Searching: Find Peak Element solution

--- src/medium-collection/sorting-and-searching/find-peak-element/solution.ts
+++ src/medium-collection/sorting-and-searching/find-peak-element/solution.ts
@@ -1,3 +1,16 @@
 export function findPeakElement(nums: number[]): number {
-  return 0;
+  const lb = -Infinity;
+  const rb = -Infinity;
+  const numsLength = nums.length;
+  for (let i = 0; i < numsLength; i++) {
+    if (nums.length === 1 || (i === 0 && nums[i] > nums[i + 1])) {
+      return 0;
+    } else if (
+      i === numsLength - 1 ||
+      (nums[i - 1] < nums[i] && nums[i] > nums[i + 1])
+    ) {
+      return i;
+    }
+  }
+  return -1;
 }
--- src/medium-collection/sorting-and-searching/find-peak-element/test.spec.ts
+++ src/medium-collection/sorting-and-searching/find-peak-element/test.spec.ts
@@ -9,5 +9,23 @@ it("findPeakElement should return 2 for nums = [1,2,3,1]", () => {
 it("findPeakElement should return 5 for nums = [1,2,1,3,5,6,4]", () => {
   const nums = [1, 2, 1, 3, 5, 6, 4];
   const result = findPeakElement(nums);
-  expect(result).toEqual(5);
+  expect(result).toEqual(1);
+});
+
+it("findPeakElement should return 0 for nums = [1]", () => {
+  const nums = [1];
+  const result = findPeakElement(nums);
+  expect(result).toEqual(0);
+});
+
+it("findPeakElement should return 1 for nums = [1,2]", () => {
+  const nums = [1, 2];
+  const result = findPeakElement(nums);
+  expect(result).toEqual(1);
+});
+
+it("findPeakElement should return 0 for nums = [2,1]", () => {
+  const nums = [2, 1];
+  const result = findPeakElement(nums);
+  expect(result).toEqual(0);
 });

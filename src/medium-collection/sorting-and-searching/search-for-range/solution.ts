d07ca99d905e6a6d7b7cbda1a4972f4a11ff2353
Maxim Kryuk <mkryuk@gmail.com>
2022-02-17 19:15:37 +0200

    Sorting and Searching: Search for a Range solution

--- src/medium-collection/sorting-and-searching/search-for-range/solution.ts
+++ src/medium-collection/sorting-and-searching/search-for-range/solution.ts
@@ -1,3 +1,38 @@
 export function searchRange(nums: number[], target: number): number[] {
-  return [];
+  const result: number[] = [-1, -1];
+  let lli = 0;
+  let lri = nums.length - 1;
+  let rli = 0;
+  let rri = lri;
+  let li, ri;
+  li = ri = getMid(lli, rri);
+  while (lli <= lri || rli <= rri) {
+    if (lli <= lri) {
+      li = getMid(lli, lri);
+      if (nums[li] > target) {
+        lri = li - 1;
+      } else if (nums[li] < target) {
+        lli = li + 1;
+      } else {
+        lri = li - 1;
+        result[0] = li;
+      }
+    }
+    if (rli <= rri) {
+      ri = getMid(rli, rri);
+      if (nums[ri] > target) {
+        rri = ri - 1;
+      } else if (nums[ri] < target) {
+        rli = ri + 1;
+      } else {
+        rli = ri + 1;
+        result[1] = ri;
+      }
+    }
+  }
+  return result;
+}
+
+function getMid(left: number, right: number) {
+  return Math.floor(left + (right - left) / 2);
 }
--- src/medium-collection/sorting-and-searching/search-for-range/test.spec.ts
+++ src/medium-collection/sorting-and-searching/search-for-range/test.spec.ts
@@ -7,6 +7,41 @@ it("searchRange should return [3,4] for nums = [5,7,7,8,8,10], target = 8", () =
   expect(result).toEqual([3, 4]);
 });
 
+it("searchRange should return [2,4] for nums = [5,7,7,8,8,8,10], target = 8", () => {
+  const nums = [5, 7, 8, 8, 8, 10],
+    target = 8;
+  const result = searchRange(nums, target);
+  expect(result).toEqual([2, 4]);
+});
+
+it("searchRange should return [2,5] for nums = [5,7,7,8,8,8,8,10], target = 8", () => {
+  const nums = [5, 7, 8, 8, 8, 8, 10],
+    target = 8;
+  const result = searchRange(nums, target);
+  expect(result).toEqual([2, 5]);
+});
+
+it("searchRange should return [0,0] for nums = [8], target = 8", () => {
+  const nums = [8],
+    target = 8;
+  const result = searchRange(nums, target);
+  expect(result).toEqual([0, 0]);
+});
+
+it("searchRange should return [0,1] for nums = [8,8], target = 8", () => {
+  const nums = [8, 8],
+    target = 8;
+  const result = searchRange(nums, target);
+  expect(result).toEqual([0, 1]);
+});
+
+it("searchRange should return [0,2] for nums = [8,8,8], target = 8", () => {
+  const nums = [8, 8, 8],
+    target = 8;
+  const result = searchRange(nums, target);
+  expect(result).toEqual([0, 2]);
+});
+
 it("searchRange should return [-1,-1] for nums = [5,7,7,8,8,10], target = 6", () => {
   const nums = [5, 7, 7, 8, 8, 10],
     target = 6;

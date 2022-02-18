f822d316cafda59a205afb08d9d72b5caa40dcd8
Maxim Kryuk <mkryuk@gmail.com>
2022-02-18 19:32:46 +0200

    Sorting and Searching: Merge Intervals solution

--- src/medium-collection/sorting-and-searching/merge-intervals/solution.ts
+++ src/medium-collection/sorting-and-searching/merge-intervals/solution.ts
@@ -1,3 +1,20 @@
 export function merge(intervals: number[][]): number[][] {
-  return [];
+  if (intervals.length === 1) {
+    return intervals;
+  }
+  let sortedIntervals = intervals.sort((a, b) => a[0] - b[0]);
+  let result: number[][] = [];
+  let [left, right] = sortedIntervals[0];
+  for (let i = 1; i < sortedIntervals.length; i++) {
+    let [nextLeft, nextRight] = sortedIntervals[i];
+    if (nextLeft <= right && nextRight > right) {
+      right = nextRight;
+    } else if (nextLeft > right) {
+      result.push([left, right]);
+      left = nextLeft;
+      right = nextRight;
+    }
+  }
+  result.push([left, right]);
+  return result;
 }
--- src/medium-collection/sorting-and-searching/merge-intervals/test.spec.ts
+++ src/medium-collection/sorting-and-searching/merge-intervals/test.spec.ts
@@ -23,3 +23,31 @@ it("merge should return [[1,5]] for intervals = [[1,4],[4,5]]", () => {
   const result = merge(intervals);
   expect(result).toEqual([[1, 5]]);
 });
+
+it("merge should return [[1,7]] for intervals = [[1,4],[4,5],[2,7]]", () => {
+  const intervals = [
+    [1, 4],
+    [4, 5],
+    [2, 7],
+  ];
+  const result = merge(intervals);
+  expect(result).toEqual([[1, 7]]);
+});
+
+it("merge should return [[1,4]] for intervals = [[1,4]]", () => {
+  const intervals = [[1, 4]];
+  const result = merge(intervals);
+  expect(result).toEqual([[1, 4]]);
+});
+
+it("merge should return [[1,4], [5,8]] for intervals = [[1,4], [5,8]]", () => {
+  const intervals = [
+    [1, 4],
+    [5, 8],
+  ];
+  const result = merge(intervals);
+  expect(result).toEqual([
+    [1, 4],
+    [5, 8],
+  ]);
+});

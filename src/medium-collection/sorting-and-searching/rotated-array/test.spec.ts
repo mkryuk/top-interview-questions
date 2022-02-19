e9224e48e4a41c6d10704938747ee9e171332577
Maxim Kryuk <mkryuk@gmail.com>
2022-02-19 15:06:12 +0200

    Sorting and Searching: Search in Rotated Sorted Array init

---
+++ src/medium-collection/sorting-and-searching/rotated-array/test.spec.ts
@@ -0,0 +1,22 @@
+import { search } from "./solution";
+
+it("search should return 4 for nums = [4,5,6,7,0,1,2], target = 0", function () {
+  const nums = [4, 5, 6, 7, 0, 1, 2],
+    target = 0;
+  const result = search(nums, target);
+  expect(result).toEqual(4);
+});
+
+it("search should return -1 for nums = [4,5,6,7,0,1,2], target = 3", function () {
+  const nums = [4, 5, 6, 7, 0, 1, 2],
+    target = 3;
+  const result = search(nums, target);
+  expect(result).toEqual(-1);
+});
+
+it("search should return -1 for nums = [1], target = 0", function () {
+  const nums = [1],
+    target = 0;
+  const result = search(nums, target);
+  expect(result).toEqual(-1);
+});

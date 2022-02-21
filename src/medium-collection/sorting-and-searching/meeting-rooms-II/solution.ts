db211f9fd7ab14160ee8f53643935bfe9111bd34
Maxim Kryuk <mkryuk@gmail.com>
2022-02-21 21:43:00 +0200

    Sorting and Searching: Meeting Rooms II solution

--- src/medium-collection/sorting-and-searching/meeting-rooms-II/solution.ts
+++ src/medium-collection/sorting-and-searching/meeting-rooms-II/solution.ts
@@ -1,3 +1,21 @@
+// The main thing is that we go through the timeline
+// and if we have started meeting, we increase the amount of the meeting rooms
+// when the meeting ends we decrease the meeting rooms needed
 export function minMeetingRooms(intervals: number[][]): number {
-  return 0;
+  const sortedStarts = intervals.sort((a, b) => a[0] - b[0]).map((a) => a[0]);
+  const sortedEnds = intervals.sort((a, b) => a[1] - b[1]).map((a) => a[1]);
+  let minMeetingRooms = 1;
+  let start = sortedStarts.shift();
+  let end = sortedEnds.shift();
+  let meetingRooms = 0;
+  while (sortedStarts.length && sortedEnds.length) {
+    while (start !== undefined && end !== undefined && start < end) {
+      start = sortedStarts.shift();
+      meetingRooms++;
+    }
+    end = sortedEnds.shift();
+    minMeetingRooms = Math.max(minMeetingRooms, meetingRooms);
+    meetingRooms--;
+  }
+  return minMeetingRooms;
 }
--- src/medium-collection/sorting-and-searching/meeting-rooms-II/test.spec.ts
+++ src/medium-collection/sorting-and-searching/meeting-rooms-II/test.spec.ts
@@ -18,3 +18,30 @@ it("minMeetingRooms should return 1 for intervals = [[7,10],[2,4]]", function ()
   const result = minMeetingRooms(intervals);
   expect(result).toEqual(1);
 });
+
+it("minMeetingRooms should return 3 for intervals = [[1,5],[3,8],[6,7],[9,15],[10,13],[11,12],[14,17],[16,18],[19,20]]", function () {
+  const intervals = [
+    [1, 5],
+    [3, 8],
+    [6, 7],
+    [9, 15],
+    [10, 13],
+    [11, 12],
+    [14, 17],
+    [16, 18],
+    [19, 20],
+  ];
+  const result = minMeetingRooms(intervals);
+  expect(result).toEqual(3);
+});
+
+it("minMeetingRooms should return 3 for intervals = [[1,8],[6,20],[9,16],[13,17]]", function () {
+  const intervals = [
+    [1, 8],
+    [6, 20],
+    [9, 16],
+    [13, 17],
+  ];
+  const result = minMeetingRooms(intervals);
+  expect(result).toEqual(3);
+});

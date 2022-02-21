36d0d786f38dcbbf0efaa9729b562a6b16c87729
Maxim Kryuk <mkryuk@gmail.com>
2022-02-21 12:57:10 +0200

    Array: Majority Element solution

--- src/easy-collection/array/majority-element/solution.ts
+++ src/easy-collection/array/majority-element/solution.ts
@@ -1,3 +1,17 @@
 export function majorityElement(nums: number[]): number {
-  return 0;
+  nums.sort((a, b) => a - b);
+  let num = nums[0];
+  let count = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === num) {
+      count++;
+    } else {
+      num = nums[i];
+      count = 1;
+    }
+    if (count > Math.floor(nums.length / 2)) {
+      return num;
+    }
+  }
+  return num;
 }

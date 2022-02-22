cdde90ddf724a426181fac1810adde660ee30c41
Maxim Kryuk <mkryuk@gmail.com>
2022-02-22 18:37:16 +0200

    Sorting and Searching: Search a 2D Matrix II init

---
+++ src/medium-collection/sorting-and-searching/search-2d-matrix-II/test.spec.ts
@@ -0,0 +1,27 @@
+import { searchMatrix } from "./solution";
+
+it("searchMatrix should return true for matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5", () => {
+  const matrix = [
+      [1, 4, 7, 11, 15],
+      [2, 5, 8, 12, 19],
+      [3, 6, 9, 16, 22],
+      [10, 13, 14, 17, 24],
+      [18, 21, 23, 26, 30],
+    ],
+    target = 5;
+  const result = searchMatrix(matrix, target);
+  expect(result).toBeTrue();
+});
+
+it("searchMatrix should return false for matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20", () => {
+  const matrix = [
+      [1, 4, 7, 11, 15],
+      [2, 5, 8, 12, 19],
+      [3, 6, 9, 16, 22],
+      [10, 13, 14, 17, 24],
+      [18, 21, 23, 26, 30],
+    ],
+    target = 20;
+  const result = searchMatrix(matrix, target);
+  expect(result).toBeFalse();
+});

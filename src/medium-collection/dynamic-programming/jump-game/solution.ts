07f4886f8f230ede0f343443754c588b04cf148b
Maxim Kryuk <mkryuk@gmail.com>
2022-03-27 00:25:42 +0200

    Dynamic Programming: Jump Game solution

--- src/medium-collection/dynamic-programming/jump-game/solution.ts
+++ src/medium-collection/dynamic-programming/jump-game/solution.ts
@@ -1,3 +1,10 @@
 export function canJump(nums: number[]): boolean {
+  let maxJump = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (i > maxJump) {
+      return false;
+    }
+    maxJump = Math.max(maxJump, i + nums[i]);
+  }
   return true;
 }

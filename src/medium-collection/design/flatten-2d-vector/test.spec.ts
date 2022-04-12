66d619ba125b6fa00da1f89bf64e24df59150caf
Maxim Kryuk <mkryuk@gmail.com>
2022-04-12 11:49:03 +0300

    Design: Flatten 2D Vector added test

--- src/medium-collection/design/flatten-2d-vector/test.spec.ts
+++ src/medium-collection/design/flatten-2d-vector/test.spec.ts
@@ -24,3 +24,10 @@ it("should return [1, 2, true, 3, true, true, 4, true, 5 false] for [[1, 2], [3]
   expect(vec.next()).toBe(5);
   expect(vec.hasNext()).toBe(false);
 });
+
+it("should return [true,1,false] for [[1]]", () => {
+  const vec = new Vector2D([[1]]);
+  expect(vec.hasNext()).toBe(true);
+  expect(vec.next()).toBe(1);
+  expect(vec.hasNext()).toBe(false);
+});

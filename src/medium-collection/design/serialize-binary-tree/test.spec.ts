769f4cef6709ef036c6c199ea3a6074159a7f1cf
Maxim Kryuk <mkryuk@gmail.com>
2022-04-15 23:26:21 +0300

    Design: Serialize and Deserialize Binary Tree added tests

--- src/medium-collection/design/serialize-binary-tree/test.spec.ts
+++ src/medium-collection/design/serialize-binary-tree/test.spec.ts
@@ -12,3 +12,27 @@ it('should deserialize "1,2,3,null,null,4,5"', () => {
   const root = arrayToTreeNode(array);
   expect(deserialize("1,2,3,null,null,4,5")).toEqual(root);
 });
+
+it('should serialize "1,2,3,null,4,null,5"', () => {
+  const array = [1, 2, 3, null, 4, null, 5];
+  const root = arrayToTreeNode(array);
+  expect(serialize(root)).toEqual("1,2,3,null,4,null,5");
+});
+
+it('should deserialize "1,2,3,null,4,null,5"', () => {
+  const array = [1, 2, 3, null, 4, null, 5];
+  const root = arrayToTreeNode(array);
+  expect(deserialize("1,2,3,null,4,null,5")).toEqual(root);
+});
+
+it('should serialize "1,2,3,4,null,5,null"', () => {
+  const array = [1, 2, 3, 4, null, 5, null];
+  const root = arrayToTreeNode(array);
+  expect(serialize(root)).toEqual("1,2,3,4,null,5,null");
+});
+
+it('should deserialize "1,2,3,4,null,5,null"', () => {
+  const array = [1, 2, 3, 4, null, 5, null];
+  const root = arrayToTreeNode(array);
+  expect(deserialize("1,2,3,4,null,5,null")).toEqual(root);
+});

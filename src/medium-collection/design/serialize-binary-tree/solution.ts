a1e43097800f61ee1d2dd0250cb49da10ae36341
Maxim Kryuk <mkryuk@gmail.com>
2022-04-16 23:04:41 +0300

    Design: Serialize and Deserialize Binary Tree added serialize

--- src/medium-collection/design/serialize-binary-tree/solution.ts
+++ src/medium-collection/design/serialize-binary-tree/solution.ts
@@ -4,7 +4,32 @@ import { TreeNode } from "../../trees-and-graphs/common";
  * Encodes a tree to a single string.
  */
 export function serialize(root: TreeNode | null): string {
-  return "";
+  if (root === null) {
+    return "";
+  }
+  let currentLayer: (TreeNode | null)[] = [root];
+  let nextLayer: (TreeNode | null)[] = [];
+  let result: (number | null)[] = [];
+  while (currentLayer.length || nextLayer.length) {
+    let layerResult: (number | null)[] = [];
+    while (currentLayer.length) {
+      let node = currentLayer.shift();
+      layerResult.push(node?.val ?? null);
+      if (currentLayer.length || node?.left || node?.right) {
+        nextLayer.push(node?.left ?? null);
+        nextLayer.push(node?.right ?? null);
+      }
+    }
+    if (layerResult.some((n) => n !== null)) {
+      result = result.concat(layerResult);
+      currentLayer = nextLayer;
+    }
+    nextLayer = [];
+    layerResult = [];
+  }
+
+  let resultStr = result.map((n) => n?.toString() ?? "null").join(",");
+  return resultStr;
 }
 
 /*

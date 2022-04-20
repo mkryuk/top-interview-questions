7a52d95d2e133ea8f06dda09d668c8514f23a6e5
Maxim Kryuk <mkryuk@gmail.com>
2022-04-20 23:00:03 +0300

    Design: Insert Delete GetRandom O(1) added test

--- src/medium-collection/design/insert-delete-random/test.spec.ts
+++ src/medium-collection/design/insert-delete-random/test.spec.ts
@@ -21,3 +21,15 @@ it("should return [true, true, false, 2, true, 2, true]", () => {
   expect(set.getRandom()).toBe(2);
   expect(set.remove(2)).toBe(true);
 });
+
+it("should correctly process get random", () => {
+  const set = new RandomizedSet();
+  expect(set.insert(1)).toBe(true);
+  expect(set.insert(10)).toBe(true);
+  expect(set.insert(5)).toBe(true);
+  expect(set.insert(3)).toBe(true);
+  expect(set.insert(2)).toBe(true);
+  expect(set.remove(3)).toBe(true);
+  expect(set.insert(3)).toBe(true);
+  expect([1, 10, 5, 3, 2]).toContain(set.getRandom());
+});

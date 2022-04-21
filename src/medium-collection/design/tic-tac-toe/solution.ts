30e3b34180d686dd510b043e209e7c0b8e23b307
Maxim Kryuk <mkryuk@gmail.com>
2022-04-22 00:11:46 +0300

    Design: Design Tic-Tac-Toe init

---
+++ src/medium-collection/design/tic-tac-toe/solution.ts
@@ -0,0 +1,7 @@
+export class TicTacToe {
+  constructor(n: number) {}
+
+  move(row: number, col: number, player: number): number {
+    return 0;
+  }
+}
---
+++ src/medium-collection/design/tic-tac-toe/test.spec.ts
@@ -0,0 +1,12 @@
+import { TicTacToe } from "./solution";
+
+it("should pass game 1", () => {
+  const ticTacToe = new TicTacToe(3);
+  expect(ticTacToe.move(0, 0, 1)).toBe(0);
+  expect(ticTacToe.move(0, 2, 2)).toBe(0);
+  expect(ticTacToe.move(2, 2, 1)).toBe(0);
+  expect(ticTacToe.move(1, 1, 2)).toBe(0);
+  expect(ticTacToe.move(2, 0, 1)).toBe(0);
+  expect(ticTacToe.move(1, 0, 2)).toBe(0);
+  expect(ticTacToe.move(2, 1, 1)).toBe(1);
+});

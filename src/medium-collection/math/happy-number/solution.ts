c93bb572edf34e95234ce39e7526ac807e39e2ca
Maxim Kryuk <mkryuk@gmail.com>
2022-04-25 14:24:56 +0300

    Math: Happy Number solution

--- src/medium-collection/math/happy-number/solution.ts
+++ src/medium-collection/math/happy-number/solution.ts
@@ -1,3 +1,21 @@
 export function isHappy(n: number): boolean {
-  return false;
+  const set = new Set();
+  while (n !== 1) {
+    if (set.has(n)) {
+      return false;
+    }
+    set.add(n);
+    n = sumDigits(n);
+  }
+  return true;
+}
+
+function sumDigits(n: number): number {
+  let result = 0;
+  while (n > 0) {
+    const lastDigit = n % 10;
+    result += Math.pow(lastDigit, 2);
+    n = Math.trunc(n / 10);
+  }
+  return result;
 }
--- src/medium-collection/math/happy-number/test.spec.ts
+++ src/medium-collection/math/happy-number/test.spec.ts
@@ -7,3 +7,15 @@ it("isHappy should return true for 19", () => {
 it("isHappy should return false for 2", () => {
   expect(isHappy(2)).toBe(false);
 });
+
+it("isHappy should return true for 32", () => {
+  expect(isHappy(32)).toBe(true);
+});
+
+it("isHappy should return true for 7", () => {
+  expect(isHappy(7)).toBe(true);
+});
+
+it("isHappy should return false for 116", () => {
+  expect(isHappy(116)).toBe(false);
+});

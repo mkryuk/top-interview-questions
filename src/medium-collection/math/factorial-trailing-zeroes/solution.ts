015bf8388df3d88fa2e703e0794de507aa2b099b
Maxim Kryuk <mkryuk@gmail.com>
2022-04-26 21:57:10 +0300

    Math: Factorial Trailing Zeroes bruteforce solution

--- src/medium-collection/math/factorial-trailing-zeroes/solution.ts
+++ src/medium-collection/math/factorial-trailing-zeroes/solution.ts
@@ -1,3 +1,14 @@
 export function trailingZeroes(n: number): number {
-  return 0;
+  const fact = factorial(BigInt(n));
+  const factStr = fact.toString();
+  let result = factStr.search(/0+$/);
+  return result === -1 ? 0 : factStr.length - result;
+}
+
+function factorial(n: bigint): bigint {
+  let result: bigint = BigInt(1);
+  while (n > 0) {
+    result = result * n--;
+  }
+  return result;
 }

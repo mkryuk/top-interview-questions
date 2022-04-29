e1e9c6a389967869a0053e041f2da5e9b213dc70
Maxim Kryuk <mkryuk@gmail.com>
2022-04-29 23:11:32 +0300

    Math: Excel Sheet Column Number solution

--- src/medium-collection/math/excel-sheet-column-number/solution.ts
+++ src/medium-collection/math/excel-sheet-column-number/solution.ts
@@ -1,3 +1,17 @@
 export function titleToNumber(columnTitle: string): number {
-  return 0;
+  let alphaNumber = createAlphabeticMap();
+  let result = 0;
+  for (let i = 0; i < columnTitle.length; i++) {
+    let num = alphaNumber.get(columnTitle[i]) ?? 0;
+    result += num * Math.pow(26, columnTitle.length - i - 1);
+  }
+  return result;
+}
+
+function createAlphabeticMap(): Map<string, number> {
+  let map = new Map<string, number>();
+  for (let i = 0; i < 26; i++) {
+    map.set(String.fromCharCode(i + 65), i + 1);
+  }
+  return map;
 }

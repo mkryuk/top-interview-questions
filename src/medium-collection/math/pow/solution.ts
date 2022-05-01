1794045695b6f0a36cdd7d0f10fafd2440bfaeb9
Maxim Kryuk <mkryuk@gmail.com>
2022-05-01 15:55:38 +0300

    Math: Pow(x, n) solution

--- src/medium-collection/math/pow/solution.ts
+++ src/medium-collection/math/pow/solution.ts
@@ -1,3 +1,12 @@
 export function myPow(x: number, n: number): number {
-  return 0;
+  if (n === 0) {
+    return 1;
+  }
+  if (n < 0) {
+    return 1 / myPow(x, -n);
+  }
+  if (n % 2 === 0) {
+    return myPow(x * x, n / 2);
+  }
+  return x * myPow(x * x, (n - 1) / 2);
 }
--- src/medium-collection/math/pow/test.spec.ts
+++ src/medium-collection/math/pow/test.spec.ts
@@ -4,8 +4,8 @@ it("myPow should return 1024.00000 for x = 2.00000, n = 10", () => {
   expect(myPow(2.0, 10)).toBe(1024.0);
 });
 
-it("myPow should return 9.26100 for x = 2.10000, n = 3", () => {
-  expect(myPow(2.1, 3)).toBe(9.261);
+it("myPow should return 8 for x = 2, n = 3", () => {
+  expect(myPow(2, 3)).toBe(8);
 });
 
 it("myPow should return 0.25000 for x = 2.00000, n = -2", () => {

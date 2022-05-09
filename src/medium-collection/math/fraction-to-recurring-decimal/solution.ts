8cb2fedd22ec82a4f73dad7071765aacf7f7892f
Maxim Kryuk <mkryuk@gmail.com>
2022-05-09 16:49:53 +0300

    Math: Fraction to Recurring Decimal solution

--- src/medium-collection/math/fraction-to-recurring-decimal/solution.ts
+++ src/medium-collection/math/fraction-to-recurring-decimal/solution.ts
@@ -2,5 +2,38 @@ export function fractionToDecimal(
   numerator: number,
   denominator: number
 ): string {
-  return "";
+  if (numerator == 0) {
+    return "0";
+  }
+  let fraction: string = "";
+  // If either one is negative (not both)
+  if (numerator < 0 !== denominator < 0) {
+    fraction += "-";
+  }
+  // Convert to Long or else abs(-2147483648) overflows
+  let dividend = Math.abs(numerator);
+  let divisor = Math.abs(denominator);
+  fraction += Math.trunc(dividend / divisor);
+  let remainder = dividend % divisor;
+  if (remainder == 0) {
+    return fraction.toString();
+  }
+  fraction += ".";
+  let map = new Map();
+  while (remainder !== 0) {
+    if (map.has(remainder)) {
+      fraction = insertString(fraction, "(", map.get(remainder));
+      fraction += ")";
+      break;
+    }
+    map.set(remainder, fraction.length);
+    remainder *= 10;
+    fraction += Math.trunc(remainder / divisor);
+    remainder %= divisor;
+  }
+  return fraction.toString();
+}
+
+function insertString(str: string, subStr: string, position: number): string {
+  return str.substring(0, position) + subStr + str.substring(position);
 }

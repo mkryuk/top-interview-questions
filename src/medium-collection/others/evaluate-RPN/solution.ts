a3032fcd5dac90816d6b7dd4efad7ac4ae09373d
Maxim Kryuk <mkryuk@gmail.com>
2022-05-13 22:57:53 +0300

    Other: Evaluate Reverse Polish Notation solution

--- src/medium-collection/others/evaluate-RPN/solution.ts
+++ src/medium-collection/others/evaluate-RPN/solution.ts
@@ -1,3 +1,33 @@
 export function evalRPN(tokens: string[]): number {
-  return 0;
+  let stack: number[] = [];
+  for (let i = 0; i < tokens.length; i++) {
+    if (isOperator(tokens[i])) {
+      let [right, left] = [stack.pop(), stack.pop()];
+      if (left === undefined || right === undefined) {
+        throw "Stack is empty";
+      }
+      stack.push(Math.trunc(operate(left, right, tokens[i])));
+    } else {
+      stack.push(parseInt(tokens[i], 10));
+    }
+  }
+  return stack.pop() ?? 0;
+}
+
+function isOperator(token: string) {
+  return token === "+" || token === "-" || token === "*" || token === "/";
+}
+
+function operate(a: number, b: number, op: string): number {
+  switch (op) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      return a / b;
+  }
+  throw "Unsupported operation";
 }

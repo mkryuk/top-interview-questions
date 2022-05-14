16b2fd383d5c1dd5900048a66d4f9841f96bd060
Maxim Kryuk <mkryuk@gmail.com>
2022-05-14 21:53:53 +0300

    Other: Majority Element solution

--- src/medium-collection/others/majority-element/solution.ts
+++ src/medium-collection/others/majority-element/solution.ts
@@ -1,3 +1,11 @@
 export function majorityElement(nums: number[]): number {
-  return 0;
+  let majority = nums[0];
+  let count = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (count === 0) {
+      majority = nums[i];
+    }
+    count += majority === nums[i] ? 1 : -1;
+  }
+  return majority;
 }

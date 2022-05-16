ae021964de4c78fa32cbf64e5f6cadff700d4a73
Maxim Kryuk <mkryuk@gmail.com>
2022-05-16 23:17:56 +0300

    Other: Find the Celebrity solution

--- src/medium-collection/others/find-the-celebrity/solution.ts
+++ src/medium-collection/others/find-the-celebrity/solution.ts
@@ -1,11 +1,32 @@
-export const knows = function (graph: number[][]) {
+export const knows = function (
+  graph: number[][],
+): (a: number, b: number) => boolean {
   return function (a: number, b: number): boolean {
     return !!graph[a][b];
   };
 };
 
-export const solution = function (knows: any) {
+export const solution = function (knows: (a: number, b: number) => boolean) {
   return function (n: number): number {
-    return 0;
+    let ci = 0;
+    for (let i = 1; i < n; i++) {
+      if (knows(ci, i)) {
+        ci = i;
+      }
+    }
+    for (let i = 0; i < n; i++) {
+      if (!knows(i, ci)) {
+        return -1;
+      }
+    }
+    for (let i = 0; i < n; i++) {
+      if (ci === i) {
+        continue;
+      }
+      if (knows(ci, i)) {
+        return -1;
+      }
+    }
+    return ci;
   };
 };

24521a880560c78a5be0e735ecdd748bd2aa7fae
Maxim Kryuk <mkryuk@gmail.com>
2022-05-19 23:04:35 +0300

    Other: Task Scheduler solution

--- src/medium-collection/others/task-scheduler/solution.ts
+++ src/medium-collection/others/task-scheduler/solution.ts
@@ -1,3 +1,21 @@
 export function leastInterval(tasks: string[], n: number): number {
-  return 0;
+  // frequencies of the tasks
+  let frequencies: number[] = new Array(26).fill(0);
+  for (let i = 0; i < tasks.length; i++) {
+    let task = tasks[i];
+    frequencies[task.charCodeAt(0) - "A".charCodeAt(0)]++;
+  }
+
+  frequencies = frequencies.sort((a, b) => a - b);
+
+  // max frequency
+  let max = frequencies[25];
+  let idleTime = (max - 1) * n;
+
+  for (let i = frequencies.length - 2; i >= 0 && idleTime > 0; --i) {
+    idleTime -= Math.min(max - 1, frequencies[i]);
+  }
+  idleTime = Math.max(0, idleTime);
+
+  return idleTime + tasks.length;
 }

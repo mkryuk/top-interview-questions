d651601b868db12daa4d71e1dceaa9deecd97bda
Maxim Kryuk <mkryuk@gmail.com>
2022-05-20 23:18:37 +0300

    Array and Strings: Product of Array Except Self solution

--- src/hard-collection/array-and-strings/product-except-self/solution.ts
+++ src/hard-collection/array-and-strings/product-except-self/solution.ts
@@ -1,3 +1,15 @@
 export function productExceptSelf(nums: number[]): number[] {
-  return nums;
+  let result = [1];
+  // calculate product of all numbers to the left of i
+  for (let i = 1; i < nums.length; i++) {
+    result[i] = result[i - 1] * nums[i - 1];
+  }
+  // calculate product of all numbers to the right of i
+  // and multiply it with the result of the left side
+  let product = nums[nums.length - 1];
+  for (let i = nums.length - 2; i >= 0; i--) {
+    result[i] *= product;
+    product *= nums[i];
+  }
+  return result;
 }
--- src/hard-collection/array-and-strings/product-except-self/test.spec.ts
+++ src/hard-collection/array-and-strings/product-except-self/test.spec.ts
@@ -4,6 +4,16 @@ it("productExceptSelf should return [24,12,8,6] for nums = [1,2,3,4]", () => {
   expect(productExceptSelf([1, 2, 3, 4])).toEqual([24, 12, 8, 6]);
 });
 
-it("productExceptSelf should return [0,0,0,0] for nums = [-1,1,0,-3,3]", () => {
-  expect(productExceptSelf([-1, 1, 0, -3, 3])).toEqual([0, 0, 0, 0]);
+it("productExceptSelf should return [0,0,9,0,0] for nums = [-1,1,0,-3,3]", () => {
+  expect(productExceptSelf([-1, 1, 0, -3, 3])).toEqual([-0, 0, 9, -0, 0]);
+});
+
+it("productExceptSelf should return [1920,960,640,480,384] for nums = [2,4,6,8,10]", () => {
+  expect(productExceptSelf([2, 4, 6, 8, 10])).toEqual([
+    1920, 960, 640, 480, 384,
+  ]);
+});
+
+it("productExceptSelf should return [24,30,40,60,120] for nums = [5,4,3,2,1]", () => {
+  expect(productExceptSelf([5, 4, 3, 2, 1])).toEqual([24, 30, 40, 60, 120]);
 });

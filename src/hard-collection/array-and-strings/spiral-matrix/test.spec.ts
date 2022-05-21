ca9e7176c347b862ee47abb0e3e4be987b1283bb
Maxim Kryuk <mkryuk@gmail.com>
2022-05-21 11:10:59 +0200

    Array and Strings: Spiral Matrix init

---
+++ src/hard-collection/array-and-strings/spiral-matrix/test.spec.ts
@@ -0,0 +1,24 @@
+import { spiralOrder } from "./solution";
+
+it("spiralOrder should return [1,2,3,6,9,8,7,4,5] for matrix = [[1,2,3],[4,5,6],[7,8,9]]", () => {
+  const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+  expect(spiralOrder(matrix)).toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
+});
+
+it("spiralOrder should return [1,2,3,4,8,12,11,10,9,5,6,7] for matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]", () => {
+  const matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+  ];
+  expect(spiralOrder(matrix)).toEqual([1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]);
+});
+
+it("spiralOrder should return [1] for matrix = [[1]]", () => {
+  const matrix = [[1]];
+  expect(spiralOrder(matrix)).toEqual([1]);
+});

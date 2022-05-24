dca9159cf5887763355c293d515a354c13953d83
Maxim Kryuk <mkryuk@gmail.com>
2022-05-24 23:20:49 +0300

    Array and Strings: 4Sum II bruteforce solution

--- src/hard-collection/array-and-strings/four-sum-count/solution.ts
+++ src/hard-collection/array-and-strings/four-sum-count/solution.ts
@@ -4,5 +4,17 @@ export function fourSumCount(
   nums3: number[],
   nums4: number[],
 ): number {
-  return 0;
+  let count = 0;
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      for (let k = 0; k < nums3.length; k++) {
+        for (let l = 0; l < nums4.length; l++) {
+          if (nums1[i] + nums2[j] + nums3[k] + nums4[l] === 0) {
+            count++;
+          }
+        }
+      }
+    }
+  }
+  return count;
 }

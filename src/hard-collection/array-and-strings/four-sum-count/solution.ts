d75ce7db308944489cc304c020d10f3c05303b95
Maxim Kryuk <mkryuk@gmail.com>
2022-05-25 19:27:50 +0300

    Array and Strings: 4Sum II solution

--- src/hard-collection/array-and-strings/four-sum-count/solution.ts
+++ src/hard-collection/array-and-strings/four-sum-count/solution.ts
@@ -4,15 +4,26 @@ export function fourSumCount(
   nums3: number[],
   nums4: number[],
 ): number {
+  let map = new Map<number, number>();
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums1.length; j++) {
+      const sum = nums1[i] + nums2[j];
+      if (!map.has(sum)) {
+        map.set(sum, 1);
+      } else {
+        map.set(sum, (map.get(sum) ?? 1) + 1);
+      }
+    }
+  }
+
   let count = 0;
   for (let i = 0; i < nums1.length; i++) {
-    for (let j = 0; j < nums2.length; j++) {
-      for (let k = 0; k < nums3.length; k++) {
-        for (let l = 0; l < nums4.length; l++) {
-          if (nums1[i] + nums2[j] + nums3[k] + nums4[l] === 0) {
-            count++;
-          }
-        }
+    for (let j = 0; j < nums1.length; j++) {
+      const sum = nums3[i] + nums4[j];
+      if (map.has(-sum)) {
+        count += map.get(-sum) ?? 1;
+      } else if (sum === 0 && map.has(sum)) {
+        count++;
       }
     }
   }
--- src/hard-collection/array-and-strings/four-sum-count/test.spec.ts
+++ src/hard-collection/array-and-strings/four-sum-count/test.spec.ts
@@ -15,3 +15,19 @@ it("fourSumCount should return 1 for nums1 = [0], nums2 = [0], nums3 = [0], nums
   const nums4 = [0];
   expect(fourSumCount(nums1, nums2, nums3, nums4)).toBe(1);
 });
+
+it("fourSumCount should return 0 for nums1 = [1], nums2 = [-1], nums3 = [0], nums4 = [1]", () => {
+  const nums1 = [1];
+  const nums2 = [-1];
+  const nums3 = [0];
+  const nums4 = [1];
+  expect(fourSumCount(nums1, nums2, nums3, nums4)).toBe(0);
+});
+
+it("fourSumCount should return 0 for nums1 = [-1,-1], nums2 = [-1,1], nums3 = [-1,1], nums4 = [1,-1]", () => {
+  const nums1 = [-1, -1];
+  const nums2 = [-1, 1];
+  const nums3 = [-1, 1];
+  const nums4 = [1, -1];
+  expect(fourSumCount(nums1, nums2, nums3, nums4)).toBe(6);
+});

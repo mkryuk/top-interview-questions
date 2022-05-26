0ce39e3463edd66f31822718887aed95dd6099d1
Maxim Kryuk <mkryuk@gmail.com>
2022-05-26 18:56:44 +0300

    Array and Strings: Container With Most Water solution

--- src/hard-collection/array-and-strings/max-area/solution.ts
+++ src/hard-collection/array-and-strings/max-area/solution.ts
@@ -1,3 +1,15 @@
 export function maxArea(height: number[]): number {
-  return 0;
+  let result = 0;
+  let left = 0;
+  let right = height.length - 1;
+  while (left < right) {
+    let area = Math.min(height[left], height[right]) * (right - left);
+    result = Math.max(result, area);
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return result;
 }

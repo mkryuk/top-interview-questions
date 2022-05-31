2d3a313eef2e8dd88b6b43e58dba8ca25084cbae
Maxim Kryuk <mkryuk@gmail.com>
2022-05-31 21:36:56 +0300

    Array and Strings: First Missing Positive solution with O(n) space

--- src/hard-collection/array-and-strings/first-missing-positive/solution.ts
+++ src/hard-collection/array-and-strings/first-missing-positive/solution.ts
@@ -1,3 +1,16 @@
 export function firstMissingPositive(nums: number[]): number {
-  return 0;
+  let set = new Set<number>();
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] >= 0) {
+      set.add(nums[i]);
+    }
+  }
+  let result = 0;
+  for (let i = 1; i <= set.size + 1; i++) {
+    if (!set.has(i)) {
+      result = i;
+      return result;
+    }
+  }
+  return result;
 }
--- src/hard-collection/array-and-strings/first-missing-positive/test.spec.ts
+++ src/hard-collection/array-and-strings/first-missing-positive/test.spec.ts
@@ -14,3 +14,18 @@ it("firstMissingPositive should return 1 for nums = [7,8,9,11,12]", () => {
   const nums = [7, 8, 9, 11, 12];
   expect(firstMissingPositive(nums)).toBe(1);
 });
+
+it("firstMissingPositive should return 11 for nums = [1,2,3,4,5,6,7,8,9,10]", () => {
+  const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  expect(firstMissingPositive(nums)).toBe(11);
+});
+
+it("firstMissingPositive should return 1 for nums = [2,3,4,5,6,7,8,9,10]", () => {
+  const nums = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+  expect(firstMissingPositive(nums)).toBe(1);
+});
+
+it("firstMissingPositive should return 5 for nums = [1,2,3,4,6,7,8,9,10]", () => {
+  const nums = [1, 2, 3, 4, 6, 7, 8, 9, 10];
+  expect(firstMissingPositive(nums)).toBe(5);
+});

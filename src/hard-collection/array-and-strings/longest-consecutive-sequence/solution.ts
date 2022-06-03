56c38e91fbc539b1a14afafb1d269b3a823c7125
Maxim Kryuk <mkryuk@gmail.com>
2022-06-03 18:07:40 +0300

    Array and Strings: Longest Consecutive Sequence solution

--- src/hard-collection/array-and-strings/longest-consecutive-sequence/solution.ts
+++ src/hard-collection/array-and-strings/longest-consecutive-sequence/solution.ts
@@ -1,3 +1,25 @@
+// Because the while loop is reached only
+// when currentNum marks the beginning of a sequence
+// (i.e. currentNum - 1 is not present in nums),
+// the while loop can only run for n iterations
+// throughout the entire runtime of the algorithm.
+// This means that despite looking like O(n * n) complexity,
+// the nested loops actually run in O(n + n) = O(n) time.
+// All other computations occur in constant time, so the overall runtime is linear.
 export function longestConsecutive(nums: number[]): number {
-  return 0;
+  let set = new Set<number>(nums);
+  let result = 0;
+  set.forEach((num) => {
+    // if this is the beginning of the sequence
+    if (!set.has(num - 1)) {
+      let currentNum = num;
+      let tempResult = 1;
+      while (set.has(currentNum + 1)) {
+        tempResult++;
+        currentNum++;
+      }
+      result = Math.max(result, tempResult);
+    }
+  });
+  return result;
 }

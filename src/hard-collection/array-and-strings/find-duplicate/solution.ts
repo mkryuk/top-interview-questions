da9510d2912ed5377e81830baa63c95462d97691
Maxim Kryuk <mkryuk@gmail.com>
2022-06-05 23:28:41 +0300

    Array and Strings: Find the Duplicate Number solution O(n) space

--- src/hard-collection/array-and-strings/find-duplicate/solution.ts
+++ src/hard-collection/array-and-strings/find-duplicate/solution.ts
@@ -1,3 +1,11 @@
 export function findDuplicate(nums: number[]): number {
+  let set = new Set<number>();
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) {
+      return nums[i];
+    } else {
+      set.add(nums[i]);
+    }
+  }
   return 0;
 }

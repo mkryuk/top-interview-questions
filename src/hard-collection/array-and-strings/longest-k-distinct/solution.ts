f1a6a871209e51b2b5784741fe1aa75d86ab4331
Maxim Kryuk <mkryuk@gmail.com>
2022-06-09 14:39:54 +0300

    Array and Strings: Longest Substring with At Most K Distinct Characters solution

--- src/hard-collection/array-and-strings/longest-k-distinct/solution.ts
+++ src/hard-collection/array-and-strings/longest-k-distinct/solution.ts
@@ -2,5 +2,18 @@ export function lengthOfLongestSubstringKDistinct(
   s: string,
   k: number,
 ): number {
-  return 0;
+  let map = new Map<string, number>();
+  let start = 0;
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+    map.set(s[i], i);
+    if (map.size > k) {
+      let arr = Array.from(map, ([key, value]) => ({ key, value }));
+      const [first] = arr.sort((a, b) => a.value - b.value);
+      start = first.value + 1;
+      map.delete(first.key);
+    }
+    result = Math.max(i - start + 1, result);
+  }
+  return result;
 }
--- src/hard-collection/array-and-strings/longest-k-distinct/test.spec.ts
+++ src/hard-collection/array-and-strings/longest-k-distinct/test.spec.ts
@@ -18,8 +18,26 @@ it('lengthOfLongestSubstringKDistinct should return 5 for s = "aaabb", k = 2', (
   expect(lengthOfLongestSubstringKDistinct(s, k)).toBe(5);
 });
 
+it('lengthOfLongestSubstringKDistinct should return 5 for s = "aaabxbcbbba", k = 2', () => {
+  const s = "aaabxbcbbba";
+  const k = 2;
+  expect(lengthOfLongestSubstringKDistinct(s, k)).toBe(5);
+});
+
 it('lengthOfLongestSubstringKDistinct should return 2 for s = "aa", k = 1', () => {
   const s = "aa";
   const k = 1;
   expect(lengthOfLongestSubstringKDistinct(s, k)).toBe(2);
+});
+
+it('lengthOfLongestSubstringKDistinct should return 1 for s = "ab", k = 1', () => {
+  const s = "ab";
+  const k = 1;
+  expect(lengthOfLongestSubstringKDistinct(s, k)).toBe(1);
+});
+
+it('lengthOfLongestSubstringKDistinct should return 0 for s = "ab", k = 0', () => {
+  const s = "ab";
+  const k = 0;
+  expect(lengthOfLongestSubstringKDistinct(s, k)).toBe(0);
 });

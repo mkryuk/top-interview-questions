fa80148da520af28009afb84b86564c6a15a38d6
Maxim Kryuk <mkryuk@gmail.com>
2022-06-12 15:34:14 +0300

    Array and Strings: Sliding Window Maximum solution TLE

--- src/hard-collection/array-and-strings/sliding-window/solution.ts
+++ src/hard-collection/array-and-strings/sliding-window/solution.ts
@@ -1,3 +1,8 @@
 export function maxSlidingWindow(nums: number[], k: number): number[] {
-  return nums;
+  let result = [];
+  for (let i = 0; i + k <= nums.length; i++) {
+    const max = Math.max(...nums.slice(i, i + k));
+    result.push(max);
+  }
+  return result;
 }

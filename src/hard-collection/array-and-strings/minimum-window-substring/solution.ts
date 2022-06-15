a6281324a4cee7a0bc620c5ea1fb4786eed2a164
Maxim Kryuk <mkryuk@gmail.com>
2022-06-15 19:57:04 +0300

    Array and Strings: Minimum Window Substring solution

--- src/hard-collection/array-and-strings/minimum-window-substring/solution.ts
+++ src/hard-collection/array-and-strings/minimum-window-substring/solution.ts
@@ -1,3 +1,48 @@
 export function minWindow(s: string, t: string): string {
-  return "";
+  if (s.length < t.length) {
+    return "";
+  }
+  const st = t.split("").sort();
+  const map = new Map<string, { count: number; indexes: number[] }>();
+  const tLen = t.length;
+  let mapLen = 0;
+  let result = "";
+  for (let i = 0; i < st.length; i++) {
+    if (map.has(st[i])) {
+      const item = map.get(st[i])!;
+      map.set(st[i], { count: item.count + 1, indexes: [] });
+    } else {
+      map.set(st[i], { count: 1, indexes: [] });
+    }
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      const item = map.get(s[i])!;
+      if (item.count <= item.indexes.length) {
+        item.indexes.shift();
+      } else {
+        mapLen++;
+      }
+      item.indexes.push(i);
+      map.set(s[i], item);
+    }
+    if (mapLen === tLen) {
+      let startIdx = Infinity;
+      let key = "";
+      map.forEach((v, k) => {
+        if (startIdx > v.indexes[0]) {
+          startIdx = v.indexes[0];
+          key = k;
+        }
+      });
+      const item = map.get(key)!;
+      item.indexes.shift();
+      map.set(key, item);
+      mapLen--;
+      const tempResult = s.substring(startIdx, i + 1);
+      result =
+        result && result.length < tempResult.length ? result : tempResult;
+    }
+  }
+  return result;
 }
--- src/hard-collection/array-and-strings/minimum-window-substring/test.spec.ts
+++ src/hard-collection/array-and-strings/minimum-window-substring/test.spec.ts
@@ -4,16 +4,33 @@ it('minWindow should return "BANC" s = "ADOBECODEBANC", t = "ABC"', () => {
   const s = "ADOBECODEBANC";
   const t = "ABC";
   const result = minWindow(s, t);
+  expect(result).toBe("BANC");
+});
+
+it('minWindow should return "ABANC" s = "ADOBECODEABANC", t = "ABCA"', () => {
+  const s = "ADOBECODEABANC";
+  const t = "ABCA";
+  const result = minWindow(s, t);
+  expect(result).toBe("ABANC");
+});
+
+it('minWindow should return "ABANC" s = "ADAABAOECODEABANC", t = "ABCA"', () => {
+  const s = "ADAABAOECODEABANC";
+  const t = "ABCA";
+  const result = minWindow(s, t);
+  expect(result).toBe("ABANC");
 });
 
 it('minWindow should return "a" s = "a", t = "a"', () => {
   const s = "a";
   const t = "a";
   const result = minWindow(s, t);
+  expect(result).toBe("a");
 });
 
 it('minWindow should return "" s = "a", t = "aa"', () => {
   const s = "a";
   const t = "aa";
   const result = minWindow(s, t);
+  expect(result).toBe("");
 });

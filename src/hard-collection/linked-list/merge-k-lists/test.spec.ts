ee4224f9c0977f6971f04936ba5baf705a43963f
Maxim Kryuk <mkryuk@gmail.com>
2022-06-18 21:37:43 +0300

    Linked List: Merge k Sorted Lists added test

--- src/hard-collection/linked-list/merge-k-lists/test.spec.ts
+++ src/hard-collection/linked-list/merge-k-lists/test.spec.ts
@@ -11,6 +11,16 @@ it("mergeKLists should return [1,1,2,3,4,4,5,6] for lists = [[1,4,5],[1,3,4],[2,
   expect(listToArray(result)).toEqual([1, 1, 2, 3, 4, 4, 5, 6]);
 });
 
+it("mergeKLists should return [1,2,3,3,4,4,6,7,8,9,12,15] for lists = [[1,4,7,9],[3,3,4],[2,6,8,12,15]]", () => {
+  const lists = [
+    arrayToList([1, 4, 7, 9]),
+    arrayToList([3, 3, 4]),
+    arrayToList([2, 6, 8, 12, 15]),
+  ];
+  const result = mergeKLists(lists);
+  expect(listToArray(result)).toEqual([1, 2, 3, 3, 4, 4, 6, 7, 8, 9, 12, 15]);
+});
+
 it("mergeKLists should return [] for lists = []", () => {
   const lists: null[] = [];
   const result = mergeKLists(lists);

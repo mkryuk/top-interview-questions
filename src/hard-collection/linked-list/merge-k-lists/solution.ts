b7c460af9a99c903a122509712d9c5a509dd9eac
Maxim Kryuk <mkryuk@gmail.com>
2022-06-19 00:17:04 +0300

    Linked List: Merge k Sorted Lists bruteforce solution

--- src/hard-collection/linked-list/merge-k-lists/solution.ts
+++ src/hard-collection/linked-list/merge-k-lists/solution.ts
@@ -1,5 +1,23 @@
 import { ListNode } from "../common";
 
 export function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
-  return null;
+  let arr: number[] = [];
+  for (let i = 0; i < lists.length; i++) {
+    let l = lists[i];
+    while (l) {
+      arr.push(l.val);
+      l = l.next;
+    }
+  }
+  if (!arr.length) {
+    return null;
+  }
+  arr.sort((a, b) => a - b);
+  let result = new ListNode(arr[0]);
+  let temp = result;
+  for (let i = 1; i < arr.length; i++) {
+    temp.next = new ListNode(arr[i]);
+    temp = temp.next;
+  }
+  return result;
 }

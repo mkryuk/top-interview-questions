d4346b6c24238e657a114ccaac02bedadb34d49f
Maxim Kryuk <mkryuk@gmail.com>
2022-06-20 22:27:44 +0300

    Linked List: Sort List init

---
+++ src/hard-collection/linked-list/sort-ilst/solution.ts
@@ -0,0 +1,5 @@
+import { ListNode } from "../common";
+
+export function sortList(head: ListNode | null): ListNode | null {
+  return null;
+}
---
+++ src/hard-collection/linked-list/sort-ilst/test.spec.ts
@@ -0,0 +1,23 @@
+import { arrayToList, listToArray } from "../common";
+import { sortList } from "./solution";
+
+it("sortList should return [1,2,3,4] for head = [4,2,1,3]", () => {
+  const headArr = [4, 2, 1, 3];
+  const head = arrayToList(headArr);
+  const result = sortList(head);
+  expect(listToArray(result)).toEqual([1, 2, 3, 4]);
+});
+
+it("sortList should return [-1,0,3,4,5] for head = [-1,5,3,4,0]", () => {
+  const headArr = [-1, 5, 3, 4, 0];
+  const head = arrayToList(headArr);
+  const result = sortList(head);
+  expect(listToArray(result)).toEqual([-1, 0, 3, 4, 5]);
+});
+
+it("sortList should return [] for head = []", () => {
+  const headArr: number[] = [];
+  const head = arrayToList(headArr);
+  const result = sortList(head);
+  expect(listToArray(result)).toEqual([]);
+});

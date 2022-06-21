4b9f5307decfad5a8ad7f796b8a22e6a79ed78ba
Maxim Kryuk <mkryuk@gmail.com>
2022-06-21 22:00:48 +0300

    Linked List: Sort List added test

--- src/hard-collection/linked-list/sort-ilst/test.spec.ts
+++ src/hard-collection/linked-list/sort-ilst/test.spec.ts
@@ -15,6 +15,13 @@ it("sortList should return [-1,0,3,4,5] for head = [-1,5,3,4,0]", () => {
   expect(listToArray(result)).toEqual([-1, 0, 3, 4, 5]);
 });
 
+it("sortList should return [-10,-2,0,1,2,5,7,9] for head = [-10,9,-2,7,0,5,2,1]", () => {
+  const headArr = [-10, 9, -2, 7, 0, 5, 2, 1];
+  const head = arrayToList(headArr);
+  const result = sortList(head);
+  expect(listToArray(result)).toEqual([-10, -2, 0, 1, 2, 5, 7, 9]);
+});
+
 it("sortList should return [] for head = []", () => {
   const headArr: number[] = [];
   const head = arrayToList(headArr);

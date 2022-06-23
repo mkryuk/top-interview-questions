c1ebbf36578b04022abf6255dcd950c2f7cc7d58
Maxim Kryuk <mkryuk@gmail.com>
2022-06-23 17:14:00 +0300

    Linked List: Sort List merge sort solution

---
+++ src/hard-collection/linked-list/sort-ilst/solution-ms.ts
@@ -0,0 +1,45 @@
+import { ListNode } from "../common";
+
+export function sortList(head: ListNode | null): ListNode | null {
+  // if there is no element or only one - return it
+  if (head === null || head.next === null) {
+    return head;
+  }
+  const mid = splitList(head);
+  const left = sortList(head);
+  const right = sortList(mid);
+  const result = mergeLists(left, right);
+  return result;
+}
+
+function splitList(head: ListNode | null): ListNode | null {
+  let slow: ListNode | null = head;
+  let fast: ListNode | null = head?.next?.next ?? null;
+  while (fast) {
+    slow = slow?.next ?? null;
+    fast = fast.next?.next ?? null;
+  }
+  let mid = slow!.next;
+  slow!.next = null;
+  return mid;
+}
+
+function mergeLists(
+  left: ListNode | null,
+  right: ListNode | null,
+): ListNode | null {
+  let head = new ListNode();
+  let tail = head;
+  while (left && right) {
+    if (left.val < right.val) {
+      tail.next = left;
+      left = left.next;
+    } else {
+      tail.next = right;
+      right = right.next;
+    }
+    tail = tail.next;
+  }
+  tail.next = left !== null ? left : right;
+  return head.next;
+}
---
+++ src/hard-collection/linked-list/sort-ilst/test-ms.spec.ts
@@ -0,0 +1,37 @@
+import { arrayToList, listToArray } from "../common";
+import { sortList } from "./solution-ms";
+
+it("sortList should return [1,2,3,4] for head = [4,2,1,3]", () => {
+  const headArr = [4, 2, 1, 3];
+  const head = arrayToList(headArr);
+  const result = sortList(head);
+  expect(listToArray(result)).toEqual([1, 2, 3, 4]);
+});
+
+it("sortList should return [-1,0,3,4,5] for head = [-1,5,3,4,0]", () => {
+  const headArr = [-1, 5, 3, 4, 0];
+  const head = arrayToList(headArr);
+  const result = sortList(head);
+  expect(listToArray(result)).toEqual([-1, 0, 3, 4, 5]);
+});
+
+it("sortList should return [-10,-2,0,1,2,5,7,9] for head = [-10,9,-2,7,0,5,2,1]", () => {
+  const headArr = [-10, 9, -2, 7, 0, 5, 2, 1];
+  const head = arrayToList(headArr);
+  const result = sortList(head);
+  expect(listToArray(result)).toEqual([-10, -2, 0, 1, 2, 5, 7, 9]);
+});
+
+it("sortList should return [-3,1,4,5,5,8,11,14,15,19] for head = [4,19,14,5,-3,1,8,5,11,15]", () => {
+  const headArr = [4, 19, 14, 5, -3, 1, 8, 5, 11, 15];
+  const head = arrayToList(headArr);
+  const result = sortList(head);
+  expect(listToArray(result)).toEqual([-3, 1, 4, 5, 5, 8, 11, 14, 15, 19]);
+});
+
+it("sortList should return [] for head = []", () => {
+  const headArr: number[] = [];
+  const head = arrayToList(headArr);
+  const result = sortList(head);
+  expect(listToArray(result)).toEqual([]);
+});

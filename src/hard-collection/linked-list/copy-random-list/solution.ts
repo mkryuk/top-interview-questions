bc81f345b6ea5f24b2b5ce3c5964a1aced010a7f
Maxim Kryuk <mkryuk@gmail.com>
2022-06-26 00:56:24 +0300

    Linked List: Copy List with Random Pointer added helpers methods for tests

--- src/hard-collection/linked-list/copy-random-list/solution.ts
+++ src/hard-collection/linked-list/copy-random-list/solution.ts
@@ -1,4 +1,38 @@
 import { Node } from "../common";
 export function copyRandomList(head: Node | null): Node | null {
-  return head;
+  let copyHead = new Node();
+  let node = head;
+  let nodeRndIndex = new Map<Node, number | null>();
+  let indexNode = new Map<number | null, Node>();
+  let index = 0;
+  let currentNode: Node | null = copyHead;
+  while (node) {
+    currentNode.next = new Node(node.val);
+    const nodeIndex = getIndexByNode(head, node.random)!;
+    nodeRndIndex.set(currentNode.next, nodeIndex);
+    indexNode.set(index++, currentNode.next);
+    currentNode = currentNode.next;
+    node = node.next;
+  }
+  currentNode = copyHead.next;
+  node = head;
+  while (currentNode) {
+    const rndIndex = nodeRndIndex.get(currentNode)!;
+    currentNode.random = rndIndex !== null ? indexNode.get(rndIndex)! : null;
+    currentNode = currentNode.next;
+  }
+  return copyHead.next;
+}
+
+function getIndexByNode(list: Node | null, node: Node | null): number | null {
+  if (node === null) {
+    return null;
+  }
+  let step: Node | null = list;
+  let i = 0;
+  while (step && step !== node) {
+    step = step.next;
+    i++;
+  }
+  return step ? i : null;
 }

a216f5258eaf74b9937c31295a43be79d9214d9b
Maxim Kryuk <mkryuk@gmail.com>
2022-06-28 01:10:26 +0300

    Trees and Graphs:: Word Ladder init

---
+++ src/hard-collection/trees-and-graphs/word-ladder/solution.ts
@@ -0,0 +1,7 @@
+export function ladderLength(
+  beginWord: string,
+  endWord: string,
+  wordList: string[],
+): number {
+  return 0;
+}
---
+++ src/hard-collection/trees-and-graphs/word-ladder/test.spec.ts
@@ -0,0 +1,17 @@
+import { ladderLength } from "./solution";
+
+it('ladderLength should return 5 for beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]', () => {
+  const beginWord = "hit";
+  const endWord = "cog";
+  const wordList = ["hot", "dot", "dog", "lot", "log", "cog"];
+  const result = ladderLength(beginWord, endWord, wordList);
+  expect(result).toEqual(5);
+});
+
+it('ladderLength should return 0 for beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]', () => {
+  const beginWord = "hit";
+  const endWord = "cog";
+  const wordList = ["hot", "dot", "dog", "lot", "log"];
+  const result = ladderLength(beginWord, endWord, wordList);
+  expect(result).toEqual(0);
+});

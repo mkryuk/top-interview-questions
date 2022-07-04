17d84d455763121bfebcd8e4ac800492862bf5d1
Maxim Kryuk <mkryuk@gmail.com>
2022-07-04 22:29:19 +0300

    Trees and Graphs: Surrounded Regions solution

--- src/hard-collection/trees-and-graphs/surrounded-regions/solution.ts
+++ src/hard-collection/trees-and-graphs/surrounded-regions/solution.ts
@@ -1,4 +1,45 @@
 /**
  Do not return anything, modify board in-place instead.
  */
-export function solve(board: string[][]): void {}
+export function solve(board: string[][]): void {
+  let m = board.length;
+  let n = board[0].length;
+  let mark = "M";
+  // we just pass over the edges
+  // and mark all connected nodes
+  for (let i = 0; i < m; i++) {
+    markNode(i, 0, board, mark);
+    markNode(i, n - 1, board, mark);
+  }
+  for (let i = 0; i < n; i++) {
+    markNode(0, i, board, mark);
+    markNode(m - 1, i, board, mark);
+  }
+  // then pass over the matrix
+  // and set all marked nodes to 'O'
+  // and not marked nodes to 'X'
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (board[i][j] === mark) {
+        board[i][j] = "O";
+      } else {
+        board[i][j] = "X";
+      }
+    }
+  }
+}
+
+function markNode(i: number, j: number, board: string[][], mark: string) {
+  let m = board.length;
+  let n = board[0].length;
+  if (i < 0 || j < 0 || i > m - 1 || j > n - 1) {
+    return;
+  }
+  if (board[i][j] === "O") {
+    board[i][j] = mark;
+    markNode(i - 1, j, board, mark);
+    markNode(i + 1, j, board, mark);
+    markNode(i, j - 1, board, mark);
+    markNode(i, j + 1, board, mark);
+  }
+}
--- src/hard-collection/trees-and-graphs/surrounded-regions/test.spec.ts
+++ src/hard-collection/trees-and-graphs/surrounded-regions/test.spec.ts
@@ -19,18 +19,19 @@ it("solve should correctly find surrounded regions for case 1", function () {
 
 it("solve should correctly find surrounded regions for case 2", function () {
   let board = [
-    ["X", "O", "O", "X"],
-    ["X", "X", "O", "X"],
-    ["X", "O", "O", "X"],
-    ["X", "X", "O", "X"],
-    ["X", "O", "O", "X"],
+    ["X", "O", "O", "O", "X"],
+    ["X", "X", "X", "O", "X"],
+    ["X", "O", "X", "O", "X"],
+    ["X", "X", "X", "O", "X"],
+    ["X", "O", "O", "O", "X"],
   ];
+
   let expected = [
-    ["X", "O", "O", "X"],
-    ["X", "X", "O", "X"],
-    ["X", "X", "O", "X"],
-    ["X", "X", "O", "X"],
-    ["X", "O", "O", "X"],
+    ["X", "O", "O", "O", "X"],
+    ["X", "X", "X", "O", "X"],
+    ["X", "X", "X", "O", "X"],
+    ["X", "X", "X", "O", "X"],
+    ["X", "O", "O", "O", "X"],
   ];
   solve(board);
   expect(board).toEqual(expected);
@@ -39,6 +40,23 @@ it("solve should correctly find surrounded regions for case 2", function () {
 it("solve should correctly find surrounded regions for case 3", function () {
   let board = [["X"]];
   let expected = [["X"]];
+  solve(board);
+  expect(board).toEqual(expected);
+});
+
+it("solve should correctly find surrounded regions for case 4", function () {
+  let board = [
+    ["X", "O", "X", "O", "X", "O"],
+    ["O", "X", "O", "X", "O", "X"],
+    ["X", "O", "X", "O", "X", "O"],
+    ["O", "X", "O", "X", "O", "X"],
+  ];
+  let expected = [
+    ["X", "O", "X", "O", "X", "O"],
+    ["O", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "O"],
+    ["O", "X", "O", "X", "O", "X"],
+  ];
   solve(board);
   expect(board).toEqual(expected);
 });

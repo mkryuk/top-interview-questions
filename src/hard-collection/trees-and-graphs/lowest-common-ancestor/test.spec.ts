8118402c3e98cd93d2c7e1552be1ad19f893759f
Maxim Kryuk <mkryuk@gmail.com>
2022-07-07 22:17:14 +0300

    Trees and Graphs: Lowest Common Ancestor of a Binary Tree added test

--- src/hard-collection/trees-and-graphs/lowest-common-ancestor/test.spec.ts
+++ src/hard-collection/trees-and-graphs/lowest-common-ancestor/test.spec.ts
@@ -2,7 +2,8 @@ import { arrayToTreeNode, TreeNode } from "../common";
 import { lowestCommonAncestor } from "./solution";
 
 it("lowestCommonAncestor should return 3 for root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1", () => {
-  const rootArr = [3, 5, 1, 6, 2, 0, 8, null, null, 7, 4];
+  // prettier-ignore
+  const rootArr = [ 3, 5, 1, 6, 2, 0, 8, null, null, 7, 4, null, null, null, null];
   const p = new TreeNode(5);
   const q = new TreeNode(1);
   const root = arrayToTreeNode(rootArr);
@@ -11,7 +12,8 @@ it("lowestCommonAncestor should return 3 for root = [3,5,1,6,2,0,8,null,null,7,4
 });
 
 it("lowestCommonAncestor should return 5 for root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4", () => {
-  const rootArr = [3, 5, 1, 6, 2, 0, 8, null, null, 7, 4];
+  // prettier-ignore
+  const rootArr = [ 3, 5, 1, 6, 2, 0, 8, null, null, 7, 4, null, null, null, null];
   const p = new TreeNode(5);
   const q = new TreeNode(4);
   const root = arrayToTreeNode(rootArr);
@@ -35,4 +37,13 @@ it("lowestCommonAncestor should return 1 for root = [1,2], p = 1, q = 2", () => 
   const root = arrayToTreeNode(rootArr);
   const result = lowestCommonAncestor(root, p, q);
   expect(result?.val).toBe(1);
+});
+
+it("lowestCommonAncestor should return 2 for rootArr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];, p = 9, q = 11", () => {
+  const rootArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  const p = new TreeNode(9);
+  const q = new TreeNode(11);
+  const root = arrayToTreeNode(rootArr);
+  const result = lowestCommonAncestor(root, p, q);
+  expect(result?.val).toBe(2);
 });

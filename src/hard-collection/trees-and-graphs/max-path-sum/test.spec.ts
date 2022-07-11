b9403f993aaec895356913c36a98c6f14bb22f61
Maxim Kryuk <mkryuk@gmail.com>
2022-07-11 21:36:50 +0300

    Trees and Graphs: Binary Tree Maximum Path Sum added test

--- src/hard-collection/trees-and-graphs/max-path-sum/test.spec.ts
+++ src/hard-collection/trees-and-graphs/max-path-sum/test.spec.ts
@@ -14,3 +14,10 @@ it("maxPathSum should return 42 for root = [-10,9,20,null,null,15,7]", () => {
   const result = maxPathSum(root);
   expect(result).toBe(42);
 });
+
+it("maxPathSum should return 40 for root = [5,3,4,7,8,6,9,2,1,10,11]", () => {
+  const arr = [5, 3, 4, 7, 8, 6, 9, 2, 1, 10, 11];
+  const root = arrayToTreeNode(arr);
+  const result = maxPathSum(root);
+  expect(result).toBe(40);
+});

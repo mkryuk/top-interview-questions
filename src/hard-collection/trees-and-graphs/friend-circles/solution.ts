157fe8b9ac32b7b4b768c121aca0b9cf36d1693d
Maxim Kryuk <mkryuk@gmail.com>
2022-07-15 17:50:46 +0300

    Trees and Graphs: Friend Circles init

---
+++ src/hard-collection/trees-and-graphs/friend-circles/solution.ts
@@ -0,0 +1,3 @@
+export function findCircleNum(isConnected: number[][]): number {
+  return 0;
+}
---
+++ src/hard-collection/trees-and-graphs/friend-circles/test.spec.ts
@@ -0,0 +1,28 @@
+import { findCircleNum } from "./solution";
+
+it("findCircleNum should return 1 for isConnected = [[1,1,0],[1,1,1],[0,1,1]]", () => {
+  const isConnected = [
+    [1, 1, 0],
+    [1, 1, 1],
+    [0, 1, 1],
+  ];
+  expect(findCircleNum(isConnected)).toBe(1);
+});
+
+it("findCircleNum should return 2 for isConnected = [[1,1,0],[1,1,0],[0,0,1]]", () => {
+  const isConnected = [
+    [1, 1, 0],
+    [1, 1, 0],
+    [0, 0, 1],
+  ];
+  expect(findCircleNum(isConnected)).toBe(2);
+});
+
+it("findCircleNum should return 3 for isConnected = [[1,0,0],[0,1,0],[0,0,1]]", () => {
+  const isConnected = [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1],
+  ];
+  expect(findCircleNum(isConnected)).toBe(2);
+});

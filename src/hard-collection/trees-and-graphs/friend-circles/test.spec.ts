37bfaeb8b177950b7d832c4327ae6b382ce68d31
Maxim Kryuk <mkryuk@gmail.com>
2022-07-17 15:01:41 +0300

    Trees and Graphs: Friend Circles add test

--- src/hard-collection/trees-and-graphs/friend-circles/test.spec.ts
+++ src/hard-collection/trees-and-graphs/friend-circles/test.spec.ts
@@ -37,3 +37,22 @@ it("findCircleNum should return 2 for isConnected = [[1,0,1,0,0],[0,1,0,1,0],[1,
   ];
   expect(findCircleNum(isConnected)).toBe(2);
 });
+
+it("findCircleNum should return 3 for complex graph of 8 nodes", () => {
+  // 1-2-3
+  //    \
+  //     7-4
+  // 5-8
+  // 6
+  const isConnected = [
+    [1, 1, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 0, 0, 0, 1, 0],
+    [0, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1, 0, 0, 1],
+    [0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 1, 0, 1, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1, 0, 0, 1],
+  ];
+  expect(findCircleNum(isConnected)).toBe(3);
+});

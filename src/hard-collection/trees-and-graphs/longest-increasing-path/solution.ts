0cb30b5e10ae87fdbaf8d656655f777d6be061d0
Maxim Kryuk <mkryuk@gmail.com>
2022-07-28 19:05:35 +0300

    Trees and Graphs: Longest Increasing Path in a Matrix solution

--- src/hard-collection/trees-and-graphs/longest-increasing-path/solution.ts
+++ src/hard-collection/trees-and-graphs/longest-increasing-path/solution.ts
@@ -1,3 +1,48 @@
 export function longestIncreasingPath(matrix: number[][]): number {
-  return 0;
+  let result = 1;
+  const pathMatrix: number[][] = new Array(matrix.length);
+  for (let i = 0; i < matrix.length; i++) {
+    pathMatrix[i] = new Array(matrix[0].length);
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      result = Math.max(result, getNodePath(i, j, matrix, pathMatrix));
+    }
+  }
+
+  return result;
+}
+
+function getNodePath(
+  i: number,
+  j: number,
+  matrix: number[][],
+  pathMatrix: number[][],
+): number {
+  if (i < 0 || j < 0 || i >= matrix.length || j >= matrix[0].length) {
+    return 0;
+  }
+  if (pathMatrix[i][j] !== undefined) {
+    return pathMatrix[i][j];
+  }
+  let path = 1;
+  const canGoRight = j < matrix[0].length - 1;
+  const canGoLeft = j > 0;
+  const canGoDown = i < matrix.length - 1;
+  const canGoUp = i > 0;
+  if (canGoRight && matrix[i][j + 1] > matrix[i][j]) {
+    path = Math.max(path, 1 + getNodePath(i, j + 1, matrix, pathMatrix));
+  }
+  if (canGoDown && matrix[i + 1][j] > matrix[i][j]) {
+    path = Math.max(path, 1 + getNodePath(i + 1, j, matrix, pathMatrix));
+  }
+  if (canGoLeft && matrix[i][j - 1] > matrix[i][j]) {
+    path = Math.max(path, 1 + getNodePath(i, j - 1, matrix, pathMatrix));
+  }
+  if (canGoUp && matrix[i - 1][j] > matrix[i][j]) {
+    path = Math.max(path, 1 + getNodePath(i - 1, j, matrix, pathMatrix));
+  }
+  pathMatrix[i][j] = path;
+  return path;
 }
--- src/hard-collection/trees-and-graphs/longest-increasing-path/test.spec.ts
+++ src/hard-collection/trees-and-graphs/longest-increasing-path/test.spec.ts
@@ -23,11 +23,20 @@ it("longestIncreasingPath should return 1 for matrix = [[1]]", () => {
   expect(longestIncreasingPath(matrix)).toBe(1);
 });
 
-it("longestIncreasingPath should return 2 for matrix = [[3,4,5],[3,2,6],[2,2,1]]", () => {
+it("longestIncreasingPath should return 4 for matrix = [[3,4,5],[3,2,6],[2,2,1]]", () => {
   const matrix = [
     [3, 4, 5],
     [3, 2, 6],
     [2, 2, 1],
   ];
-  expect(longestIncreasingPath(matrix)).toBe(2);
+  expect(longestIncreasingPath(matrix)).toBe(4);
+});
+
+it("longestIncreasingPath should return 5 for matrix = [[9,9,2],[4,6,8],[2,1,1]];", () => {
+  const matrix = [
+    [9, 9, 2],
+    [4, 6, 8],
+    [2, 1, 1],
+  ];
+  expect(longestIncreasingPath(matrix)).toBe(5);
 });

384306ca715aa087029ee57c22263c09b3316ae6
Maxim Kryuk <mkryuk@gmail.com>
2022-07-31 23:21:08 +0300

    Trees and Graphs: Alien Dictionary added test

--- src/hard-collection/trees-and-graphs/alien-order/test.spec.ts
+++ src/hard-collection/trees-and-graphs/alien-order/test.spec.ts
@@ -23,3 +23,27 @@ it('alienOrder should return "ab" for words = ["a","ab","ad"]', () => {
   const result = alienOrder(words);
   expect(result).toBe("abd");
 });
+
+it('alienOrder should return "abcdef" for words = ["ab","bc","cde","cdef"]', () => {
+  const words = ["ab", "bc", "cde", "cdef"];
+  const result = alienOrder(words);
+  expect(result).toBe("abcdef");
+});
+
+it('alienOrder should return "abcdefgh" for words = ["ab","cd","ef","gh"]', () => {
+  const words = ["ab", "cd", "ef", "gh"];
+  const result = alienOrder(words);
+  expect(result).toBe("abcdefgh");
+});
+
+it('alienOrder should return "abcdefghij" for words = ["a","bc","def","ghij"]', () => {
+  const words = ["a", "bc", "def", "ghij"];
+  const result = alienOrder(words);
+  expect(result).toBe("abcdefghij");
+});
+
+it('alienOrder should return "abcdef" for words = ["abc","bcd","cde","def"]', () => {
+  const words = ["abc", "bcd", "cde", "def"];
+  const result = alienOrder(words);
+  expect(result).toBe("abcdef");
+});

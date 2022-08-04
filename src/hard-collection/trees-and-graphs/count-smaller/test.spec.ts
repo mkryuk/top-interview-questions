eb13a0c77df2681a906889a4615ac8de813d3c23
Maxim Kryuk <mkryuk@gmail.com>
2022-08-04 19:20:09 +0300

    Trees and Graphs: Count of Smaller Numbers After Self added test

--- src/hard-collection/trees-and-graphs/count-smaller/test.spec.ts
+++ src/hard-collection/trees-and-graphs/count-smaller/test.spec.ts
@@ -17,3 +17,9 @@ it("countSmaller should return [0,0] for nums = [-1,-1]", () => {
   const result = countSmaller(nums);
   expect(result).toEqual([0, 0]);
 });
+
+it("countSmaller should return [0,7,0,5,0,3,0,1,0] for nums = [1,9,2,8,3,7,4,6,5]", () => {
+  const nums = [1, 9, 2, 8, 3, 7, 4, 6, 5];
+  const result = countSmaller(nums);
+  expect(result).toEqual([0, 7, 0, 5, 0, 3, 0, 1, 0]);
+});

fa772b5596dba9b16f48129c62a43ee1860c0092
Maxim Kryuk <mkryuk@gmail.com>
2022-08-06 22:33:03 +0300

    Trees and Graphs: Count of Smaller Numbers After Self bruteforece solution TLE

--- src/hard-collection/trees-and-graphs/count-smaller/solution.ts
+++ src/hard-collection/trees-and-graphs/count-smaller/solution.ts
@@ -1,3 +1,14 @@
 export function countSmaller(nums: number[]): number[] {
-  return [];
+  let result: number[] = [];
+  result.unshift(0);
+  for (let i = nums.length - 2; i >= 0; i--) {
+    let count = 0;
+    for (let j = i; j < nums.length; j++) {
+      if (nums[i] > nums[j]) {
+        count++;
+      }
+    }
+    result.unshift(count);
+  }
+  return result;
 }

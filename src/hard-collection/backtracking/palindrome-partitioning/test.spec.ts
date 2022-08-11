93cd749ba6609abfee1789dd9104c7524d537760
Maxim Kryuk <mkryuk@gmail.com>
2022-08-11 23:14:12 +0300

    Backtracking: Palindrome Partitioning added tests

--- src/hard-collection/backtracking/palindrome-partitioning/test.spec.ts
+++ src/hard-collection/backtracking/palindrome-partitioning/test.spec.ts
@@ -16,3 +16,39 @@ it('partition should return [["a"]] for s = "a"', () => {
   const actual = partition(s);
   expect(actual).toEqual(expected);
 });
+
+it('partition should return correct result for s = "aabccb"', () => {
+  const s = "aabccb";
+  const expected = [
+    ["a", "a", "b", "c", "c", "b"],
+    ["a", "a", "b", "cc", "b"],
+    ["a", "a", "bccb"],
+    ["aa", "b", "c", "c", "b"],
+    ["aa", "b", "cc", "b"],
+    ["aa", "bccb"],
+  ];
+  const actual = partition(s);
+  expect(actual).toEqual(expected);
+});
+
+it('partition should return correct result for s = "aabccbaa"', () => {
+  const s = "aabccbaa";
+  const expected = [
+    ["a", "a", "b", "c", "c", "b", "a", "a"],
+    ["a", "a", "b", "c", "c", "b", "aa"],
+    ["a", "a", "b", "cc", "b", "a", "a"],
+    ["a", "a", "b", "cc", "b", "aa"],
+    ["a", "a", "bccb", "a", "a"],
+    ["a", "a", "bccb", "aa"],
+    ["a", "abccba", "a"],
+    ["aa", "b", "c", "c", "b", "a", "a"],
+    ["aa", "b", "c", "c", "b", "aa"],
+    ["aa", "b", "cc", "b", "a", "a"],
+    ["aa", "b", "cc", "b", "aa"],
+    ["aa", "bccb", "a", "a"],
+    ["aa", "bccb", "aa"],
+    ["aabccbaa"],
+  ];
+  const actual = partition(s);
+  expect(actual).toEqual(expected);
+});

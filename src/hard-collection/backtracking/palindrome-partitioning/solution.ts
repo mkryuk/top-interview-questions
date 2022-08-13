a48f1b628d314f3f284e10ec13a0fd2a772081c5
Maxim Kryuk <mkryuk@gmail.com>
2022-08-13 22:34:43 +0300

    Backtracking: Palindrome Partitioning dfs solution

--- src/hard-collection/backtracking/palindrome-partitioning/solution.ts
+++ src/hard-collection/backtracking/palindrome-partitioning/solution.ts
@@ -1,3 +1,33 @@
 export function partition(s: string): string[][] {
-  return [];
+  const result: string[][] = [];
+  dfs(0, result, [], s);
+  return result;
+}
+
+function dfs(
+  start: number,
+  result: string[][],
+  currentList: string[],
+  s: string,
+): void {
+  if (start >= s.length) {
+    result.push([...currentList]);
+  }
+  for (let end = start; end < s.length; end++) {
+    if (isPalindrome(s, start, end)) {
+      const res = s.substring(start, end + 1);
+      currentList.push(res);
+      dfs(end + 1, result, currentList, s);
+      currentList.pop();
+    }
+  }
+}
+
+function isPalindrome(s: string, start: number, end: number): boolean {
+  while (start < end) {
+    if (s[start++] !== s[end--]) {
+      return false;
+    }
+  }
+  return true;
 }
--- src/hard-collection/trees-and-graphs/count-smaller/test-merge-sort.spec.ts
+++ src/hard-collection/trees-and-graphs/count-smaller/test-merge-sort.spec.ts
@@ -12,25 +12,25 @@ it("countSmaller should return [3,1,2,0,0] for nums = [5,2,6,1,3]", () => {
   expect(result).toEqual([3, 1, 2, 0, 0]);
 });
 
-xit("countSmaller should return [0] for nums = [-1]", () => {
+it("countSmaller should return [0] for nums = [-1]", () => {
   const nums = [-1];
   const result = countSmaller(nums);
   expect(result).toEqual([0]);
 });
 
-xit("countSmaller should return [0,0] for nums = [-1,-1]", () => {
+it("countSmaller should return [0,0] for nums = [-1,-1]", () => {
   const nums = [-1, -1];
   const result = countSmaller(nums);
   expect(result).toEqual([0, 0]);
 });
 
-xit("countSmaller should return [0,7,0,5,0,3,0,1,0] for nums = [1,9,2,8,3,7,4,6,5]", () => {
+it("countSmaller should return [0,7,0,5,0,3,0,1,0] for nums = [1,9,2,8,3,7,4,6,5]", () => {
   const nums = [1, 9, 2, 8, 3, 7, 4, 6, 5];
   const result = countSmaller(nums);
   expect(result).toEqual([0, 7, 0, 5, 0, 3, 0, 1, 0]);
 });
 
-xit("countSmaller should pass test 32", () => {
+it("countSmaller should pass test 32", () => {
   const nums = [
     26, 78, 27, 100, 33, 67, 90, 23, 66, 5, 38, 7, 35, 23, 52, 22, 83, 51, 98,
     69, 81, 32, 78, 28, 94, 13, 2, 97, 3, 76, 99, 51, 9, 21, 84, 66, 65, 36,

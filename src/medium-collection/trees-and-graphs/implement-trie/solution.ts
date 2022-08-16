64dd94e40606a1a1671bf83434de9a3ef1a7a5ec
Maxim Kryuk <mkryuk@gmail.com>
2022-08-16 19:20:35 +0300

    Trees and Graphs: Implement Trie solution

--- src/medium-collection/trees-and-graphs/implement-trie/solution.ts
+++ src/medium-collection/trees-and-graphs/implement-trie/solution.ts
@@ -1,13 +1,45 @@
 export class Trie {
-  constructor() {}
+  readonly keyLength: number = 26;
+  children: Trie[];
+  lastNode: boolean;
+  constructor() {
+    this.children = new Array(this.keyLength);
+    this.lastNode = false;
+  }
 
-  insert(word: string): void {}
+  insert(word: string): void {
+    let node: Trie = this;
+    for (let i = 0; i < word.length; i++) {
+      const idx = word[i].charCodeAt(0) - "a".charCodeAt(0);
+      if (!node.children[idx]) {
+        node.children[idx] = new Trie();
+      }
+      node = node.children[idx];
+    }
+    node.lastNode = true;
+  }
 
   search(word: string): boolean {
-    return false;
+    let node: Trie = this;
+    for (let i = 0; i < word.length; i++) {
+      const idx = word[i].charCodeAt(0) - "a".charCodeAt(0);
+      if (!node.children[idx]) {
+        return false;
+      }
+      node = node.children[idx];
+    }
+    return node.lastNode;
   }
 
   startsWith(prefix: string): boolean {
-    return false;
+    let node: Trie = this;
+    for (let i = 0; i < prefix.length; i++) {
+      const idx = prefix[i].charCodeAt(0) - "a".charCodeAt(0);
+      if (!node.children[idx]) {
+        return false;
+      }
+      node = node.children[idx];
+    }
+    return true;
   }
 }

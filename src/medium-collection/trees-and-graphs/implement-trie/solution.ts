65ded6f73601daf10a26e5baf05f32518fb7218e
Maxim Kryuk <mkryuk@gmail.com>
2022-08-18 23:01:18 +0300

    Trees and Graphs: added delete to Trie implementation

--- src/medium-collection/trees-and-graphs/implement-trie/solution.ts
+++ src/medium-collection/trees-and-graphs/implement-trie/solution.ts
@@ -42,4 +42,43 @@ export class Trie {
     }
     return true;
   }
+
+  isEmpty() {
+    // If there are no children
+    return !this.children.some((n) => n !== undefined);
+  }
+
+  delete(word: string) {
+    this.deleteWord(this, word, 0);
+  }
+
+  // return true if node should be deleted
+  private deleteWord(node: Trie, word: string, depth: number): boolean {
+    if (node === undefined) {
+      return true;
+    }
+
+    if (depth === word.length) {
+      if (node.lastNode) {
+        node.lastNode = false;
+      }
+      if (node.isEmpty()) {
+        return true;
+      }
+      return false;
+    }
+
+    const index = word[depth].charCodeAt(0) - "a".charCodeAt(0);
+    if (this.deleteWord(node.children[index], word, depth + 1)) {
+      delete node.children[index];
+    }
+
+    // If node does not have any child (its only child got
+    // deleted), and it is not end of another word.
+    if (node.isEmpty() && node.lastNode == false) {
+      return true;
+    }
+
+    return false;
+  }
 }
--- src/medium-collection/trees-and-graphs/implement-trie/test.spec.ts
+++ src/medium-collection/trees-and-graphs/implement-trie/test.spec.ts
@@ -1,6 +1,6 @@
 import { Trie } from "./solution";
 
-it("should test trie", () => {
+it("should insert search words in trie", () => {
   const trie = new Trie();
   trie.insert("apple");
   expect(trie.search("apple")).toBe(true);
@@ -8,4 +8,47 @@ it("should test trie", () => {
   expect(trie.startsWith("app")).toBe(true);
   trie.insert("app");
   expect(trie.search("app")).toBe(true);
+});
+
+it("should delete words from trie", () => {
+  const trie = new Trie();
+  trie.insert("apple");
+  trie.insert("application");
+  expect(trie.search("apple")).toBeTrue();
+  expect(trie.search("application")).toBeTrue();
+  trie.delete("apple");
+  expect(trie.search("apple")).toBeFalse();
+  expect(trie.search("application")).toBeTrue();
+  trie.delete("application");
+  expect(trie.search("application")).toBeFalse();
+  expect(trie.startsWith("app")).toBeFalse();
+});
+
+it("should delete similar words from trie", () => {
+  const words = [
+    "oath",
+    "pea",
+    "eat",
+    "rain",
+    "oathi",
+    "oathk",
+    "oathf",
+    "oate",
+    "oathii",
+    "oathfi",
+    "oathfii",
+  ];
+  const trie = new Trie();
+  words.forEach((word) => {
+    trie.insert(word);
+  });
+
+  while (words.length) {
+    const word = words.pop()!;
+    trie.delete(word);
+    expect(trie.search(word)).toBeFalse();
+    words.forEach((w) => {
+      expect(trie.search(w)).toBeTrue();
+    });
+  }
 });

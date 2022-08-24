371f584a6ef5d7f4ea9ef2ca2ed35c8b31eb70dc
Maxim Kryuk <mkryuk@gmail.com>
2022-08-24 15:19:19 +0300

    Backtracking: Wildcard Matching added tests

--- src/hard-collection/backtracking/wildcard-matching/test.spec.ts
+++ src/hard-collection/backtracking/wildcard-matching/test.spec.ts
@@ -18,5 +18,19 @@ it('isMatch should return false for s = "cb", p = "?a"', () => {
   const s = "cb";
   const p = "?a";
   const result = isMatch(s, p);
+  expect(result).toBeFalse();
+});
+
+it('isMatch should return true for s = "baaaabbb", p = "b?*b*"', () => {
+  const s = "baaaabbb";
+  const p = "b?*b*";
+  const result = isMatch(s, p);
   expect(result).toBeTrue();
+});
+
+it('isMatch should return false for s = "baaaabbb", p = "b?*c*"', () => {
+  const s = "baaaabbb";
+  const p = "b?*c*";
+  const result = isMatch(s, p);
+  expect(result).toBeFalse();
 });

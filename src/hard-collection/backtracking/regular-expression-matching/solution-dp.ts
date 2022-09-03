dd9b2dd4a9a7b0e5a4a584fbbb55f8776af52608
Maxim Kryuk <mkryuk@gmail.com>
2022-09-03 16:20:35 +0300

    Backtracking: Regular Expression Matching dynamic programming solution

---
+++ src/hard-collection/backtracking/regular-expression-matching/solution-dp.ts
@@ -0,0 +1,35 @@
+export function isMatch(s: string, p: string): boolean {
+  let cache: boolean[][] = new Array(s.length + 1);
+  for (let i = 0; i < s.length + 1; i++) {
+    cache[i] = new Array(p.length + 1);
+  }
+  return dp(0, 0, s, p, cache);
+}
+
+function dp(
+  si: number,
+  pi: number,
+  s: string,
+  p: string,
+  cache: boolean[][],
+): boolean {
+  if (cache[si][pi] !== undefined) {
+    return cache[si][pi];
+  }
+
+  let result;
+  if (pi === p.length) {
+    result = si === s.length;
+  } else {
+    const firstMatch = si < s.length && (p[pi] === s[si] || p[pi] === ".");
+    if (pi + 1 < p.length && p[pi + 1] === "*") {
+      result =
+        dp(si, pi + 2, s, p, cache) ||
+        (firstMatch && dp(si + 1, pi, s, p, cache));
+    } else {
+      result = firstMatch && dp(si + 1, pi + 1, s, p, cache);
+    }
+  }
+  cache[si][pi] = result;
+  return result;
+}

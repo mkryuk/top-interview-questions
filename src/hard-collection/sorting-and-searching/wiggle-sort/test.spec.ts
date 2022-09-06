295e6d73e14dc23aa5ce82b2f543643f85cf47fd
Maxim Kryuk <mkryuk@gmail.com>
2022-09-06 14:35:29 +0300

    Sorting and Searching: Wiggle Sort II added test

--- src/hard-collection/sorting-and-searching/wiggle-sort/test.spec.ts
+++ src/hard-collection/sorting-and-searching/wiggle-sort/test.spec.ts
@@ -1,15 +1,33 @@
 import { wiggleSort } from "./solution";
 
-it("wiggleSort should sort nums = [1,5,1,1,6,4] as [1,6,1,5,1,4]", () => {
+it("wiggleSort should sort nums = [1,5,1,1,6,4]", () => {
   const nums = [1, 5, 1, 1, 6, 4];
-  const expected = [1, 6, 1, 5, 1, 4];
   wiggleSort(nums);
-  expect(nums).toEqual(expected);
+  expect(checkIsWiggle(nums)).toBeTrue();
 });
 
-it("wiggleSort should sort nums = [1,3,2,2,3,1] as [2,3,1,3,1,2]", () => {
+it("wiggleSort should sort nums = [1,3,2,2,3,1]", () => {
   const nums = [1, 3, 2, 2, 3, 1];
-  const expected = [2, 3, 1, 3, 1, 2];
   wiggleSort(nums);
-  expect(nums).toEqual(expected);
+  expect(checkIsWiggle(nums)).toBeTrue();
 });
+
+it("wiggleSort should sort nums = [2,1,2,2,1,1,5,4,2,2]", () => {
+  const nums = [2, 1, 2, 2, 1, 1, 5, 4, 2, 2];
+  wiggleSort(nums);
+  expect(checkIsWiggle(nums)).toBeTrue();
+});
+
+function checkIsWiggle(arr: number[]): boolean {
+  if (arr.length < 2) {
+    return true;
+  }
+  for (let i = 1; i < arr.length; i++) {
+    if (i % 2 === 1 && arr[i] <= arr[i - 1]) {
+      return false;
+    } else if (i % 2 === 0 && arr[i] >= arr[i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}

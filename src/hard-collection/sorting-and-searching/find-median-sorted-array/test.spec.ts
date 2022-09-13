119df27202455fc1e536035bdab48807f6cec40c
Maxim Kryuk <mkryuk@gmail.com>
2022-09-13 21:36:16 +0300

    Sorting and Searching: Median of Two Sorted Arrays added test

--- src/hard-collection/sorting-and-searching/find-median-sorted-array/test.spec.ts
+++ src/hard-collection/sorting-and-searching/find-median-sorted-array/test.spec.ts
@@ -1,15 +1,22 @@
 import { findMedianSortedArrays } from "./solution";
 
-it("findMedianSortedArrays should return 2.0 for nums1 = [1,3], nums2 = [2]", () => {
+it("findMedianSortedArrays should return 2 for nums1 = [1,3], nums2 = [2]", () => {
   const nums1 = [1, 3];
   const nums2 = [2];
   const result = findMedianSortedArrays(nums1, nums2);
-  expect(result).toEqual(2.0);
+  expect(result).toEqual(2);
 });
 
 it("findMedianSortedArrays should return 2.5 for nums1 = [1,2], nums2 = [3,4]", () => {
   const nums1 = [1, 2];
   const nums2 = [3, 4];
   const result = findMedianSortedArrays(nums1, nums2);
-  expect(result).toEqual(2.0);
+  expect(result).toEqual(2.5);
+});
+
+it("findMedianSortedArrays should return 5 for nums1 = [1,3,11] nums2 = [-2,7,12]", () => {
+  const nums1 = [1, 3, 11];
+  const nums2 = [-2, 7, 12];
+  const result = findMedianSortedArrays(nums1, nums2);
+  expect(result).toEqual(5);
 });

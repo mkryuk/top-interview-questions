000c00a3a7dfadfca79f4c6840bf1108907581fb
Maxim Kryuk <mkryuk@gmail.com>
2022-09-15 19:56:01 +0300

    Sorting and Searching: Median of Two Sorted Arrays solution

--- src/hard-collection/sorting-and-searching/find-median-sorted-array/solution.ts
+++ src/hard-collection/sorting-and-searching/find-median-sorted-array/solution.ts
@@ -2,5 +2,27 @@ export function findMedianSortedArrays(
   nums1: number[],
   nums2: number[],
 ): number {
-  return 0;
+  let result = [];
+  let i = 0;
+  let k = 0;
+  let j = 0;
+  while (i < nums1.length && k < nums2.length) {
+    if (nums1[i] < nums2[k]) {
+      result[j++] = nums1[i++];
+    } else {
+      result[j++] = nums2[k++];
+    }
+  }
+  while (i < nums1.length) {
+    result[j++] = nums1[i++];
+  }
+  while (k < nums2.length) {
+    result[j++] = nums2[k++];
+  }
+  const n = result.length;
+  const mid = Math.trunc(n / 2);
+  if (n % 2 !== 0) {
+    return result[mid];
+  }
+  return (result[mid - 1] + result[mid]) / 2;
 }

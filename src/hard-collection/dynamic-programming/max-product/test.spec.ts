b744f20d987391e05ef6c8b03d432354eda2dc06
Maxim Kryuk <mkryuk@gmail.com>
2022-09-18 23:47:21 +0300

    Dynamic Programming: Maximum Product Subarray added test

--- src/hard-collection/dynamic-programming/max-product/test.spec.ts
+++ src/hard-collection/dynamic-programming/max-product/test.spec.ts
@@ -17,3 +17,9 @@ it("maxProduct should return 12 nums = [2,3,-2,4,3]", () => {
   const result = maxProduct(nums);
   expect(result).toEqual(12);
 });
+
+it("maxProduct should return 288 nums = [2,3,-2,4,3,-2]", () => {
+  const nums = [2, 3, -2, 4, 3, -2];
+  const result = maxProduct(nums);
+  expect(result).toEqual(288);
+});

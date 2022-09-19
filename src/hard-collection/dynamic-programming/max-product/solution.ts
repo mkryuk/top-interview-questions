9ae4b39d06c56eaafd44850ff27e2d84e446ff3e
Maxim Kryuk <mkryuk@gmail.com>
2022-09-19 21:24:22 +0300

    Dynamic Programming: Maximum Product Subarray dfs solution

--- src/hard-collection/dynamic-programming/max-product/solution.ts
+++ src/hard-collection/dynamic-programming/max-product/solution.ts
@@ -1,3 +1,25 @@
 export function maxProduct(nums: number[]): number {
-  return 0;
+  let result = dfs(nums, 0);
+  return result.max;
+}
+
+function dfs(
+  numbs: number[],
+  i: number,
+): { neg: number; pos: number; max: number } {
+  if (i === numbs.length - 1) {
+    return {
+      pos: numbs[i],
+      neg: numbs[i],
+      max: numbs[i],
+    };
+  }
+
+  const temp = dfs(numbs, i + 1);
+  const neg = temp.neg * numbs[i];
+  const pos = temp.pos * numbs[i];
+  temp.pos = Math.max(numbs[i], neg, pos);
+  temp.neg = Math.min(numbs[i], neg, pos);
+  temp.max = Math.max(temp.pos, temp.neg, temp.max);
+  return temp;
 }

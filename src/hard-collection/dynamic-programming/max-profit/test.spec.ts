9b3e17e70df64092854648bbc5afe22bcc7f3f00
Maxim Kryuk <mkryuk@gmail.com>
2022-09-28 21:37:35 +0300

    Dynamic Programming: Best Time to Buy and Sell Stock with Cooldown added test

--- src/hard-collection/dynamic-programming/max-profit/test.spec.ts
+++ src/hard-collection/dynamic-programming/max-profit/test.spec.ts
@@ -11,3 +11,15 @@ it("maxProfit should return 0 for prices = prices = [1]", () => {
   const result = maxProfit(prices);
   expect(result).toBe(0);
 });
+
+it("maxProfit should return 4 for prices = prices = [1,2,3,4,5]", () => {
+  const prices = [1, 2, 3, 4, 5];
+  const result = maxProfit(prices);
+  expect(result).toBe(4);
+});
+
+it("maxProfit should return 4 for prices = prices = [1,2,0,3,4]", () => {
+  const prices = [1, 2, 0, 3, 4];
+  const result = maxProfit(prices);
+  expect(result).toBe(4);
+});

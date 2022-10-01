43234802b2f3774c651a37e35e9e3f16428c9e4a
Maxim Kryuk <mkryuk@gmail.com>
2022-10-01 22:34:54 +0300

    Dynamic Programming: Perfect Squares solution

--- src/hard-collection/dynamic-programming/perfect-squares/solution.ts
+++ src/hard-collection/dynamic-programming/perfect-squares/solution.ts
@@ -1,3 +1,20 @@
 export function numSquares(n: number): number {
-  return 0;
+  const sqrtIdx = Math.trunc(Math.sqrt(n)) + 1;
+  const sqrtNums: number[] = new Array(sqrtIdx);
+  // pre calculate square numbers
+  for (let i = 0; i < sqrtIdx; i++) {
+    sqrtNums[i] = i * i;
+  }
+  const result = new Array(n + 1).fill(Infinity);
+  result[0] = 0;
+
+  for (let i = 1; i <= n; i++) {
+    for (let k = 1; k < sqrtIdx; k++) {
+      if (i < sqrtNums[k]) {
+        break;
+      }
+      result[i] = Math.min(result[i], result[i - sqrtNums[k]] + 1);
+    }
+  }
+  return result[n];
 }

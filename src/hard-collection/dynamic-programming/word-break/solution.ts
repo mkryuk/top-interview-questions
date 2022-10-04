ba68269f64a6b50f15700f9e071e7b6866e04f6b
Maxim Kryuk <mkryuk@gmail.com>
2022-10-04 23:00:21 +0300

    Dynamic Programming: Word Break solution

--- src/hard-collection/dynamic-programming/word-break/solution.ts
+++ src/hard-collection/dynamic-programming/word-break/solution.ts
@@ -1,3 +1,30 @@
 export function wordBreak(s: string, wordDict: string[]): boolean {
+  let result = stringHasWord(s, 0, wordDict, new Map());
+  return result;
+}
+
+function stringHasWord(
+  s: string,
+  pos: number,
+  wordDict: string[],
+  map: Map<number, boolean>,
+): boolean {
+  if (pos === s.length) {
+    return true;
+  }
+  if (map.has(pos)) {
+    return map.get(pos)!;
+  }
+  for (let i = 0; i < wordDict.length; i++) {
+    if (s.startsWith(wordDict[i], pos)) {
+      let result = stringHasWord(s, pos + wordDict[i].length, wordDict, map);
+      if (result) {
+        return true;
+      }
+    }
+  }
+  // if previous check for this position was failed
+  // we should not to check it again
+  map.set(pos, false);
   return false;
 }
--- src/hard-collection/dynamic-programming/word-break/test.spec.ts
+++ src/hard-collection/dynamic-programming/word-break/test.spec.ts
@@ -34,3 +34,21 @@ it('wordBreak should return false for s = "abcabcd", wordDict = ["abc","cab","cd
   const result = wordBreak(s, wordDict);
   expect(result).toBeFalse();
 });
+
+it('wordBreak should return false for s = "aaaaaaa.....b", wordDict = ["a","aa","aaa" ...]', () => {
+  const s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
+  const wordDict = [
+    "a",
+    "aa",
+    "aaa",
+    "aaaa",
+    "aaaaa",
+    "aaaaaa",
+    "aaaaaaa",
+    "aaaaaaaa",
+    "aaaaaaaaa",
+    "aaaaaaaaaa",
+  ];
+  const result = wordBreak(s, wordDict);
+  expect(result).toBeFalse();
+});

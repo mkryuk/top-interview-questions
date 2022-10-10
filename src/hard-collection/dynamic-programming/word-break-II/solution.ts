20f128ed6da43b02285b76f4041ecfc98645f7bf
Maxim Kryuk <mkryuk@gmail.com>
2022-10-10 21:17:27 +0300

    Dynamic Programming: Word Break II dfs (dynamic top down) solution

--- src/hard-collection/dynamic-programming/word-break-II/solution.ts
+++ src/hard-collection/dynamic-programming/word-break-II/solution.ts
@@ -1,3 +1,31 @@
 export function wordBreak(s: string, wordDict: string[]): string[] {
-  return [];
+  const wordSet = new Set<string>(wordDict);
+  const sentences = dfs(s, wordSet, new Map<string, string[][]>());
+  const result = sentences.map((sentence) => sentence.join(" "));
+  return result;
+}
+
+function dfs(
+  s: string,
+  wordSet: Set<string>,
+  wordMap: Map<string, string[][]>,
+): string[][] {
+  if (s.length === 0) {
+    return [[]];
+  }
+  if (wordMap.has(s)) {
+    return wordMap.get(s)!;
+  }
+  wordMap.set(s, []);
+  for (let i = 1; i <= s.length; i++) {
+    const word = s.substring(0, i);
+    if (wordSet.has(word)) {
+      const subSentence = dfs(s.substring(i), wordSet, wordMap);
+      for (let i = 0; i < subSentence.length; i++) {
+        const newSentence = [word, ...subSentence[i]];
+        wordMap.get(s)!.push(newSentence);
+      }
+    }
+  }
+  return wordMap.get(s)!;
 }

f6dbfaf6ea597b23647d5409d6453b7dbcf68ad0
Maxim Kryuk <mkryuk@gmail.com>
2022-10-12 20:18:03 +0300

    Dynamic Programming: Burst Balloons added test

--- src/hard-collection/dynamic-programming/max-coins/test.spec.ts
+++ src/hard-collection/dynamic-programming/max-coins/test.spec.ts
@@ -11,3 +11,15 @@ it("maxCoins should return 10 for nums = [1,5]", () => {
   const result = maxCoins(nums);
   expect(result).toBe(10);
 });
+
+it("maxCoins should return 60 for nums = [5,1,5,1]", () => {
+  const nums = [5, 1, 5, 1];
+  const result = maxCoins(nums);
+  expect(result).toBe(60);
+});
+
+it("maxCoins should return 30 for nums = [5,0,0,5]", () => {
+  const nums = [5, 0, 0, 5];
+  const result = maxCoins(nums);
+  expect(result).toBe(30);
+});

3a892571d3e1ea49f1ea3605ef2f9e76b27fd5c0
Maxim Kryuk <mkryuk@gmail.com>
2022-10-17 20:34:00 +0300

    Design: LRU Cache added test

--- src/hard-collection/design/lru-cache/test.spec.ts
+++ src/hard-collection/design/lru-cache/test.spec.ts
@@ -1,9 +1,5 @@
 import { LRUCache } from "./solution";
-// Input
-// ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
-// [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
-// Output
-// [null, null, null, 1, null, -1, null, -1, 3, 4]
+
 it("LRUCache should pass test 1", () => {
   const lRUCache = new LRUCache(2);
   lRUCache.put(1, 1); // cache is {1=1}
@@ -15,4 +11,21 @@ it("LRUCache should pass test 1", () => {
   expect(lRUCache.get(1)).toEqual(-1); // return -1 (not found)
   expect(lRUCache.get(3)).toEqual(3); // return 3
   expect(lRUCache.get(4)).toEqual(4); // return 4
+});
+
+it("LRUCache should pass test 2", () => {
+  const lRUCache = new LRUCache(3);
+  lRUCache.put(1, 1); // cache is {1=1}
+  lRUCache.put(2, 2); // cache is {1=1, 2=2}
+  lRUCache.put(3, 3); // cache is {1=1, 2=2, 3=3}
+  expect(lRUCache.get(1)).toEqual(1); // return 1
+  expect(lRUCache.get(2)).toEqual(2); // return 1
+  lRUCache.put(4, 4); // cache is {1=1, 2=2, 4=4}
+  expect(lRUCache.get(2)).toEqual(2); // return 2
+  lRUCache.put(5, 5); // cache is {2=2, 4=4, 5=5}
+  expect(lRUCache.get(1)).toEqual(-1); // return -1 (not found)
+  expect(lRUCache.get(2)).toEqual(2); // return 2
+  expect(lRUCache.get(3)).toEqual(-1); // return -1 (not found)
+  expect(lRUCache.get(4)).toEqual(4); // return 4
+  expect(lRUCache.get(5)).toEqual(5); // return 5
 });

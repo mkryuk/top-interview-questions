7b3da35e3a23bddc7098352e85066be2321cce00
Maxim Kryuk <mkryuk@gmail.com>
2022-10-18 17:20:11 +0300

    Design: LRU Cache solution

--- src/hard-collection/design/lru-cache/solution.ts
+++ src/hard-collection/design/lru-cache/solution.ts
@@ -1,9 +1,101 @@
-export class LRUCache {
-  constructor(capacity: number) {}
+class CacheNode {
+  prev: CacheNode | null;
+  next: CacheNode | null;
+  key: number;
+  value: number;
+  constructor(key: number, value: number) {
+    this.prev = null;
+    this.next = null;
+    this.key = key;
+    this.value = value;
+  }
+}
 
-  get(key: number): number {
-    return 0;
+export class LRUCache {
+  cache: Map<number, CacheNode>;
+  capacity: number;
+  highKey: number = -1;
+  lowKey: number = -1;
+  constructor(capacity: number) {
+    this.cache = new Map<number, CacheNode>();
+    this.capacity = capacity;
   }
 
-  put(key: number, value: number): void {}
+  get(key: number): number {
+    if (this.cache.has(key)) {
+      const curNode = this.cache.get(key)!;
+      this.updatePriority(key);
+      return curNode.value;
+    }
+    return -1;
+  }
+
+  put(key: number, value: number): void {
+    if (this.cache.has(key)) {
+      this.updateKey(key, value);
+      return;
+    }
+    this.addKey(key, value);
+  }
+
+  private updateKey(key: number, value: number) {
+    const curNode = this.cache.get(key)!;
+    this.updatePriority(key);
+    curNode.value = value;
+  }
+
+  private updatePriority(key: number) {
+    const curNode = this.cache.get(key)!;
+    if (key === this.highKey) {
+      // we should not modify the priority
+      return;
+    }
+    if (key === this.lowKey) {
+      // curNode is the low key node
+      this.lowKey = curNode.next!.key;
+      // since this is new low node
+      // remove prev node pointer
+      curNode.next!.prev = null;
+    } else {
+      // not high and not low
+      // we are somewhere in the middle
+      const prevNode = curNode.prev!;
+      const nextNode = curNode.next!;
+      // point prev node and next node
+      // around current node
+      prevNode.next = nextNode;
+      nextNode.prev = prevNode;
+    }
+    // point prev high node to the current
+    // and vice versa
+    const prevHighNode = this.cache.get(this.highKey)!;
+    this.highKey = key;
+    prevHighNode.next = curNode;
+    curNode.prev = prevHighNode;
+    curNode.next = null;
+  }
+
+  private addKey(key: number, value: number) {
+    const newNode = new CacheNode(key, value);
+    if (this.cache.size === 0) {
+      // just add first node
+      this.highKey = key;
+      this.lowKey = key;
+      this.cache.set(key, newNode);
+      return;
+    }
+
+    const prevHighNode = this.cache.get(this.highKey)!;
+    prevHighNode.next = newNode;
+    newNode.prev = prevHighNode;
+    if (this.cache.size >= this.capacity) {
+      const lowNode = this.cache.get(this.lowKey)!;
+      const newLowNode = lowNode.next!;
+      newLowNode.prev = null;
+      this.cache.delete(this.lowKey);
+      this.lowKey = newLowNode.key;
+    }
+    this.cache.set(key, newNode);
+    this.highKey = key;
+  }
 }
--- src/hard-collection/design/lru-cache/test.spec.ts
+++ src/hard-collection/design/lru-cache/test.spec.ts
@@ -29,3 +29,34 @@ it("LRUCache should pass test 2", () => {
   expect(lRUCache.get(4)).toEqual(4); // return 4
   expect(lRUCache.get(5)).toEqual(5); // return 5
 });
+
+it("LRUCache should pass test 3", () => {
+  const lRUCache = new LRUCache(1);
+  lRUCache.put(1, 1); // cache is {1=1}
+  lRUCache.put(2, 2); // cache is {2=2}
+  lRUCache.put(3, 3); // cache is {3=3}
+  expect(lRUCache.get(1)).toEqual(-1); // return -1 (not found)
+  expect(lRUCache.get(2)).toEqual(-1); // return -1 (not found)
+  expect(lRUCache.get(3)).toEqual(3); // return 3
+  lRUCache.put(4, 4); // cache is {4=4}
+  expect(lRUCache.get(3)).toEqual(-1); // return -1 (not found)
+  expect(lRUCache.get(4)).toEqual(4); // return 4
+});
+
+it("LRUCache should pass test 3", () => {
+  const lRUCache = new LRUCache(3);
+  lRUCache.put(1, 1); // cache is {1=1}
+  lRUCache.put(2, 2); // cache is {1=1, 2=2}
+  lRUCache.put(3, 3); // cache is {1=1, 2=2, 3=3}
+  lRUCache.put(4, 4); // cache is {2=2, 3=3, 4=4}
+  expect(lRUCache.get(4)).toEqual(4); // return 4
+  expect(lRUCache.get(3)).toEqual(3); // return 3
+  expect(lRUCache.get(2)).toEqual(2); // return 2
+  expect(lRUCache.get(1)).toEqual(-1); // return -1 (not found)
+  lRUCache.put(5, 5); // cache is {4=4}
+  expect(lRUCache.get(1)).toEqual(-1); // return -1 (not found)
+  expect(lRUCache.get(2)).toEqual(2); // return 2
+  expect(lRUCache.get(3)).toEqual(3); // return 3
+  expect(lRUCache.get(4)).toEqual(-1); // return -1 (not found)
+  expect(lRUCache.get(5)).toEqual(5); // return 5
+});

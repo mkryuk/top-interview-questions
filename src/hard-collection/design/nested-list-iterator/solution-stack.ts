123ae4657b3ef0f8ad04cf6a34405a73da0c203a
Maxim Kryuk <mkryuk@gmail.com>
2022-10-22 23:59:34 +0300

    Design: Flatten Nested List Iterator stack solution

---
+++ src/hard-collection/design/nested-list-iterator/solution-stack.ts
@@ -0,0 +1,34 @@
+import { NestedInteger } from "./nested-integer";
+
+export class NestedIterator {
+  stack: NestedInteger[] = [];
+  constructor(nestedList: NestedInteger[]) {
+    this.stack = [];
+    this.addToStack(nestedList);
+  }
+
+  hasNext(): boolean {
+    if (!this.stack.length) {
+      return false;
+    }
+    while (this.stack.length > 0) {
+      let top = this.stack[this.stack.length - 1];
+      if (top.isInteger()) {
+        return true;
+      }
+      this.stack.pop()!;
+      this.addToStack(top.getList());
+    }
+    return false;
+  }
+
+  next(): number {
+    return this.stack.pop()!.getInteger()!;
+  }
+
+  addToStack(list: NestedInteger[]) {
+    for (let i = list.length - 1; i >= 0; i--) {
+      this.stack.push(list[i]);
+    }
+  }
+}
---
+++ src/hard-collection/design/nested-list-iterator/test-stack.spec.ts
@@ -0,0 +1,51 @@
+import { NestedInteger } from "./nested-integer";
+import { NestedIterator } from "./solution-stack";
+
+it("NestedIterator should return [1,1,2,1,1] for nestedList = [[1,1],2,[1,1]]", () => {
+  const nestedListArray: any[] = [[1, 1], 2, [1, 1]];
+  const nestedInteger = arrayToNestedInteger(nestedListArray);
+  var nestedIterator = new NestedIterator(nestedInteger.getList());
+  var result: number[] = [];
+  while (nestedIterator.hasNext()) result.push(nestedIterator.next());
+  expect(result).toEqual([1, 1, 2, 1, 1]);
+});
+
+it("NestedIterator should return [1,4,6] for nestedList = [1,[4,[6]]]", () => {
+  const nestedListArray: any[] = [1, [4, [6]]];
+  const nestedInteger = arrayToNestedInteger(nestedListArray);
+  var nestedIterator = new NestedIterator(nestedInteger.getList());
+  var result: number[] = [];
+  while (nestedIterator.hasNext()) result.push(nestedIterator.next());
+  expect(result).toEqual([1, 4, 6]);
+});
+
+it("NestedIterator should return [1,2,3,4,5,6,7] for nestedList = [1,[2,3,[4],[[5],6]],7]", () => {
+  const nestedListArray: any[] = [1, [2, 3, [4], [[5], 6]], 7];
+  const nestedInteger = arrayToNestedInteger(nestedListArray);
+  var nestedIterator = new NestedIterator(nestedInteger.getList());
+  var result: number[] = [];
+  while (nestedIterator.hasNext()) result.push(nestedIterator.next());
+  expect(result).toEqual([1, 2, 3, 4, 5, 6, 7]);
+});
+
+it("NestedIterator should return [] for nestedList = [[]]", () => {
+  const nestedListArray: any[] = [[]];
+  const nestedInteger = arrayToNestedInteger(nestedListArray);
+  var nestedIterator = new NestedIterator(nestedInteger.getList());
+  var result: number[] = [];
+  while (nestedIterator.hasNext()) result.push(nestedIterator.next());
+  expect(result).toEqual([]);
+});
+
+function arrayToNestedInteger(item: any): NestedInteger {
+  var result: NestedInteger = new NestedInteger();
+  if (Array.isArray(item)) {
+    for (let i = 0; i < item.length; i++) {
+      const ni = arrayToNestedInteger(item[i]);
+      result.add(ni);
+    }
+  } else {
+    result.setInteger(item);
+  }
+  return result;
+}

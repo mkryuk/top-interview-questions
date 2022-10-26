688f9934d2e845a2cdd438acbb0b23e646f1e75a
Maxim Kryuk <mkryuk@gmail.com>
2022-10-26 22:38:55 +0300

    Design: Find Median from Data Stream insertion sort solution

--- src/hard-collection/design/median-finder/solution.ts
+++ src/hard-collection/design/median-finder/solution.ts
@@ -1,9 +1,47 @@
 export class MedianFinder {
-  constructor() {}
-
-  addNum(num: number): void {}
+  numbers: number[];
+  constructor() {
+    this.numbers = [];
+  }
+  addNum(num: number): void {
+    const index = this.findIndex(this.numbers, num);
+    this.numbers = this.insertValue(this.numbers, num, index);
+  }
 
   findMedian(): number {
-    return 0;
+    if (this.numbers.length % 2 === 0) {
+      const midIndex = this.numbers.length / 2;
+      const left = this.numbers[midIndex - 1];
+      const right = this.numbers[midIndex];
+      return (left + right) / 2;
+    }
+    const midIndex = Math.trunc(this.numbers.length / 2);
+    return this.numbers[midIndex];
+  }
+
+  findIndex(array: number[], value: number): number {
+    let left = 0;
+    let right = array.length;
+    while (left < right) {
+      let mid = left + Math.trunc((right - left) / 2);
+      if (array[mid] < value) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+    return left;
+  }
+
+  insertValue(array: number[], value: number, index: number): number[] {
+    const newNumbers: number[] = [];
+    for (let i = 0; i < index; i++) {
+      newNumbers.push(array[i]);
+    }
+    newNumbers.push(value);
+    for (let i = index; i < array.length; i++) {
+      newNumbers.push(array[i]);
+    }
+    return newNumbers;
   }
 }

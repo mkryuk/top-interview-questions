79927bb5d1689ac24092eaed3fda60aceaf29612
Maxim Kryuk <mkryuk@gmail.com>
2022-10-29 16:47:52 +0300

    Design: Range Sum Query 2D - Mutable init

---
+++ src/hard-collection/design/num-matrix/solution.ts
@@ -0,0 +1,9 @@
+export class NumMatrix {
+  constructor(matrix: number[][]) {}
+
+  update(row: number, col: number, val: number): void {}
+
+  sumRegion(row1: number, col1: number, row2: number, col2: number): number {
+    return 0;
+  }
+}

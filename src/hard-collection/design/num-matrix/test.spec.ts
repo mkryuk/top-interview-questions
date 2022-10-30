0d5b2de875e577c25bdcc574f2a84d5218b0ee12
Maxim Kryuk <mkryuk@gmail.com>
2022-10-30 14:36:39 +0200

    Design: Range Sum Query 2D - Mutable added test

--- src/hard-collection/design/num-matrix/test.spec.ts
+++ src/hard-collection/design/num-matrix/test.spec.ts
@@ -9,7 +9,23 @@ it("NumMatrix should pass test 1", () => {
     [1, 0, 3, 0, 5],
   ];
   var numMatrix = new NumMatrix(matrix);
-  numMatrix.sumRegion(2, 1, 4, 3); // return 8 (i.e. sum of the left red rectangle)
+  expect(numMatrix.sumRegion(2, 1, 4, 3)).toEqual(8); // return 8 (i.e. sum of the left red rectangle)
   numMatrix.update(3, 2, 2); // matrix changes from left image to right image
-  numMatrix.sumRegion(2, 1, 4, 3); // return 10 (i.e. sum of the right red rectangle)
+  expect(numMatrix.sumRegion(2, 1, 4, 3)).toEqual(10); // return 10 (i.e. sum of the right red rectangle)
+});
+
+it("NumMatrix should pass test 2", () => {
+  const matrix = [
+    [3, 0, 1, 4, 2],
+    [5, 6, 3, 2, 1],
+    [1, 2, 0, 1, 5],
+    [4, 1, 0, 1, 7],
+    [1, 0, 3, 0, 5],
+  ];
+  var numMatrix = new NumMatrix(matrix);
+  expect(numMatrix.sumRegion(0, 0, 2, 2)).toEqual(21);
+  expect(numMatrix.sumRegion(2, 2, 4, 4)).toEqual(22);
+  numMatrix.update(2, 2, 3);
+  expect(numMatrix.sumRegion(0, 0, 2, 2)).toEqual(24);
+  expect(numMatrix.sumRegion(2, 2, 4, 4)).toEqual(25);
 });

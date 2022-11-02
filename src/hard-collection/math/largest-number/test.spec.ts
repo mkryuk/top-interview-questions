5911c88d09d9493243e607056cd322b2319dd10c
Maxim Kryuk <mkryuk@gmail.com>
2022-11-02 20:45:25 +0200

    Math: Largest Number added test

--- src/hard-collection/math/largest-number/test.spec.ts
+++ src/hard-collection/math/largest-number/test.spec.ts
@@ -11,3 +11,21 @@ it('largestNumber should return "9534330" for nums = [3,30,34,5,9]', () => {
   const result = largestNumber(nums);
   expect(result).toEqual("9534330");
 });
+
+it('largestNumber should return "5755348419" for nums = [5,48,419,53,57]', () => {
+  const nums = [5, 48, 419, 53, 57];
+  const result = largestNumber(nums);
+  expect(result).toEqual("5755348419");
+});
+
+it('largestNumber should return "5755448419" for nums = [5,48,419,54,57]', () => {
+  const nums = [5, 48, 419, 54, 57];
+  const result = largestNumber(nums);
+  expect(result).toEqual("5755448419");
+});
+
+it('largestNumber should return "5756548419" for nums = [5,48,419,57,56]', () => {
+  const nums = [5, 48, 419, 57, 56];
+  const result = largestNumber(nums);
+  expect(result).toEqual("5756548419");
+});

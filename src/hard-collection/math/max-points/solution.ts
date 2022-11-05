236e797b4b9e9c34cbcc26451a79710bda4ffc65
Maxim Kryuk <mkryuk@gmail.com>
2022-11-05 22:56:31 +0200

    Math: Max Points on a Line solution

--- src/hard-collection/math/max-points/solution.ts
+++ src/hard-collection/math/max-points/solution.ts
@@ -1,3 +1,30 @@
 export function maxPoints(points: number[][]): number {
-  return 0;
+  let result = 0;
+
+  for (let i = 0; i < points.length; i++) {
+    const pt1 = points[i];
+    const map = new Map<number, number>();
+    for (let k = 0; k < points.length; k++) {
+      if (k === i) {
+        continue;
+      }
+      const pt2 = points[k];
+      const sl = slope(pt1, pt2);
+      if (!map.get(sl)) {
+        // 2 because two points pt1 and pt2
+        // are already on the same line
+        map.set(sl, 2);
+      } else {
+        map.set(sl, map.get(sl)! + 1);
+      }
+      result = Math.max(result, map.get(sl)!);
+    }
+  }
+  return result;
+}
+
+function slope(pt1: number[], pt2: number[]): number {
+  const [x1, y1] = pt1;
+  const [x2, y2] = pt2;
+  return (y2 - y1) / (x2 - x1);
 }

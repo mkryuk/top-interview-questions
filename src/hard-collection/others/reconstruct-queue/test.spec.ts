269d06e9f431886f2bd8464f3f1cbe9d84abefb4
Maxim Kryuk <mkryuk@gmail.com>
2022-11-06 21:18:25 +0200

    Others: Queue Reconstruction by Height init

---
+++ src/hard-collection/others/reconstruct-queue/test.spec.ts
@@ -0,0 +1,41 @@
+import { reconstructQueue } from "./solution";
+
+it("reconstructQueue should return [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]] for people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]", () => {
+  const people = [
+    [7, 0],
+    [4, 4],
+    [7, 1],
+    [5, 0],
+    [6, 1],
+    [5, 2],
+  ];
+  const result = reconstructQueue(people);
+  expect(result).toEqual([
+    [5, 0],
+    [7, 0],
+    [5, 2],
+    [6, 1],
+    [4, 4],
+    [7, 1],
+  ]);
+});
+
+it("reconstructQueue should return [[4,0],[5,0],[2,2],[3,2],[1,4],[6,0]] for people = [[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]]", () => {
+  const people = [
+    [6, 0],
+    [5, 0],
+    [4, 0],
+    [3, 2],
+    [2, 2],
+    [1, 4],
+  ];
+  const result = reconstructQueue(people);
+  expect(result).toEqual([
+    [4, 0],
+    [5, 0],
+    [2, 2],
+    [3, 2],
+    [1, 4],
+    [6, 0],
+  ]);
+});

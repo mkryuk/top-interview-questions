fa1e0bf2f36d3d3f05e4a7e712550c57bcf5e06d
Maxim Kryuk <mkryuk@gmail.com>
2022-11-07 17:15:18 +0200

    Others: Queue Reconstruction by Height solution

--- src/hard-collection/others/reconstruct-queue/solution.ts
+++ src/hard-collection/others/reconstruct-queue/solution.ts
@@ -1,3 +1,29 @@
 export function reconstructQueue(people: number[][]): number[][] {
-  return [];
+  people.sort((a, b) => {
+    const [ha, ka] = a;
+    const [hb, kb] = b;
+    if (ha === hb) {
+      return ka - kb;
+    }
+    return ha - hb;
+  });
+  const result = new Array(people.length);
+  for (let i = 0; i < people.length; i++) {
+    const idx = getIndex(people[i], result);
+    result[idx] = people[i];
+  }
+  return result;
+}
+
+function getIndex(item: number[], result: number[][]) {
+  const [h, k] = item;
+  let toSkip = k + 1;
+  let index = k;
+  for (let i = 0; toSkip; i++) {
+    if (result[i] === undefined || result[i][0] >= h) {
+      toSkip--;
+    }
+    index = i;
+  }
+  return index;
 }

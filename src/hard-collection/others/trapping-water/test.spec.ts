fa0712b1bdd5132d956a84d7c78eddfb87ece320
Maxim Kryuk <mkryuk@gmail.com>
2022-11-07 18:10:29 +0200

    Others: Trapping Rain Water init

---
+++ src/hard-collection/others/trapping-water/test.spec.ts
@@ -0,0 +1,13 @@
+import { trap } from "./solution";
+
+it("trap should return 6 for height = [0,1,0,2,1,0,1,3,2,1,2,1]", () => {
+  const height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+  const result = trap(height);
+  expect(result).toEqual(6);
+});
+
+it("trap should return 9 for height = [4,2,0,3,2,5]", () => {
+  const height = [4, 2, 0, 3, 2, 5];
+  const result = trap(height);
+  expect(result).toEqual(9);
+});

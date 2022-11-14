584f5b49d6e31ea2cb6eb6328b541c983b9209fb
Maxim Kryuk <mkryuk@gmail.com>
2022-11-14 20:28:49 +0200

    Others: Largest Rectangle in Histogram init

---
+++ src/hard-collection/others/largest-rectangle-area/test.spec.ts
@@ -0,0 +1,13 @@
+import { largestRectangleArea } from "./solution";
+
+it("largestRectangleArea should return 10 for heights = [2,1,5,6,2,3]", () => {
+  const heights = [2, 1, 5, 6, 2, 3];
+  const result = largestRectangleArea(heights);
+  expect(result).toEqual(10);
+});
+
+it("largestRectangleArea should return 4 for heights = [2,4]", () => {
+  const heights = [2, 4];
+  const result = largestRectangleArea(heights);
+  expect(result).toEqual(4);
+});

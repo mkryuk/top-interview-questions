4f291c0e39ec844059d26ba6ff5e60409b6e8713
Maxim Kryuk <mkryuk@gmail.com>
2022-11-16 18:40:12 +0200

    Others: Largest Rectangle in Histogram stack solution

---
+++ src/hard-collection/others/largest-rectangle-area/solution-stack.ts
@@ -0,0 +1,24 @@
+export function largestRectangleArea(heights: number[]): number {
+  const stack: number[] = [-1];
+  let maxArea = 0;
+  for (let i = 0; i < heights.length; i++) {
+    while (
+      stack[stack.length - 1] !== -1 &&
+      heights[stack[stack.length - 1]] >= heights[i]
+    ) {
+      const currentHeight = heights[stack.pop()!];
+      const currentWidth = i - stack[stack.length - 1] - 1;
+      const currentArea = currentHeight * currentWidth;
+      maxArea = Math.max(maxArea, currentArea);
+    }
+    stack.push(i);
+  }
+
+  while (stack[stack.length - 1] !== -1) {
+    const currentHeight = heights[stack.pop()!];
+    const currentWidth = heights.length - stack[stack.length - 1] - 1;
+    const currentArea = currentHeight * currentWidth;
+    maxArea = Math.max(maxArea, currentArea);
+  }
+  return maxArea;
+}
--- src/hard-collection/others/largest-rectangle-area/test-stack.spec.ts
+++ src/hard-collection/others/largest-rectangle-area/test-stack.spec.ts
@@ -1,4 +1,4 @@
-import { largestRectangleArea } from "./solution-cache";
+import { largestRectangleArea } from "./solution-stack";
 
 it("largestRectangleArea should return 10 for heights = [2,1,5,6,2,3]", () => {
   const heights = [2, 1, 5, 6, 2, 3];
@@ -20,6 +20,30 @@ it("largestRectangleArea should pass test with equal heights in time", () => {
   const passTime = countPassTime(() => {
     const result = largestRectangleArea(heights);
     expect(result).toEqual(2100000);
+  });
+  expect(passTime).toBeLessThan(100);
+});
+
+it("largestRectangleArea should pass with increase heights test in time", () => {
+  const heights: number[] = [];
+  for (let i = 0; i < 50000; i++) {
+    heights.push(i);
+  }
+  const passTime = countPassTime(() => {
+    const result = largestRectangleArea(heights);
+    expect(result).toEqual(625000000);
+  });
+  expect(passTime).toBeLessThan(100);
+});
+
+it("largestRectangleArea should pass with decrease heights test in time", () => {
+  const heights: number[] = [];
+  for (let i = 50000; i > 0; i--) {
+    heights.push(i);
+  }
+  const passTime = countPassTime(() => {
+    const result = largestRectangleArea(heights);
+    expect(result).toEqual(625025000);
   });
   expect(passTime).toBeLessThan(100);
 });

11d84c2f3490b80aa208f9b8d79123dcb0f69726
Maxim Kryuk <mkryuk@gmail.com>
2022-11-19 16:51:33 +0200

    Trees and Graphs: Count Complete Tree Nodes O(n) solution

--- src/hard-collection/trees-and-graphs/count-complete-nodes/solution.ts
+++ src/hard-collection/trees-and-graphs/count-complete-nodes/solution.ts
@@ -1,5 +1,12 @@
 import { TreeNode } from "../common";
 
+// this runs in O(n) time complexity
 export function countNodes(root: TreeNode | null): number {
-  return 0;
+  if (!root) {
+    return 0;
+  }
+  let result = 1;
+  result += countNodes(root.right);
+  result += countNodes(root.left);
+  return result;
 }
--- src/hard-collection/trees-and-graphs/count-complete-nodes/test.spec.ts
+++ src/hard-collection/trees-and-graphs/count-complete-nodes/test.spec.ts
@@ -21,3 +21,24 @@ it("countNodes should return 1 for nodes = [1]", () => {
   const result = countNodes(root);
   expect(result).toEqual(1);
 });
+
+it("countNodes should return 11 for nodes = [1,2,3,4,5,6,7,8,9,10]", () => {
+  const nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const root = arrayToTreeNode(nodes);
+  const result = countNodes(root);
+  expect(result).toEqual(10);
+});
+
+it("countNodes should return 11 for nodes = [1,2,3,4,5,6,7,8,9,10,11]", () => {
+  const nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  const root = arrayToTreeNode(nodes);
+  const result = countNodes(root);
+  expect(result).toEqual(11);
+});
+
+it("countNodes should return 11 for nodes = [1,2,3,4,5,6,7,8,9,10,12]", () => {
+  const nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  const root = arrayToTreeNode(nodes);
+  const result = countNodes(root);
+  expect(result).toEqual(12);
+});

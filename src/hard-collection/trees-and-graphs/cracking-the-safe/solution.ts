ea44be40e5fe341248b5a8dfaabc8367813b1bc3
Maxim Kryuk <mkryuk@gmail.com>
2022-11-24 22:16:32 +0200

    Trees and Graphs: Cracking the Safe solution

--- src/hard-collection/trees-and-graphs/cracking-the-safe/solution.ts
+++ src/hard-collection/trees-and-graphs/cracking-the-safe/solution.ts
@@ -1,3 +1,31 @@
 export function crackSafe(n: number, k: number): string {
-  return "";
+  if (n === 1 && k === 1) {
+    return "0";
+  }
+  const visited = new Set<string>();
+  let sequences: string[] = [];
+  // fill prefix with "000" in case n = 4
+  const prefix = "0".repeat(n - 1);
+  dfs(prefix, sequences, visited, k);
+  sequences.push(prefix);
+  return sequences.join("");
+}
+
+function dfs(
+  prefix: string,
+  sequences: string[],
+  visited: Set<string>,
+  k: number,
+): void {
+  for (let i = 0; i < k; i++) {
+    const sequence = prefix + i;
+    if (visited.has(sequence)) {
+      continue;
+    }
+    visited.add(sequence);
+    // in case of sequence "0001"
+    // we go deeper with prefix "001"
+    dfs(sequence.slice(1), sequences, visited, k);
+    sequences.push(i.toString());
+  }
 }

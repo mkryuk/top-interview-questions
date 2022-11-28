17ed249db309fac3d2d36319584e7d36bd8512a0
Maxim Kryuk <mkryuk@gmail.com>
2022-11-28 21:57:53 +0200

    Trees and Graphs: Diameter of Binary Tree solution

--- src/hard-collection/trees-and-graphs/diameter-of-binary-tree/solution.ts
+++ src/hard-collection/trees-and-graphs/diameter-of-binary-tree/solution.ts
@@ -1,5 +1,19 @@
 import { TreeNode } from "../common";
 
 export function diameterOfBinaryTree(root: TreeNode | null): number {
-  return 0;
+  let [result] = getNodeLength(root);
+  return result;
+}
+
+function getNodeLength(root: TreeNode | null): [d: number, l: number] {
+  if (!root) {
+    return [0, 0];
+  }
+  const [lDim, left] = getNodeLength(root.left);
+  const [rDim, right] = getNodeLength(root.right);
+  // get biggest diameter between current, left or right node
+  const diameter = Math.max(left + right, lDim, rDim);
+  // get longest path for the node including current node
+  const length = Math.max(left + 1, right + 1);
+  return [diameter, length];
 }
--- src/hard-collection/trees-and-graphs/diameter-of-binary-tree/test.spec.ts
+++ src/hard-collection/trees-and-graphs/diameter-of-binary-tree/test.spec.ts
@@ -14,3 +14,58 @@ it("diameterOfBinaryTree should return 1 for root = [1,2]", () => {
   const result = diameterOfBinaryTree(root);
   expect(result).toEqual(1);
 });
+
+it("diameterOfBinaryTree should return 4 for root = [1,2,3,4,5,6,7]", () => {
+  const arr = [1, 2, 3, 4, 5, 6, 7];
+  const root = arrayToTreeNode(arr);
+  const result = diameterOfBinaryTree(root);
+  expect(result).toEqual(4);
+});
+
+it("diameterOfBinaryTree should return 4 for root = [1,2,3,4,null,null,7]", () => {
+  const arr = [1, 2, 3, 4, null, null, 7];
+  const root = arrayToTreeNode(arr);
+  const result = diameterOfBinaryTree(root);
+  expect(result).toEqual(4);
+});
+
+it("diameterOfBinaryTree should return 4 for root = [1,2,3,null,5,6,null]", () => {
+  const arr = [1, 2, 3, null, 5, 6, null];
+  const root = arrayToTreeNode(arr);
+  const result = diameterOfBinaryTree(root);
+  expect(result).toEqual(4);
+});
+
+it("diameterOfBinaryTree should return 5 for root = [1,2,3,4,5,6,7,8]", () => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+  const root = arrayToTreeNode(arr);
+  const result = diameterOfBinaryTree(root);
+  expect(result).toEqual(5);
+});
+
+it("diameterOfBinaryTree should return 5 for root = [1,2,3,4,5,6,7,null,9]", () => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, null, 9];
+  const root = arrayToTreeNode(arr);
+  const result = diameterOfBinaryTree(root);
+  expect(result).toEqual(5);
+});
+
+it("diameterOfBinaryTree should return 5 for root = [1,2,3,4,5,6,7,null,9]", () => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, null, 9];
+  const root = arrayToTreeNode(arr);
+  const result = diameterOfBinaryTree(root);
+  expect(result).toEqual(5);
+});
+
+it("diameterOfBinaryTree should return 5 for left side of the tree", () => {
+  // prettier-ignore
+  const arr = 
+  [                                     1,
+                         2,                                   null, 
+             4,                 5,                null,                   null,
+     null,         6,       7,      8,      null,       null,       null,       null, 
+  null, null, null, null, 9, 10, 11, 12, null, null, null, null, null, null, null, null];
+  const root = arrayToTreeNode(arr);
+  const result = diameterOfBinaryTree(root);
+  expect(result).toEqual(5);
+});

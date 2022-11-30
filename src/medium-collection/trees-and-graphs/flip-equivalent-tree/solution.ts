f5f3a2810fdf7ba12cd2df9e9dc8a10413093361
Maxim Kryuk <mkryuk@gmail.com>
2022-11-30 22:33:16 +0200

    Trees and Graphs: Flip Equivalent Binary Trees solution

--- src/medium-collection/trees-and-graphs/flip-equivalent-tree/solution.ts
+++ src/medium-collection/trees-and-graphs/flip-equivalent-tree/solution.ts
@@ -4,5 +4,32 @@ export function flipEquiv(
   root1: TreeNode | null,
   root2: TreeNode | null,
 ): boolean {
-  return false;
+  if (root1 === null && root2 === null) {
+    return true;
+  }
+  if (root1?.val !== root2?.val) {
+    return false;
+  }
+  // it should be equal straight or flipped
+  const straightEqual =
+    root1 &&
+    root2 &&
+    root1?.left?.val === root2?.left?.val &&
+    root1?.right?.val === root2?.right?.val;
+  const flippedEqual =
+    root1 &&
+    root2 &&
+    root1?.left?.val === root2?.right?.val &&
+    root1?.right?.val === root2?.left?.val;
+  if (!straightEqual && !flippedEqual) {
+    return false;
+  }
+  if (straightEqual) {
+    return (
+      flipEquiv(root1?.left, root2?.left) && flipEquiv(root1.right, root2.right)
+    );
+  }
+  return (
+    flipEquiv(root1?.left, root2?.right) && flipEquiv(root1.right, root2.left)
+  );
 }
--- src/medium-collection/trees-and-graphs/flip-equivalent-tree/test.spec.ts
+++ src/medium-collection/trees-and-graphs/flip-equivalent-tree/test.spec.ts
@@ -3,12 +3,14 @@ import { flipEquiv } from "./solution";
 
 it("flipEquiv should return true for tes 1", () => {
   const arr1 = [1, 2, 3, 4, 5, 6, null, null, null, 7, 8];
-  const arr2 = [1, 3, 2, null, 6, 4, 5, null, null, null, null, 8, 7];
+  // prettier-ignore
+  const arr2 = [1, 3, 2, null, 6, 4, 5, null, null, null, null, null,null, 8, 7];
   const root1 = arrayToTreeNode(arr1);
   const root2 = arrayToTreeNode(arr2);
   const result = flipEquiv(root1, root2);
   expect(result).toBeTrue();
 });
+
 it("flipEquiv should return true for tes 2", () => {
   const arr1: number[] = [];
   const arr2: number[] = [];
@@ -17,9 +19,28 @@ it("flipEquiv should return true for tes 2", () => {
   const result = flipEquiv(root1, root2);
   expect(result).toBeTrue();
 });
+
 it("flipEquiv should return true for tes 3", () => {
   const arr1: number[] = [];
   const arr2 = [1];
+  const root1 = arrayToTreeNode(arr1);
+  const root2 = arrayToTreeNode(arr2);
+  const result = flipEquiv(root1, root2);
+  expect(result).toBeFalse();
+});
+
+it("flipEquiv should return true for tes 4", () => {
+  const arr1 = [1, 2, 3];
+  const arr2 = [1, 2, null, 3];
+  const root1 = arrayToTreeNode(arr1);
+  const root2 = arrayToTreeNode(arr2);
+  const result = flipEquiv(root1, root2);
+  expect(result).toBeFalse();
+});
+
+it("flipEquiv should return true for tes 5", () => {
+  const arr1: number[] = [1];
+  const arr2: number[] = [2];
   const root1 = arrayToTreeNode(arr1);
   const root2 = arrayToTreeNode(arr2);
   const result = flipEquiv(root1, root2);

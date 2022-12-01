9b64ea276356b316e0688e40771702cb1182dac3
Maxim Kryuk <mkryuk@gmail.com>
2022-12-02 00:23:47 +0200

    Array and Strings: Find Players With Zero or One Losses solution

--- src/medium-collection/array-and-strings/find-words/solution.ts
+++ src/medium-collection/array-and-strings/find-words/solution.ts
@@ -1,3 +1,29 @@
 export function findWinners(matches: number[][]): number[][] {
-  return [];
+  const winners = new Map<number, number>();
+  const losers = new Map<number, number>();
+  matches.forEach(([winner, loser]) => {
+    if (winners.has(winner)) {
+      winners.set(winner, winners.get(winner)! + 1);
+    } else {
+      winners.set(winner, 1);
+    }
+    if (losers.has(loser)) {
+      losers.set(loser, losers.get(loser)! + 1);
+    } else {
+      losers.set(loser, 1);
+    }
+  });
+  const winnersArr: number[] = [];
+  const losersArr: number[] = [];
+  winners.forEach((quant, winner) => {
+    if (!losers.has(winner)) {
+      winnersArr.push(winner);
+    }
+  });
+  losers.forEach((quant, loser) => {
+    if (quant === 1) {
+      losersArr.push(loser);
+    }
+  });
+  return [winnersArr.sort((a, b) => a - b), losersArr.sort((a, b) => a - b)];
 }

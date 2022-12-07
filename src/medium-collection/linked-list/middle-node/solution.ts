5be606536d024a76d8d67b43868356912d7ec63f
Maxim Kryuk <mkryuk@gmail.com>
2022-12-07 22:17:58 +0200

    Linked List: Middle of the Linked List solution

--- src/medium-collection/linked-list/middle-node/solution.ts
+++ src/medium-collection/linked-list/middle-node/solution.ts
@@ -1,5 +1,11 @@
 import { ListNode } from "../common";
 
 export function middleNode(head: ListNode | null): ListNode | null {
-  return null;
+  let fast: ListNode | null = new ListNode(0, head);
+  let slow: ListNode | null = new ListNode(0, head);
+  while (fast) {
+    fast = fast.next?.next ?? null;
+    slow = slow?.next ?? null;
+  }
+  return slow;
 }
--- src/medium-collection/linked-list/middle-node/test.spec.ts
+++ src/medium-collection/linked-list/middle-node/test.spec.ts
@@ -1,6 +1,38 @@
 import { arrayToList, listToArray } from "../common";
 import { middleNode } from "./solution";
 
+it("middleNode should return [1] for head = [1]", () => {
+  const nodes = [1];
+  const head = arrayToList(nodes);
+  const node = middleNode(head);
+  const result = listToArray(node);
+  expect(result).toEqual([1]);
+});
+
+it("middleNode should return [2] for head = [1,2]", () => {
+  const nodes = [1, 2];
+  const head = arrayToList(nodes);
+  const node = middleNode(head);
+  const result = listToArray(node);
+  expect(result).toEqual([2]);
+});
+
+it("middleNode should return [2,3] for head = [1,2,3]", () => {
+  const nodes = [1, 2, 3];
+  const head = arrayToList(nodes);
+  const node = middleNode(head);
+  const result = listToArray(node);
+  expect(result).toEqual([2, 3]);
+});
+
+it("middleNode should return [2,3,4] for head = [1,2,3,4]", () => {
+  const nodes = [1, 2, 3, 4];
+  const head = arrayToList(nodes);
+  const node = middleNode(head);
+  const result = listToArray(node);
+  expect(result).toEqual([3, 4]);
+});
+
 it("middleNode should return [3,4,5] for head = [1,2,3,4,5]", () => {
   const nodes = [1, 2, 3, 4, 5];
   const head = arrayToList(nodes);

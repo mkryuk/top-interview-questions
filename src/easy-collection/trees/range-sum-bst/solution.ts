04178ae5d6288267f637c7a1789edeae2cb7953f
Maxim Kryuk <mkryuk@gmail.com>
2022-12-08 23:41:20 +0200

    Trees and Graphs: Range Sum of BST solution

--- src/easy-collection/trees/range-sum-bst/solution.ts
+++ src/easy-collection/trees/range-sum-bst/solution.ts
@@ -5,5 +5,22 @@ export function rangeSumBST(
   low: number,
   high: number,
 ): number {
-  return 0;
+  let result = 0;
+  if (!root) {
+    return result;
+  }
+
+  if (root.val >= low && root.val <= high) {
+    result =
+      root.val +
+      rangeSumBST(root.left, low, high) +
+      rangeSumBST(root.right, low, high);
+  }
+  if (root.val < low) {
+    result = rangeSumBST(root.right, low, high);
+  }
+  if (root.val > high) {
+    result = rangeSumBST(root.left, low, high);
+  }
+  return result;
 }

19e37c2b0a9d7721f891a3f6250af0bf195595ac
Maxim Kryuk <mkryuk@gmail.com>
2022-12-10 23:19:24 +0200

    Trees and Graphs: Maximum Difference Between Node and Ancestor solution

--- src/medium-collection/trees-and-graphs/max-ancestor-diff/solution.ts
+++ src/medium-collection/trees-and-graphs/max-ancestor-diff/solution.ts
@@ -1,5 +1,20 @@
 import { TreeNode } from "../common";
 
 export function maxAncestorDiff(root: TreeNode | null): number {
-  return 0;
+  if (!root) {
+    return 0;
+  }
+  return dfs(root, root.val, root.val);
+}
+
+function dfs(root: TreeNode | null, min: number, max: number): number {
+  if (!root) {
+    return 0;
+  }
+  min = Math.min(min, max, root.val);
+  max = Math.max(min, max, root.val);
+  const diff = max - min;
+  const left = dfs(root.left, min, max);
+  const right = dfs(root.right, min, max);
+  return Math.max(diff, left, right);
 }
--- src/medium-collection/trees-and-graphs/max-ancestor-diff/test.spec.ts
+++ src/medium-collection/trees-and-graphs/max-ancestor-diff/test.spec.ts
@@ -9,7 +9,8 @@ it("maxAncestorDiff should return 7 for root = [8,3,10,1,6,null,14,null,null,4,7
 });
 
 it("maxAncestorDiff should return 3 for root = [1,null,2,null,0,3]", () => {
-  const nodes = [1, null, 2, null, 0, 3];
+  // prettier-ignore
+  const nodes = [1, null, 2, null, null, null, 0, null, null, null, null, null, null, 3, null];
   const root = arrayToTreeNode(nodes);
   const result = maxAncestorDiff(root);
   expect(result).toEqual(3);

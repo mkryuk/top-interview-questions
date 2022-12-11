4a5dcafa5d7c503c4eb05627b8c6766e8ebdaafb
Maxim Kryuk <mkryuk@gmail.com>
2022-12-12 00:23:01 +0200

    Math: Palindrome Number solution

--- src/easy-collection/math/palindrome-number/solution.ts
+++ src/easy-collection/math/palindrome-number/solution.ts
@@ -1,3 +1,10 @@
 export function isPalindrome(x: number): boolean {
-  return false;
+  const str = x.toString();
+  const n = str.length;
+  for (let i = 0; i < Math.trunc(n / 2); i++) {
+    if (str[i] !== str[n - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
--- src/easy-collection/math/palindrome-number/test.spec.ts
+++ src/easy-collection/math/palindrome-number/test.spec.ts
@@ -17,3 +17,27 @@ it("isPalindrome should return false for x = 10", () => {
   const result = isPalindrome(x);
   expect(result).toEqual(false);
 });
+
+it("isPalindrome should return true for x = 1221", () => {
+  const x = 1221;
+  const result = isPalindrome(x);
+  expect(result).toEqual(true);
+});
+
+it("isPalindrome should return false for x = 12320", () => {
+  const x = 12320;
+  const result = isPalindrome(x);
+  expect(result).toEqual(false);
+});
+
+it("isPalindrome should return false for x = 12210", () => {
+  const x = 12210;
+  const result = isPalindrome(x);
+  expect(result).toEqual(false);
+});
+
+it("isPalindrome should return false for x = 100", () => {
+  const x = 100;
+  const result = isPalindrome(x);
+  expect(result).toEqual(false);
+});

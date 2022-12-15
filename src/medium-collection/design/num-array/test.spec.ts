ee7ce701dcdb301e41c5808ed7e1985acd0aef16
Maxim Kryuk <mkryuk@gmail.com>
2022-12-15 22:51:15 +0200

    Design: Range Sum Query - Mutable added test

--- src/medium-collection/design/num-array/test.spec.ts
+++ src/medium-collection/design/num-array/test.spec.ts
@@ -6,3 +6,10 @@ it("NumArray should pass test 1", () => {
   numArray.update(1, 2); // nums = [1, 2, 5]
   expect(numArray.sumRange(0, 2)).toEqual(8); // return 1 + 2 + 5 = 8
 });
+
+it("NumArray should pass test 2", () => {
+  let numArray = new NumArray([1, 3, 5, 2, 4, 7, 9, 10]);
+  expect(numArray.sumRange(2, 4)).toEqual(11);
+  numArray.update(3, 3);
+  expect(numArray.sumRange(0, 2)).toEqual(12);
+});

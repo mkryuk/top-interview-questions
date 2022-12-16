92999551f8f3362dc5a55f4567b0ad4d8d97fb65
Maxim Kryuk <mkryuk@gmail.com>
2022-12-16 23:56:30 +0200

    Design: Range Sum Query - Mutable added test

--- src/medium-collection/design/num-array/test.spec.ts
+++ src/medium-collection/design/num-array/test.spec.ts
@@ -2,14 +2,27 @@ import { NumArray } from "./solution";
 
 it("NumArray should pass test 1", () => {
   let numArray = new NumArray([1, 3, 5]);
-  expect(numArray.sumRange(0, 2)).toEqual(9); // return 1 + 3 + 5 = 9
+  let result = numArray.sumRange(0, 2);
+  expect(result).toEqual(9); // return 1 + 3 + 5 = 9
   numArray.update(1, 2); // nums = [1, 2, 5]
-  expect(numArray.sumRange(0, 2)).toEqual(8); // return 1 + 2 + 5 = 8
+  result = numArray.sumRange(0, 2);
+  expect(result).toEqual(8); // return 1 + 2 + 5 = 8
 });
 
 it("NumArray should pass test 2", () => {
+  let numArray = new NumArray([1, 3, 5, 7]);
+  let result = numArray.sumRange(0, 3);
+  expect(result).toEqual(16);
+  numArray.update(1, 2);
+  result = numArray.sumRange(0, 2);
+  expect(result).toEqual(8);
+});
+
+it("NumArray should pass test 3", () => {
   let numArray = new NumArray([1, 3, 5, 2, 4, 7, 9, 10]);
-  expect(numArray.sumRange(2, 4)).toEqual(11);
+  let result = numArray.sumRange(2, 4);
+  expect(result).toEqual(11);
   numArray.update(3, 3);
-  expect(numArray.sumRange(0, 2)).toEqual(12);
+  result = numArray.sumRange(0, 2);
+  expect(result).toEqual(12);
 });

32040fb391e67d334647b6cd7b1b54cbd3bbe389
Maxim Kryuk <mkryuk@gmail.com>
2022-12-19 18:31:54 +0200

    Trees and Graphs: Find if Path Exists in Graph dfs solution

--- src/easy-collection/trees/valid-path/solution.ts
+++ src/easy-collection/trees/valid-path/solution.ts
@@ -4,5 +4,42 @@ export function validPath(
   source: number,
   destination: number,
 ): boolean {
+  let hashSet = new Map<number, Set<number>>();
+  edges.forEach(([left, right]) => {
+    if (!hashSet.has(left)) {
+      hashSet.set(left, new Set<number>());
+    }
+    if (!hashSet.has(right)) {
+      hashSet.set(right, new Set<number>());
+    }
+    hashSet.get(left)!.add(right);
+    hashSet.get(right)!.add(left);
+  });
+
+  return dfs(source, destination, hashSet, new Set());
+}
+
+function dfs(
+  source: number,
+  destination: number,
+  hashSet: Map<number, Set<number>>,
+  visited: Set<number>,
+): boolean {
+  if (source === destination) {
+    return true;
+  }
+  if (!visited.has(source)) {
+    visited.add(source);
+    const nodeSet = hashSet.get(source);
+    if (nodeSet && nodeSet.has(destination)) {
+      return true;
+    }
+    const nodes = Array.from(nodeSet ?? []);
+    for (let i = 0; i < nodes.length; i++) {
+      if (dfs(nodes[i], destination, hashSet, visited)) {
+        return true;
+      }
+    }
+  }
   return false;
 }
--- src/easy-collection/trees/valid-path/test.spec.ts
+++ src/easy-collection/trees/valid-path/test.spec.ts
@@ -27,3 +27,12 @@ it("validPath should return false for n = 6, edges = [[0,1],[0,2],[3,5],[5,4],[4
   const result = validPath(n, edges, source, destination);
   expect(result).toBeFalse();
 });
+
+it("validPath should return true for n = 1, edges = [], source = 0, destination = 0", () => {
+  const n = 1;
+  const edges: number[][] = [];
+  const source = 0;
+  const destination = 0;
+  const result = validPath(n, edges, source, destination);
+  expect(result).toBeTrue();
+});

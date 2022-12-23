ba3dfc238ae5a8376a8e216068979704275441e7
Maxim Kryuk <mkryuk@gmail.com>
2022-12-24 00:48:21 +0200

    Trees and Graphs: Possible Bipartition added test case with collision in the middle

--- src/medium-collection/trees-and-graphs/possible-bipartition/test-union-find.spec.ts
+++ src/medium-collection/trees-and-graphs/possible-bipartition/test-union-find.spec.ts
@@ -48,3 +48,16 @@ it("possibleBipartition should return false for n = 10, dislikes = [[1,2],[3,4],
   const result = possibleBipartition(n, dislikes);
   expect(result).toBeTrue();
 });
+
+it("possibleBipartition should return false for n = 8, dislikes = [[1,2],[3,4],[3,5],[4,5],[7,8]]", () => {
+  const n = 8;
+  const dislikes = [
+    [1, 2],
+    [3, 4],
+    [3, 5],
+    [4, 5],
+    [7, 8],
+  ];
+  const result = possibleBipartition(n, dislikes);
+  expect(result).toBeFalse();
+});

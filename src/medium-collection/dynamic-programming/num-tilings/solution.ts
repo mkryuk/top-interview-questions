231832f9594a64bc897da94ec55d1c1747171a14
Maxim Kryuk <mkryuk@gmail.com>
2022-12-24 20:54:15 +0200

    Dynamic Programming: Domino and Tromino Tiling solution

--- src/medium-collection/dynamic-programming/num-tilings/solution.ts
+++ src/medium-collection/dynamic-programming/num-tilings/solution.ts
@@ -1,3 +1,14 @@
 export function numTilings(n: number): number {
-  return 0;
+  let mod = 10 ** 9 + 7;
+  const arrLen = n < 4 ? 4 : n;
+  const dp = new Array(arrLen).fill(0);
+  dp[1] = 1;
+  dp[2] = 2;
+  dp[3] = 5;
+  if (n <= 3) return dp[n];
+  for (let i = 4; i <= n; ++i) {
+    dp[i] = 2 * dp[i - 1] + dp[i - 3];
+    dp[i] %= mod;
+  }
+  return dp[n];
 }

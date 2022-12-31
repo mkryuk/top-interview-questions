fca0692092f68429620957b463f3c689b1d8b630
Maxim Kryuk <mkryuk@gmail.com>
2022-12-31 23:33:34 +0200

    Array and Strings: Remove Stones to Minimize the Total added test

--- src/medium-collection/array-and-strings/min-stone-sum/test.spec.ts
+++ src/medium-collection/array-and-strings/min-stone-sum/test.spec.ts
@@ -13,3 +13,10 @@ it("minStoneSum should return 12 for piles = [4,3,6,7], k = 3", () => {
   const result = minStoneSum(piles, k);
   expect(result).toEqual(12);
 });
+
+it("minStoneSum should return 8 for piles = [4,4,4,2], k = 3", () => {
+  const piles = [4, 4, 4, 2];
+  const k = 3;
+  const result = minStoneSum(piles, k);
+  expect(result).toEqual(8);
+});

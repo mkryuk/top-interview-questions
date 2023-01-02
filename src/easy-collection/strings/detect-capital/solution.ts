0cc86500c429e723a4947560610e8d3b28c6e3c1
Maxim Kryuk <mkryuk@gmail.com>
2023-01-02 23:41:59 +0200

    Array and Strings: Detect Capital solution

--- src/easy-collection/strings/detect-capital/solution.ts
+++ src/easy-collection/strings/detect-capital/solution.ts
@@ -1,3 +1,51 @@
 export function detectCapitalUse(word: string): boolean {
-  return false;
+  return isOnlyFirstCapital(word) || areAllCapital(word) || areAllLower(word);
+}
+
+function areAllLower(word: string): boolean {
+  for (let i = 0; i < word.length; i++) {
+    const charCode = word.charCodeAt(i);
+    if (!isLower(charCode)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function areAllCapital(word: string): boolean {
+  for (let i = 0; i < word.length; i++) {
+    const charCode = word.charCodeAt(i);
+    if (!isCapital(charCode)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isOnlyFirstCapital(word: string): boolean {
+  let charCode = word.charCodeAt(0);
+  if (!isCapital(charCode)) {
+    return false;
+  }
+  for (let i = 1; i < word.length; i++) {
+    charCode = word.charCodeAt(i);
+    if (!isLower(charCode)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isLower(charCode: number): boolean {
+  if (charCode < 97 || charCode > 122) {
+    return false;
+  }
+  return true;
+}
+
+function isCapital(charCode: number): boolean {
+  if (charCode < 65 || charCode > 90) {
+    return false;
+  }
+  return true;
 }
--- src/easy-collection/strings/detect-capital/test.spec.ts
+++ src/easy-collection/strings/detect-capital/test.spec.ts
@@ -9,5 +9,17 @@ it('detectCapitalUse should return true for word = "USA"', () => {
 it('detectCapitalUse should return false for word = "FlaG"', () => {
   const word = "FlaG";
   const result = detectCapitalUse(word);
+  expect(result).toBeFalse();
+});
+
+it('detectCapitalUse should return true for word = "ggg"', () => {
+  const word = "ggg";
+  const result = detectCapitalUse(word);
+  expect(result).toBeTrue();
+});
+
+it('detectCapitalUse should return true for word = "Leetcode"', () => {
+  const word = "Leetcode";
+  const result = detectCapitalUse(word);
   expect(result).toBeTrue();
 });

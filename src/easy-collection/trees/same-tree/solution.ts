0777ac68bb9cf53f6b2e1ceb5bd41e25dc5816ff
Maxim Kryuk <mkryuk@gmail.com>
2023-01-10 20:08:21 +0200

    Trees and Graphs: Same Tree recursive solution

--- src/easy-collection/trees/same-tree/solution.ts
+++ src/easy-collection/trees/same-tree/solution.ts
@@ -1,5 +1,14 @@
 import { TreeNode } from "../common";
 
 export function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
-  return false;
+  if (p === null && q === null) {
+    return true;
+  }
+  if (p === null || q === null) {
+    return false;
+  }
+  if (p.val !== q.val) {
+    return false;
+  }
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 }

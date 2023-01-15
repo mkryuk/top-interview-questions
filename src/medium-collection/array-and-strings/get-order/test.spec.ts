a19ada342c6805be1910116898a8c0957ea5f7de
Maxim Kryuk <mkryuk@gmail.com>
2023-01-15 22:39:38 +0200

    Array and Strings: Single-Threaded CPU added tests

--- src/medium-collection/array-and-strings/get-order/test.spec.ts
+++ src/medium-collection/array-and-strings/get-order/test.spec.ts
@@ -22,3 +22,42 @@ it("getOrder should return [4,3,2,0,1] for tasks = [[7,10],[7,12],[7,5],[7,4],[7
   const result = getOrder(tasks);
   expect(result).toEqual([4, 3, 2, 0, 1]);
 });
+
+it("getOrder should return [0,5,4,3,1,2] for tasks = [[1,7],[7,10],[7,12],[7,5],[7,4],[7,2]]", () => {
+  const tasks = [
+    [1, 7],
+    [7, 10],
+    [7, 12],
+    [7, 5],
+    [7, 4],
+    [7, 2],
+  ];
+  const result = getOrder(tasks);
+  expect(result).toEqual([0, 5, 4, 3, 1, 2]);
+});
+
+it("getOrder should return [2,5,4,3,0,1] for tasks = [[7,10],[7,12],[1,8],[7,5],[7,4],[7,2]]", () => {
+  const tasks = [
+    [7, 10],
+    [7, 12],
+    [1, 8],
+    [7, 5],
+    [7, 4],
+    [7, 2],
+  ];
+  const result = getOrder(tasks);
+  expect(result).toEqual([5, 4, 3, 2, 0, 1]);
+});
+
+it("getOrder should return [2,5,4,3,0,1] for tasks = [[7,10],[7,12],[7,5],[7,4],[7,2],[1,6]]", () => {
+  const tasks = [
+    [7, 10],
+    [7, 12],
+    [7, 5],
+    [7, 4],
+    [7, 2],
+    [1, 6],
+  ];
+  const result = getOrder(tasks);
+  expect(result).toEqual([5, 4, 3, 2, 0, 1]);
+});

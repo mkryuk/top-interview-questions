a5a9233f1c6b7191ce1e397ee68af182b596402d
Maxim Kryuk <mkryuk@gmail.com>
2023-01-25 18:40:19 +0200

    Trees and Graphs: Snakes and Ladders added tests

--- src/medium-collection/trees-and-graphs/snakes-and-ladders/test.spec.ts
+++ src/medium-collection/trees-and-graphs/snakes-and-ladders/test.spec.ts
@@ -21,3 +21,55 @@ it("snakesAndLadders should pass test 2", () => {
   const result = snakesAndLadders(board);
   expect(result).toEqual(1);
 });
+
+it("snakesAndLadders should pass test 3", () => {
+  const board = [
+    [-1, -1, -1],
+    [-1, -1, -1],
+    [-1, -1, -1],
+  ];
+  const result = snakesAndLadders(board);
+  expect(result).toEqual(2);
+});
+
+it("snakesAndLadders should pass test 4", () => {
+  const board = [
+    [-1, -1, -1],
+    [-1, -1, -1],
+    [-1, 9, -1],
+  ];
+  const result = snakesAndLadders(board);
+  expect(result).toEqual(1);
+});
+
+it("snakesAndLadders should pass test 5", () => {
+  const board = [
+    [-1, -1, -1],
+    [-1, -1, -1],
+    [-1, 9, -1],
+  ];
+  const result = snakesAndLadders(board);
+  expect(result).toEqual(1);
+});
+
+it("snakesAndLadders should pass test 6", () => {
+  const board = [
+    [-1, -1, -1, -1],
+    [-1, -1, -1, -1],
+    [16, -1, -1, -1],
+    [-1, -1, -1, -1],
+  ];
+  const result = snakesAndLadders(board);
+  expect(result).toEqual(2);
+});
+
+it("snakesAndLadders should pass test 7", () => {
+  const board = [
+    [-1, -1, -1, -1],
+    [-1, -1, -1, -1],
+    [-1, 16, -1, -1],
+    [-1, -1, -1, -1],
+  ];
+  const result = snakesAndLadders(board);
+  expect(result).toEqual(1);
+});

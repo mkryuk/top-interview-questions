48bcaacd79b3620152a04e0c7fc32b450fe5a95a
Maxim Kryuk <mkryuk@gmail.com>
2023-01-28 00:25:03 +0200

    Math: Integer To Roman solution

--- src/medium-collection/math/integer-to-roman/solution.ts
+++ src/medium-collection/math/integer-to-roman/solution.ts
@@ -1,3 +1,27 @@
 export function intToRoman(num: number): string {
-  return "";
+  const numToRoman = [
+    { num: 1000, roman: "M" },
+    { num: 900, roman: "CM" },
+    { num: 500, roman: "D" },
+    { num: 400, roman: "CD" },
+    { num: 100, roman: "C" },
+    { num: 90, roman: "XC" },
+    { num: 50, roman: "L" },
+    { num: 40, roman: "XL" },
+    { num: 10, roman: "X" },
+    { num: 9, roman: "IX" },
+    { num: 5, roman: "V" },
+    { num: 4, roman: "IV" },
+    { num: 1, roman: "I" },
+  ];
+  let result = "";
+  let romanIdx = 0;
+  while (num > 0) {
+    while (Math.trunc(num / numToRoman[romanIdx].num) > 0) {
+      result += numToRoman[romanIdx].roman;
+      num -= numToRoman[romanIdx].num;
+    }
+    romanIdx++;
+  }
+  return result;
 }

c3759d0ff3b773ba43e9de0d3d1306a82c665bf9
Maxim Kryuk <mkryuk@gmail.com>
2023-01-30 18:00:27 +0200

    Math: N-th Tribonacci Number init solution

--- src/easy-collection/math/tribonacci/solution.ts
+++ src/easy-collection/math/tribonacci/solution.ts
@@ -1,3 +1,8 @@
 export function tribonacci(n: number): number {
-  return 0;
+  const results = [0, 1, 1];
+  for (let i = 3; i <= n; i++) {
+    results[i % 3] =
+      results[i % 3] + results[(i + 1) % 3] + results[(i + 2) % 3];
+  }
+  return results[n % 3];
 }
--- src/easy-collection/math/tribonacci/test.spec.ts
+++ src/easy-collection/math/tribonacci/test.spec.ts
@@ -6,6 +6,24 @@ it("tribonacci should return 4 for n = 4", () => {
   expect(result).toEqual(4);
 });
 
+it("tribonacci should return 7 for n = 5", () => {
+  const n = 5;
+  const result = tribonacci(n);
+  expect(result).toEqual(7);
+});
+
+it("tribonacci should return 13 for n = 6", () => {
+  const n = 6;
+  const result = tribonacci(n);
+  expect(result).toEqual(13);
+});
+
+it("tribonacci should return 24 for n = 7", () => {
+  const n = 7;
+  const result = tribonacci(n);
+  expect(result).toEqual(24);
+});
+
 it("tribonacci should return 1389537 for n = 25", () => {
   const n = 25;
   const result = tribonacci(n);

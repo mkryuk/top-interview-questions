041e3d6e828983833ff8679d19012496cd323f5c
Maxim Kryuk <mkryuk@gmail.com>
2023-02-02 18:00:31 +0200

    Array: Next Greater Element I solution

--- src/easy-collection/array/next-greater-element/solution.ts
+++ src/easy-collection/array/next-greater-element/solution.ts
@@ -1,3 +1,11 @@
 export function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
-  return [];
+  const map: Map<number, number> = new Map();
+  const stack: number[] = [];
+  for (const num of nums2) {
+    while (stack.length && stack[stack.length - 1] < num) {
+      map.set(stack.pop()!, num);
+    }
+    stack.push(num);
+  }
+  return nums1.map((num) => map.get(num) || -1);
 }

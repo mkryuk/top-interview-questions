918aa6264f7da6b3537e2759b7f378ed53b1de5d
Maxim Kryuk <mkryuk@gmail.com>
2023-02-08 18:43:19 +0200

    Array and Strings: Fruit Into Baskets solution

--- src/medium-collection/array-and-strings/fruit-into-baskets/solution.ts
+++ src/medium-collection/array-and-strings/fruit-into-baskets/solution.ts
@@ -1,3 +1,27 @@
 export function totalFruit(fruits: number[]): number {
-  return 0;
+  let currentTree = fruits[0];
+  let prevTree = -1;
+  let fruitsFromCurrentTree = 0;
+  let totalCurrent = 0;
+  let totalMax = 0;
+
+  for (let i = 0; i < fruits.length; i++) {
+    if (fruits[i] === currentTree) {
+      totalCurrent++;
+      fruitsFromCurrentTree++;
+    } else if (fruits[i] === prevTree) {
+      prevTree = currentTree;
+      currentTree = fruits[i];
+      totalCurrent++;
+      fruitsFromCurrentTree = 1;
+    } else {
+      prevTree = currentTree;
+      currentTree = fruits[i];
+      totalCurrent = fruitsFromCurrentTree + 1;
+      fruitsFromCurrentTree = 1;
+    }
+    totalMax = Math.max(totalCurrent, totalMax);
+  }
+
+  return totalMax;
 }

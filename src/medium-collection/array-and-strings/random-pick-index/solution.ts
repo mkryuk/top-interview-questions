eec4d57efdb8a7506eee33454479fe7ea41f488c
Maxim Kryuk <mkryuk@gmail.com>
2023-02-10 17:51:00 +0200

    Array and Strings: Random Pick Index solution

--- src/medium-collection/array-and-strings/random-pick-index/solution.ts
+++ src/medium-collection/array-and-strings/random-pick-index/solution.ts
@@ -1,7 +1,18 @@
 export class Solution {
-  constructor(nums: number[]) {}
+  hash: Map<number, number[]>;
+  constructor(nums: number[]) {
+    this.hash = new Map<number, number[]>();
+    for (let i = 0; i < nums.length; i++) {
+      if (!this.hash.has(nums[i])) {
+        this.hash.set(nums[i], []);
+      }
+      this.hash.get(nums[i])!.push(i);
+    }
+  }
 
   pick(target: number): number {
-    return 0;
+    let indexes = this.hash.get(target)!;
+    let random = Math.floor(Math.random() * indexes.length);
+    return indexes[random];
   }
 }

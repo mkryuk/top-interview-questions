25fe33c03c9c75a12d005287a26aee38dd548136
Maxim Kryuk <mkryuk@gmail.com>
2023-02-13 21:33:55 +0200

    Trees and Graphs: Minimum Fuel Cost to Report to the Capital added tests

--- src/medium-collection/trees-and-graphs/minimum-fuel-cost/test.spec.ts
+++ src/medium-collection/trees-and-graphs/minimum-fuel-cost/test.spec.ts
@@ -40,6 +40,29 @@ describe("Minimum Fuel Cost to Report to the Capital", () => {
     expect(result).toBe(8);
   });
 
+  it("minimumFuelCost should return 3 for roads = [[0,1],[0,2],[0,3]], seats = 5", () => {
+    const roads = [
+      [0, 1],
+      [0, 2],
+      [0, 3],
+    ];
+    const seats = 5;
+    expect(minimumFuelCost(roads, seats)).toEqual(3);
+  });
+
+  it("minimumFuelCost should return 7 for roads = [[3,1],[3,2],[1,0],[0,4],[0,5],[4,6]], seats = 2", () => {
+    const roads = [
+      [3, 1],
+      [3, 2],
+      [1, 0],
+      [0, 4],
+      [0, 5],
+      [4, 6],
+    ];
+    const seats = 2;
+    expect(minimumFuelCost(roads, seats)).toEqual(7);
+  });
+
   it("minimumFuelCost should return 0 for roads = [], seats = 1", () => {
     const roads: number[][] = [];
     const seats = 1;

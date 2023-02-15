c0bcadfd8ac9540d798dcbb0452ae082b75f7469
Maxim Kryuk <mkryuk@gmail.com>
2023-02-15 20:18:40 +0200

    Trees and Graphs: Minimum Fuel Cost to Report to the Capital bfs solution

---
+++ src/medium-collection/trees-and-graphs/minimum-fuel-cost/solution-bfs.ts
@@ -0,0 +1,45 @@
+export function minimumFuelCost(roads: number[][], seats: number): number {
+  const n: number = roads.length + 1;
+  const tree: Map<number, number[]> = new Map();
+  const degree: number[] = new Array(n).fill(0);
+
+  for (const [from, to] of roads) {
+    tree.set(from, (tree.get(from) || []).concat(to));
+    tree.set(to, (tree.get(to) || []).concat(from));
+    degree[from]++;
+    degree[to]++;
+  }
+
+  return calculateFuel(n, tree, degree, seats);
+}
+
+function calculateFuel(
+  n: number,
+  adj: Map<number, number[]>,
+  degree: number[],
+  seats: number,
+): number {
+  const q: number[] = [];
+  for (let i = 1; i < n; i++) {
+    if (degree[i] === 1) {
+      q.push(i);
+    }
+  }
+
+  const representatives: number[] = new Array(n).fill(1);
+  let fuel: number = 0;
+
+  while (q.length > 0) {
+    const node: number = q.shift() as number;
+    fuel += Math.ceil(representatives[node] / seats);
+
+    for (const neighbor of adj.get(node) || []) {
+      degree[neighbor]--;
+      representatives[neighbor] += representatives[node];
+      if (degree[neighbor] === 1 && neighbor !== 0) {
+        q.push(neighbor);
+      }
+    }
+  }
+  return fuel;
+}
---
+++ src/medium-collection/trees-and-graphs/minimum-fuel-cost/test-bfs.spec.ts
@@ -0,0 +1,84 @@
+import { minimumFuelCost } from "./solution-bfs";
+
+describe("Minimum Fuel Cost to Report to the Capital", () => {
+  it("minimumFuelCost should return 3 for roads = [[0,1],[0,2],[0,3]], seats = 5", () => {
+    const roads = [
+      [0, 1],
+      [0, 2],
+      [0, 3],
+    ];
+    const seats = 5;
+    const result = minimumFuelCost(roads, seats);
+    expect(result).toBe(3);
+  });
+
+  it("minimumFuelCost should return 7 for roads = [[3,1],[3,2],[1,0],[0,4],[0,5],[4,6]], seats = 2", () => {
+    const roads = [
+      [3, 1],
+      [3, 2],
+      [1, 0],
+      [0, 4],
+      [0, 5],
+      [4, 6],
+    ];
+    const seats = 2;
+    const result = minimumFuelCost(roads, seats);
+    expect(result).toBe(7);
+  });
+
+  it("minimumFuelCost should return 8 for roads = [[0,1],[1,2],[0,3],[3,4],[0,5],[0,6]], seats = 1", () => {
+    const roads = [
+      [0, 1],
+      [1, 2],
+      [0, 3],
+      [3, 4],
+      [0, 5],
+      [0, 6],
+    ];
+    const seats = 1;
+    const result = minimumFuelCost(roads, seats);
+    expect(result).toBe(8);
+  });
+
+  it("minimumFuelCost should return 3 for roads = [[0,1],[0,2],[0,3]], seats = 5", () => {
+    const roads = [
+      [0, 1],
+      [0, 2],
+      [0, 3],
+    ];
+    const seats = 5;
+    expect(minimumFuelCost(roads, seats)).toEqual(3);
+  });
+
+  it("minimumFuelCost should return 7 for roads = [[3,1],[3,2],[1,0],[0,4],[0,5],[4,6]], seats = 2", () => {
+    const roads = [
+      [3, 1],
+      [3, 2],
+      [1, 0],
+      [0, 4],
+      [0, 5],
+      [4, 6],
+    ];
+    const seats = 2;
+    expect(minimumFuelCost(roads, seats)).toEqual(7);
+  });
+
+  it("minimumFuelCost should return 0 for roads = [], seats = 1", () => {
+    const roads: number[][] = [];
+    const seats = 1;
+    const result = minimumFuelCost(roads, seats);
+    expect(result).toBe(0);
+  });
+
+  it("minimumFuelCost should return 4 for roads = [[0,1],[0,2],[1,3],[1,4]] seats = 5", () => {
+    const roads = [
+      [0, 1],
+      [0, 2],
+      [1, 3],
+      [1, 4],
+    ];
+    const seats = 5;
+    const result = minimumFuelCost(roads, seats);
+    expect(result).toBe(4);
+  });
+});

11a57306634b79879de2c5e83fd1540e60aa7b2b
Maxim Kryuk <mkryuk@gmail.com>
2023-02-19 16:39:33 +0200

    Math: Multiply Strings added tests

--- src/medium-collection/math/multiply-strings/test.spec.ts
+++ src/medium-collection/math/multiply-strings/test.spec.ts
@@ -14,6 +14,20 @@ it('multiply should return "56088" for num1 = "123", num2 = "456"', () => {
   expect(result).toEqual("56088");
 });
 
+it('multiply should return "64452" for num1 = "123", num2 = "524"', () => {
+  const num1 = "123";
+  const num2 = "524";
+  const result = multiply(num1, num2);
+  expect(result).toEqual("64452");
+});
+
+it('multiply should return "20880" for num1 = "696", num2 = "30"', () => {
+  const num1 = "696";
+  const num2 = "30";
+  const result = multiply(num1, num2);
+  expect(result).toEqual("20880");
+});
+
 it('multiply should return "0" for num1 = "2", num2 = "0"', () => {
   const num1 = "2";
   const num2 = "0";
@@ -25,7 +39,7 @@ it('multiply should return "42" for num1 = "42", num2 = "1"', () => {
   const num1 = "42";
   const num2 = "1";
   const result = multiply(num1, num2);
-  expect(result).toEqual("1");
+  expect(result).toEqual("42");
 });
 
 it('multiply should return "12345678898765432110" for num1 = "1234567890", num2 = "9999999999"', () => {

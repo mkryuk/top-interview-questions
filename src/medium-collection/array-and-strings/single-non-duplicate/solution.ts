12a3b102ca47f0f56b6e96cf5df27f8759f68eed
Maxim Kryuk <mkryuk@gmail.com>
2023-02-21 15:48:56 +0200

    Array and Strings: Single Element in a Sorted Array solution

--- src/medium-collection/array-and-strings/single-non-duplicate/solution.ts
+++ src/medium-collection/array-and-strings/single-non-duplicate/solution.ts
@@ -1,3 +1,10 @@
 export function singleNonDuplicate(nums: number[]): number {
-  return 0;
+  let result = nums[0];
+  for (let i = 1; i < nums.length; i += 2) {
+    if (result !== nums[i]) {
+      break;
+    }
+    result = nums[i + 1];
+  }
+  return result;
 }

f6734a16ade04e0f1933308599ae19bddeecd142
Maxim Kryuk <mkryuk@gmail.com>
2023-02-24 15:05:05 +0200

    Array and Strings: Minimum Number of Food Buckets to Feed the Hamsters solution

--- src/medium-collection/array-and-strings/minimum-buckets/solution.ts
+++ src/medium-collection/array-and-strings/minimum-buckets/solution.ts
@@ -1,3 +1,21 @@
 export function minimumBuckets(hamsters: string): number {
-  return 0;
+  const h: string[] = hamsters.split("");
+  for (let i = 0; i < h.length; i++) {
+    if (h[i] === "H") {
+      if (i - 1 >= 0 && h[i - 1] === "F") {
+        continue;
+      }
+
+      if (i + 1 < h.length && h[i + 1] === ".") {
+        h[i + 1] = "F";
+      } else if (i - 1 >= 0 && h[i - 1] === ".") {
+        h[i - 1] = "F";
+      } else return -1;
+    }
+  }
+  let count = 0;
+  for (let i = 0; i < h.length; i++) {
+    if (h[i] === "F") count++;
+  }
+  return count;
 }

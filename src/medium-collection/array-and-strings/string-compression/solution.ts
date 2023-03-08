7c6d13bdf3950c964258dcac6a3678f2bda0904d
Maxim Kryuk <mkryuk@gmail.com>
2023-03-08 20:04:45 +0200

    Array and Strings: String Compression solution

--- src/medium-collection/array-and-strings/string-compression/solution.ts
+++ src/medium-collection/array-and-strings/string-compression/solution.ts
@@ -1,3 +1,22 @@
 export function compress(chars: string[]): number {
-  return 0;
+  // add additional ending symbol
+  // not char, to process last char
+  chars.push("/");
+  let read = 0;
+  let write = 0;
+
+  for (let i = 1; i < chars.length; i++) {
+    if (chars[i] !== chars[read]) {
+      let diff = i - read;
+      chars[write++] = chars[read];
+      if (diff > 1) {
+        let sDiff = diff.toString();
+        for (let j = 0; j < sDiff.length; j++) {
+          chars[write++] = sDiff[j];
+        }
+      }
+      read = i;
+    }
+  }
+  return write;
 }
--- src/medium-collection/array-and-strings/string-compression/test.spec.ts
+++ src/medium-collection/array-and-strings/string-compression/test.spec.ts
@@ -5,12 +5,14 @@ describe("String Compression", () => {
     const chars = ["a", "a", "b", "b", "c", "c", "c"];
     const result = compress(chars);
     expect(result).toEqual(6);
+    expect(chars.slice(0, result)).toEqual(["a", "2", "b", "2", "c", "3"]);
   });
 
   it('compress should return 1 for chars = ["a"]', () => {
     const chars = ["a"];
     const result = compress(chars);
     expect(result).toEqual(1);
+    expect(chars.slice(0, result)).toEqual(["a"]);
   });
 
   it('compress should return 4 for chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]', () => {
@@ -18,5 +20,20 @@ describe("String Compression", () => {
     const chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"];
     const result = compress(chars);
     expect(result).toEqual(4);
+    expect(chars.slice(0, result)).toEqual(["a", "b", "1", "2"]);
+  });
+
+  it('compress should return 6 for chars = ["a","a","a","b","b","a","a"]', () => {
+    const chars = ["a", "a", "a", "b", "b", "a", "a"];
+    const result = compress(chars);
+    expect(result).toEqual(6);
+    expect(chars.slice(0, result)).toEqual(["a", "3", "b", "2", "a", "2"]);
+  });
+
+  it('compress should return 3 for chars = ["a","b","c"]', () => {
+    const chars = ["a", "b", "c"];
+    const result = compress(chars);
+    expect(result).toEqual(3);
+    expect(chars.slice(0, result)).toEqual(["a", "b", "c"]);
   });
 });

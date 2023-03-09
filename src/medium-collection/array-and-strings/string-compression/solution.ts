39cfaf6e40fc9d79d97067e7a7cb9bf0998cb7cf
Maxim Kryuk <mkryuk@gmail.com>
2023-03-09 18:02:59 +0200

    Array and Strings: String Compression solution

--- src/medium-collection/array-and-strings/string-compression/solution.ts
+++ src/medium-collection/array-and-strings/string-compression/solution.ts
@@ -1,21 +1,17 @@
 export function compress(chars: string[]): number {
-  // add additional ending symbol
-  // not char, to process last char
-  chars.push("/");
-  let read = 0;
+  let read = 1;
   let write = 0;
-
-  for (let i = 1; i < chars.length; i++) {
-    if (chars[i] !== chars[read]) {
-      let diff = i - read;
-      chars[write++] = chars[read];
-      if (diff > 1) {
-        let sDiff = diff.toString();
-        for (let j = 0; j < sDiff.length; j++) {
-          chars[write++] = sDiff[j];
-        }
+  for (let i = 0; i < chars.length; i = read) {
+    while (read < chars.length && chars[read] === chars[i]) {
+      read++;
+    }
+    chars[write++] = chars[i];
+    let diff = read - i;
+    if (diff > 1) {
+      let sDiff = diff.toString();
+      for (let j = 0; j < sDiff.length; j++) {
+        chars[write++] = sDiff[j];
       }
-      read = i;
     }
   }
   return write;

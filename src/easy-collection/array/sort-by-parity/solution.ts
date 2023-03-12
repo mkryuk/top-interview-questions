c17355ba172ebe230179eab850778a5c0d5e3403
Maxim Kryuk <mkryuk@gmail.com>
2023-03-13 00:04:45 +0200

    Array and Strings: Sort Array By Parity solution

--- src/easy-collection/array/sort-by-parity/solution.ts
+++ src/easy-collection/array/sort-by-parity/solution.ts
@@ -1,3 +1,18 @@
 export function sortArrayByParity(nums: number[]): number[] {
-  return [];
+  let oddIndex = -1;
+  for (let i = 0; i < nums.length; i++) {
+    // we should find the first odd number first
+    if (nums[i] % 2 === 1 && oddIndex === -1) {
+      oddIndex = i;
+    }
+    // if we already found the odd number
+    // and found the even number after it - swap them
+    if (oddIndex !== -1 && nums[i] % 2 === 0) {
+      let temp = nums[i];
+      nums[i] = nums[oddIndex];
+      nums[oddIndex] = temp;
+      oddIndex = oddIndex + 1;
+    }
+  }
+  return nums;
 }

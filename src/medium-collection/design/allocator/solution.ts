3c730389029345e396c73e8d1e5beb1bc6380fca
Maxim Kryuk <mkryuk@gmail.com>
2023-03-14 19:36:21 +0200

    Design: Design Memory Allocator solution

--- src/medium-collection/design/allocator/solution.ts
+++ src/medium-collection/design/allocator/solution.ts
@@ -1,11 +1,55 @@
 export class Allocator {
-  constructor(n: number) {}
+  occupied: Map<number, number[][]>;
+
+  mArr: number[];
+  constructor(n: number) {
+    this.occupied = new Map();
+    this.mArr = new Array(n).fill(0);
+  }
 
   allocate(size: number, mID: number): number {
-    return 0;
+    let index = this.findIndex(size);
+    if (index === -1) {
+      return index;
+    }
+    for (let i = index; i < index + size; i++) {
+      this.mArr[i] = mID;
+    }
+    if (this.occupied.has(mID)) {
+      this.occupied.get(mID)?.push([index, index + size]);
+    } else {
+      this.occupied.set(mID, [[index, index + size]]);
+    }
+    return index;
   }
 
   free(mID: number): number {
-    return 0;
+    let ranges = this.occupied.get(mID);
+    let count = 0;
+    ranges?.forEach(([start, to]) => {
+      for (let i = start; i < to; i++) {
+        this.mArr[i] = 0;
+        count++;
+      }
+    });
+    this.occupied.delete(mID);
+    return count;
+  }
+
+  private findIndex(size: number): number {
+    let count = 0;
+    let idx = 0;
+    for (let i = 0; i < this.mArr.length; i++) {
+      if (this.mArr[i] === 0) {
+        count++;
+      } else {
+        idx = i + 1;
+        count = 0;
+      }
+      if (count === size) {
+        return idx;
+      }
+    }
+    return -1;
   }
 }
--- src/medium-collection/design/allocator/test.spec.ts
+++ src/medium-collection/design/allocator/test.spec.ts
@@ -1,6 +1,6 @@
 import { Allocator } from "./solution";
 
-describe("Design Memory Allocator", () => {
+fdescribe("Design Memory Allocator", () => {
   it("should pass test 1", () => {
     var loc = new Allocator(10);
     expect(loc.allocate(1, 1)).toEqual(0); // The leftmost block's first index is 0. The memory array becomes [1,_,_,_,_,_,_,_,_,_]. We return 0.
@@ -13,5 +13,17 @@ describe("Design Memory Allocator", () => {
     expect(loc.free(1)).toEqual(3); // Free all memory units with mID 1. The memory array becomes [_,_,3,4,4,4,_,_,_,_]. We return 3 since there are 3 units with mID 1.
     expect(loc.allocate(10, 2)).toEqual(-1); // We can not find any free block with 10 consecutive free memory units, so we return -1.
     expect(loc.free(7)).toEqual(0); // Free all memory units with mID 7. The memory array remains the same since there is no memory unit with mID 7. We return 0.
+  });
+
+  it("should pass test 2", () => {
+    const loc = new Allocator(20);
+    expect(loc.allocate(12, 1)).toEqual(0);
+    expect(loc.allocate(5, 2)).toEqual(12);
+    expect(loc.allocate(4, 3)).toEqual(-1);
+    expect(loc.allocate(3, 3)).toEqual(17);
+    expect(loc.free(2)).toEqual(5);
+    expect(loc.allocate(3, 2)).toEqual(12);
+    expect(loc.free(2)).toEqual(3);
+    expect(loc.free(2)).toEqual(0);
   });
 });

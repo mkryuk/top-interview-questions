79ff81ad2ccaaf312faebd3142fe4ae746eac71f
Maxim Kryuk <mkryuk@gmail.com>
2023-03-17 18:59:16 +0200

    Array and Strings: Can Place Flowers init

---
+++ src/easy-collection/array/can-place-flowers/test.spec.ts
@@ -0,0 +1,17 @@
+import { canPlaceFlowers } from "./solution";
+
+describe("Can Place Flowers", () => {
+  it("canPlaceFlowers should return true for flowerbed = [1,0,0,0,1], n = 1", () => {
+    const flowerbed = [1, 0, 0, 0, 1];
+    const n = 1;
+    const result = canPlaceFlowers(flowerbed, n);
+    expect(result).toBeTrue();
+  });
+
+  it("canPlaceFlowers should return false for flowerbed = [1,0,0,0,1], n = 2", () => {
+    const flowerbed = [1, 0, 0, 0, 1];
+    const n = 2;
+    const result = canPlaceFlowers(flowerbed, n);
+    expect(result).toBeFalse();
+  });
+});

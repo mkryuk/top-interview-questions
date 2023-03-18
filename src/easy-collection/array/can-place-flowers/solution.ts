08051ee3a1e008947d91dd24bde02c16d6ba8c44
Maxim Kryuk <mkryuk@gmail.com>
2023-03-18 19:37:17 +0200

    Array and Strings: Can Place Flowers solution

--- src/easy-collection/array/can-place-flowers/solution.ts
+++ src/easy-collection/array/can-place-flowers/solution.ts
@@ -1,3 +1,15 @@
 export function canPlaceFlowers(flowerbed: number[], n: number): boolean {
-  return false;
+  for (let i = 0; i < flowerbed.length && n !== 0; i++) {
+    if (flowerbed[i] === 0) {
+      const left = i === 0 || flowerbed[i - 1] === 0;
+      const right = i === flowerbed.length - 1 || flowerbed[i + 1] === 0;
+      if (left && right) {
+        n--;
+        // we move cursor one forward 
+        // since we should not place new flower nearby
+        i++;
+      }
+    }
+  }
+  return n === 0;
 }
--- src/easy-collection/array/can-place-flowers/test.spec.ts
+++ src/easy-collection/array/can-place-flowers/test.spec.ts
@@ -14,4 +14,39 @@ describe("Can Place Flowers", () => {
     const result = canPlaceFlowers(flowerbed, n);
     expect(result).toBeFalse();
   });
+
+  it("canPlaceFlowers should return false for flowerbed = [1,0,0,0,0,1], n = 2", () => {
+    const flowerbed = [1, 0, 0, 0, 0, 1];
+    const n = 2;
+    const result = canPlaceFlowers(flowerbed, n);
+    expect(result).toBeFalse();
+  });
+
+  it("canPlaceFlowers should return true for flowerbed = [0], n = 1", () => {
+    const flowerbed = [0];
+    const n = 1;
+    const result = canPlaceFlowers(flowerbed, n);
+    expect(result).toBeTrue();
+  });
+
+  it("canPlaceFlowers should return true for flowerbed = [0,0], n = 1", () => {
+    const flowerbed = [0, 0];
+    const n = 1;
+    const result = canPlaceFlowers(flowerbed, n);
+    expect(result).toBeTrue();
+  });
+
+  it("canPlaceFlowers should return false for flowerbed = [1,0], n = 1", () => {
+    const flowerbed = [1, 0];
+    const n = 1;
+    const result = canPlaceFlowers(flowerbed, n);
+    expect(result).toBeFalse();
+  });
+
+  it("canPlaceFlowers should return false for flowerbed = [0,1], n = 1", () => {
+    const flowerbed = [0, 1];
+    const n = 1;
+    const result = canPlaceFlowers(flowerbed, n);
+    expect(result).toBeFalse();
+  });
 });

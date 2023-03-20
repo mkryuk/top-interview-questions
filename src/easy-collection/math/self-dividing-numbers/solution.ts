c0eaa694383324a0e0708ea4d3acc3f887462107
Maxim Kryuk <mkryuk@gmail.com>
2023-03-20 21:51:34 +0200

    Math: Self Dividing Numbers solution

--- src/easy-collection/math/self-dividing-numbers/solution.ts
+++ src/easy-collection/math/self-dividing-numbers/solution.ts
@@ -1,3 +1,19 @@
 export function selfDividingNumbers(left: number, right: number): number[] {
-  return [];
+  let result = [];
+  for (let i = left; i <= right; i++) {
+    if (isSelfDividing(i)) {
+      result.push(i);
+    }
+  }
+  return result;
+}
+
+function isSelfDividing(num: number): boolean {
+  let strNum = num.toString();
+  for (let i = 0; i < strNum.length; i++) {
+    if (strNum[i] === "0" || num % parseInt(strNum[i], 10) !== 0) {
+      return false;
+    }
+  }
+  return true;
 }

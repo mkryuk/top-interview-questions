5526022de1ab1be233e5d3ea0279bd49b526c857
Maxim Kryuk <mkryuk@gmail.com>
2023-03-22 22:33:27 +0200

    Array and Strings: Number of Zero-Filled Subarrays solution

--- src/medium-collection/array-and-strings/zero-filled-subarray/solution.ts
+++ src/medium-collection/array-and-strings/zero-filled-subarray/solution.ts
@@ -1,3 +1,24 @@
 export function zeroFilledSubarray(nums: number[]): number {
-  return 0;
+  let result = 0;
+  let zeroCount = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) {
+      zeroCount++;
+    } else {
+      result += getGroupCount(zeroCount);
+      zeroCount = 0;
+    }
+  }
+  if (zeroCount !== 0) {
+    result += getGroupCount(zeroCount);
+  }
+  return result;
+}
+
+function getGroupCount(num: number): number {
+  let result = 0;
+  while (num !== 0) {
+    result += num--;
+  }
+  return result;
 }

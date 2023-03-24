bcbf9a2da6a876fa5134852850706e515cf6d8be
Maxim Kryuk <mkryuk@gmail.com>
2023-03-24 21:31:48 +0200

    Array and Strings: Maximum Number of Weeks for Which You Can Work added tests

--- src/medium-collection/array-and-strings/number-of-weeks/test.spec.ts
+++ src/medium-collection/array-and-strings/number-of-weeks/test.spec.ts
@@ -12,4 +12,22 @@ describe("Maximum Number of Weeks for Which You Can Work", () => {
     const result = numberOfWeeks(milestones);
     expect(result).toEqual(7);
   });
+
+  it("numberOfWeeks should return 10 for milestones = [1, 2, 3, 4]", () => {
+    const milestones = [1, 2, 3, 4];
+    const result = numberOfWeeks(milestones);
+    expect(result).toEqual(10);
+  });
+
+  it("numberOfWeeks should return 15 for milestones = [1, 2, 3, 4, 5]", () => {
+    const milestones = [1, 2, 3, 4, 5];
+    const result = numberOfWeeks(milestones);
+    expect(result).toEqual(15);
+  });
+
+  it("numberOfWeeks should return 11 for milestones = [2, 3, 2, 4]", () => {
+    const milestones = [2, 3, 2, 4];
+    const result = numberOfWeeks(milestones);
+    expect(result).toEqual(11);
+  });
 });

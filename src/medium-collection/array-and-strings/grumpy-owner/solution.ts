09e34765794a5d1a0b4716655bff5a6d3c38c002
Maxim Kryuk <mkryuk@gmail.com>
2023-03-31 21:43:24 +0300

    Array and Strings: Grumpy Bookstore Owner init

---
+++ src/medium-collection/array-and-strings/grumpy-owner/solution.ts
@@ -0,0 +1,7 @@
+export function maxSatisfied(
+  customers: number[],
+  grumpy: number[],
+  minutes: number,
+): number {
+  return 0;
+}
---
+++ src/medium-collection/array-and-strings/grumpy-owner/test.spec.ts
@@ -0,0 +1,19 @@
+import { maxSatisfied } from "./solution";
+
+describe("Grumpy Bookstore Owner", () => {
+  it("maxSatisfied should pass test 1", () => {
+    const customers = [1, 0, 1, 2, 1, 1, 7, 5];
+    const grumpy = [0, 1, 0, 1, 0, 1, 0, 1];
+    const minutes = 3;
+    const result = maxSatisfied(customers, grumpy, minutes);
+    expect(result).toEqual(16);
+  });
+
+  it("maxSatisfied should pass test 2", () => {
+    const customers = [1];
+    const grumpy = [0];
+    const minutes = 1;
+    const result = maxSatisfied(customers, grumpy, minutes);
+    expect(result).toEqual(11);
+  });
+});

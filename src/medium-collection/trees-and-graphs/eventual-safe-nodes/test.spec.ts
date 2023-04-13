68495476584e2d641f790809a44dbc39b9443bee
Maxim Kryuk <mkryuk@gmail.com>
2023-04-13 21:19:39 +0300

    Strings and Graphs: Find Eventual Safe States init

---
+++ src/medium-collection/trees-and-graphs/eventual-safe-nodes/test.spec.ts
@@ -0,0 +1,15 @@
+import { eventualSafeNodes } from "./solution";
+
+describe("Find Eventual Safe States", () => {
+  it("eventualSafeNodes should return [2,4,5,6] for graph = [[1,2],[2,3],[5],[0],[5],[],[]]", () => {
+    const graph = [[1, 2], [2, 3], [5], [0], [5], [], []];
+    const result = eventualSafeNodes(graph);
+    expect(result).toEqual([2, 4, 5, 6]);
+  });
+
+  it("eventualSafeNodes should return [4] for graph = [[1,2,3,4],[1,2],[3,4],[0,4],[]]", () => {
+    const graph = [[1, 2, 3, 4], [1, 2], [3, 4], [0, 4], []];
+    const result = eventualSafeNodes(graph);
+    expect(result).toEqual([4]);
+  });
+});

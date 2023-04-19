19c74815a821b6fbb2fd6431631069f89527bab6
Maxim Kryuk <mkryuk@gmail.com>
2023-04-19 19:14:47 +0300

    Strings and Graphs: Longest ZigZag Path in a Binary Tree init

---
+++ src/medium-collection/trees-and-graphs/longest-zig-zag/solution.ts
@@ -0,0 +1,5 @@
+import { TreeNode } from "../common";
+
+export function longestZigZag(root: TreeNode | null): number {
+  return 0;
+}
---
+++ src/medium-collection/trees-and-graphs/longest-zig-zag/test.spec.ts
@@ -0,0 +1,27 @@
+import { arrayToTreeNode } from "../common";
+import { longestZigZag } from "./solution";
+
+describe("Longest ZigZag Path in a Binary Tree", () => {
+  it("longestZigZag should return 3 for root = [1,null,1,1,1,null,null,1,1,null,1,null,null,null,1,null,1]", () => {
+    // prettier-ignore
+    const nodes = [1,null,1,1,1,null,null,1,1,null,1,null,null,null,1,null,1];
+    const root = arrayToTreeNode(nodes);
+    const result = longestZigZag(root);
+    expect(result).toEqual(3);
+  });
+
+  it("longestZigZag should return 4 for root = [1,1,1,null,1,null,null,1,1,null,1]", () => {
+    // prettier-ignore
+    const nodes = [1,1,1,null,1,null,null,1,1,null,1];
+    const root = arrayToTreeNode(nodes);
+    const result = longestZigZag(root);
+    expect(result).toEqual(4);
+  });
+  it("longestZigZag should return 0 for root = [1]", () => {
+    // prettier-ignore
+    const nodes = [1];
+    const root = arrayToTreeNode(nodes);
+    const result = longestZigZag(root);
+    expect(result).toEqual(0);
+  });
+});

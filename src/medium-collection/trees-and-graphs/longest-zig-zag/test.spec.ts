a7b6f383934d2b83b9f43a48e5df3e89981c9b6b
Maxim Kryuk <mkryuk@gmail.com>
2023-04-20 22:00:04 +0300

    Strings and Graphs: Longest ZigZag Path in a Binary Tree added tests

--- src/medium-collection/trees-and-graphs/longest-zig-zag/test.spec.ts
+++ src/medium-collection/trees-and-graphs/longest-zig-zag/test.spec.ts
@@ -17,11 +17,32 @@ describe("Longest ZigZag Path in a Binary Tree", () => {
     const result = longestZigZag(root);
     expect(result).toEqual(4);
   });
+
   it("longestZigZag should return 0 for root = [1]", () => {
-    // prettier-ignore
     const nodes = [1];
     const root = arrayToTreeNode(nodes);
     const result = longestZigZag(root);
     expect(result).toEqual(0);
+  });
+
+  it("longestZigZag should return 1 for root = [1,1]", () => {
+    const nodes = [1, 1];
+    const root = arrayToTreeNode(nodes);
+    const result = longestZigZag(root);
+    expect(result).toEqual(1);
+  });
+
+  it("longestZigZag should return 1 for root = [1,null,1]", () => {
+    const nodes = [1, null, 1];
+    const root = arrayToTreeNode(nodes);
+    const result = longestZigZag(root);
+    expect(result).toEqual(1);
+  });
+
+  it("longestZigZag should return 3 for root = [1,null,1,1,null,null,1]", () => {
+    const nodes = [1, null, 1, 1, null, null, 1];
+    const root = arrayToTreeNode(nodes);
+    const result = longestZigZag(root);
+    expect(result).toEqual(3);
   });
 });

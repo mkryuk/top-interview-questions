2dfcb99d786298d1d01f96139f787e79269a21aa
Maxim Kryuk <mkryuk@gmail.com>
2023-04-23 18:24:29 +0300

    Arrays and Strings: Last Stone Weight Heap solution

--- src/easy-collection/array/last-stone-weight/solution.ts
+++ src/easy-collection/array/last-stone-weight/solution.ts
@@ -1,3 +1,15 @@
+import { Heap } from "../../../hard-collection/design/heap/heap";
+
 export function lastStoneWeight(stones: number[]): number {
-  return 0;
+  var heap = new Heap<number>((left, right) => right - left);
+  stones.forEach((s) => heap.insert(s));
+  while (heap.size() > 1) {
+    const left = heap.pop()!;
+    const right = heap.pop()!;
+    const result = left - right;
+    if (result > 0) {
+      heap.insert(result);
+    }
+  }
+  return heap.top()!;
 }

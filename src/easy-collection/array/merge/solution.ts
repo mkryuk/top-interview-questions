45c02a71d16bf31034a136574a2d1f19ab2c056c
Maxim Kryuk <mkryuk@gmail.com>
2023-04-25 01:25:45 +0300

    Array and Strings: Merge Sorted Array solution

--- src/easy-collection/array/merge/solution.ts
+++ src/easy-collection/array/merge/solution.ts
@@ -6,4 +6,15 @@ export function merge(
   m: number,
   nums2: number[],
   n: number,
-): void {}
+): void {
+  let index1 = m - 1;
+  let index2 = n - 1;
+  let index = m + n - 1;
+  while (index >= 0 && index2 >= 0) {
+    if (index1 >= 0 && nums1[index1] >= nums2[index2]) {
+      nums1[index--] = nums1[index1--];
+    } else {
+      nums1[index--] = nums2[index2--];
+    }
+  }
+}

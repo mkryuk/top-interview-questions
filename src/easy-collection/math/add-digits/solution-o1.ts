f0ce983f34b5f953c35b94b8e4c08981a05ae36d
Maxim Kryuk <mkryuk@gmail.com>
2023-04-28 00:03:51 +0300

    Math: Add Digits solution O(1) added comment

--- src/easy-collection/math/add-digits/solution-o1.ts
+++ src/easy-collection/math/add-digits/solution-o1.ts
@@ -2,5 +2,8 @@ export function addDigits(num: number): number {
   // if (num == 0) return 0;
   // if (num % 9 == 0) return 9;
   // return num % 9;
+  // Mathematical: Digital Root
+  // The original number is divisible by 9 if 
+  // and only if the sum of its digits is divisible by 9
   return num == 0 ? 0 : 1 + ((num - 1) % 9);
 }

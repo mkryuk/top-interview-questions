82d102dc20c7f21b08121640f84504ba415a718d
Maxim Kryuk <mkryuk@gmail.com>
2023-04-30 00:26:04 +0300

    Array and Strings: Element Appearing More Than 25% In Sorted Array solution

--- src/easy-collection/array/find-special-integer/solution.ts
+++ src/easy-collection/array/find-special-integer/solution.ts
@@ -1,3 +1,17 @@
 export function findSpecialInteger(arr: number[]): number {
-  return 0;
+  let num = Math.trunc(arr.length / 4);
+  let count = 1;
+  let result = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === result) {
+      count++;
+    } else {
+      result = arr[i];
+      count = 1;
+    }
+    if (count > num) {
+      break;
+    }
+  }
+  return result;
 }
--- src/easy-collection/array/find-special-integer/test.spec.ts
+++ src/easy-collection/array/find-special-integer/test.spec.ts
@@ -12,4 +12,18 @@ describe("Element Appearing More Than 25% In Sorted Array", () => {
     const result = findSpecialInteger(arr);
     expect(result).toEqual(1);
   });
+
+  it("findSpecialInteger should return 3 for arr = [1,1,2,2,3,3,3,4]", () => {
+    const arr = [1, 1, 2, 2, 3, 3, 3, 4];
+    const result = findSpecialInteger(arr);
+    expect(result).toEqual(3);
+  });
+
+  it("findSpecialInteger should return 3 for arr = [1,1,1,1,1,1,2,3,4,5,6,7,8,9,10,11,12,12,12,12]", () => {
+    const arr = [
+      1, 1, 1, 1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12, 12, 12,
+    ];
+    const result = findSpecialInteger(arr);
+    expect(result).toEqual(1);
+  });
 });

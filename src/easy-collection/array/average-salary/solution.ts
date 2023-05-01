7933cc6ddf9b18003fc3a760d68ab35a76166fa6
Maxim Kryuk <mkryuk@gmail.com>
2023-05-01 19:12:06 +0300

    Array and Strings: Average Salary Excluding the Minimum and Maximum Salary solution

--- src/easy-collection/array/average-salary/solution.ts
+++ src/easy-collection/array/average-salary/solution.ts
@@ -1,3 +1,12 @@
 export function average(salary: number[]): number {
-  return 0;
+  let max = salary[0];
+  let min = salary[0];
+  let sum = salary[0];
+  for (let i = 1; i < salary.length; i++) {
+    max = Math.max(max, salary[i]);
+    min = Math.min(min, salary[i]);
+    sum += salary[i];
+  }
+  sum -= max + min;
+  return sum / (salary.length - 2);
 }

3b2544bc51988d31d597a5b381567fd096de709f
Maxim Kryuk <mkryuk@gmail.com>
2023-05-01 18:55:30 +0300

    Array and Strings: Average Salary Excluding the Minimum and Maximum Salary init

---
+++ src/easy-collection/array/average-salary/test.spec.ts
@@ -0,0 +1,15 @@
+import { average } from "./solution";
+
+describe("Average Salary Excluding the Minimum and Maximum Salary", () => {
+  it("average should return 2500 for salary = [4000,3000,1000,2000]", () => {
+    const salary = [4000, 3000, 1000, 2000];
+    const result = average(salary);
+    expect(result).toEqual(2500);
+  });
+
+  it("average should return 2000 for salary = [1000,2000,3000]", () => {
+    const salary = [1000, 2000, 3000];
+    const result = average(salary);
+    expect(result).toEqual(2500);
+  });
+});

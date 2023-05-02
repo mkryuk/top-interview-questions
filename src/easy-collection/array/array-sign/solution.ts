6ed10e6dc34ab94b8d5f98c8e64d8b828fb7dfe9
Maxim Kryuk <mkryuk@gmail.com>
2023-05-02 21:53:21 +0300

    Array and Strings: Sign of the Product of an Array solution

--- src/easy-collection/array/array-sign/solution.ts
+++ src/easy-collection/array/array-sign/solution.ts
@@ -1,3 +1,12 @@
 export function arraySign(nums: number[]): number {
-  return 0;
+  let result = 1;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) {
+      return 0;
+    }
+    if (nums[i] < 0) {
+      result = -1 * result;
+    }
+  }
+  return result;
 }

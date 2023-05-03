caba9afb98b97611f06c8777079515a11398d868
Maxim Kryuk <mkryuk@gmail.com>
2023-05-03 19:25:09 +0300

    Array and Strings: Find the Difference of Two Arrays solution

--- src/easy-collection/array/find-difference/solution.ts
+++ src/easy-collection/array/find-difference/solution.ts
@@ -1,3 +1,19 @@
 export function findDifference(nums1: number[], nums2: number[]): number[][] {
-  return [];
+  const nums1Set = new Set<number>(nums1);
+  const nums2Set = new Set<number>(nums2);
+  const notInNums1 = [];
+  const notInNums2 = [];
+
+  const uniqueElements = new Set<number>([...nums1, ...nums2]);
+
+  for (const num of Array.from(uniqueElements)) {
+    if (!nums2Set.has(num)) {
+      notInNums2.push(num);
+    }
+    if (!nums1Set.has(num)) {
+      notInNums1.push(num);
+    }
+  }
+
+  return [notInNums2, notInNums1];
 }

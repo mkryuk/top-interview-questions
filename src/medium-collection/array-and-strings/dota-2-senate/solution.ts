37a4e2a4d0748b9e57ed1cf99505e83cd13e3cb1
Maxim Kryuk <mkryuk@gmail.com>
2023-05-04 23:02:17 +0300

    Array and Strings: Dota2 Senate solution

--- src/medium-collection/array-and-strings/dota-2-senate/solution.ts
+++ src/medium-collection/array-and-strings/dota-2-senate/solution.ts
@@ -1,3 +1,25 @@
 export function predictPartyVictory(senate: string): string {
-  return "";
+  const radiantQueue: number[] = [];
+  const direQueue: number[] = [];
+
+  for (let i = 0; i < senate.length; i++) {
+    if (senate[i] === "R") {
+      radiantQueue.push(i);
+    } else {
+      direQueue.push(i);
+    }
+  }
+
+  while (radiantQueue.length > 0 && direQueue.length > 0) {
+    if (radiantQueue[0] < direQueue[0]) {
+      radiantQueue.push(radiantQueue[0] + senate.length);
+    } else {
+      direQueue.push(direQueue[0] + senate.length);
+    }
+
+    radiantQueue.shift();
+    direQueue.shift();
+  }
+
+  return radiantQueue.length > 0 ? "Radiant" : "Dire";
 }

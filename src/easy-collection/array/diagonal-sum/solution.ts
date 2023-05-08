f5750f055adeb3871444e81c025838d2ec99e404
Maxim Kryuk <mkryuk@gmail.com>
2023-05-08 22:33:39 +0300

    Array and Strings: Matrix Diagonal Sum solution

--- src/easy-collection/array/diagonal-sum/solution.ts
+++ src/easy-collection/array/diagonal-sum/solution.ts
@@ -1,3 +1,14 @@
 export function diagonalSum(mat: number[][]): number {
-  return 0;
+  let result = 0;
+  const n = mat.length - 1;
+  const isOdd = mat.length % 2;
+  let center = Math.floor(mat.length / 2);
+  for (let i = 0; i < mat.length; i++) {
+    if (isOdd && i === center) {
+      result += mat[center][center];
+      continue;
+    }
+    result += mat[i][i] + mat[i][n - i];
+  }
+  return result;
 }
--- src/easy-collection/array/diagonal-sum/test.spec.ts
+++ src/easy-collection/array/diagonal-sum/test.spec.ts
@@ -27,4 +27,15 @@ describe("Matrix Diagonal Sum", () => {
     const result = diagonalSum(mat);
     expect(result).toEqual(5);
   });
+
+  it("diagonalSum should pass test 4", () => {
+    const mat = [
+      [7, 3, 1, 9],
+      [3, 4, 6, 9],
+      [6, 9, 6, 6],
+      [9, 5, 8, 5],
+    ];
+    const result = diagonalSum(mat);
+    expect(result).toEqual(55);
+  });
 });

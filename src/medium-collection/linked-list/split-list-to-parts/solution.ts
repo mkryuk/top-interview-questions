be03d39be3f5a5a140747975377161bd92a1cab7
Maxim Kryuk <mkryuk@gmail.com>
2023-05-10 23:40:18 +0300

    Linked List: Split Linked List in Parts solution

--- src/medium-collection/linked-list/split-list-to-parts/solution.ts
+++ src/medium-collection/linked-list/split-list-to-parts/solution.ts
@@ -4,5 +4,27 @@ export function splitListToParts(
   head: ListNode | null,
   k: number,
 ): Array<ListNode | null> {
-  return [];
+  let count = 0;
+  let currentNode = head;
+  while (currentNode) {
+    count++;
+    currentNode = currentNode.next;
+  }
+  let num = Math.trunc(count / k);
+  let rest = count % k;
+  let result: Array<ListNode | null> = [];
+  currentNode = head;
+  for (let i = 0; i < k; i++) {
+    let node = new ListNode(0);
+    let cursor = node;
+    for (let j = 0; j < num + (i < rest ? 1 : 0); j++) {
+      cursor.next = new ListNode(currentNode!.val);
+      cursor = cursor.next;
+      if (currentNode !== null) {
+        currentNode = currentNode.next;
+      }
+    }
+    result.push(node.next);
+  }
+  return result;
 }

3f24d50b6e4b5059ce87596b5400ba4fe7b3c269
Maxim Kryuk <mkryuk@gmail.com>
2023-05-15 23:34:51 +0300

    Array and Strings: Check If Two String Arrays are Equivalent reduce solution

---
+++ src/easy-collection/array/array-strings-are-equal/solution-reduce.ts
@@ -0,0 +1,8 @@
+export function arrayStringsAreEqual(
+  word1: string[],
+  word2: string[],
+): boolean {
+  const word1String = word1.reduce((acc, curr) => acc + curr);
+  const word2String = word2.reduce((acc, curr) => acc + curr);
+  return word1String === word2String;
+}

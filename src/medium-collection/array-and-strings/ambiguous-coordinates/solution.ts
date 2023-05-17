084f455c8a4aab7b09cf6cfa63d5fa53ba74f33c
Maxim Kryuk <mkryuk@gmail.com>
2023-05-17 23:22:22 +0300

    Array and Strings: Ambiguous Coordinates solution

--- src/medium-collection/array-and-strings/ambiguous-coordinates/solution.ts
+++ src/medium-collection/array-and-strings/ambiguous-coordinates/solution.ts
@@ -1,3 +1,49 @@
 export function ambiguousCoordinates(s: string): string[] {
-  return [];
+  let result: string[] = [];
+  let str = s.substring(1, s.length - 1); // Remove brackets
+
+  for (let i = 1; i < str.length; i++) {
+    let left = str.substring(0, i);
+    let right = str.substring(i);
+
+    let leftCombinations = insertDecimal(left);
+    let rightCombinations = insertDecimal(right);
+
+    for (let l of leftCombinations) {
+      for (let r of rightCombinations) {
+        result.push(`(${l}, ${r})`);
+      }
+    }
+  }
+
+  return result;
+}
+
+function insertDecimal(s: string): string[] {
+  let result: string[] = [];
+  if (isValid(s, true)) {
+    result.push(s); // Push s if it is valid without decimal point
+  }
+  if (s !== "0") {
+    // Do not insert decimal point when s is '0'
+    for (let i = 1; i < s.length; i++) {
+      let integerPart = s.substring(0, i);
+      let decimalPart = s.substring(i);
+      if (isValid(integerPart, true) && isValid(decimalPart, false)) {
+        result.push(integerPart + "." + decimalPart);
+      }
+    }
+  }
+
+  return result;
+}
+
+function isValid(s: string, isIntegerPart: boolean): boolean {
+  if (isIntegerPart && s.length > 1 && s[0] === "0") {
+    return false;
+  } else if (!isIntegerPart && s[s.length - 1] === "0") {
+    return false;
+  }
+
+  return true;
 }
--- src/medium-collection/array-and-strings/ambiguous-coordinates/test.spec.ts
+++ src/medium-collection/array-and-strings/ambiguous-coordinates/test.spec.ts
@@ -1,28 +1,38 @@
 import { ambiguousCoordinates } from "./solution";
 
+function compareArrays(left: string[], right: string[]): boolean {
+  if (left.length !== right.length) {
+    return false;
+  }
+  return left.every((l) => right.includes(l));
+}
+
 describe("Ambiguous Coordinates", () => {
   it('ambiguousCoordinates should return ["(1, 2.3)","(1, 23)","(1.2, 3)","(12, 3)"] for s = "(123)"', () => {
     const s = "(123)";
     const result = ambiguousCoordinates(s);
-    expect(result).toEqual(["(1, 2.3)", "(1, 23)", "(1.2, 3)", "(12, 3)"]);
+    const expected = ["(1, 2.3)", "(1, 23)", "(1.2, 3)", "(12, 3)"];
+    expect(compareArrays(result, expected)).toBeTrue();
   });
 
   it('ambiguousCoordinates should return ["(0, 1.23)","(0, 12.3)","(0, 123)","(0.1, 2.3)","(0.1, 23)","(0.12, 3)"] for s = "(0123)"', () => {
     const s = "(0123)";
     const result = ambiguousCoordinates(s);
-    expect(result).toEqual([
+    const expected = [
       "(0, 1.23)",
       "(0, 12.3)",
       "(0, 123)",
       "(0.1, 2.3)",
       "(0.1, 23)",
       "(0.12, 3)",
-    ]);
+    ];
+    expect(compareArrays(result, expected)).toBeTrue();
   });
 
   it('ambiguousCoordinates should return ["(0, 0.011)","(0.001, 1)"] for s = "(00011)"', () => {
     const s = "(00011)";
     const result = ambiguousCoordinates(s);
-    expect(result).toEqual(["(0, 0.011)", "(0.001, 1)"]);
+    const expected = ["(0, 0.011)", "(0.001, 1)"];
+    expect(compareArrays(result, expected)).toBeTrue();
   });
 });

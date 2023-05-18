e11d3cb93ec8f7c5dfe295fdd59a57a7a5130e04
Maxim Kryuk <mkryuk@gmail.com>
2023-05-19 00:19:18 +0300

    Linked List: Maximum Twin Sum of a Linked List solution

--- src/medium-collection/linked-list/pair-sum/solution.ts
+++ src/medium-collection/linked-list/pair-sum/solution.ts
@@ -1,5 +1,21 @@
 import { ListNode } from "../common";
 
 export function pairSum(head: ListNode | null): number {
-  return 0;
+  let nodes: number[] = [];
+  while (head !== null) {
+    nodes.push(head.val);
+    head = head.next;
+  }
+
+  const middle = nodes.length / 2;
+  let result = 0;
+  for (
+    let left = middle - 1, right = middle;
+    right < nodes.length;
+    left--, right++
+  ) {
+    result = Math.max(result, nodes[left] + nodes[right]);
+  }
+
+  return result;
 }
--- src/medium-collection/linked-list/pair-sum/test.spec.ts
+++ src/medium-collection/linked-list/pair-sum/test.spec.ts
@@ -13,13 +13,6 @@ describe("Maximum Twin Sum of a Linked List:", () => {
     const nodes = [4, 2, 2, 3];
     const head = arrayToList(nodes);
     const result = pairSum(head);
-    expect(result).toEqual(6);
-  });
-
-  it("pairSum should return 7 for head = [5,4,2,1]", () => {
-    const nodes = [5, 4, 2, 1];
-    const head = arrayToList(nodes);
-    const result = pairSum(head);
     expect(result).toEqual(7);
   });
 

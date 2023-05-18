c6817b0305e95df6cd03ec86dba51c2180b1b153
Maxim Kryuk <mkryuk@gmail.com>
2023-05-18 23:52:12 +0300

    Linked List: Maximum Twin Sum of a Linked List init

---
+++ src/medium-collection/linked-list/pair-sum/solution.ts
@@ -0,0 +1,5 @@
+import { ListNode } from "../common";
+
+export function pairSum(head: ListNode | null): number {
+  return 0;
+}
---
+++ src/medium-collection/linked-list/pair-sum/test.spec.ts
@@ -0,0 +1,32 @@
+import { arrayToList } from "../common";
+import { pairSum } from "./solution";
+
+describe("Maximum Twin Sum of a Linked List:", () => {
+  it("pairSum should return 6 for head = [5,4,2,1]", () => {
+    const nodes = [5, 4, 2, 1];
+    const head = arrayToList(nodes);
+    const result = pairSum(head);
+    expect(result).toEqual(6);
+  });
+
+  it("pairSum should return 7 for head = [4,2,2,3]", () => {
+    const nodes = [4, 2, 2, 3];
+    const head = arrayToList(nodes);
+    const result = pairSum(head);
+    expect(result).toEqual(6);
+  });
+
+  it("pairSum should return 7 for head = [5,4,2,1]", () => {
+    const nodes = [5, 4, 2, 1];
+    const head = arrayToList(nodes);
+    const result = pairSum(head);
+    expect(result).toEqual(7);
+  });
+
+  it("pairSum should return 100001 for head = [1,100000]", () => {
+    const nodes = [1, 100000];
+    const head = arrayToList(nodes);
+    const result = pairSum(head);
+    expect(result).toEqual(100001);
+  });
+});

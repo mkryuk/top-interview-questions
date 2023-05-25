954f6bc2df6472622d09c2fa2d318e49d13adf1b
Maxim Kryuk <mkryuk@gmail.com>
2023-05-26 00:22:42 +0300

    Array and Strings: Remove Element solution

--- src/easy-collection/array/remove-element/test.spec.ts
+++ src/easy-collection/array/remove-element/test.spec.ts
@@ -6,7 +6,7 @@ describe("Remove Element:", () => {
     const val = 3;
     const result = removeElement(nums, val);
     expect(result).toEqual(2);
-    expect(result).not.toContain(val);
+    expect(nums.slice(0, result)).not.toContain(val);
   });
 
   it("removeElement should return 5 for nums = [0,1,2,2,3,0,4,2], val = 2", () => {
@@ -14,6 +14,30 @@ describe("Remove Element:", () => {
     const val = 2;
     const result = removeElement(nums, val);
     expect(result).toEqual(5);
-    expect(result).not.toContain(val);
+    expect(nums.slice(0, result)).not.toContain(val);
+  });
+
+  it("removeElement should return 5 for nums = [3,2,2,3], val = 3", () => {
+    const nums = [3, 2, 2, 3];
+    const val = 3;
+    const result = removeElement(nums, val);
+    expect(result).toEqual(2);
+    expect(nums.slice(0, result)).not.toContain(val);
+  });
+
+  it("removeElement should return 5 for nums = [3,2,2,2], val = 2", () => {
+    const nums = [3, 2, 2, 2];
+    const val = 2;
+    const result = removeElement(nums, val);
+    expect(result).toEqual(1);
+    expect(nums.slice(0, result)).not.toContain(val);
+  });
+
+  it("removeElement should return 0 for nums = [1], val = 1", () => {
+    const nums = [1];
+    const val = 1;
+    const result = removeElement(nums, val);
+    expect(result).toEqual(0);
+    expect(nums.slice(0, result)).not.toContain(val);
   });
 });

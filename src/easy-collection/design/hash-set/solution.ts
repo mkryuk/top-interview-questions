867f11d2092b0211f01d0147deaf8ae6eb605738
Maxim Kryuk <mkryuk@gmail.com>
2023-05-30 19:15:15 +0300

    Design: Design HashSet array solution

--- src/easy-collection/design/hash-set/solution.ts
+++ src/easy-collection/design/hash-set/solution.ts
@@ -1,11 +1,16 @@
 export class MyHashSet {
+  hashSet: number[] = [];
   constructor() {}
 
-  add(key: number): void {}
+  add(key: number): void {
+    this.hashSet[key] = 1;
+  }
 
-  remove(key: number): void {}
+  remove(key: number): void {
+    delete this.hashSet[key];
+  }
 
   contains(key: number): boolean {
-    return false;
+    return this.hashSet[key] === 1;
   }
 }

a5136f9199f75ca0890c1d50cf3da980e80f5f19
Maxim Kryuk <mkryuk@gmail.com>
2023-06-01 22:45:26 +0300

    Array and Strings: Shortest Path in Binary Matrix initk

---
+++ src/medium-collection/array-and-strings/shortest-path-binary-matrix/test.spec.ts
@@ -0,0 +1,32 @@
+import { shortestPathBinaryMatrix } from "./solution";
+
+describe("Shortest Path in Binary Matrix:", () => {
+  it("shortestPathBinaryMatrix should return 2 for grid = [[0,1],[1,0]]", () => {
+    const grid = [
+      [0, 1],
+      [1, 0],
+    ];
+    const result = shortestPathBinaryMatrix(grid);
+    expect(result).toEqual(2);
+  });
+
+  it("shortestPathBinaryMatrix should return 4 for grid = [[0,0,0],[1,1,0],[1,1,0]]", () => {
+    const grid = [
+      [0, 0, 0],
+      [1, 1, 0],
+      [1, 1, 0],
+    ];
+    const result = shortestPathBinaryMatrix(grid);
+    expect(result).toEqual(4);
+  });
+
+  it("shortestPathBinaryMatrix should return -1 for grid = [[1,0,0],[1,1,0],[1,1,0]]", () => {
+    const grid = [
+      [1, 0, 0],
+      [1, 1, 0],
+      [1, 1, 0],
+    ];
+    const result = shortestPathBinaryMatrix(grid);
+    expect(result).toEqual(-1);
+  });
+});

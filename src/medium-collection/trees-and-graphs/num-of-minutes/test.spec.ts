cfc42d894652a57753edca21727b376c4811b4e8
Maxim Kryuk <mkryuk@gmail.com>
2023-06-04 23:00:19 +0300

    Trees and Graphs: Time Needed to Inform All Employees added tests

--- src/medium-collection/trees-and-graphs/num-of-minutes/test.spec.ts
+++ src/medium-collection/trees-and-graphs/num-of-minutes/test.spec.ts
@@ -18,4 +18,31 @@ describe("Time Needed to Inform All Employees:", () => {
     const result = numOfMinutes(n, headID, manager, informTime);
     expect(result).toEqual(1);
   });
+
+  it("numOfMinutes numOfMinutes should return 3 for n = 9, headID = O, manager = [-1,0,0,0,1,2,3,5,5], informTime = [1,1,1,1,0,1,0,0,0]", () => {
+    const n = 9;
+    const headID = 0;
+    const manager = [-1, 0, 0, 0, 1, 2, 3, 5, 5];
+    const informTime = [1, 1, 1, 1, 0, 1, 0, 0, 0];
+    const result = numOfMinutes(n, headID, manager, informTime);
+    expect(result).toEqual(3);
+  });
+
+  it("numOfMinutes numOfMinutes should return 3 for n = 9, headID = O, manager = [-1,0,0,0,1,2,3,5,5], informTime = [1,2,1,1,0,1,0,0,0]", () => {
+    const n = 9;
+    const headID = 0;
+    const manager = [-1, 0, 0, 0, 1, 2, 3, 5, 5];
+    const informTime = [1, 2, 1, 1, 0, 1, 0, 0, 0];
+    const result = numOfMinutes(n, headID, manager, informTime);
+    expect(result).toEqual(3);
+  });
+
+  it("numOfMinutes numOfMinutes should return 3 for n = 9, headID = O, manager = [-1,0,0,0,1,2,3,5,5], informTime = [1,1,2,1,0,1,0,0,0]", () => {
+    const n = 9;
+    const headID = 0;
+    const manager = [-1, 0, 0, 0, 1, 2, 3, 5, 5];
+    const informTime = [1, 1, 2, 1, 0, 1, 0, 0, 0];
+    const result = numOfMinutes(n, headID, manager, informTime);
+    expect(result).toEqual(4);
+  });
 });

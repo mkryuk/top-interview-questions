d686a6a5baa04b8db024669125922d889f86158d
Maxim Kryuk <mkryuk@gmail.com>
2023-06-05 23:15:27 +0300

    Trees and Graphs: Time Needed to Inform All Employees solution

--- src/medium-collection/trees-and-graphs/num-of-minutes/solution.ts
+++ src/medium-collection/trees-and-graphs/num-of-minutes/solution.ts
@@ -4,5 +4,29 @@ export function numOfMinutes(
   manager: number[],
   informTime: number[],
 ): number {
-  return 0;
+  const subordinatesMap = new Map<number, number[]>();
+  for (let i = 0; i < manager.length; i++) {
+    const managerId = manager[i];
+    if (!subordinatesMap.has(managerId)) {
+      subordinatesMap.set(managerId, []);
+    }
+    subordinatesMap.get(managerId)!.push(i);
+  }
+  return dfs(headID, subordinatesMap, informTime);
+}
+
+function dfs(
+  manager: number,
+  subordinatesMap: Map<number, number[]>,
+  informTime: number[],
+): number {
+  if (!subordinatesMap.has(manager)) {
+    return 0;
+  }
+  let subordinates = subordinatesMap.get(manager)!;
+  let time = 0;
+  for (let i = 0; i < subordinates.length; i++) {
+    time = Math.max(time, dfs(subordinates[i], subordinatesMap, informTime));
+  }
+  return informTime[manager] + time;
 }

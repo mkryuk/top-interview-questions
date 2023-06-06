58c1ef0a1ddb2febd3029fd620d1ef1f1f4474aa
Maxim Kryuk <mkryuk@gmail.com>
2023-06-06 22:30:39 +0300

    Array and Strings: Can Make Arithmetic Progression From Sequence solution

--- src/easy-collection/array/make-arithmetic-progression/solution.ts
+++ src/easy-collection/array/make-arithmetic-progression/solution.ts
@@ -1,3 +1,10 @@
 export function canMakeArithmeticProgression(arr: number[]): boolean {
-  return false;
+  arr.sort((a, b) => a - b);
+  const diff = arr[1] - arr[0];
+  for (let i = 2; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] !== diff) {
+      return false;
+    }
+  }
+  return true;
 }

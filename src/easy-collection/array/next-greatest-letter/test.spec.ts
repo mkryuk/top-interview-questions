36f87ea5bea9f68b876e5eded55477711eec35bd
Maxim Kryuk <mkryuk@gmail.com>
2023-06-09 18:06:30 +0300

    Array and Strings: Find Smallest Letter Greater Than Target init

---
+++ src/easy-collection/array/next-greatest-letter/test.spec.ts
@@ -0,0 +1,24 @@
+import { nextGreatestLetter } from "./solution";
+
+describe("Find Smallest Letter Greater Than Target:", () => {
+  it('nextGreatestLetter should return "c" for letters = ["c","f","j"], target = "a"', () => {
+    const letters = ["c", "f", "j"];
+    const target = "a";
+    const result = nextGreatestLetter(letters, target);
+    expect(result).toEqual("c");
+  });
+
+  it('nextGreatestLetter should return "f" for letters = ["c","f","j"], target = "c"', () => {
+    const letters = ["c", "f", "j"];
+    const target = "c";
+    const result = nextGreatestLetter(letters, target);
+    expect(result).toEqual("f");
+  });
+
+  it('nextGreatestLetter should return "x" for letters = ["x","x","y","y"], target = "z"', () => {
+    const letters = ["x", "x", "y", "y"];
+    const target = "z";
+    const result = nextGreatestLetter(letters, target);
+    expect(result).toEqual("x");
+  });
+});

daf86621bef3b0082fc14d24971a1ec6591ba968
Maxim Kryuk <mkryuk@gmail.com>
2023-06-14 21:59:18 +0300

    Array and Strings: Equal Row and Column Pairs init

---
+++ src/medium-collection/array-and-strings/equal-pairs/solution.ts
@@ -0,0 +1,3 @@
+export function equalPairs(grid: number[][]): number {
+  return 0;
+}
---
+++ src/medium-collection/array-and-strings/equal-pairs/test.spec.ts
@@ -0,0 +1,24 @@
+import { equalPairs } from "./solution";
+
+describe("Equal Row and Column Pairs:", () => {
+  it("equalPairs should return 1 for grid = [[3,2,1],[1,7,6],[2,7,7]]", () => {
+    const grid = [
+      [3, 2, 1],
+      [1, 7, 6],
+      [2, 7, 7],
+    ];
+    const result = equalPairs(grid);
+    expect(result).toEqual(1);
+  });
+
+  it("equalPairs should return 2 for grid = [[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]]", () => {
+    const grid = [
+      [3, 1, 2, 2],
+      [1, 4, 4, 5],
+      [2, 4, 2, 2],
+      [2, 4, 2, 2],
+    ];
+    const result = equalPairs(grid);
+    expect(result).toEqual(1);
+  });
+});

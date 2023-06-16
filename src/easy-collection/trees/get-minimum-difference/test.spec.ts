949d142eb7016b30f9968439935f2167de818fa7
Maxim Kryuk <mkryuk@gmail.com>
2023-06-16 14:12:05 +0300

    Trees and Graphs: Minimum Absolute Difference in BST init

---
+++ src/easy-collection/trees/get-minimum-difference/test.spec.ts
@@ -0,0 +1,18 @@
+import { arrayToTreeNode } from "../common";
+import { getMinimumDifference } from "./solution";
+
+describe("Minimum Absolute Difference in BST:", () => {
+  it("getMinimumDifference should return 1 for root = [4,2,6,1,3]", () => {
+    const nodes = [4, 2, 6, 1, 3];
+    const root = arrayToTreeNode(nodes);
+    const result = getMinimumDifference(root);
+    expect(result).toEqual(1);
+  });
+
+  it("getMinimumDifference should return 1 for root = [1,0,48,null,null,12,49]", () => {
+    const nodes = [1, 0, 48, null, null, 12, 49];
+    const root = arrayToTreeNode(nodes);
+    const result = getMinimumDifference(root);
+    expect(result).toEqual(1);
+  });
+});

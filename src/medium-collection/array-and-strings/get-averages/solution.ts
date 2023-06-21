9d15b49adfa8ea27b4012c208034460d882f7e9f
Maxim Kryuk <mkryuk@gmail.com>
2023-06-21 17:44:28 +0300

    Array and Strings: K Radius Subarray Averages solution

--- src/medium-collection/array-and-strings/get-averages/solution.ts
+++ src/medium-collection/array-and-strings/get-averages/solution.ts
@@ -1,3 +1,18 @@
 export function getAverages(nums: number[], k: number): number[] {
-  return [];
+  let result = new Array(nums.length).fill(-1);
+  if (nums.length < k * 2 + 1) {
+    return result;
+  }
+  const subLen = k * 2 + 1;
+  var sum = 0;
+  for (let i = 0; i < subLen; i++) {
+    sum += nums[i];
+  }
+  result[k] = Math.trunc(sum / subLen);
+  for (let i = k + 1; i < nums.length - k; i++) {
+    sum -= nums[i - k - 1];
+    sum += nums[i + k];
+    result[i] = Math.trunc(sum / subLen);
+  }
+  return result;
 }

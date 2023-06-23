7c710fab291e918c7404f590d004a926d752d0c9
Maxim Kryuk <mkryuk@gmail.com>
2023-06-23 22:27:51 +0300

    Array and Strings: Best Time to Buy and Sell Stock with Transaction Fee solution

--- src/medium-collection/array-and-strings/max-profit-with-fee/solution.ts
+++ src/medium-collection/array-and-strings/max-profit-with-fee/solution.ts
@@ -1,3 +1,15 @@
 export function maxProfit(prices: number[], fee: number): number {
-  return 0;
+  let buy: number[] = [];
+  let sell: number[] = [];
+  let n = prices.length;
+  // we should spend prices[0] to buy, so the profit is -price[0]
+  buy[0] = -prices[0];
+  sell[0] = 0;
+  for (let i = 1; i < n; i++) {
+    // current buy profit is the diff between prev sell and current price
+    buy[i] = Math.max(buy[i - 1], sell[i - 1] - prices[i]);
+    // current sell profit is the diff between prev buy and current price
+    sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i] - fee);
+  }
+  return sell[n - 1];
 }

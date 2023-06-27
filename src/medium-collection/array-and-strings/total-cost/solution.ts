f098c3fdc3e570f8bad10424debdc1518b04179c
Maxim Kryuk <mkryuk@gmail.com>
2023-06-27 23:57:08 +0300

    Array and Strings: Total Cost to Hire K Workers solution

--- src/medium-collection/array-and-strings/total-cost/solution.ts
+++ src/medium-collection/array-and-strings/total-cost/solution.ts
@@ -1,7 +1,52 @@
+import { Heap } from "../../../hard-collection/design/heap/heap";
+
 export function totalCost(
   costs: number[],
   k: number,
   candidates: number,
 ): number {
-  return 0;
+  let leftQueue = new Heap<number>((a, b) => a - b);
+  let rightQueue = new Heap<number>((a, b) => a - b);
+
+  // leftQueue stores the first k workers.
+  // rightQueue stores at most last k workers without any workers from the first k workers.
+  for (let i = 0; i < candidates; i++) {
+    leftQueue.insert(costs[i]);
+  }
+  for (
+    let i = Math.max(candidates, costs.length - candidates);
+    i < costs.length;
+    i++
+  ) {
+    rightQueue.insert(costs[i]);
+  }
+
+  let result = 0;
+  let leftIndex = candidates;
+  let rightIndex = costs.length - 1 - candidates;
+
+  for (let i = 0; i < k; i++) {
+    if (
+      rightQueue.isEmpty() ||
+      (!leftQueue.isEmpty() && leftQueue.top()! <= rightQueue.top()!)
+    ) {
+      result += leftQueue.pop()!;
+
+      // Only refill the queue if there are workers outside the two queues.
+      if (leftIndex <= rightIndex) {
+        leftQueue.insert(costs[leftIndex]);
+        leftIndex++;
+      }
+    } else {
+      result += rightQueue.pop()!;
+
+      // Only refill the queue if there are workers outside the two queues.
+      if (leftIndex <= rightIndex) {
+        rightQueue.insert(costs[rightIndex]);
+        rightIndex--;
+      }
+    }
+  }
+
+  return result;
 }
--- src/medium-collection/array-and-strings/total-cost/test.spec.ts
+++ src/medium-collection/array-and-strings/total-cost/test.spec.ts
@@ -16,4 +16,27 @@ describe("Total Cost to Hire K Workers:", () => {
     const result = totalCost(costs, k, candidates);
     expect(result).toEqual(4);
   });
+
+  it("totalCost should return 1407 for test 3", () => {
+    const costs = [
+      28, 35, 21, 13, 21, 72, 35, 52, 74, 92, 25, 65, 77, 1, 73, 32, 43, 68, 8,
+      100, 84, 80, 14, 88, 42, 53, 98, 69, 64, 40, 60, 23, 99, 83, 5, 21, 76,
+      34,
+    ];
+    const k = 32;
+    const candidates = 12;
+    const result = totalCost(costs, k, candidates);
+    expect(result).toEqual(1407);
+  });
+
+  it("totalCost should return 1407 for test 3", () => {
+    const costs = [
+      18, 64, 12, 21, 21, 78, 36, 58, 88, 58, 99, 26, 92, 91, 53, 10, 24, 25,
+      20, 92, 73, 63, 51, 65, 87, 6, 17, 32, 14, 42, 46, 65, 43, 9, 75,
+    ];
+    const k = 13;
+    const candidates = 23;
+    const result = totalCost(costs, k, candidates);
+    expect(result).toEqual(223);
+  });
 });

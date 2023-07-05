e8177de12f7d92eb56fdcda1bbccc94b0b71e04a
Maxim Kryuk <mkryuk@gmail.com>
2023-07-05 22:45:37 +0300

    Array and Strings: Single Number II solution

--- src/medium-collection/array-and-strings/single-number-II/solution.ts
+++ src/medium-collection/array-and-strings/single-number-II/solution.ts
@@ -1,3 +1,17 @@
 export function singleNumber(nums: number[]): number {
-  return 0;
+  let ones = 0;
+  let twos = 0;
+  // The idea is to count the number of 1s in each position
+  // of the binary representation of numbers in the array.
+  // If the number of 1s is not a multiple of 3,
+  // it means that the single number has a 1 in that position.
+  for (let i = 0; i < nums.length; i++) {
+    ones = (ones ^ nums[i]) & ~twos;
+    twos = (twos ^ nums[i]) & ~ones;
+  }
+  // In the solution, ones is a variable that keeps track of bits which have been encountered one time so far,
+  // and twos is a variable that keeps track of bits which have been encountered two times so far.
+  // When a bit has been encountered three times, it will be present in both ones and twos.
+  // Therefore, we mask ones and twos with ~twos and ~ones respectively.
+  return ones;
 }

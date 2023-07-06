5e6a77060b29e63dbf9998c1bced1e31a8f30df7
Maxim Kryuk <mkryuk@gmail.com>
2023-07-06 21:23:00 +0300

    Array and Strings: Minimum Size Subarray Sum solution

--- src/medium-collection/array-and-strings/min-sub-array-len/solution.ts
+++ src/medium-collection/array-and-strings/min-sub-array-len/solution.ts
@@ -1,12 +1,11 @@
 export function minSubArrayLen(target: number, nums: number[]): number {
   let result = Number.MAX_SAFE_INTEGER;
   let window = 0;
-  let lIdx = 0;
-  for (let rIdx = 0; rIdx < nums.length; rIdx++) {
-    window += nums[rIdx];
-    while (lIdx <= rIdx && window >= target) {
-      result = Math.min(result, rIdx - lIdx + 1);
-      window -= nums[lIdx++];
+  for (let left = 0, right = 0; right < nums.length; right++) {
+    window += nums[right];
+    while (left <= right && window >= target) {
+      result = Math.min(result, right - left + 1);
+      window -= nums[left++];
     }
   }
   return result === Number.MAX_SAFE_INTEGER ? 0 : result;

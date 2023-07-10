a254716481023cf479c7eef35572c8ad1d6bc3bd
Maxim Kryuk <mkryuk@gmail.com>
2023-07-10 13:36:10 +0300

    Array and Strings: Partition to K Equal Sum Subsets solution

--- src/medium-collection/array-and-strings/can-partition-subsets/solution.ts
+++ src/medium-collection/array-and-strings/can-partition-subsets/solution.ts
@@ -1,3 +1,51 @@
 export function canPartitionKSubsets(nums: number[], k: number): boolean {
+  let sum = nums.reduce((a, b) => a + b, 0);
+  if (sum % k !== 0) {
+    return false; // If total sum of array is not divisible by k, no equal partitioning is possible.
+  }
+
+  let target = sum / k; // The sum of numbers in each subset.
+  nums.sort((a, b) => b - a); // Sorting in decreasing order will make the algorithm faster.
+
+  if (nums[0] > target) {
+    return false; // The largest number is greater than the target sum. Hence, partitioning is not possible.
+  }
+
+  let subsets = new Array(k).fill(0); // Initialize k subsets with sum 0.
+  return partition(0, nums, subsets, target);
+}
+
+function partition(
+  index: number,
+  nums: number[],
+  subsets: number[],
+  target: number,
+): boolean {
+  if (index === nums.length) {
+    // If we reach the end of the array, check if all subsets are equal to target.
+    return subsets.every((subset) => subset === target);
+  }
+
+  for (let i = 0; i < subsets.length; i++) {
+    if (subsets[i] + nums[index] <= target) {
+      // Add the current number to the current subset.
+      subsets[i] += nums[index];
+
+      if (partition(index + 1, nums, subsets, target)) {
+        // Continue to add to next subsets.
+        return true;
+      }
+
+      subsets[i] -= nums[index]; // If adding the current number to the subset did not solve the problem, remove it.
+    }
+
+    // If the current subset is empty, we should not try the remaining empty subsets.
+    // That's because we're just doing the same thing but in a different order (for example: subset[0] = nums[0] and subset[1] = nums[1] is the same as subset[1] = nums[0] and subset[0] = nums[1]).
+    // Therefore, we can make a decision here to only choose the first empty subset.
+    if (subsets[i] === 0) {
+      break;
+    }
+  }
+
   return false;
 }
--- src/medium-collection/array-and-strings/can-partition-subsets/test.spec.ts
+++ src/medium-collection/array-and-strings/can-partition-subsets/test.spec.ts
@@ -14,4 +14,21 @@ describe("Partition to K Equal Sum Subsets:", () => {
     const result = canPartitionKSubsets(nums, k);
     expect(result).toBeFalse();
   });
+
+  it("canPartitionKSubsets should return false for nums = [3522,181,521,515,304,123,2512,312,922,407,146,1932,4037,2646,3871,269], k = 5", () => {
+    const nums = [
+      3522, 181, 521, 515, 304, 123, 2512, 312, 922, 407, 146, 1932, 4037, 2646,
+      3871, 269,
+    ];
+    const k = 5;
+    const result = canPartitionKSubsets(nums, k);
+    expect(result).toBeTrue();
+  });
+
+  it("canPartitionKSubsets should return false for nums = [1,3,1,1,2,1], k = 3", () => {
+    const nums = [1, 3, 1, 1, 2, 1];
+    const k = 3;
+    const result = canPartitionKSubsets(nums, k);
+    expect(result).toBeTrue();
+  });
 });

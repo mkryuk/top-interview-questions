d04a5e98aeaf1414131615125207801c3d26a861
Maxim Kryuk <mkryuk@gmail.com>
2023-07-13 17:37:29 +0300

    Trees and Graphs: All Nodes Distance K in Binary Tree solution

--- src/medium-collection/trees-and-graphs/distance-k/solution.ts
+++ src/medium-collection/trees-and-graphs/distance-k/solution.ts
@@ -5,5 +5,56 @@ export function distanceK(
   target: TreeNode | null,
   k: number,
 ): number[] {
-  return [];
+  if (root === null || target === null) {
+    return [];
+  }
+  let map = new Map<number, TreeNode | null>();
+  createMap(root, null, map);
+  let result: number[] = [];
+  let visited = new Set<number>();
+  dfs(target, 0, k, result, visited);
+  let distance = 1;
+  let parent: TreeNode | null = map.get(target.val)!;
+  while (distance <= k && parent !== null) {
+    if (distance === k) {
+      result.push(parent.val!);
+      break;
+    }
+    dfs(parent, distance, k, result, visited);
+    distance++;
+    parent = map.get(parent.val)!;
+  }
+  return result;
+}
+
+function createMap(
+  node: TreeNode | null,
+  parent: TreeNode | null,
+  map: Map<number, TreeNode | null>,
+) {
+  if (node === null) {
+    return;
+  }
+  map.set(node.val, parent);
+  createMap(node.left, node, map);
+  createMap(node.right, node, map);
+}
+
+function dfs(
+  node: TreeNode | null,
+  distance: number,
+  k: number,
+  result: number[],
+  visited: Set<number>,
+) {
+  if (node === null || visited.has(node.val)) {
+    return;
+  }
+  visited.add(node.val);
+  if (distance === k) {
+    result.push(node.val);
+    return;
+  }
+  dfs(node.left, distance + 1, k, result, visited);
+  dfs(node.right, distance + 1, k, result, visited);
 }
--- src/medium-collection/trees-and-graphs/distance-k/test.spec.ts
+++ src/medium-collection/trees-and-graphs/distance-k/test.spec.ts
@@ -7,7 +7,7 @@ describe("All Nodes Distance K in Binary Tree:", () => {
     const target = 5;
     const k = 2;
     const root = arrayToTreeNode(nodes);
-    const targetNode = new TreeNode(target);
+    const targetNode = findTarget(root, target);
     const result = distanceK(root, targetNode, k);
     expect(arrayEquals([7, 4, 1], result)).toBeTrue();
   });
@@ -16,13 +16,30 @@ describe("All Nodes Distance K in Binary Tree:", () => {
     const target = 1;
     const k = 3;
     const root = arrayToTreeNode(nodes);
-    const targetNode = new TreeNode(target);
+    const targetNode = findTarget(root, target);
     const result = distanceK(root, targetNode, k);
     expect(arrayEquals([], result)).toBeTrue();
   });
 });
 
-export function arrayEquals(a: number[], b: number[]): boolean {
+function findTarget(root: TreeNode | null, target: number): TreeNode | null {
+  let queue = [root];
+  while (queue.length) {
+    const node = queue.shift()!;
+    if (node.val === target) {
+      return node;
+    }
+    if (node.left !== null) {
+      queue.push(node.left);
+    }
+    if (node.right !== null) {
+      queue.push(node.right);
+    }
+  }
+  return null;
+}
+
+function arrayEquals(a: number[], b: number[]): boolean {
   return (
     Array.isArray(a) &&
     Array.isArray(b) &&

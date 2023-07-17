ac2178788d8436cc942cd810fbddaf3b2d2c21b4
Maxim Kryuk <mkryuk@gmail.com>
2023-07-18 00:27:53 +0300

    Linked List: Add Two Numbers II solution

--- src/medium-collection/linked-list/add-two-numbers-II/solution.ts
+++ src/medium-collection/linked-list/add-two-numbers-II/solution.ts
@@ -4,5 +4,44 @@ export function addTwoNumbers(
   l1: ListNode | null,
   l2: ListNode | null,
 ): ListNode | null {
-  return null;
+  let l1Reversed = reverseList(l1);
+  let l2Reversed = reverseList(l2);
+  let result: number[] = [];
+  let index = 0;
+  while (l1Reversed || l2Reversed) {
+    let sum = 0;
+    let currentIndex = index;
+    sum =
+      (l1Reversed?.val ?? 0) +
+      (l2Reversed?.val ?? 0) +
+      (result[currentIndex] ?? 0);
+    if (sum > 9) {
+      result[currentIndex++] = sum % 10;
+      sum = Math.trunc(sum / 10);
+    }
+    result[currentIndex] = sum;
+    index++;
+    l1Reversed = l1Reversed?.next ?? null;
+    l2Reversed = l2Reversed?.next ?? null;
+  }
+  let resultList = new ListNode();
+  let head = resultList;
+  while (result.length) {
+    head.next = new ListNode(result.pop()!);
+    head = head.next;
+  }
+  return resultList.next;
+}
+
+function reverseList(list: ListNode | null): ListNode | null {
+  let prev: ListNode | null = null;
+  let current: ListNode | null = list;
+  let next: ListNode | null = list?.next ?? null;
+  while (current) {
+    next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
 }
--- src/medium-collection/linked-list/add-two-numbers-II/test.spec.ts
+++ src/medium-collection/linked-list/add-two-numbers-II/test.spec.ts
@@ -31,4 +31,24 @@ describe("Add Two Numbers II:", () => {
     const resultArray = listToArray(resultList);
     expect(resultArray).toEqual([0]);
   });
+
+  it("addTwoNumbers should return [1,1,0] for l1 = [1,0], l2 = [1,0,0]", () => {
+    const l1 = [1, 0];
+    const l2 = [1, 0, 0];
+    const list1 = arrayToList(l1);
+    const list2 = arrayToList(l2);
+    const resultList = addTwoNumbers(list1, list2);
+    const resultArray = listToArray(resultList);
+    expect(resultArray).toEqual([1, 1, 0]);
+  });
+
+  it("addTwoNumbers should return [1,0,0] for l1 = [1], l2 = [9,9]", () => {
+    const l1 = [1];
+    const l2 = [9, 9];
+    const list1 = arrayToList(l1);
+    const list2 = arrayToList(l2);
+    const resultList = addTwoNumbers(list1, list2);
+    const resultArray = listToArray(resultList);
+    expect(resultArray).toEqual([1, 0, 0]);
+  });
 });

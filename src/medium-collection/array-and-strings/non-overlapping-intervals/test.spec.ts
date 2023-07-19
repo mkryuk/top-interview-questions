9028f325e697a7b17ec55285075e3281ccdecffc
Maxim Kryuk <mkryuk@gmail.com>
2023-07-19 12:36:30 +0300

    Array and Strings: Non-overlapping Intervals init

---
+++ src/medium-collection/array-and-strings/non-overlapping-intervals/test.spec.ts
@@ -0,0 +1,33 @@
+import { eraseOverlapIntervals } from "./solution";
+
+describe("Non-overlapping Intervals:", () => {
+  it("eraseOverlapIntervals should return 1 for intervals = [[1,2],[2,3],[3,4],[1,3]]", () => {
+    const intervals = [
+      [1, 2],
+      [2, 3],
+      [3, 4],
+      [1, 3],
+    ];
+    const result = eraseOverlapIntervals(intervals);
+    expect(result).toEqual(1);
+  });
+
+  it("eraseOverlapIntervals should return 2 for intervals = [[1,2],[1,2],[1,2]]", () => {
+    const intervals = [
+      [1, 2],
+      [1, 2],
+      [1, 2],
+    ];
+    const result = eraseOverlapIntervals(intervals);
+    expect(result).toEqual(2);
+  });
+
+  it("eraseOverlapIntervals should return 0 for intervals = [[1,2],[2,3]]", () => {
+    const intervals = [
+      [1, 2],
+      [2, 3],
+    ];
+    const result = eraseOverlapIntervals(intervals);
+    expect(result).toEqual(1);
+  });
+});

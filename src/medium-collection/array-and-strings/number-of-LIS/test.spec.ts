6840c11699e16716a3ae820549926de1579029e7
Maxim Kryuk <mkryuk@gmail.com>
2023-07-21 11:27:28 +0300

    Array and Strings: Number of Longest Increasing Subsequence init

---
+++ src/medium-collection/array-and-strings/number-of-LIS/test.spec.ts
@@ -0,0 +1,15 @@
+import { findNumberOfLIS } from "./solution";
+
+describe("Number of Longest Increasing Subsequence:", () => {
+  it("findNumberOfLIS should return 2 for nums = [1,3,5,4,7]", () => {
+    const nums = [1, 3, 5, 4, 7];
+    const result = findNumberOfLIS(nums);
+    expect(result).toEqual(2);
+  });
+
+  it("findNumberOfLIS should return 5 for nums = [2,2,2,2,2]", () => {
+    const nums = [2, 2, 2, 2, 2];
+    const result = findNumberOfLIS(nums);
+    expect(result).toEqual(5);
+  });
+});

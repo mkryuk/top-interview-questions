bca3cf2c09b9a19cbf7490e060a53eb4d37c277a
Maxim Kryuk <mkryuk@gmail.com>
2023-07-26 14:30:56 +0300

    Array and Strings: Peak Index in a Mountain Array solution

--- src/medium-collection/array-and-strings/peak-index-in-mountain-array/solution.ts
+++ src/medium-collection/array-and-strings/peak-index-in-mountain-array/solution.ts
@@ -1,3 +1,15 @@
 export function peakIndexInMountainArray(arr: number[]): number {
+  let left = 1;
+  let right = arr.length - 1;
+  while (left <= right) {
+    let mid = left + Math.trunc((right - left) / 2);
+    if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) {
+      return mid;
+    } else if (arr[mid - 1] < arr[mid] && arr[mid] < arr[mid + 1]) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
   return 0;
 }

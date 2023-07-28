2c9a4bca930e198e23202b3ce613a5cfbbc6b327
Maxim Kryuk <mkryuk@gmail.com>
2023-07-28 21:54:27 +0300

    Array and Strings: Minimum Speed to Arrive on Time solution

--- src/medium-collection/array-and-strings/min-speed-on-time/solution.ts
+++ src/medium-collection/array-and-strings/min-speed-on-time/solution.ts
@@ -1,3 +1,20 @@
 export function minSpeedOnTime(dist: number[], hour: number): number {
-  return 0;
+  let left = 0;
+  let right = 1e7;
+  let result = -1;
+  while (left <= right) {
+    let mid = left + Math.trunc((right - left) / 2);
+    let resHour = 0;
+    for (let i = 0; i < dist.length - 1; i++) {
+      resHour += Math.ceil(dist[i] / mid);
+    }
+    resHour += dist[dist.length - 1] / mid;
+    if (resHour <= hour) {
+      result = result === -1 ? mid : Math.min(result, mid);
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return result;
 }
--- src/medium-collection/array-and-strings/min-speed-on-time/test.spec.ts
+++ src/medium-collection/array-and-strings/min-speed-on-time/test.spec.ts
@@ -12,7 +12,7 @@ describe("Minimum Speed to Arrive on Time:", () => {
     const dist = [1, 3, 2];
     const hour = 2.7;
     const result = minSpeedOnTime(dist, hour);
-    expect(result).toEqual(1);
+    expect(result).toEqual(3);
   });
 
   it("minSpeedOnTime should return -1 for dist = [1,3,2], hour = 1.9", () => {
@@ -20,5 +20,12 @@ describe("Minimum Speed to Arrive on Time:", () => {
     const hour = 1.9;
     const result = minSpeedOnTime(dist, hour);
     expect(result).toEqual(-1);
+  });
+
+  it("minSpeedOnTime should return 10000000 for dist = [1,1,100000], hour = 2.01", () => {
+    const dist = [1, 1, 100000];
+    const hour = 2.01;
+    const result = minSpeedOnTime(dist, hour);
+    expect(result).toEqual(10000000);
   });
 });

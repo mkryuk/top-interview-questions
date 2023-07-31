5f67f6cf6555100fa1ff9c1388c49a0206cf92f6
Maxim Kryuk <mkryuk@gmail.com>
2023-08-01 00:51:07 +0300

    Array and Strings: Least Number of Unique Integers after K Removals solution

--- src/medium-collection/array-and-strings/least-number-of-unique-integers/solution.ts
+++ src/medium-collection/array-and-strings/least-number-of-unique-integers/solution.ts
@@ -1,3 +1,19 @@
 export function findLeastNumOfUniqueInts(arr: number[], k: number): number {
-  return 0;
+  let map = new Map<number, number>();
+  for (let i = 0; i < arr.length; i++) {
+    if (!map.has(arr[i])) {
+      map.set(arr[i], 0);
+    }
+    let count = map.get(arr[i])!;
+    map.set(arr[i], count + 1);
+  }
+  let sortedMap = [...Array.from(map)].sort((a, b) => a[1] - b[1]);
+  let index = 0;
+  while (k-- > 0) {
+    sortedMap[index][1]--;
+    if (sortedMap[index][1] === 0) {
+      index++;
+    }
+  }
+  return sortedMap.length - index;
 }

710ae4323ba544245998463a7819806d8d8dddbb
Maxim Kryuk <mkryuk@gmail.com>
2023-08-08 12:20:49 +0300

    Sorting and Searching: Search a 2D Matrix solution

--- src/medium-collection/sorting-and-searching/search-2d-matrix/solution.ts
+++ src/medium-collection/sorting-and-searching/search-2d-matrix/solution.ts
@@ -1,3 +1,38 @@
 export function searchMatrix(matrix: number[][], target: number): boolean {
+  const m = matrix.length;
+  const n = matrix[0].length;
+  let start = 0;
+  let end = m;
+  let row = -1;
+  // search the row we should start from
+  while (start <= end && start < m) {
+    let tempRow = start + Math.trunc((end - start) / 2);
+    if (matrix[tempRow][0] > target) {
+      end = tempRow - 1;
+    } else {
+      row = tempRow;
+      start = tempRow + 1;
+    }
+  }
+
+  // if there is no row that starts from the number
+  // that less or equal to the target return false
+  if (row === -1) {
+    return false;
+  }
+
+  start = 0;
+  end = n;
+  // search the target in the row
+  while (start <= end && start < n) {
+    let col = start + Math.trunc((end - start) / 2);
+    if (matrix[row][col] > target) {
+      end = col - 1;
+    } else if (matrix[row][col] < target) {
+      start = col + 1;
+    } else {
+      return true;
+    }
+  }
   return false;
 }
--- src/medium-collection/sorting-and-searching/search-2d-matrix/test.spec.ts
+++ src/medium-collection/sorting-and-searching/search-2d-matrix/test.spec.ts
@@ -11,6 +11,7 @@ describe("Search a 2D Matrix:", () => {
     const result = searchMatrix(matrix, target);
     expect(result).toBeTrue();
   });
+
   it("searchMatrix should return false for matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13", () => {
     const matrix = [
       [1, 3, 5, 7],
@@ -18,6 +19,52 @@ describe("Search a 2D Matrix:", () => {
       [23, 30, 34, 60],
     ];
     const target = 13;
+    const result = searchMatrix(matrix, target);
+    expect(result).toBeFalse();
+  });
+
+  it("searchMatrix should return true for matrix = [[1]], target = 1", () => {
+    const matrix = [[1]];
+    const target = 1;
+    const result = searchMatrix(matrix, target);
+    expect(result).toBeTrue();
+  });
+
+  it("searchMatrix should return true for matrix = [[1,3]], target = 3", () => {
+    const matrix = [[1, 3]];
+    const target = 3;
+    const result = searchMatrix(matrix, target);
+    expect(result).toBeTrue();
+  });
+
+  it("searchMatrix should return true for matrix = [[1,2,3],[4,5,6],[7,8,9]] target = 9", () => {
+    const matrix = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+    const target = 9;
+    const result = searchMatrix(matrix, target);
+    expect(result).toBeTrue();
+  });
+
+  it("searchMatrix should return true for matrix = [[1,2,3],[4,5,6],[7,8,9]] target = 1", () => {
+    const matrix = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+    const target = 1;
+    const result = searchMatrix(matrix, target);
+    expect(result).toBeTrue();
+  });
+
+  it("searchMatrix should return false for matrix = [[5,6,7],[8,9,10]] target = 1", () => {
+    const matrix = [
+      [5, 6, 7],
+      [8, 9, 10],
+    ];
+    const target = 1;
     const result = searchMatrix(matrix, target);
     expect(result).toBeFalse();
   });

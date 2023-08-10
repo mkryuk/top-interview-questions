92a89457c8ec69eea81bd6a50c67a7bfcc9fc9ba
Maxim Kryuk <mkryuk@gmail.com>
2023-08-10 23:43:01 +0300

    Array and Strings: Minimize the Maximum Difference of Pairs solution

--- src/medium-collection/array-and-strings/minimize-max/solution.ts
+++ src/medium-collection/array-and-strings/minimize-max/solution.ts
@@ -1,3 +1,27 @@
 export function minimizeMax(nums: number[], p: number): number {
-  return 0;
+  nums.sort((a, b) => a - b);
+  let left = 0;
+  let right = nums[nums.length - 1] - nums[0];
+  while (left < right) {
+    let mid = left + Math.trunc((right - left) / 2);
+    if (countPairs(nums, mid) >= p) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return left;
+}
+
+function countPairs(nums: number[], threshold: number): number {
+  let count = 0;
+  let index = 0;
+  while (index < nums.length - 1) {
+    if (nums[index + 1] - nums[index] <= threshold) {
+      count++;
+      index++;
+    }
+    index++;
+  }
+  return count;
 }
--- src/medium-collection/array-and-strings/minimize-max/test.spec.ts
+++ src/medium-collection/array-and-strings/minimize-max/test.spec.ts
@@ -14,4 +14,32 @@ describe("Minimize the Maximum Difference of Pairs:", () => {
     const result = minimizeMax(nums, p);
     expect(result).toEqual(0);
   });
+
+  it("minimizeMax should return 0 for nums = [0,5,3,4], p = 0", () => {
+    const nums = [0, 5, 3, 4];
+    const p = 0;
+    const result = minimizeMax(nums, p);
+    expect(result).toEqual(0);
+  });
+
+  it("minimizeMax should return 1 for nums = [3,4,2,3,2,1,2], p = 3", () => {
+    const nums = [3, 4, 2, 3, 2, 1, 2];
+    const p = 3;
+    const result = minimizeMax(nums, p);
+    expect(result).toEqual(1);
+  });
+
+  it("minimizeMax should return 1 for nums = [3,3,5,1,0,5,6,6], p = 4", () => {
+    const nums = [3, 3, 5, 1, 0, 5, 6, 6];
+    const p = 4;
+    const result = minimizeMax(nums, p);
+    expect(result).toEqual(1);
+  });
+
+  it("minimizeMax should return 1 for nums = [3,0,5,0,0,1,6], p = 3", () => {
+    const nums = [3, 0, 5, 0, 0, 1, 6];
+    const p = 3;
+    const result = minimizeMax(nums, p);
+    expect(result).toEqual(1);
+  });
 });

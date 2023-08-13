c064dcd647ebe8e3f145d5dd87a113ff3dde4e3a
Maxim Kryuk <mkryuk@gmail.com>
2023-08-13 16:27:23 +0300

    Dynamic Programming: Coin Change II added top-down solution

--- src/medium-collection/dynamic-programming/coin-change-II/solution.ts
+++ src/medium-collection/dynamic-programming/coin-change-II/solution.ts
@@ -1,3 +1,29 @@
 export function change(amount: number, coins: number[]): number {
-  return 0;
+  const memo = Array.from({ length: coins.length }, () =>
+    new Array(amount + 1).fill(-1),
+  );
+
+  return coinsCombinationsNumber(0, coins, amount, memo);
+}
+
+function coinsCombinationsNumber(
+  i: number,
+  coins: number[],
+  amount: number,
+  memo: number[][],
+): number {
+  if (amount === 0) {
+    return 1;
+  } else if (i === coins.length) {
+    return 0;
+  } else if (memo[i][amount] !== -1) {
+    return memo[i][amount];
+  } else if (coins[i] > amount) {
+    memo[i][amount] = coinsCombinationsNumber(i + 1, coins, amount, memo);
+    return memo[i][amount];
+  }
+  memo[i][amount] =
+    coinsCombinationsNumber(i, coins, amount - coins[i], memo) +
+    coinsCombinationsNumber(i + 1, coins, amount, memo);
+  return memo[i][amount];
 }

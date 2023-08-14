a1dc049372a953985e4253109a5b765b0a6cc18a
Maxim Kryuk <mkryuk@gmail.com>
2023-08-14 23:06:03 +0300

    Dynamic Programming: Coin Change II added bottom-up solution

---
+++ src/medium-collection/dynamic-programming/coin-change-II/solution-bottom-up.ts
@@ -0,0 +1,18 @@
+export function change(amount: number, coins: number[]): number {
+  let memo = Array.from({ length: coins.length + 1 }, () =>
+    new Array(amount + 1).fill(0),
+  );
+  for (let i = 0; i < coins.length; i++) {
+    memo[i][0] = 1;
+  }
+  for (let i = coins.length - 1; i >= 0; i--) {
+    for (let j = 1; j <= amount; j++) {
+      if (coins[i] > j) {
+        memo[i][j] = memo[i + 1][j];
+      } else {
+        memo[i][j] = memo[i][j - coins[i]] + memo[i + 1][j];
+      }
+    }
+  }
+  return memo[0][amount];
+}

3cb1600a95f783d8173229a55bb98c4315594d95
Maxim Kryuk <mkryuk@gmail.com>
2023-08-16 19:29:48 +0300

    Linked List: Partition List solution

--- src/medium-collection/linked-list/partition-list/solution.ts
+++ src/medium-collection/linked-list/partition-list/solution.ts
@@ -1,5 +1,28 @@
 import { ListNode } from "../common";
 
 export function partition(head: ListNode | null, x: number): ListNode | null {
-  return null;
+  let left = new ListNode();
+  let right = new ListNode();
+  let headNode = head;
+  // create two partitions for storing nodes
+  let leftNode = left;
+  let rightNode = right;
+  while (headNode) {
+    // take current node
+    const node = headNode;
+    // move head forward
+    headNode = headNode.next;
+    // isolate current node
+    node.next = null;
+    if (node.val < x) {
+      leftNode.next = node;
+      leftNode = leftNode.next;
+    } else {
+      rightNode.next = node;
+      rightNode = rightNode.next;
+    }
+  }
+  // link two partitions together
+  leftNode.next = right.next;
+  return left.next;
 }

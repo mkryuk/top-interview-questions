d036217aa9f7d381f494ed6686acd242ee175db9
Maxim Kryuk <mkryuk@gmail.com>
2023-08-18 15:41:30 +0300

    Array and Strings: 01 Matrix solution

--- src/medium-collection/array-and-strings/binary-matrix/solution.ts
+++ src/medium-collection/array-and-strings/binary-matrix/solution.ts
@@ -1,3 +1,45 @@
 export function updateMatrix(mat: number[][]): number[][] {
-  return [];
+  const m = mat.length;
+  const n = mat[0].length;
+  const dirs = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+  ]; // up, down, left, right
+  let result = Array.from({ length: m }, () =>
+    new Array<number>(n).fill(Number.MAX_SAFE_INTEGER),
+  );
+  let queue: [number, number][] = [];
+
+  // Add all 0's to the queue and initialize result matrix
+  for (let x = 0; x < m; x++) {
+    for (let y = 0; y < n; y++) {
+      if (mat[x][y] === 0) {
+        queue.push([x, y]);
+        result[x][y] = 0;
+      }
+    }
+  }
+
+  while (queue.length > 0) {
+    let [x, y] = queue.shift()!;
+    for (const [dx, dy] of dirs) {
+      const newX = x + dx;
+      const newY = y + dy;
+      // Check boundaries and result value
+      if (
+        newX >= 0 &&
+        newX < m &&
+        newY >= 0 &&
+        newY < n &&
+        result[newX][newY] > result[x][y] + 1
+      ) {
+        result[newX][newY] = result[x][y] + 1;
+        queue.push([newX, newY]);
+      }
+    }
+  }
+
+  return result;
 }
--- src/medium-collection/array-and-strings/binary-matrix/test.spec.ts
+++ src/medium-collection/array-and-strings/binary-matrix/test.spec.ts
@@ -28,4 +28,80 @@ describe("01 Matrix:", () => {
       [1, 2, 1],
     ]);
   });
+
+  it("updateMatrix should return [[0,1,2],[1,2,3],[2,3,4]] for mat = [[0,1,1],[1,1,1],[1,1,1]]", () => {
+    const mat = [
+      [0, 1, 1],
+      [1, 1, 1],
+      [1, 1, 1],
+    ];
+    const result = updateMatrix(mat);
+    expect(result).toEqual([
+      [0, 1, 2],
+      [1, 2, 3],
+      [2, 3, 4],
+    ]);
+  });
+
+  it("updateMatrix should return [[0,1,2],[1,2,3],[2,1,2],[1,0,1]] for mat = [[0,1,1],[1,1,1],[1,1,1],[1,0,1]]", () => {
+    const mat = [
+      [0, 1, 1],
+      [1, 1, 1],
+      [1, 1, 1],
+      [1, 0, 1],
+    ];
+    const result = updateMatrix(mat);
+    expect(result).toEqual([
+      [0, 1, 2],
+      [1, 2, 3],
+      [2, 1, 2],
+      [1, 0, 1],
+    ]);
+  });
+
+  it("updateMatrix should return [[4,3,4],[3,2,3],[2,1,2],[1,0,1]] for mat = [[1,1,1],[1,1,1],[1,1,1],[1,0,1]]", () => {
+    const mat = [
+      [1, 1, 1],
+      [1, 1, 1],
+      [1, 1, 1],
+      [1, 0, 1],
+    ];
+    const result = updateMatrix(mat);
+    expect(result).toEqual([
+      [4, 3, 4],
+      [3, 2, 3],
+      [2, 1, 2],
+      [1, 0, 1],
+    ]);
+  });
+
+  it("updateMatrix should return [[5,4,3],[4,3,2],[3,2,1],[2,1,0]] for mat = [[1,1,1],[1,1,1],[1,1,1],[1,1,0]]", () => {
+    const mat = [
+      [1, 1, 1],
+      [1, 1, 1],
+      [1, 1, 1],
+      [1, 1, 0],
+    ];
+    const result = updateMatrix(mat);
+    expect(result).toEqual([
+      [5, 4, 3],
+      [4, 3, 2],
+      [3, 2, 1],
+      [2, 1, 0],
+    ]);
+  });
+
+  it("updateMatrix should return [[0,1,2],[1,0,1],[2,1,0]] for mat = [[0,1,1],[1,0,1],[1,1,0]]", () => {
+    const mat = [
+      [0, 1, 1],
+      [1, 0, 1],
+      [1, 1, 0],
+    ];
+    const result = updateMatrix(mat);
+    expect(result).toEqual([
+      [0, 1, 2],
+      [1, 0, 1],
+      [2, 1, 0],
+    ]);
+  });
 });

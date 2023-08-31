ee88b97dc3457cb36f05cfbb61b230e457c85e0d
Maxim Kryuk <mkryuk@gmail.com>
2023-08-31 20:07:51 +0300

    Array and Strings: Minimum Penalty for a Shop solution

--- src/medium-collection/array-and-strings/best-closing-time/solution.ts
+++ src/medium-collection/array-and-strings/best-closing-time/solution.ts
@@ -1,3 +1,24 @@
 export function bestClosingTime(customers: string): number {
-  return 0;
+  let yCount = new Array(customers.length + 1).fill(0);
+  for (let i = customers.length - 1; i >= 0; i--) {
+    if (customers[i] === "Y") {
+      yCount[i] = yCount[i + 1] + 1;
+    } else {
+      yCount[i] = yCount[i + 1];
+    }
+  }
+
+  let nCount = 0;
+  let result = 0;
+  let penalty = yCount[0];
+  for (let i = 1; i < customers.length + 1; i++) {
+    if (customers[i - 1] === "N") {
+      nCount++;
+    }
+    if (penalty > yCount[i] + nCount) {
+      penalty = yCount[i] + nCount;
+      result = i;
+    }
+  }
+  return result;
 }
--- src/medium-collection/array-and-strings/best-closing-time/test.spec.ts
+++ src/medium-collection/array-and-strings/best-closing-time/test.spec.ts
@@ -10,12 +10,18 @@ describe("Minimum Penalty for a Shop:", () => {
   it('bestClosingTime should return 0 for customers = "NNNNN"', () => {
     const customers = "NNNNN";
     const result = bestClosingTime(customers);
-    expect(result).toEqual(2);
+    expect(result).toEqual(0);
   });
 
   it('bestClosingTime should return 4 for customers = "YYYY"', () => {
     const customers = "YYYY";
     const result = bestClosingTime(customers);
     expect(result).toEqual(4);
+  });
+
+  it('bestClosingTime should return 6 for customers = "YNNYYYNYNY"', () => {
+    const customers = "YNNYYYNYNY";
+    const result = bestClosingTime(customers);
+    expect(result).toEqual(6);
   });
 });

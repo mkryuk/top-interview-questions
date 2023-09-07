9b814002a40baf73cc1af55c74f13808d801d96e
Maxim Kryuk <mkryuk@gmail.com>
2023-09-07 22:29:34 +0300

    Array and Strings: Divide Array in Sets of K Consecutive Numbers solution

--- src/medium-collection/array-and-strings/hand-of-straights/solution.ts
+++ src/medium-collection/array-and-strings/hand-of-straights/solution.ts
@@ -1,3 +1,37 @@
 export function isNStraightHand(hand: number[], groupSize: number): boolean {
-  return false;
+  const cardCount: { [key: number]: number } = {};
+
+  // 1. Count occurrences of each card.
+  for (const card of hand) {
+    cardCount[card] = (cardCount[card] || 0) + 1;
+  }
+
+  // 2. Sort the unique values.
+  const uniqueCards = Object.keys(cardCount)
+    .map(Number)
+    .sort((a, b) => a - b);
+
+  // 3. Check for consecutive groups.
+  for (const card of uniqueCards) {
+    const count = cardCount[card];
+
+    if (count > 0) {
+      // Iterate for the groupSize, starting from the current card.
+      for (let i = 1; i < groupSize; i++) {
+        // If the next card (card + i) does not exist or
+        // its count is less than the current card's count,
+        // it's impossible to form a group. So, return false.
+        if (!cardCount[card + i] || cardCount[card + i] < count) {
+          return false;
+        }
+
+        // If the card exists and its count is sufficient,
+        // reduce its count by the current card's count
+        // since we are using these cards to form a group.
+        cardCount[card + i] -= count;
+      }
+    }
+  }
+
+  return true;
 }

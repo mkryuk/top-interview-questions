b56f8129e571828a244b8271501c509eaf7e8718
Maxim Kryuk <mkryuk@gmail.com>
2023-09-08 18:51:47 +0300

    Math: Sum Multiples solution

--- src/easy-collection/math/sum-multiples/solution.ts
+++ src/easy-collection/math/sum-multiples/solution.ts
@@ -1,3 +1,9 @@
 export function sumOfMultiples(n: number): number {
-  return 0;
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 || i % 5 === 0 || i % 7 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
 }

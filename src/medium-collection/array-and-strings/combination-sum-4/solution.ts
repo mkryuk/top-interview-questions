cb6508f395f5821559a8ca224a89baea677c2e23
Maxim Kryuk <mkryuk@gmail.com>
2023-09-09 13:31:38 +0300

    Array and Strings: Combination Sum IV init

---
+++ src/medium-collection/array-and-strings/combination-sum-4/solution.ts
@@ -0,0 +1,3 @@
+export function combinationSum4(nums: number[], target: number): number {
+  return 0;
+}
---
+++ src/medium-collection/array-and-strings/combination-sum-4/test.spec.ts
@@ -0,0 +1,17 @@
+import { combinationSum4 } from "./solution";
+
+describe("Combination Sum IV:", () => {
+  it("combinationSum4 should return 7 for nums = [1,2,3], target = 4", () => {
+    const nums = [1, 2, 3];
+    const target = 4;
+    const result = combinationSum4(nums, target);
+    expect(result).toEqual(7);
+  });
+
+  it("combinationSum4 should return 0 for nums = [9], target = 3", () => {
+    const nums = [9];
+    const target = 3;
+    const result = combinationSum4(nums, target);
+    expect(result).toEqual(0);
+  });
+});

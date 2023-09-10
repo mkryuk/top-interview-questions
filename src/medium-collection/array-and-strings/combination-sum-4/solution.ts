2809bca1a4c6653cf09d34b0be56c7dde938c58c
Maxim Kryuk <mkryuk@gmail.com>
2023-09-10 19:28:40 +0300

    Array and Strings: Combination Sum IV solution

--- src/medium-collection/array-and-strings/combination-sum-4/solution.ts
+++ src/medium-collection/array-and-strings/combination-sum-4/solution.ts
@@ -1,3 +1,23 @@
 export function combinationSum4(nums: number[], target: number): number {
-  return 0;
+  let map = new Map<number, number>();
+  dfs(nums, target, map);
+  return map.get(target)!;
+}
+
+function dfs(nums: number[], target: number, map: Map<number, number>): number {
+  if (target < 0) {
+    return 0;
+  }
+  if (map.has(target)) {
+    return map.get(target)!;
+  }
+  if (target === 0) {
+    return 1;
+  }
+  let result = 0;
+  for (let i = 0; i < nums.length; i++) {
+    result += dfs(nums, target - nums[i], map);
+  }
+  map.set(target, result);
+  return result;
 }
--- src/medium-collection/array-and-strings/combination-sum-4/test.spec.ts
+++ src/medium-collection/array-and-strings/combination-sum-4/test.spec.ts
@@ -14,4 +14,11 @@ describe("Combination Sum IV:", () => {
     const result = combinationSum4(nums, target);
     expect(result).toEqual(0);
   });
+
+  it("combinationSum4 should return 0 for nums = [4,2,1], target = 32", () => {
+    const nums = [4, 2, 1];
+    const target = 32;
+    const result = combinationSum4(nums, target);
+    expect(result).toEqual(39882198);
+  });
 });

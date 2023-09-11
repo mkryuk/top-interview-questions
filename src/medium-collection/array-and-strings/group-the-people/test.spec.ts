b2170e9ba0103dbd59fc980f760faae28038db68
Maxim Kryuk <mkryuk@gmail.com>
2023-09-11 18:40:41 +0300

    Array and Strings: Group the People Given the Group Size They Belong To init

---
+++ src/medium-collection/array-and-strings/group-the-people/test.spec.ts
@@ -0,0 +1,15 @@
+import { groupThePeople } from "./solution";
+
+describe("Group the People Given the Group Size They Belong To:", () => {
+  it("groupThePeople should return [[5],[0,1,2],[3,4,6]] for groupSizes = [3,3,3,3,3,1,3]", () => {
+    const groupSizes = [3, 3, 3, 3, 3, 1, 3];
+    const result = groupThePeople(groupSizes);
+    expect(result).toEqual([[5], [0, 1, 2], [3, 4, 6]]);
+  });
+
+  it("groupThePeople should return [[1],[0,5],[2,3,4]] for groupSizes = [2,1,3,3,3,2]", () => {
+    const groupSizes = [2, 1, 3, 3, 3, 2];
+    const result = groupThePeople(groupSizes);
+    expect(result).toEqual([[1], [0, 5], [2, 3, 4]]);
+  });
+});

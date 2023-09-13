09624e2dea258aea256ffe09323f586c0627a111
Maxim Kryuk <mkryuk@gmail.com>
2023-09-13 17:58:05 +0300

    Array and Strings: Difference Between Element Sum and Digit Sum of an Array solution

--- src/easy-collection/array/difference-of-sum/solution.ts
+++ src/easy-collection/array/difference-of-sum/solution.ts
@@ -1,3 +1,18 @@
 export function differenceOfSum(nums: number[]): number {
-  return 0;
+  let elementSum = 0;
+  let digitSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    digitSum += getDIgitSum(nums[i]);
+    elementSum += nums[i];
+  }
+  return Math.abs(digitSum - elementSum);
+}
+
+function getDIgitSum(number: number): number {
+  let result = 0;
+  while (number > 0) {
+    result += number % 10;
+    number = Math.trunc(number / 10);
+  }
+  return result;
 }

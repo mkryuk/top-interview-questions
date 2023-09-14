8b1ef20cf4d0276107dc3f65b6cbbad0fd6917d0
Maxim Kryuk <mkryuk@gmail.com>
2023-09-14 14:15:45 +0300

    Array and Strings: Number of Ways to Split Array init

---
+++ src/medium-collection/array-and-strings/ways-to-split-array/solution.ts
@@ -0,0 +1,3 @@
+export function waysToSplitArray(nums: number[]): number {
+  return 0;
+}
---
+++ src/medium-collection/array-and-strings/ways-to-split-array/test.spec.ts
@@ -0,0 +1,15 @@
+import { waysToSplitArray } from "./solution";
+
+describe("Number of Ways to Split Array:", () => {
+  it("waysToSplitArray should return 2 for nums = [10,4,-8,7]", () => {
+    const nums = [10, 4, -8, 7];
+    const result = waysToSplitArray(nums);
+    expect(result).toEqual(2);
+  });
+
+  it("waysToSplitArray should return 2 for nums = [2,3,1,0]", () => {
+    const nums = [2, 3, 1, 0];
+    const result = waysToSplitArray(nums);
+    expect(result).toEqual(2);
+  });
+});

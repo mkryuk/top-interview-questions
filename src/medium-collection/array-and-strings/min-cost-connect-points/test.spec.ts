1da548bf8e8ad59bddeff65740040b59c9e70d4f
Maxim Kryuk <mkryuk@gmail.com>
2023-09-16 20:45:57 +0300

    Array and Strings: Min Cost to Connect All Points added test

--- src/medium-collection/array-and-strings/min-cost-connect-points/test.spec.ts
+++ src/medium-collection/array-and-strings/min-cost-connect-points/test.spec.ts
@@ -22,4 +22,16 @@ describe("Min Cost to Connect All Points:", () => {
     const result = minCostConnectPoints(points);
     expect(result).toEqual(18);
   });
+
+  it("minCostConnectPoints should return 16 for points = [[0,0],[2,2],[0,4],[4,0],[4,4]]", () => {
+    const points = [
+      [0, 0],
+      [2, 2],
+      [0, 4],
+      [4, 0],
+      [4, 4],
+    ];
+    const result = minCostConnectPoints(points);
+    expect(result).toEqual(16);
+  });
 });

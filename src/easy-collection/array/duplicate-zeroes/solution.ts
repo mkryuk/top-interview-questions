07d6e2b5ad574452dde264563afe4dccb4b1b7bd
Maxim Kryuk <mkryuk@gmail.com>
2023-09-20 16:07:34 +0300

    Array and Strings: Duplicate Zeros solution

--- src/easy-collection/array/duplicate-zeroes/solution.ts
+++ src/easy-collection/array/duplicate-zeroes/solution.ts
@@ -1,1 +1,10 @@
-export function duplicateZeros(arr: number[]): void {}
+export function duplicateZeros(arr: number[]): void {
+  let n = arr.length;
+  for (let i = 0; i < n; i++) {
+    if (arr[i] === 0) {
+      arr.splice(i, 0, 0);
+      i++;
+    }
+  }
+  arr.splice(n);
+}

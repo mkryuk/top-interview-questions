272e5929c82459527ed72f78667a4b1b8eb005e7
Maxim Kryuk <mkryuk@gmail.com>
2023-09-21 15:12:22 +0300

    Linked List: Remove Duplicates from Sorted List solution

--- src/easy-collection/linked-list/delete-duplicates/solution.ts
+++ src/easy-collection/linked-list/delete-duplicates/solution.ts
@@ -1,5 +1,18 @@
 import { ListNode } from "../common";
 
 export function deleteDuplicates(head: ListNode | null): ListNode | null {
-  return null;
+  if (head === null) {
+    return null;
+  }
+  let result = new ListNode(head.val);
+  let currentNode = result;
+  while (head !== null) {
+    if (head.val !== currentNode.val) {
+      const node = new ListNode(head.val);
+      currentNode.next = node;
+      currentNode = node;
+    }
+    head = head.next;
+  }
+  return result;
 }

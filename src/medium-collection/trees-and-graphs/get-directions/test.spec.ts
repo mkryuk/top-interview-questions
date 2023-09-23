aaaaacbf5a1e4d02fecd48adcd26dd3a10102640
Maxim Kryuk <mkryuk@gmail.com>
2023-09-23 12:53:41 +0300

    Trees and Graphs: Step-By-Step Directions From a Binary Tree Node to Another added test

--- src/medium-collection/trees-and-graphs/get-directions/test.spec.ts
+++ src/medium-collection/trees-and-graphs/get-directions/test.spec.ts
@@ -19,4 +19,31 @@ describe("Step-By-Step Directions From a Binary Tree Node to Another:", () => {
     const result = getDirections(root, startValue, destValue);
     expect(result).toEqual("L");
   });
+
+  it('getDirections should return "R" for root = [2,null,1], startValue = 2, destValue = 1', () => {
+    const nodes = [2, null, 1];
+    const startValue = 2;
+    const destValue = 1;
+    const root = arrayToTreeNode(nodes);
+    const result = getDirections(root, startValue, destValue);
+    expect(result).toEqual("R");
+  });
+
+  it('getDirections should return "U" for root = [5,1,2,3,null,6,4], startValue = 3, destValue = 1', () => {
+    const nodes = [5, 1, 2, 3, null, 6, 4];
+    const startValue = 3;
+    const destValue = 1;
+    const root = arrayToTreeNode(nodes);
+    const result = getDirections(root, startValue, destValue);
+    expect(result).toEqual("U");
+  });
+
+  it('getDirections should return "UULL" for root = [5,1,2,3,null,6,4], startValue = 4, destValue = 3', () => {
+    const nodes = [5, 1, 2, 3, null, 6, 4];
+    const startValue = 4;
+    const destValue = 3;
+    const root = arrayToTreeNode(nodes);
+    const result = getDirections(root, startValue, destValue);
+    expect(result).toEqual("UULL");
+  });
 });

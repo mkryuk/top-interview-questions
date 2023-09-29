bb83b0b6bd74c4bf4d068ac2cf64ce8a5dc421bf
Maxim Kryuk <mkryuk@gmail.com>
2023-09-29 14:26:43 +0300

    Array and Strings: Unique Length-3 Palindromic Subsequences solution

--- src/medium-collection/array-and-strings/count-palindromic-subsequence/solution.ts
+++ src/medium-collection/array-and-strings/count-palindromic-subsequence/solution.ts
@@ -1,3 +1,18 @@
 export function countPalindromicSubsequence(s: string): number {
-  return 0;
+  let map = new Map<string, number[]>();
+  for (let i = 0; i < s.length; i++) {
+    if (!map.has(s[i])) {
+      map.set(s[i], []);
+    }
+    map.get(s[i])!.push(i);
+  }
+  let arr = Array.from(map);
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let [_, positions] = arr[i];
+    let first = positions[0];
+    let last = positions[positions.length - 1];
+    result += new Set(s.slice(first + 1, last)).size;
+  }
+  return result;
 }

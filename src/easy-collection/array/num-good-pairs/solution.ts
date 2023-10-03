fdf9c75adc681fa44575ca59f940fea629749d92
Maxim Kryuk <mkryuk@gmail.com>
2023-10-03 23:27:22 +0300

    Array and Strings: Number of Good Pairs solution

--- src/easy-collection/array/num-good-pairs/solution.ts
+++ src/easy-collection/array/num-good-pairs/solution.ts
@@ -1,3 +1,16 @@
 export function numIdenticalPairs(nums: number[]): number {
-  return 0;
+  let map = new Map<number, number>();
+  for (const item of nums) {
+    if (!map.has(item)) {
+      map.set(item, 0);
+    }
+    map.set(item, map.get(item)! + 1);
+  }
+  let result = 0;
+  map.forEach((value) => {
+    if (value > 1) {
+      result += Math.trunc((value * (value - 1)) / 2);
+    }
+  });
+  return result;
 }

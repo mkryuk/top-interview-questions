e028e6d102d8a9de6c50b08eb92300c74f430fad
Maxim Kryuk <mkryuk@gmail.com>
2023-10-05 22:16:11 +0300

    Array and Strings: Last Stone Weight II added test

--- src/medium-collection/array-and-strings/last-stone-weight-II/test.spec.ts
+++ src/medium-collection/array-and-strings/last-stone-weight-II/test.spec.ts
@@ -12,4 +12,22 @@ describe("Last Stone Weight II:", () => {
     const result = lastStoneWeightII(stones);
     expect(result).toEqual(5);
   });
+
+  it("lastStoneWeightII should return 6 for stones = [32,26,33,21,40]", () => {
+    const stones = [32, 26, 33, 21, 40];
+    const result = lastStoneWeightII(stones);
+    expect(result).toEqual(6);
+  });
+
+  it("lastStoneWeightII should return 6 for stones = [34,26,33,21,40]", () => {
+    const stones = [34, 26, 33, 21, 40];
+    const result = lastStoneWeightII(stones);
+    expect(result).toEqual(6);
+  });
+
+  it("lastStoneWeightII should return 1 for stones = [40,33,21,13]", () => {
+    const stones = [40, 33, 21, 13];
+    const result = lastStoneWeightII(stones);
+    expect(result).toEqual(1);
+  });
 });

2b68c91079bf4a88f43898788fb7306289bb2f62
Maxim Kryuk <mkryuk@gmail.com>
2023-10-08 19:58:08 +0300

    Array and Strings: Swap Adjacent in LR String added tests

--- src/medium-collection/array-and-strings/can-transform/test.spec.ts
+++ src/medium-collection/array-and-strings/can-transform/test.spec.ts
@@ -8,10 +8,45 @@ describe("Swap Adjacent in LR String:", () => {
     expect(result).toBeTrue();
   });
 
+  it('canTransform should return false for start = "XRLXXRRLX", end = "RXXLRXRXL"', () => {
+    const start = "XRLXXRRLX";
+    const end = "RXXLRXRXL";
+    const result = canTransform(start, end);
+    expect(result).toBeFalse();
+  });
+
   it('canTransform should return false for start = "X", end = "L"', () => {
     const start = "X";
     const end = "L";
     const result = canTransform(start, end);
     expect(result).toBeFalse();
+  });
+
+  it('canTransform should return false for start = "RXR", end = "XXR"', () => {
+    const start = "RXR";
+    const end = "XXR";
+    const result = canTransform(start, end);
+    expect(result).toBeFalse();
+  });
+
+  it('canTransform should return false for start = "LXXLXRLXXL", end = "XLLXRXLXLX"', () => {
+    const start = "LXXLXRLXXL";
+    const end = "XLLXRXLXLX";
+    const result = canTransform(start, end);
+    expect(result).toBeFalse();
+  });
+
+  it('canTransform should return true for start = "LXXXXL", end = "LLXXXX"', () => {
+    const start = "LXXXXL";
+    const end = "LLXXXX";
+    const result = canTransform(start, end);
+    expect(result).toBeTrue();
+  });
+
+  it('canTransform should return true for start = "RXXXXR", end = "XXXXRR"', () => {
+    const start = "RXXXXR";
+    const end = "XXXXRR";
+    const result = canTransform(start, end);
+    expect(result).toBeTrue();
   });
 });

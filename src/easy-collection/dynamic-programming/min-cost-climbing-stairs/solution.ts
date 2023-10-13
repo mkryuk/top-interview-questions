0bb05330e96f29d8c957882c14e79e1a71aace20
Maxim Kryuk <mkryuk@gmail.com>
2023-10-13 16:06:03 +0300

    Dynamic Programming: Min Cost Climbing Stairs solution

--- src/easy-collection/dynamic-programming/min-cost-climbing-stairs/solution.ts
+++ src/easy-collection/dynamic-programming/min-cost-climbing-stairs/solution.ts
@@ -1,3 +1,9 @@
 export function minCostClimbingStairs(cost: number[]): number {
-  return 0;
+  let prevCost = new Array(cost.length + 1).fill(0);
+  prevCost[0] = cost[0];
+  prevCost[1] = cost[1];
+  for (let i = 2; i <= cost.length; i++) {
+    prevCost[i] = (cost[i] ?? 0) + Math.min(prevCost[i - 1], prevCost[i - 2]);
+  }
+  return prevCost[cost.length];
 }

e3f27449a91e52f9cd42fadf28d25bd9484750e2
Maxim Kryuk <mkryuk@gmail.com>
2023-10-15 19:30:54 +0300

    Dynamic Programming: Painting the Walls added test

--- src/hard-collection/dynamic-programming/paint-walls/test.spec.ts
+++ src/hard-collection/dynamic-programming/paint-walls/test.spec.ts
@@ -14,4 +14,18 @@ describe("Painting the Walls:", () => {
     const result = paintWalls(cost, time);
     expect(result).toEqual(4);
   });
+
+  it("paintWalls should return 12 for cost = [2,3,4,2], time = [1,1,2,1]", () => {
+    const cost = [8, 7, 5, 15];
+    const time = [1, 1, 2, 1];
+    const result = paintWalls(cost, time);
+    expect(result).toEqual(12);
+  });
+
+  it("paintWalls should return 63 for cost = [42,8,28,35,21,13,21,35], time = [2,1,1,1,2,1,1,2]", () => {
+    const cost = [42, 8, 28, 35, 21, 13, 21, 35];
+    const time = [2, 1, 1, 1, 2, 1, 1, 2];
+    const result = paintWalls(cost, time);
+    expect(result).toEqual(63);
+  });
 });

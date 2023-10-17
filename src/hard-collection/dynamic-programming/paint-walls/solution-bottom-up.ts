8c5b1b614f7a9a8eb70c46b215b827845a0053a9
Maxim Kryuk <mkryuk@gmail.com>
2023-10-17 16:25:07 +0300

    Dynamic Programming: Painting the Walls bottom up solution

---
+++ src/hard-collection/dynamic-programming/paint-walls/solution-bottom-up.ts
@@ -0,0 +1,17 @@
+export function paintWalls(cost: number[], time: number[]): number {
+  const n: number = cost.length;
+  const dp: number[][] = Array.from({ length: n + 1 }, () =>
+    Array(n + 1).fill(0),
+  );
+  for (let i = 1; i <= n; i++) {
+    dp[n][i] = Infinity;
+  }
+  for (let i = n - 1; i >= 0; i--) {
+    for (let remain = 1; remain <= n; remain++) {
+      const paidPaint = cost[i] + dp[i + 1][Math.max(0, remain - 1 - time[i])];
+      const freePaint = dp[i + 1][remain];
+      dp[i][remain] = Math.min(paidPaint, freePaint);
+    }
+  }
+  return dp[0][n];
+}
---
+++ src/hard-collection/dynamic-programming/paint-walls/test-bottom-up.spec.ts
@@ -0,0 +1,31 @@
+import { paintWalls } from "./solution-bottom-up";
+
+describe("Painting the Walls:", () => {
+  it("paintWalls should return 3 for cost = [1,2,3,2], time = [1,2,3,2]", () => {
+    const cost = [1, 2, 3, 2];
+    const time = [1, 2, 3, 2];
+    const result = paintWalls(cost, time);
+    expect(result).toEqual(3);
+  });
+
+  it("paintWalls should return 4 for cost = [2,3,4,2], time = [1,1,1,1]", () => {
+    const cost = [2, 3, 4, 2];
+    const time = [1, 1, 1, 1];
+    const result = paintWalls(cost, time);
+    expect(result).toEqual(4);
+  });
+
+  it("paintWalls should return 12 for cost = [2,3,4,2], time = [1,1,2,1]", () => {
+    const cost = [8, 7, 5, 15];
+    const time = [1, 1, 2, 1];
+    const result = paintWalls(cost, time);
+    expect(result).toEqual(12);
+  });
+
+  it("paintWalls should return 63 for cost = [42,8,28,35,21,13,21,35], time = [2,1,1,1,2,1,1,2]", () => {
+    const cost = [42, 8, 28, 35, 21, 13, 21, 35];
+    const time = [2, 1, 1, 1, 2, 1, 1, 2];
+    const result = paintWalls(cost, time);
+    expect(result).toEqual(63);
+  });
+});

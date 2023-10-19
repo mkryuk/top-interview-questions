b2f57ec35d29e9ae30cc0c48025a62e512da400f
Maxim Kryuk <mkryuk@gmail.com>
2023-10-19 21:27:18 +0300

    Trees and Graphs: Validate Binary Tree Nodes solution

--- src/medium-collection/trees-and-graphs/validate-binary-tree-nodes/solution.ts
+++ src/medium-collection/trees-and-graphs/validate-binary-tree-nodes/solution.ts
@@ -3,5 +3,57 @@ export function validateBinaryTreeNodes(
   leftChild: number[],
   rightChild: number[],
 ): boolean {
-  return false;
+  let map = new Array(n).fill(0);
+  for (let i = 0; i < leftChild.length; i++) {
+    if (leftChild[i] !== -1) {
+      map[leftChild[i]]++;
+    }
+    if (rightChild[i] !== -1) {
+      map[rightChild[i]]++;
+    }
+    if (map[leftChild[i]] > 1) {
+      return false;
+    }
+    if (map[rightChild[i]] > 1) {
+      return false;
+    }
+  }
+  let zeroes = 0;
+  for (let i = 0; i < map.length; i++) {
+    if (map[i] === 0) {
+      zeroes++;
+    }
+  }
+  if (zeroes !== 1) {
+    return false;
+  }
+  // find the root
+  let root = map.indexOf(0);
+  let visited = new Array(n).fill(0);
+  dfs(root, leftChild, rightChild, visited);
+  // check if we have visited all nodes
+  for (let i = 0; i < visited.length; i++) {
+    if (visited[i] !== 1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function dfs(
+  node: number,
+  leftChild: number[],
+  rightChild: number[],
+  visited: number[],
+) {
+  if (visited[node] > 1) {
+    return;
+  }
+  visited[node]++;
+  if (leftChild[node] !== -1) {
+    dfs(leftChild[node], leftChild, rightChild, visited);
+  }
+  if (rightChild[node] !== -1) {
+    dfs(rightChild[node], leftChild, rightChild, visited);
+  }
 }
--- src/medium-collection/trees-and-graphs/validate-binary-tree-nodes/test.spec.ts
+++ src/medium-collection/trees-and-graphs/validate-binary-tree-nodes/test.spec.ts
@@ -24,4 +24,44 @@ describe("Validate Binary Tree Nodes:", () => {
     const result = validateBinaryTreeNodes(n, leftChild, rightChild);
     expect(result).toBeFalse();
   });
+
+  it("validateBinaryTreeNodes should return false for n = 4, leftChild = [1,-1,3,-1], rightChild = [-1,-1,-1,-1]", () => {
+    const n = 4;
+    const leftChild = [1, -1, 3, -1];
+    const rightChild = [-1, -1, -1, -1];
+    const result = validateBinaryTreeNodes(n, leftChild, rightChild);
+    expect(result).toBeFalse();
+  });
+
+  it("validateBinaryTreeNodes should return true for n = 4, leftChild = [1,2,3,-1], rightChild = [-1,-1,-1,-1]", () => {
+    const n = 4;
+    const leftChild = [1, 2, 3, -1];
+    const rightChild = [-1, -1, -1, -1];
+    const result = validateBinaryTreeNodes(n, leftChild, rightChild);
+    expect(result).toBeTrue();
+  });
+
+  it("validateBinaryTreeNodes should return true for n = 4, leftChild = [1,0,3,-1], rightChild = [-1,-1,-1,-1]", () => {
+    const n = 4;
+    const leftChild = [1, 0, 3, -1];
+    const rightChild = [-1, -1, -1, -1];
+    const result = validateBinaryTreeNodes(n, leftChild, rightChild);
+    expect(result).toBeFalse();
+  });
+
+  it("validateBinaryTreeNodes should return true for n = 4, leftChild = [1,-1,2,-1], rightChild = [3,-1,-1,-1]", () => {
+    const n = 4;
+    const leftChild = [1, -1, 2, -1];
+    const rightChild = [3, -1, -1, -1];
+    const result = validateBinaryTreeNodes(n, leftChild, rightChild);
+    expect(result).toBeFalse();
+  });
+
+  it("validateBinaryTreeNodes should return true for n = 4, leftChild = [3,-1,1,-1], rightChild = [-1,-1,0,-1]", () => {
+    const n = 4;
+    const leftChild = [3, -1, 1, -1];
+    const rightChild = [-1, -1, 0, -1];
+    const result = validateBinaryTreeNodes(n, leftChild, rightChild);
+    expect(result).toBeTrue();
+  });
 });

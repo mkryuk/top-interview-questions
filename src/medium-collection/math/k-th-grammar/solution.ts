06f696e6a26fd5d1bced427c2e0dfa042da76ee8
Maxim Kryuk <mkryuk@gmail.com>
2023-10-26 19:43:56 +0300

    Math: K-th Symbol in Grammar solution

--- src/medium-collection/math/k-th-grammar/solution.ts
+++ src/medium-collection/math/k-th-grammar/solution.ts
@@ -1,3 +1,10 @@
 export function kthGrammar(n: number, k: number): number {
-  return 0;
+  // Convert 'k - 1' to a binary string. We subtract 1 because 'k' is 1-indexed.
+  // Then, split the string into an array using '1' as the separator.
+  // The length of this array minus one will give the number of '1's in the binary representation.
+  const count = (k - 1).toString(2).split("1").length - 1;
+
+  // Determine if the number of '1's is even or odd.
+  // If it's even, the kth symbol is 0, otherwise, it's 1.
+  return count % 2 === 0 ? 0 : 1;
 }

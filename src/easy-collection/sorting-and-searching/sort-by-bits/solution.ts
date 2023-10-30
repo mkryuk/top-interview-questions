0b756c2d47147e14192cd4d029a2e0b1f88f4bf0
Maxim Kryuk <mkryuk@gmail.com>
2023-10-30 15:38:43 +0200

    Sorting and Searching: Sort Integers by The Number of 1 Bits solution

--- src/easy-collection/sorting-and-searching/sort-by-bits/solution.ts
+++ src/easy-collection/sorting-and-searching/sort-by-bits/solution.ts
@@ -1,3 +1,15 @@
 export function sortByBits(arr: number[]): number[] {
-  return [];
+  return arr.sort((a, b) => {
+    let tempA = a;
+    let tempB = b;
+    let bitsA = 0;
+    let bitsB = 0;
+    while (tempA > 0 || tempB > 0) {
+      bitsA += tempA & 1;
+      bitsB += tempB & 1;
+      tempA >>= 1;
+      tempB >>= 1;
+    }
+    return bitsA - bitsB || a - b;
+  });
 }

675817a20f109450a79c596e22d64a46dc185154
Maxim Kryuk <mkryuk@gmail.com>
2023-10-31 11:19:48 +0200

    Array and Strings: Find The Original Array of Prefix Xor solution

--- src/medium-collection/array-and-strings/find-array/solution.ts
+++ src/medium-collection/array-and-strings/find-array/solution.ts
@@ -1,3 +1,9 @@
 export function findArray(pref: number[]): number[] {
-  return [];
+  let prev = 0;
+  let result: number[] = [];
+  for (let i = 0; i < pref.length; i++) {
+    result[i] = pref[i] ^ prev;
+    prev = pref[i];
+  }
+  return result;
 }

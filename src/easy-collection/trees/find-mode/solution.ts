69a188221dcaab6685f61f389b937f3230fcf6c8
Maxim Kryuk <mkryuk@gmail.com>
2023-11-01 21:15:24 +0200

    Trees and Graphs: Find Mode in Binary Search Tree init

---
+++ src/easy-collection/trees/find-mode/solution.ts
@@ -0,0 +1,5 @@
+import { TreeNode } from "../common";
+
+export function findMode(root: TreeNode | null): number[] {
+  return [];
+}
---
+++ src/easy-collection/trees/find-mode/test.spec.ts
@@ -0,0 +1,18 @@
+import { arrayToTreeNode } from "../common";
+import { findMode } from "./solution";
+
+describe("Find Mode in Binary Search Tree: ", () => {
+  it("findMode should return [2] for root = [1,null,2,2]", () => {
+    const nodes = [1, null, 2, 2];
+    const root = arrayToTreeNode(nodes);
+    const result = findMode(root);
+    expect(result).toEqual([2]);
+  });
+
+  it("findMode should return [0] for root = [0]", () => {
+    const nodes = [0];
+    const root = arrayToTreeNode(nodes);
+    const result = findMode(root);
+    expect(result).toEqual([0]);
+  });
+});

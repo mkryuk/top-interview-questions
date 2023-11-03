c7c3d16fa945418af104a241c361c3a36566b4d8
Maxim Kryuk <mkryuk@gmail.com>
2023-11-03 17:22:09 +0200

    Array and Strings: Build an Array With Stack Operations solution

--- src/medium-collection/array-and-strings/build-array/solution.ts
+++ src/medium-collection/array-and-strings/build-array/solution.ts
@@ -1,3 +1,15 @@
 export function buildArray(target: number[], n: number): string[] {
-  return [];
+  let result: ("Push" | "Pop")[] = [];
+  let targetIndex = 0;
+  let num = 1;
+  while (targetIndex < target.length) {
+    result.push("Push");
+    if (target[targetIndex] === num) {
+      targetIndex++;
+    } else {
+      result.push("Pop");
+    }
+    num++;
+  }
+  return result;
 }

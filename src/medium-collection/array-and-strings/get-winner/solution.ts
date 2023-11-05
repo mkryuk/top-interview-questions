778f798510882bb3720fd4f79951da3769fe14cc
Maxim Kryuk <mkryuk@gmail.com>
2023-11-05 16:19:09 +0200

    Array and Strings:  Find the Winner of an Array Game solution

--- src/medium-collection/array-and-strings/get-winner/solution.ts
+++ src/medium-collection/array-and-strings/get-winner/solution.ts
@@ -1,3 +1,15 @@
 export function getWinner(arr: number[], k: number): number {
-  return 0;
+  let winner = arr[0];
+  let count = 0;
+  let index = 1;
+  while (count < k && index < arr.length) {
+    if (winner > arr[index]) {
+      count++;
+    } else {
+      count = 1;
+      winner = arr[index];
+    }
+    index++;
+  }
+  return winner;
 }
--- src/medium-collection/array-and-strings/get-winner/test.spec.ts
+++ src/medium-collection/array-and-strings/get-winner/test.spec.ts
@@ -14,4 +14,11 @@ describe("Find the Winner of an Array Game:", () => {
     const result = getWinner(arr, k);
     expect(result).toEqual(3);
   });
+
+  it("getWinner should return 99 for arr = [1,11,22,33,44,55,66,77,88,99], k = 1000000000", () => {
+    const arr = [1, 11, 22, 33, 44, 55, 66, 77, 88, 99];
+    const k = 1000000000;
+    const result = getWinner(arr, k);
+    expect(result).toEqual(99);
+  });
 });

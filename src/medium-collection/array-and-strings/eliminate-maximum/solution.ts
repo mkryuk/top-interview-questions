0ccf47d99200d77fefe21bda97bf74ee534949da
Maxim Kryuk <mkryuk@gmail.com>
2023-11-07 22:04:59 +0200

    Array and Strings: Eliminate Maximum Number of Monsters solution

--- src/medium-collection/array-and-strings/eliminate-maximum/solution.ts
+++ src/medium-collection/array-and-strings/eliminate-maximum/solution.ts
@@ -1,3 +1,33 @@
 export function eliminateMaximum(dist: number[], speed: number[]): number {
-  return 0;
+  let timeLeft = [];
+  let n = dist.length;
+  for (let i = 0; i < n; i++) {
+    timeLeft[i] = Math.ceil(dist[i] / speed[i]);
+  }
+
+  let monstersPerMinute = new Array(n).fill(0);
+  for (let i = 0; i < n; i++) {
+    if (timeLeft[i] >= n) {
+      continue;
+    }
+    monstersPerMinute[timeLeft[i]]++;
+  }
+
+  let shots = 0;
+  for (let i = 0; i < n; i++) {
+    if (monstersPerMinute[i] === 0) {
+      // we shoot the next monster or we can think of it
+      // as saving that shot for the next monster
+      shots++;
+      continue;
+    }
+    // if less monsters have arrived this minute than we have saved shots
+    if (monstersPerMinute[i] <= shots) {
+      shots = shots - monstersPerMinute[i] + 1;
+    } else {
+      return i;
+    }
+  }
+
+  return n;
 }

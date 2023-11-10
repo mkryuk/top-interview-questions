7f9613a60874a7f9741b2fd1116fc3f643dcde00
Maxim Kryuk <mkryuk@gmail.com>
2023-11-10 18:33:56 +0200

    Array and Strings: Restore the Array From Adjacent Pairs solution

--- src/medium-collection/array-and-strings/restore-array/solution.ts
+++ src/medium-collection/array-and-strings/restore-array/solution.ts
@@ -1,3 +1,39 @@
 export function restoreArray(adjacentPairs: number[][]): number[] {
-  return [];
+  let adjacencyList = new Map<number, number[]>();
+  adjacentPairs.forEach(([left, right]) => {
+    if (!adjacencyList.has(left)) {
+      adjacencyList.set(left, []);
+    }
+    if (!adjacencyList.has(right)) {
+      adjacencyList.set(right, []);
+    }
+    adjacencyList.get(left)!.push(right);
+    adjacencyList.get(right)!.push(left);
+  });
+
+  let start = 0;
+  for (let key of adjacencyList.keys()) {
+    if (adjacencyList.get(key)!.length === 1) {
+      start = key;
+      break;
+    }
+  }
+
+  let stack = [start];
+  let result = [];
+  let visited = new Set();
+  while (stack.length > 0) {
+    let current = stack.pop()!;
+    if (visited.has(current)) {
+      continue;
+    }
+    result.push(current);
+    visited.add(current);
+
+    let next = adjacencyList.get(current)!.find((n) => !visited.has(n));
+    if (next !== undefined) {
+      stack.push(next);
+    }
+  }
+  return result;
 }

16beb808a46932ddb2614d8d5792eb180d682b46
Maxim Kryuk <mkryuk@gmail.com>
2023-11-12 19:50:53 +0200

    Design: Design Graph With Shortest Path Calculator solution

--- src/hard-collection/design/graph/solution.ts
+++ src/hard-collection/design/graph/solution.ts
@@ -1,9 +1,69 @@
-export class Graph {
-  constructor(n: number, edges: number[][]) {}
+type Edge = [number, number, number]; // [from, to, cost]
 
-  addEdge(edge: number[]): void {}
+export class Graph {
+  private adjacencyList: Map<number, Edge[]>;
+
+  constructor(n: number, edges: Edge[]) {
+    this.adjacencyList = new Map();
+
+    for (let i = 0; i < n; i++) {
+      this.adjacencyList.set(i, []);
+    }
+
+    edges.forEach((edge) => this.addEdge(edge));
+  }
+
+  addEdge(edge: Edge): void {
+    const [from, to, cost] = edge;
+    this.adjacencyList.get(from)?.push([from, to, cost]);
+  }
 
   shortestPath(node1: number, node2: number): number {
-    return 0;
+    const distances = new Array(this.adjacencyList.size).fill(Infinity);
+    distances[node1] = 0;
+
+    const pq = new PriorityQueue<[number, number]>((a, b) => a[1] - b[1]);
+    pq.enqueue([node1, 0]);
+
+    while (!pq.isEmpty()) {
+      const [node, cost] = pq.dequeue();
+
+      if (node === node2) {
+        return cost;
+      }
+
+      this.adjacencyList.get(node)?.forEach(([_, neighbor, edgeCost]) => {
+        const newCost = cost + edgeCost;
+        if (newCost < distances[neighbor]) {
+          distances[neighbor] = newCost;
+          pq.enqueue([neighbor, newCost]);
+        }
+      });
+    }
+
+    return distances[node2] === Infinity ? -1 : distances[node2];
+  }
+}
+
+class PriorityQueue<T> {
+  private data: T[];
+  private comparator: (a: T, b: T) => number;
+
+  constructor(comparator: (a: T, b: T) => number) {
+    this.data = [];
+    this.comparator = comparator;
+  }
+
+  enqueue(item: T): void {
+    this.data.push(item);
+    this.data.sort(this.comparator);
+  }
+
+  dequeue(): T {
+    return this.data.shift()!;
+  }
+
+  isEmpty(): boolean {
+    return this.data.length === 0;
   }
 }

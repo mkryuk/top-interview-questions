1005cbac0a10c501c3eec9deae3a5a2d9f8866c7
Maxim Kryuk <mkryuk@gmail.com>
2023-11-13 15:05:33 +0200

    Sorting and Searching: Sort Vowels in a String solution

--- src/medium-collection/sorting-and-searching/sort-vowels/solution.ts
+++ src/medium-collection/sorting-and-searching/sort-vowels/solution.ts
@@ -1,3 +1,20 @@
 export function sortVowels(s: string): string {
-  return "";
+  let vowels = new Set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]);
+  let vowelsInAString: string[] = [];
+  for (const char of s) {
+    if (vowels.has(char)) {
+      vowelsInAString.push(char);
+    }
+  }
+  vowelsInAString.sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0));
+  let result: string[] = [];
+  let vowelIndex = 0;
+  for (const char of s) {
+    if (vowels.has(char)) {
+      result.push(vowelsInAString[vowelIndex++]);
+    } else {
+      result.push(char);
+    }
+  }
+  return result.join("");
 }

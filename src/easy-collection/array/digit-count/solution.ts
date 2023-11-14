16309c9052b1c414333a0085d6292ddeb8a94eb5
Maxim Kryuk <mkryuk@gmail.com>
2023-11-14 18:49:27 +0200

    Array and Strings: Check if Number Has Equal Digit Count and Digit Value solution

--- src/easy-collection/array/digit-count/solution.ts
+++ src/easy-collection/array/digit-count/solution.ts
@@ -1,3 +1,15 @@
 export function digitCount(num: string): boolean {
-  return false;
+  let map = new Map<string, number>();
+  for (let i = 0; i < num.length; i++) {
+    if (!map.has(num[i])) {
+      map.set(num[i], 0);
+    }
+    map.set(num[i], map.get(num[i])! + 1);
+  }
+  for (let i = 0; i < num.length; i++) {
+    if ((map.get(`${i}`) ?? 0) !== parseInt(num[i], 10)) {
+      return false;
+    }
+  }
+  return true;
 }

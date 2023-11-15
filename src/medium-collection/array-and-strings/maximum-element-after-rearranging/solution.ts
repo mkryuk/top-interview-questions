52c865e9e311aaceeb4848be03fb91fb27123a89
Maxim Kryuk <mkryuk@gmail.com>
2023-11-15 19:40:29 +0200

    Array and Strings: Maximum Element After Decreasing and Rearranging solution

--- src/medium-collection/array-and-strings/maximum-element-after-rearranging/solution.ts
+++ src/medium-collection/array-and-strings/maximum-element-after-rearranging/solution.ts
@@ -1,5 +1,14 @@
 export function maximumElementAfterDecrementingAndRearranging(
   arr: number[],
 ): number {
-  return 0;
+  arr.sort((a, b) => a - b);
+  arr[0] = 1;
+  let result = 1;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] > 1) {
+      arr[i] = arr[i - 1] + 1;
+    }
+    result = Math.max(result, arr[i]);
+  }
+  return result;
 }

dcaaa53fb2613f12e874ed760212a1ae1d73c84f
Maxim Kryuk <mkryuk@gmail.com>
2023-11-17 17:28:45 +0200

    Array and Strings: Minimize Maximum Pair Sum in Array solution

--- src/medium-collection/array-and-strings/min-pair-sum/solution.ts
+++ src/medium-collection/array-and-strings/min-pair-sum/solution.ts
@@ -1,3 +1,10 @@
 export function minPairSum(nums: number[]): number {
-  return 0;
+  nums.sort((a, b) => a - b);
+  let max = 0;
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    max = Math.max(max, nums[left++] + nums[right--]);
+  }
+  return max;
 }

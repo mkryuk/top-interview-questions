aa47a7640a27de88b123cbedd965af50a837fc06
Maxim Kryuk <mkryuk@gmail.com>
2023-11-19 20:15:04 +0200

    Array and Strings: Reduction Operations to Make the Array Elements Equal solution

--- src/medium-collection/array-and-strings/reduction-operations/solution.ts
+++ src/medium-collection/array-and-strings/reduction-operations/solution.ts
@@ -1,3 +1,21 @@
 export function reductionOperations(nums: number[]): number {
-  return 0;
+  let result = 0;
+  nums.sort((a, b) => a - b);
+  // Start index of the current group of equal elements
+  let left = 0;
+  let n = nums.length;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[left]) {
+      continue;
+    }
+
+    // If the current element is different (i.e., a new larger number)
+    // Add the count of elements to the right of the current element, which is n - i.
+    result += n - i;
+
+    // Update the left pointer for the next group of identical elements
+    left = i;
+  }
+  return result;
 }

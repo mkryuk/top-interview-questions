a7ed4a6e67af384324e2ec155d5a555e1e11a18a
Maxim Kryuk <mkryuk@gmail.com>
2023-11-22 19:23:37 +0200

    Array and Strings: Diagonal Traverse II solution

--- src/medium-collection/array-and-strings/find-diagonal-order/solution.ts
+++ src/medium-collection/array-and-strings/find-diagonal-order/solution.ts
@@ -1,3 +1,20 @@
 export function findDiagonalOrder(nums: number[][]): number[] {
-  return [];
+  let diagonalTable: number[][] = [];
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums[i].length; j++) {
+      // items from the same diagonal have same indexes sum
+      let diagonalIndex = i + j;
+      if (diagonalTable[diagonalIndex] === undefined) {
+        diagonalTable[diagonalIndex] = [];
+      }
+      diagonalTable[diagonalIndex].push(nums[i][j]);
+    }
+  }
+  let result = [];
+  for (let i = 0; i < diagonalTable.length; i++) {
+    for (let j = diagonalTable[i].length - 1; j >= 0; j--) {
+      result.push(diagonalTable[i][j]);
+    }
+  }
+  return result;
 }

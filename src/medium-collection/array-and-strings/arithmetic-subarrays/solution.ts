0180e12500c3f60efdd31636a5d530e2f7c8f907
Maxim Kryuk <mkryuk@gmail.com>
2023-11-23 20:59:44 +0200

    Array and Strings: Arithmetic Subarrays solution

--- src/medium-collection/array-and-strings/arithmetic-subarrays/solution.ts
+++ src/medium-collection/array-and-strings/arithmetic-subarrays/solution.ts
@@ -3,5 +3,24 @@ export function checkArithmeticSubarrays(
   l: number[],
   r: number[],
 ): boolean[] {
-  return [];
+  let result = [];
+  for (let i = 0; i < l.length; i++) {
+    let slice = nums.slice(l[i], r[i] + 1);
+    slice.sort((a, b) => a - b);
+    result.push(isArrayArithmetic(slice));
+  }
+  return result;
+}
+
+function isArrayArithmetic(nums: number[]): boolean {
+  if (nums.length < 2) {
+    return false;
+  }
+  let diff = nums[1] - nums[0];
+  for (let i = 2; i < nums.length; i++) {
+    if (nums[i] - nums[i - 1] !== diff) {
+      return false;
+    }
+  }
+  return true;
 }

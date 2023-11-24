6da18dfbdbcf6d14635004feb14eb2da271615ec
Maxim Kryuk <mkryuk@gmail.com>
2023-11-24 16:55:35 +0200

    Array and Strings: Maximum Number of Coins You Can Get solution

--- src/medium-collection/array-and-strings/max-coins/solution.ts
+++ src/medium-collection/array-and-strings/max-coins/solution.ts
@@ -1,3 +1,12 @@
 export function maxCoins(piles: number[]): number {
-  return 0;
+  piles.sort((a, b) => a - b);
+  // we should skip a third of the piles for Bob
+  let start = Math.trunc(piles.length / 3);
+  let result = 0;
+  for (let i = start; i < piles.length; i += 2) {
+    // we will take every second big pile
+    result += piles[i];
+  }
+
+  return result;
 }

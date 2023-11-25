19a37a4fbe0b5ce45ec9465c8a77b99189658b63
Maxim Kryuk <mkryuk@gmail.com>
2023-11-25 21:21:18 +0200

    Array and Strings: Sum of Absolute Differences in a Sorted Array solution

--- src/medium-collection/array-and-strings/sum-absolute-differences/solution.ts
+++ src/medium-collection/array-and-strings/sum-absolute-differences/solution.ts
@@ -3,11 +3,11 @@ export function getSumAbsoluteDifferences(nums: number[]): number[] {
   let totalSum = nums.reduce((prev, curr) => prev + curr, 0);
   let result = [];
   for (let i = 0; i < nums.length; i++) {
-    leftSum += nums[i - 1] ?? 0;
     let rightSum = totalSum - leftSum - nums[i];
     let leftDifference = nums[i] * i - leftSum;
     let rightDifference = rightSum - nums[i] * (nums.length - i - 1);
     let difference = leftDifference + rightDifference;
+    leftSum += nums[i];
     result.push(difference);
   }
   return result;

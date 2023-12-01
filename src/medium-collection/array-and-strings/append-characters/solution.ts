265c5474be64799b404874c68a640c238d9cb75b
Maxim Kryuk <mkryuk@gmail.com>
2023-12-02 00:18:11 +0200

    Array and Strings: Append Characters to String to Make Subsequence solution

--- src/medium-collection/array-and-strings/append-characters/solution.ts
+++ src/medium-collection/array-and-strings/append-characters/solution.ts
@@ -1,3 +1,10 @@
 export function appendCharacters(s: string, t: string): number {
-  return 0;
+  let tIndex = 0;
+  for (let sIndex = 0; sIndex < s.length; sIndex++) {
+    if (s[sIndex] !== t[tIndex]) {
+      continue;
+    }
+    tIndex++;
+  }
+  return t.length - tIndex;
 }

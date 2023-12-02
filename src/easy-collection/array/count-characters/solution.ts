49f9fb11b05c7d17d398c108318de1ef4efdd668
Maxim Kryuk <mkryuk@gmail.com>
2023-12-03 01:21:32 +0200

    Array and Strings: Find Words That Can Be Formed by Characters solution

--- src/easy-collection/array/count-characters/solution.ts
+++ src/easy-collection/array/count-characters/solution.ts
@@ -1,3 +1,26 @@
 export function countCharacters(words: string[], chars: string): number {
-  return 0;
+  const charsMap = new Map<string, number>();
+  for (const char of chars) {
+    charsMap.set(char, (charsMap.get(char) || 0) + 1);
+  }
+
+  let result = 0;
+  for (const word of words) {
+    let isGood = true;
+    const wordMap = new Map(charsMap);
+    // Check if the word can be formed
+    for (const char of word) {
+      if (!wordMap.has(char) || wordMap.get(char) === 0) {
+        isGood = false;
+        break;
+      }
+      wordMap.set(char, wordMap.get(char)! - 1);
+    }
+
+    if (isGood) {
+      result += word.length;
+    }
+  }
+
+  return result;
 }

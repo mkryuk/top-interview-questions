d5416f1fd34732b5eecf0b2d8ed4b1125e8a70dc
Maxim Kryuk <mkryuk@gmail.com>
2023-12-03 21:54:53 +0200

    Array and Strings: Minimum Time Visiting All Points solution

--- src/easy-collection/array/min-time-to-visit-all-points/solution.ts
+++ src/easy-collection/array/min-time-to-visit-all-points/solution.ts
@@ -1,3 +1,11 @@
 export function minTimeToVisitAllPoints(points: number[][]): number {
-  return 0;
+  let result = 0;
+  for (let i = 0; i < points.length - 1; i++) {
+    const [x1, y1] = points[i];
+    const [x2, y2] = points[i + 1];
+    const xDiff = Math.abs(x2 - x1);
+    const yDiff = Math.abs(y2 - y1);
+    result += Math.max(xDiff, yDiff);
+  }
+  return result;
 }

dc8a83be593db291191c1fb1eafa64f1acc306b8
Maxim Kryuk <mkryuk@gmail.com>
2023-12-04 18:25:34 +0200

    Array and Strings: Largest 3-Same-Digit Number in String solution

--- src/easy-collection/strings/largest-good-integer/solution.ts
+++ src/easy-collection/strings/largest-good-integer/solution.ts
@@ -1,3 +1,15 @@
 export function largestGoodInteger(num: string): string {
-  return "";
+  let maxGoodInteger = "";
+
+  for (let i = 0; i < num.length - 2; i++) {
+    const currentChar = num[i];
+    if (num[i + 1] === currentChar && num[i + 2] === currentChar) {
+      const currentGoodInteger = num.substring(i, i + 3);
+      if (currentGoodInteger > maxGoodInteger) {
+        maxGoodInteger = currentGoodInteger;
+      }
+    }
+  }
+
+  return maxGoodInteger;
 }

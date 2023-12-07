c64725d770618f0ea7b9be3b031bc1607d0941dc
Maxim Kryuk <mkryuk@gmail.com>
2023-12-07 19:26:13 +0200

    Math: Largest Odd Number in String solution

--- src/easy-collection/math/largest-odd-number/solution.ts
+++ src/easy-collection/math/largest-odd-number/solution.ts
@@ -1,3 +1,9 @@
 export function largestOddNumber(num: string): string {
+  let n = num.length;
+  for (let i = n; i >= 0; i--) {
+    if (parseInt(num[i], 10) % 2 > 0) {
+      return num.substring(0, i + 1);
+    }
+  }
   return "";
 }

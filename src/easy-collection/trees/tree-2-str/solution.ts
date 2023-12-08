e3b627711d804249059f6e45d6da1c7e7a49467f
Maxim Kryuk <mkryuk@gmail.com>
2023-12-08 19:33:56 +0200

    Trees and Graphs: Construct String from Binary Tree solution

--- src/easy-collection/trees/tree-2-str/solution.ts
+++ src/easy-collection/trees/tree-2-str/solution.ts
@@ -1,5 +1,16 @@
 import { TreeNode } from "../common";
 
 export function tree2str(root: TreeNode | null): string {
-  return "";
+  let result = "";
+  if (root === null) {
+    return "";
+  }
+  result = root.val.toString();
+  if (root.left || root.right) {
+    result += `(${tree2str(root.left)})`;
+  }
+  if (root.right) {
+    result += `(${tree2str(root.right)})`;
+  }
+  return result;
 }

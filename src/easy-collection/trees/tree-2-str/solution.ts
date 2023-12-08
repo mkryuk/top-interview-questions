2b6da09168353bfb9556ef196d0eb557772e0faf
Maxim Kryuk <mkryuk@gmail.com>
2023-12-08 18:20:59 +0200

    Trees and Graphs: Construct String from Binary Tree init

---
+++ src/easy-collection/trees/tree-2-str/solution.ts
@@ -0,0 +1,5 @@
+import { TreeNode } from "../common";
+
+export function tree2str(root: TreeNode | null): string {
+  return "";
+}
---
+++ src/easy-collection/trees/tree-2-str/test.spec.ts
@@ -0,0 +1,18 @@
+import { arrayToTreeNode } from "../common";
+import { tree2str } from "./solution";
+
+describe("Construct String from Binary Tree:", () => {
+  it('tree2str should return "1(2(4))(3)" for root = [1,2,3,4]', () => {
+    const nodes = [1, 2, 3, 4];
+    const root = arrayToTreeNode(nodes);
+    const result = tree2str(root);
+    expect(result).toEqual("1(2(4))(3)");
+  });
+
+  it('tree2str should return "1(2()(4))(3)" for root = [1,2,3,null,4]', () => {
+    const nodes = [1, 2, 3, null, 4];
+    const root = arrayToTreeNode(nodes);
+    const result = tree2str(root);
+    expect(result).toEqual("1(2()(4))(3)");
+  });
+});

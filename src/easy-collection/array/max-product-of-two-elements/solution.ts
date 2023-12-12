0478208513d9c03d97636de9a457a48cc5d42efe
Maxim Kryuk <mkryuk@gmail.com>
2023-12-12 13:27:23 +0200

    Array and Strings: Maximum Product of Two Elements in an Array solution

--- src/easy-collection/array/max-product-of-two-elements/solution.ts
+++ src/easy-collection/array/max-product-of-two-elements/solution.ts
@@ -1,3 +1,10 @@
 export function maxProduct(nums: number[]): number {
-  return 0;
+  for (let i = 2; i < nums.length; i++) {
+    if (nums[0] < nums[1] && nums[0] < nums[i]) {
+      nums[0] = nums[i];
+    } else if (nums[1] < nums[i]) {
+      nums[1] = nums[i];
+    }
+  }
+  return (nums[0] - 1) * (nums[1] - 1);
 }

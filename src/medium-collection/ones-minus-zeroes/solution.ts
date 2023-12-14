20a82a01ac64793a8c0185b7225f7c7c2a47357f
Maxim Kryuk <mkryuk@gmail.com>
2023-12-14 14:07:26 +0200

    Arrays and Strings: Difference Between Ones and Zeros in Row and Column solution

--- src/medium-collection/ones-minus-zeroes/solution.ts
+++ src/medium-collection/ones-minus-zeroes/solution.ts
@@ -1,3 +1,25 @@
 export function onesMinusZeros(grid: number[][]): number[][] {
-  return [];
+  let m = grid.length;
+  let n = grid[0].length;
+  let onesRow = new Array(m).fill(0);
+  let onesCol = new Array(n).fill(0);
+  for (let row = 0; row < m; row++) {
+    for (let col = 0; col < n; col++) {
+      if (grid[row][col] === 1) {
+        onesRow[row]++;
+        onesCol[col]++;
+      }
+    }
+  }
+
+  let result: number[][] = [];
+  for (let row = 0; row < m; row++) {
+    let rowItems = new Array(n);
+    for (let col = 0; col < n; col++) {
+      rowItems[col] =
+        onesRow[row] + onesCol[col] - (m - onesRow[row]) - (n - onesCol[col]);
+    }
+    result.push(rowItems);
+  }
+  return result;
 }

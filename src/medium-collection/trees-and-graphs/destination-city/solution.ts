7eeb57ff345edad4a6ab0c6e6c781e2ef4d2578d
Maxim Kryuk <mkryuk@gmail.com>
2023-12-15 15:07:46 +0200

    Trees and Graphs: Destination City solution

--- src/medium-collection/trees-and-graphs/destination-city/solution.ts
+++ src/medium-collection/trees-and-graphs/destination-city/solution.ts
@@ -1,3 +1,13 @@
 export function destCity(paths: string[][]): string {
+  let outSet = new Set<string>();
+  for (const [out] of paths) {
+    outSet.add(out);
+  }
+
+  for (const [out, dest] of paths) {
+    if (!outSet.has(dest)) {
+      return dest;
+    }
+  }
   return "";
 }

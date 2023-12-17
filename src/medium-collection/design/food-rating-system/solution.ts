30e47d51e28280da5ed33904dd228bdfc02aebb4
Maxim Kryuk <mkryuk@gmail.com>
2023-12-17 20:42:47 +0200

    Design: Design a Food Rating System solution

--- src/medium-collection/design/food-rating-system/solution.ts
+++ src/medium-collection/design/food-rating-system/solution.ts
@@ -1,16 +1,43 @@
-export class FoodRatings {
-  constructor(foods: string[], cuisines: string[], ratings: number[]) {}
+import { Heap } from "../../../hard-collection/design/heap/heap";
 
-  changeRating(food: string, newRating: number): void {}
+type FoodObj = { food: string; rating: number };
+type CuisineObj = { cuisine: string; rating: number };
+
+export class FoodRatings {
+  foodMap: Map<string, CuisineObj>;
+  cuisineMap: Map<string, Heap<FoodObj>>;
+
+  constructor(foods: string[], cuisines: string[], ratings: number[]) {
+    const n = foods.length;
+    this.foodMap = new Map();
+    this.cuisineMap = new Map();
+    for (let i = 0; i < n; i++) {
+      this.foodMap.set(foods[i], { cuisine: cuisines[i], rating: ratings[i] });
+      if (!this.cuisineMap.has(cuisines[i])) {
+        const pq = new Heap<FoodObj>((a: FoodObj, b: FoodObj) => {
+          return a.rating !== b.rating
+            ? b.rating - a.rating
+            : a.food.localeCompare(b.food);
+        });
+        this.cuisineMap.set(cuisines[i], pq);
+      }
+      this.cuisineMap
+        .get(cuisines[i])
+        ?.push({ food: foods[i], rating: ratings[i] });
+    }
+  }
+
+  changeRating(food: string, newRating: number): void {
+    const cuisine = this.foodMap.get(food)!.cuisine;
+    this.cuisineMap.get(cuisine)?.push({ food, rating: newRating });
+    this.foodMap.set(food, { cuisine, rating: newRating });
+  }
 
   highestRated(cuisine: string): string {
-    return "";
+    const pq = this.cuisineMap.get(cuisine);
+    while (this.foodMap.get(pq!.top()!.food)!.rating !== pq!.top()!.rating) {
+      pq?.pop();
+    }
+    return pq!.top()!.food;
   }
 }
-
-/**
- * Your FoodRatings object will be instantiated and called as such:
- * var obj = new FoodRatings(foods, cuisines, ratings)
- * obj.changeRating(food,newRating)
- * var param_2 = obj.highestRated(cuisine)
- */

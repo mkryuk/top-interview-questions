e65aaf6003f1ad93aa38e858f10b12905ccd4b34
Maxim Kryuk <mkryuk@gmail.com>
2023-12-18 12:30:05 +0200

    Array and Strings: Maximum Product Difference Between Two Pairs solution

--- src/easy-collection/array/max-product-difference/solution.ts
+++ src/easy-collection/array/max-product-difference/solution.ts
@@ -1,3 +1,5 @@
 export function maxProductDifference(nums: number[]): number {
-  return 0;
+  nums.sort((a, b) => a - b);
+  const n = nums.length;
+  return nums[n - 1] * nums[n - 2] - nums[0] * nums[1];
 }

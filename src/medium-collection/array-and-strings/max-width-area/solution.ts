54562fb49a3a3b073f95090548ef3f1da53bc3dd
Maxim Kryuk <mkryuk@gmail.com>
2023-12-21 16:25:10 +0200

    Array and Strings: Widest Vertical Area Between Two Points Containing No Points solution

--- src/medium-collection/array-and-strings/max-width-area/solution.ts
+++ src/medium-collection/array-and-strings/max-width-area/solution.ts
@@ -1,3 +1,8 @@
 export function maxWidthOfVerticalArea(points: number[][]): number {
-  return 0;
+  points.sort((a, b) => a[0] - b[0]);
+  let result = 0;
+  for (let i = 1; i < points.length; i++) {
+    result = Math.max(result, points[i][0] - points[i - 1][0]);
+  }
+  return result;
 }

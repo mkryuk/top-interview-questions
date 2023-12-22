0d8fc39db03e00fd6f46e8783067236aee73262f
Maxim Kryuk <mkryuk@gmail.com>
2023-12-22 16:25:51 +0200

    Array and Strings: Maximum Score After Splitting a String init

---
+++ src/easy-collection/strings/max-score/test.spec.ts
@@ -0,0 +1,21 @@
+import { maxScore } from "./solution";
+
+describe("Maximum Score After Splitting a String:", () => {
+  it('maxScore should return 5 for s = "011101"', () => {
+    const s = "011101";
+    const result = maxScore(s);
+    expect(result).toEqual(5);
+  });
+
+  it('maxScore should return 5 for s = "00111"', () => {
+    const s = "00111";
+    const result = maxScore(s);
+    expect(result).toEqual(5);
+  });
+
+  it('maxScore should return 3 for s = "1111"', () => {
+    const s = "1111";
+    const result = maxScore(s);
+    expect(result).toEqual(3);
+  });
+});

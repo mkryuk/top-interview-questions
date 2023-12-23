f73d8148b1d1d56cee73003bc5690c8c4dbbba38
Maxim Kryuk <mkryuk@gmail.com>
2023-12-23 23:16:45 +0200

    Array and Strings: Path Crossing solution

--- src/easy-collection/strings/path-crossing/solution.ts
+++ src/easy-collection/strings/path-crossing/solution.ts
@@ -1,3 +1,35 @@
 export function isPathCrossing(path: string): boolean {
+  let visited = new Set<string>();
+  let point = {
+    x: 0,
+    y: 0,
+    toString() {
+      return `${this.x},${this.y}`;
+    },
+  };
+
+  visited.add(point.toString());
+
+  for (const direction of path) {
+    switch (direction) {
+      case "N":
+        point.y++;
+        break;
+      case "E":
+        point.x++;
+        break;
+      case "S":
+        point.y--;
+        break;
+      case "W":
+        point.x--;
+        break;
+    }
+    if (visited.has(point.toString())) {
+      return true;
+    }
+    visited.add(point.toString());
+  }
+
   return false;
 }

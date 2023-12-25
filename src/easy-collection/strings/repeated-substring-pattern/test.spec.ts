a39e843a32d91ee78d4a87ec25a5153eaf73e264
Maxim Kryuk <mkryuk@gmail.com>
2023-12-25 13:48:39 +0200

    Array and Strings: Repeated Substring Pattern init

---
+++ src/easy-collection/strings/repeated-substring-pattern/test.spec.ts
@@ -0,0 +1,21 @@
+import { repeatedSubstringPattern } from "./solution";
+
+describe("Repeated Substring Pattern:", () => {
+  it('repeatedSubstringPattern should return true for s = "abab"', () => {
+    const s = "abab";
+    const result = repeatedSubstringPattern(s);
+    expect(result).toBeTrue();
+  });
+
+  it('repeatedSubstringPattern should return false for s = "aba"', () => {
+    const s = "aba";
+    const result = repeatedSubstringPattern(s);
+    expect(result).toBeFalse();
+  });
+
+  it('repeatedSubstringPattern should return true for s = "abcabcabcabc"', () => {
+    const s = "abcabcabcabc";
+    const result = repeatedSubstringPattern(s);
+    expect(result).toBeTrue();
+  });
+});

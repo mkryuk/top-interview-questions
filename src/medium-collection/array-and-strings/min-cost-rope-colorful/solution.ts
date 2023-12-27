2177d73223adf7a53a1043e93dfd0ccc5571613c
Maxim Kryuk <mkryuk@gmail.com>
2023-12-27 18:41:46 +0200

    Array and Strings: Minimum Time to Make Rope Colorful solution

--- src/medium-collection/array-and-strings/min-cost-rope-colorful/solution.ts
+++ src/medium-collection/array-and-strings/min-cost-rope-colorful/solution.ts
@@ -1,3 +1,22 @@
 export function minCost(colors: string, neededTime: number[]): number {
-  return 0;
+  let windowSum = 0;
+  let maxWindowValue = 0;
+  let totalTime = 0;
+  for (let i = 1; i < colors.length; i++) {
+    // When consecutive balloons are of the same color
+    if (colors[i] === colors[i - 1]) {
+      windowSum = windowSum === 0 ? neededTime[i - 1] : windowSum;
+      maxWindowValue =
+        maxWindowValue === 0 ? neededTime[i - 1] : maxWindowValue;
+      windowSum += neededTime[i];
+      maxWindowValue = Math.max(maxWindowValue, neededTime[i]);
+    }
+    // If it's the last balloon or different from the previous one
+    if (colors[i] !== colors[i - 1] || i === colors.length - 1) {
+      totalTime += windowSum - maxWindowValue;
+      windowSum = 0;
+      maxWindowValue = 0;
+    }
+  }
+  return totalTime;
 }

3a6030ebdbf72bd8a9a02650a2f3376a1d55e024
Maxim Kryuk <mkryuk@gmail.com>
2023-12-27 13:09:48 +0200

    Array and Strings: Minimum Time to Make Rope Colorful init

---
+++ src/medium-collection/array-and-strings/min-cost-rope-colorful/test.spec.ts
@@ -0,0 +1,24 @@
+import { minCost } from "./solution";
+
+describe("Minimum Time to Make Rope Colorful:", () => {
+  it('minCost should return 3 for colors = "abaac", neededTime = [1,2,3,4,5]', () => {
+    const colors = "abaac";
+    const neededTime = [1, 2, 3, 4, 5];
+    const result = minCost(colors, neededTime);
+    expect(result).toEqual(3);
+  });
+
+  it('minCost should return 0 for colors = "abc", neededTime = [1,2,3]', () => {
+    const colors = "abc";
+    const neededTime = [1, 2, 3];
+    const result = minCost(colors, neededTime);
+    expect(result).toEqual(0);
+  });
+
+  it('minCost should return 2 for colors = "aabaa", neededTime = [1,2,3,4,1]', () => {
+    const colors = "aabaa";
+    const neededTime = [1, 2, 3, 4, 1];
+    const result = minCost(colors, neededTime);
+    expect(result).toEqual(2);
+  });
+});

5bd3c67f593123508ec4fcdcb7efcfcd62fe94ff
Maxim Kryuk <mkryuk@gmail.com>
2023-12-31 11:04:16 +0200

    Array and Strings: Redistribute Characters to Make All Strings Equal solution

--- src/easy-collection/array/make-equal/solution.ts
+++ src/easy-collection/array/make-equal/solution.ts
@@ -1,3 +1,18 @@
 export function makeEqual(words: string[]): boolean {
-  return false;
+  let charNum = new Map<string, number>();
+  for (const word of words) {
+    for (const char of word) {
+      if (!charNum.has(char)) {
+        charNum.set(char, 0);
+      }
+      charNum.set(char, charNum.get(char)! + 1);
+    }
+  }
+
+  for (const [char, num] of charNum) {
+    if (num % words.length !== 0) {
+      return false;
+    }
+  }
+  return true;
 }

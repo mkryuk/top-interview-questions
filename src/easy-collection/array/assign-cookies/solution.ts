56ebb34df98e1e4b881e3bf358f1f4fe5ada8af8
Maxim Kryuk <mkryuk@gmail.com>
2024-01-01 19:16:28 +0200

    Array and Strings: Assign Cookies solution

--- src/easy-collection/array/assign-cookies/solution.ts
+++ src/easy-collection/array/assign-cookies/solution.ts
@@ -1,3 +1,12 @@
 export function findContentChildren(g: number[], s: number[]): number {
-  return 0;
+  g.sort((a, b) => b - a);
+  s.sort((a, b) => b - a);
+  let result = 0;
+  for (let gIdx = 0, sIdx = 0; gIdx < g.length && sIdx < s.length; gIdx++) {
+    if (s[sIdx] >= g[gIdx]) {
+      result++;
+      sIdx++;
+    }
+  }
+  return result;
 }

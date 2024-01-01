f753ccb3b0c2c4e8cce3a40744b6fa227f5574a4
Maxim Kryuk <mkryuk@gmail.com>
2024-01-01 17:58:38 +0200

    Array and Strings: Largest Substring Between Two Equal Characters solution

--- src/easy-collection/array/largest-substring-between-characters/solution.ts
+++ src/easy-collection/array/largest-substring-between-characters/solution.ts
@@ -1,3 +1,17 @@
 export function maxLengthBetweenEqualCharacters(s: string): number {
-  return 0;
+  const charSection = new Map<string, number[]>();
+  for (let i = 0; i < s.length; i++) {
+    if (!charSection.has(s[i])) {
+      charSection.set(s[i], [i, -1]);
+    } else {
+      let [start] = charSection.get(s[i])!;
+      charSection.set(s[i], [start, i]);
+    }
+  }
+
+  let result = -1;
+  for (const [char, [start, end]] of charSection) {
+    result = Math.max(result, end - start - 1);
+  }
+  return result;
 }

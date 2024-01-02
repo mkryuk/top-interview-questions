5d7975395a13931503d533f2813f934fedc9e669
Maxim Kryuk <mkryuk@gmail.com>
2024-01-02 21:20:47 +0200

    Array and Strings: Convert an Array Into a 2D Array With Conditions solution

--- src/medium-collection/array-and-strings/find-matrix/solution.ts
+++ src/medium-collection/array-and-strings/find-matrix/solution.ts
@@ -1,3 +1,19 @@
 export function findMatrix(nums: number[]): number[][] {
-  return [];
+  nums.sort((a, b) => a - b);
+  let result: number[][] = [[nums[0]]];
+  let rowIndex = 0;
+  let row: number[];
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) {
+      if (rowIndex + 1 === result.length) {
+        result.push([]);
+      }
+      row = result[++rowIndex];
+    } else {
+      rowIndex = 0;
+      row = result[rowIndex];
+    }
+    row.push(nums[i]);
+  }
+  return result;
 }
--- src/medium-collection/array-and-strings/find-matrix/test.spec.ts
+++ src/medium-collection/array-and-strings/find-matrix/test.spec.ts
@@ -1,15 +1,15 @@
 import { findMatrix } from "./solution";
 
 describe("Convert an Array Into a 2D Array With Conditions:", () => {
-  it("findMatrix should return [[1,3,4,2],[1,3],[1]] for nums = [1,3,4,1,2,3,1]", () => {
+  it("findMatrix should return [[1,2,3,4],[1,3],[1]] for nums = [1,3,4,1,2,3,1]", () => {
     const nums = [1, 3, 4, 1, 2, 3, 1];
     const result = findMatrix(nums);
-    expect(result).toEqual([[1, 3, 4, 2], [1, 3], [1]]);
+    expect(result).toEqual([[1, 2, 3, 4], [1, 3], [1]]);
   });
 
-  it("findMatrix should return [[4,3,2,1]] for nums = [1,2,3,4]", () => {
+  it("findMatrix should return [[1,2,3,4]] for nums = [1,2,3,4]", () => {
     const nums = [1, 2, 3, 4];
     const result = findMatrix(nums);
-    expect(result).toEqual([[4, 3, 2, 1]]);
+    expect(result).toEqual([[1, 2, 3, 4]]);
   });
 });

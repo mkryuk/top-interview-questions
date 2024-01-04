d356a9514a64b5e7688cb00f3e3d8eab9273d35e
Maxim Kryuk <mkryuk@gmail.com>
2024-01-04 17:29:41 +0200

    Array and Strings: Number of Laser Beams in a Bank solution

--- src/medium-collection/array-and-strings/number-of-beams/solution.ts
+++ src/medium-collection/array-and-strings/number-of-beams/solution.ts
@@ -1,3 +1,22 @@
 export function numberOfBeams(bank: string[]): number {
-  return 0;
+  let result = 0;
+  let firstRowDeviceCount = 0;
+  for (const row of bank) {
+    let secondRowDeviceCount = countBits(row);
+    if (secondRowDeviceCount > 0) {
+      result += firstRowDeviceCount * secondRowDeviceCount;
+      firstRowDeviceCount = secondRowDeviceCount;
+    }
+  }
+  return result;
+}
+
+function countBits(row: string): number {
+  let result = 0;
+  for (let i = 0; i < row.length; i++) {
+    if (row[i] === "1") {
+      result++;
+    }
+  }
+  return result;
 }

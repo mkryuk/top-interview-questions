157b299a9114df6c509c0e95632b5c1cfb458275
Maxim Kryuk <mkryuk@gmail.com>
2024-01-09 18:20:13 +0200

    Trees and Graphs: Leaf-Similar Trees init

---
+++ src/easy-collection/trees/leaf-similar/solution.ts
@@ -0,0 +1,8 @@
+import { TreeNode } from "../common";
+
+export function leafSimilar(
+  root1: TreeNode | null,
+  root2: TreeNode | null,
+): boolean {
+  return false;
+}
---
+++ src/easy-collection/trees/leaf-similar/test.spec.ts
@@ -0,0 +1,23 @@
+import { arrayToTreeNode } from "../common";
+import { leafSimilar } from "./solution";
+
+describe("Leaf-Similar Trees:", () => {
+  it("leafSimilar should return true for root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]", () => {
+    const nodes1 = [3, 5, 1, 6, 2, 9, 8, null, null, 7, 4];
+    // prettier-ignore
+    const nodes2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8];
+    const root1 = arrayToTreeNode(nodes1);
+    const root2 = arrayToTreeNode(nodes2);
+    const result = leafSimilar(root1, root2);
+    expect(result).toBeTrue();
+  });
+
+  it("leafSimilar should return false for root1 = [1,2,3], root2 = [1,3,2]", () => {
+    const nodes1 = [1, 2, 3];
+    const nodes2 = [1, 3, 2];
+    const root1 = arrayToTreeNode(nodes1);
+    const root2 = arrayToTreeNode(nodes2);
+    const result = leafSimilar(root1, root2);
+    expect(result).toBeFalse();
+  });
+});

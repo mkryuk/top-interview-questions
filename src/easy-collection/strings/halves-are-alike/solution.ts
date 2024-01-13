195a16a51062bfff0a060a0d455b8b49181c7716
Maxim Kryuk <mkryuk@gmail.com>
2024-01-13 12:28:51 +0200

    Array and Strings: Determine if String Halves Are Alike solution

--- src/easy-collection/strings/halves-are-alike/solution.ts
+++ src/easy-collection/strings/halves-are-alike/solution.ts
@@ -1,3 +1,15 @@
 export function halvesAreAlike(s: string): boolean {
-  return false;
+  const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  const vowelsSet = new Set<string>(vowels);
+  let leftVowels = 0;
+  let rightVowels = 0;
+  for (let i = 0, j = s.length / 2; j < s.length; i++, j++) {
+    if (vowelsSet.has(s[i])) {
+      leftVowels++;
+    }
+    if (vowelsSet.has(s[j])) {
+      rightVowels++;
+    }
+  }
+  return leftVowels === rightVowels;
 }

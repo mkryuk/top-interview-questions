e284797e124523fa6cb9b22c85353579a65f5ba9
Maxim Kryuk <mkryuk@gmail.com>
2024-01-17 20:42:46 +0200

    Array and Strings: Unique Number of Occurrences solution

--- src/easy-collection/array/unique-occurrences/solution.ts
+++ src/easy-collection/array/unique-occurrences/solution.ts
@@ -1,3 +1,14 @@
 export function uniqueOccurrences(arr: number[]): boolean {
-  return false;
+  let numCount = new Map<number, number>();
+  for (const num of arr) {
+    numCount.set(num, (numCount.get(num) ?? 0) + 1);
+  }
+  let countSet = new Set<number>();
+  for (const count of numCount.values()) {
+    if (countSet.has(count)) {
+      return false;
+    }
+    countSet.add(count);
+  }
+  return true;
 }

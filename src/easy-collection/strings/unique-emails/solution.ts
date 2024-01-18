fc12fb3d032de7bb4995ff6aaa8b0b0502a30241
Maxim Kryuk <mkryuk@gmail.com>
2024-01-18 15:04:36 +0200

    Array and Strings: Unique Email Addresses solution

--- src/easy-collection/strings/unique-emails/solution.ts
+++ src/easy-collection/strings/unique-emails/solution.ts
@@ -1,3 +1,12 @@
 export function numUniqueEmails(emails: string[]): number {
-  return 0;
+  let emailsSet = new Set<string>();
+  for (const email of emails) {
+    let atIndex = email.indexOf("@");
+    let plusIndex = email.indexOf("+");
+    let index = plusIndex === -1 ? atIndex : plusIndex;
+    let name = email.substring(0, index);
+    name = name.replace(/\./g, "");
+    emailsSet.add(name + email.substring(atIndex));
+  }
+  return emailsSet.size;
 }

3e76954fb07497c7120ff3cc90027dccb375e923
Maxim Kryuk <mkryuk@gmail.com>
2024-01-19 23:55:02 +0200

    Dynamic Programming: Minimum Falling Path Sum solution

--- src/medium-collection/dynamic-programming/min-falling-path-sum/solution.ts
+++ src/medium-collection/dynamic-programming/min-falling-path-sum/solution.ts
@@ -1,3 +1,16 @@
 export function minFallingPathSum(matrix: number[][]): number {
-  return 0;
+  let n = matrix.length;
+  for (let row = 1; row < n; row++) {
+    for (let col = 0; col < n; col++) {
+      let minValue = matrix[row - 1][col];
+      if (col > 0) {
+        minValue = Math.min(minValue, matrix[row - 1][col - 1]);
+      }
+      if (col < n - 1) {
+        minValue = Math.min(minValue, matrix[row - 1][col + 1]);
+      }
+      matrix[row][col] = matrix[row][col] + minValue;
+    }
+  }
+  return Math.min(...matrix[n - 1]);
 }

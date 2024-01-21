a694f52c4a62c83fbb0d475d5b554188086b5342
Maxim Kryuk <mkryuk@gmail.com>
2024-01-21 20:07:46 +0200

    Dynamic Programming: Sum of Subarray Minimums solution

--- src/medium-collection/dynamic-programming/sum-subarrays-min/solution.ts
+++ src/medium-collection/dynamic-programming/sum-subarrays-min/solution.ts
@@ -1,3 +1,21 @@
 export function sumSubarrayMins(arr: number[]): number {
-  return 0;
+  const MOD: number = 1e9 + 7;
+  let sum: number = 0;
+  let stack: number[] = [];
+
+  // We use a trick of adding a virtual element at the beginning and the end of the array.
+  arr.unshift(0);
+  arr.push(0);
+
+  for (let i = 0; i < arr.length; i++) {
+    while (stack.length > 0 && arr[stack[stack.length - 1]] > arr[i]) {
+      let current = stack.pop()!;
+      let left = stack[stack.length - 1];
+      // Contribution of arr[current] in subarrays is arr[current] * (i - current) * (current - left)
+      sum = (sum + arr[current] * (i - current) * (current - left)) % MOD;
+    }
+    stack.push(i);
+  }
+
+  return sum;
 }

8ec76f3ae642e70bbf696d73b63c7d4b39b13588
Maxim Kryuk <mkryuk@gmail.com>
2024-01-22 17:22:01 +0200

    Array and Strings: Set Mismatch solution

--- src/easy-collection/set-mismatch/solution.ts
+++ src/easy-collection/set-mismatch/solution.ts
@@ -1,3 +1,20 @@
 export function findErrorNums(nums: number[]): number[] {
-  return [];
+  const n = nums.length;
+  const set = new Set<number>();
+  let duplicated = 0;
+  let missed = 0;
+  for (const num of nums) {
+    if (set.has(num)) {
+      duplicated = num;
+      continue;
+    }
+    set.add(num);
+  }
+  for (let i = 1; i <= n; i++) {
+    if (!set.has(i)) {
+      missed = i;
+      break;
+    }
+  }
+  return [duplicated, missed];
 }

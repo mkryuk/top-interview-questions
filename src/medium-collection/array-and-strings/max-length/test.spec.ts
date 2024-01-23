20e1b1d9858ce764426ee86f7b3c671d234d6385
Maxim Kryuk <mkryuk@gmail.com>
2024-01-23 15:59:32 +0200

    Array and Strings: Maximum Length of a Concatenated String with Unique Characters init

---
+++ src/medium-collection/array-and-strings/max-length/test.spec.ts
@@ -0,0 +1,21 @@
+import { maxLength } from "./solution";
+
+describe("Maximum Length of a Concatenated String with Unique Characters:", () => {
+  it('maxLength should return 4 for arr = ["un","iq","ue"]', () => {
+    const arr = ["un", "iq", "ue"];
+    const result = maxLength(arr);
+    expect(result).toEqual(4);
+  });
+
+  it('maxLength should return 6 for arr = ["cha","r","act","ers"]', () => {
+    const arr = ["cha", "r", "act", "ers"];
+    const result = maxLength(arr);
+    expect(result).toEqual(6);
+  });
+
+  it('maxLength should return 6 for arr = ["abcdefghijklmnopqrstuvwxyz"]', () => {
+    const arr = ["abcdefghijklmnopqrstuvwxyz"];
+    const result = maxLength(arr);
+    expect(result).toEqual(26);
+  });
+});

3e9315757ba0d645f2b72318f5b0ae137365bfb7
Maxim Kryuk <mkryuk@gmail.com>
2024-01-24 20:21:52 +0200

    Array and Strings: Most Visited Sector in a Circular Track solution

--- src/easy-collection/array/most-visited/solution.ts
+++ src/easy-collection/array/most-visited/solution.ts
@@ -1,3 +1,21 @@
 export function mostVisited(n: number, rounds: number[]): number[] {
-  return [];
+  let visitedCounts = new Array(n).fill(0);
+  let result: number[] = [];
+  for (let i = 0; i < rounds.length - 1; i++) {
+    let start = rounds[i];
+    const end = rounds[i + 1];
+    while (start !== end) {
+      visitedCounts[start - 1]++;
+      start = (start % n) + 1;
+    }
+  }
+  // Increment for the last sector of the last round
+  visitedCounts[rounds[rounds.length - 1] - 1]++;
+  const mostVisited = Math.max(...visitedCounts);
+  for (let i = 0; i < visitedCounts.length; i++) {
+    if (visitedCounts[i] === mostVisited) {
+      result.push(i + 1);
+    }
+  }
+  return result;
 }

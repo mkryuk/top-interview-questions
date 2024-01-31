542063aa0af4f9e9bca3761775c0c2c2fb79c769
Maxim Kryuk <mkryuk@gmail.com>
2024-01-31 23:13:21 +0200

    Array and Strings: Daily Temperatures solution

--- src/easy-collection/array/daily-temperatures/solution.ts
+++ src/easy-collection/array/daily-temperatures/solution.ts
@@ -1,3 +1,15 @@
 export function dailyTemperatures(temperatures: number[]): number[] {
-  return [];
+  const result: number[] = new Array(temperatures.length).fill(0);
+  const stack: number[] = [];
+  for (let i = 0; i < temperatures.length; i++) {
+    while (
+      stack.length > 0 &&
+      temperatures[i] > temperatures[stack[stack.length - 1]]
+    ) {
+      const idx = stack.pop()!;
+      result[idx] = i - idx;
+    }
+    stack.push(i);
+  }
+  return result;
 }

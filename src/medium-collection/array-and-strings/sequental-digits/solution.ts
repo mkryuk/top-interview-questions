cbea3ac1712ca6c1521516bca2d393d77984852c
Maxim Kryuk <mkryuk@gmail.com>
2024-02-03 00:22:11 +0200

    Array and Strings: Sequential Digits solution

--- src/medium-collection/array-and-strings/sequental-digits/solution.ts
+++ src/medium-collection/array-and-strings/sequental-digits/solution.ts
@@ -1,3 +1,21 @@
 export function sequentialDigits(low: number, high: number): number[] {
-  return [];
+  let result: number[] = [];
+  for (let i = 1; i <= 9; i++) {
+    addSequence(i, low, high, result);
+  }
+  return result.sort((a, b) => a - b);
+}
+
+function addSequence(start: number, low: number, high: number, seq: number[]) {
+  let num = start;
+  for (let i = start; i <= 9; i++) {
+    if (num >= low && num <= high) {
+      seq.push(num);
+    }
+    const next = (num % 10) + 1;
+    if (next > 9 || num > high) {
+      break;
+    }
+    num = num * 10 + next;
+  }
 }

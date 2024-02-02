9df33e365c2e56e313c852d113d7731ffe220ccb
Maxim Kryuk <mkryuk@gmail.com>
2024-02-02 14:47:27 +0200

    Array and Strings: Divide Array Into Arrays With Max Difference solution

--- src/medium-collection/array-and-strings/divide-array/solution.ts
+++ src/medium-collection/array-and-strings/divide-array/solution.ts
@@ -1,3 +1,18 @@
 export function divideArray(nums: number[], k: number): number[][] {
-  return [];
+  // Sort the nums array to make it easier to find elements with differences ≤ k
+  nums.sort((a, b) => a - b);
+
+  const result: number[][] = [];
+
+  for (let i = 0; i < nums.length; i += 3) {
+    // Check if the difference between the max and min in the current sub-array of size 3 is ≤ k
+    if (nums[i + 2] - nums[i] <= k) {
+      result.push([nums[i], nums[i + 1], nums[i + 2]]);
+    } else {
+      // If it's not possible to satisfy the condition, return an empty array
+      return [];
+    }
+  }
+
+  return result;
 }

28efd01db9e3e56798798e1bca6a9cc14db1291f
Maxim Kryuk <mkryuk@gmail.com>
2024-02-06 19:30:52 +0200

    Array and Strings: Find Missing and Repeated Values solution

--- src/easy-collection/array/find-missing-and-repeated-values/solution.ts
+++ src/easy-collection/array/find-missing-and-repeated-values/solution.ts
@@ -1,3 +1,16 @@
 export function findMissingAndRepeatedValues(grid: number[][]): number[] {
-  return [];
+  let n = grid.length;
+  let numbers = new Array(n * n).fill(0);
+  let duplicated = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (numbers[grid[i][j] - 1] !== 0) {
+        duplicated = grid[i][j];
+      } else {
+        numbers[grid[i][j] - 1] = 1;
+      }
+    }
+  }
+  const missing = numbers.findIndex((n) => n === 0) + 1;
+  return [duplicated, missing];
 }

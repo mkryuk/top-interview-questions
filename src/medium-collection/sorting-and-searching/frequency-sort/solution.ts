0de6f2cbe95397423df5bc4dfff22f1da5b764c9
Maxim Kryuk <mkryuk@gmail.com>
2024-02-07 19:00:30 +0200

    Sorting and Searching: Sort Characters By Frequency

--- src/medium-collection/sorting-and-searching/frequency-sort/solution.ts
+++ src/medium-collection/sorting-and-searching/frequency-sort/solution.ts
@@ -1,3 +1,12 @@
 export function frequencySort(s: string): string {
-  return "";
+  let map = new Map<string, number>();
+  for (const char of s) {
+    map.set(char, (map.get(char) ?? 0) + 1);
+  }
+  let result = "";
+  map = new Map([...map.entries()].sort(([ch1, fr1], [ch2, fr2]) => fr2 - fr1));
+  for (const [char, frequency] of map) {
+    result += char.repeat(frequency);
+  }
+  return result;
 }
--- src/medium-collection/sorting-and-searching/frequency-sort/test.spec.ts
+++ src/medium-collection/sorting-and-searching/frequency-sort/test.spec.ts
@@ -4,18 +4,24 @@ describe("Sort Characters By Frequency:", () => {
   it('frequencySort should return "eert" for s = "tree"', () => {
     const s = "tree";
     const result = frequencySort(s);
-    expect(result).toEqual("eert");
+    expect(result).toEqual("eetr");
   });
 
   it('frequencySort should return "aaaccc" for s = "cccaaa"', () => {
     const s = "cccaaa";
     const result = frequencySort(s);
-    expect(result).toEqual("aaaccc");
+    expect(result).toEqual("cccaaa");
   });
 
   it('frequencySort should return "bbAa" for s = "Aabb"', () => {
     const s = "Aabb";
     const result = frequencySort(s);
     expect(result).toEqual("bbAa");
+  });
+
+  it('frequencySort should return "eeeelloovtcd" for s = "loveleetcode"', () => {
+    const s = "loveleetcode";
+    const result = frequencySort(s);
+    expect(result).toEqual("eeeelloovtcd");
   });
 });

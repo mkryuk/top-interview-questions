f6b180ad19eb455f57701bd3e98650f1cacbc4d6
Maxim Kryuk <mkryuk@gmail.com>
2024-02-13 11:55:23 +0200

    Array and Strings: Find First Palindromic String in the Array solution

--- src/easy-collection/array/first-palindrome/solution.ts
+++ src/easy-collection/array/first-palindrome/solution.ts
@@ -1,3 +1,21 @@
 export function firstPalindrome(words: string[]): string {
+  for (let i = 0; i < words.length; i++) {
+    if (isPalindrome(words[i])) {
+      return words[i];
+    }
+  }
   return "";
+}
+
+function isPalindrome(word: string): boolean {
+  let first = 0;
+  let last = word.length - 1;
+  while (first < last) {
+    if (word[first] !== word[last]) {
+      return false;
+    }
+    first++;
+    last--;
+  }
+  return true;
 }

cd1f7abea9bc82b3a3d896136f3989085ce44a89
Maxim Kryuk <mkryuk@gmail.com>
2024-02-14 22:42:08 +0200

    Array and Strings: Rearrange Array Elements by Sign solution

--- src/medium-collection/array-and-strings/rearrange-array/solution.ts
+++ src/medium-collection/array-and-strings/rearrange-array/solution.ts
@@ -1,3 +1,16 @@
 export function rearrangeArray(nums: number[]): number[] {
-  return [];
+  const n = nums.length;
+  const result = new Array(n);
+  let evenIndex = 0;
+  let oddIndex = 1;
+  for (let i = 0; i < n; i++) {
+    if (nums[i] > 0) {
+      result[evenIndex] = nums[i];
+      evenIndex += 2;
+    } else {
+      result[oddIndex] = nums[i];
+      oddIndex += 2;
+    }
+  }
+  return result;
 }

4649545371d173b86fa67457ed47da46e5ed009f
Maxim Kryuk <mkryuk@gmail.com>
2024-02-15 13:27:39 +0200

    Array and Strings: Find Polygon With the Largest Perimeter solution

--- src/medium-collection/array-and-strings/longest-perimeter/solution.ts
+++ src/medium-collection/array-and-strings/longest-perimeter/solution.ts
@@ -1,3 +1,12 @@
 export function largestPerimeter(nums: number[]): number {
-  return 0;
+  nums.sort((a, b) => a - b);
+  let result = -1;
+  let prevSum = 0;
+  for (let num of nums) {
+    if (num < prevSum) {
+      result = prevSum + num;
+    }
+    prevSum += num;
+  }
+  return result;
 }

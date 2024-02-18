9040a01a4d24a3d3f795ed30340f842ca0157167
Maxim Kryuk <mkryuk@gmail.com>
2024-02-18 13:25:15 +0200

    Array and Strings: Meeting Rooms III init

---
+++ src/hard-collection/array-and-strings/most-booked/solution.ts
@@ -0,0 +1,3 @@
+export function mostBooked(n: number, meetings: [number, number][]): number {
+  return 0;
+}
---
+++ src/hard-collection/array-and-strings/most-booked/test.spec.ts
@@ -0,0 +1,26 @@
+import { mostBooked } from "./solution";
+
+describe("Meeting Rooms III:", () => {
+  it("mostBooked should return 0 for n = 2, meetings = [[0,10],[1,5],[2,7],[3,4]]", () => {
+    const n = 2;
+    const meetings: [number, number][] = [
+      [0, 10],
+      [1, 5],
+      [2, 7],
+      [3, 4],
+    ];
+    const result = mostBooked(n, meetings);
+  });
+
+  it("mostBooked should return 1 for n = 3, meetings = [[1,20],[2,10],[3,5],[4,9],[6,8]]", () => {
+    const n = 3;
+    const meetings: [number, number][] = [
+      [1, 20],
+      [2, 10],
+      [3, 5],
+      [4, 9],
+      [6, 8],
+    ];
+    const result = mostBooked(n, meetings);
+  });
+});

c424280049bca8c49f92da9b4101e00d837102ce
Maxim Kryuk <mkryuk@gmail.com>
2024-02-19 21:24:26 +0200

    Array and Strings: Meeting Rooms III solution

--- src/hard-collection/array-and-strings/most-booked/solution.ts
+++ src/hard-collection/array-and-strings/most-booked/solution.ts
@@ -1,3 +1,38 @@
 export function mostBooked(n: number, meetings: [number, number][]): number {
-  return 0;
+  const roomBookCount: number[] = new Array(n).fill(0);
+  const roomAvailableTime: number[] = new Array(n).fill(0);
+  meetings.sort((a, b) => a[0] - b[0]);
+
+  for (let i = 0; i < meetings.length; i++) {
+    const [start, end] = meetings[i];
+    let roomAvailable = false;
+    let minRoomIdx = -1;
+    let availableTime = Number.MAX_SAFE_INTEGER;
+    for (let j = 0; j < n; j++) {
+      if (roomAvailableTime[j] < availableTime) {
+        availableTime = roomAvailableTime[j];
+        minRoomIdx = j;
+      }
+      if (roomAvailableTime[j] <= start) {
+        roomAvailable = true;
+        roomBookCount[j]++;
+        roomAvailableTime[j] = end;
+        break;
+      }
+    }
+    if (!roomAvailable) {
+      roomBookCount[minRoomIdx]++;
+      roomAvailableTime[minRoomIdx] += end - start;
+    }
+  }
+
+  let bookedCount = -1;
+  let result = -1;
+  for (let i = 0; i < n; i++) {
+    if (roomBookCount[i] > bookedCount) {
+      bookedCount = roomBookCount[i];
+      result = i;
+    }
+  }
+  return result;
 }

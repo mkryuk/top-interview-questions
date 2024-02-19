f450fc33446599026a27dccbc173de48650e0190
Maxim Kryuk <mkryuk@gmail.com>
2024-02-19 23:21:26 +0200

    Math: Power of Two init

---
+++ src/easy-collection/math/power-of-two/test.spec.ts
@@ -0,0 +1,21 @@
+import { isPowerOfTwo } from "./solution";
+
+describe("Power of Two:", () => {
+  it("isPowerOfTwo should return true for n = 1", () => {
+    const n = 1;
+    const result = isPowerOfTwo(n);
+    expect(result).toBeTrue();
+  });
+
+  it("isPowerOfTwo should return true for n = 16", () => {
+    const n = 16;
+    const result = isPowerOfTwo(n);
+    expect(result).toBeTrue();
+  });
+
+  it("isPowerOfTwo should return true for n = 3", () => {
+    const n = 3;
+    const result = isPowerOfTwo(n);
+    expect(result).toBeFalse();
+  });
+});

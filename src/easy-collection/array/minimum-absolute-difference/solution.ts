1850501409f3e147968c89f0bf67829c0ca6fd61
Maxim Kryuk <mkryuk@gmail.com>
2024-02-21 14:33:39 +0200

    Array and Strings: Minimum Absolute Difference solution

--- src/easy-collection/array/minimum-absolute-difference/solution.ts
+++ src/easy-collection/array/minimum-absolute-difference/solution.ts
@@ -1,3 +1,15 @@
 export function minimumAbsDifference(arr: number[]): number[][] {
-  return [];
+  let result: number[][] = [];
+  let minDiff = Number.MAX_SAFE_INTEGER;
+  arr.sort((a, b) => a - b);
+  for (let i = 1; i < arr.length; i++) {
+    let diff = Math.abs(arr[i] - arr[i - 1]);
+    if (diff < minDiff) {
+      minDiff = diff;
+      result = [[arr[i - 1], arr[i]]];
+    } else if (diff === minDiff) {
+      result.push([arr[i - 1], arr[i]]);
+    }
+  }
+  return result;
 }

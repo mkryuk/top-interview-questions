d770ff43bafe77f903b5e9e659e4bd43da58eead
Maxim Kryuk <mkryuk@gmail.com>
2024-02-23 23:42:13 +0200

    Trees and Graphs: Cheapest Flights Within K Stops init

---
+++ src/medium-collection/trees-and-graphs/find-cheapest-price/solution.ts
@@ -0,0 +1,9 @@
+export function findCheapestPrice(
+  n: number,
+  flights: number[][],
+  src: number,
+  dst: number,
+  k: number,
+): number {
+  return 0;
+}
---
+++ src/medium-collection/trees-and-graphs/find-cheapest-price/test.spec.ts
@@ -0,0 +1,47 @@
+import { findCheapestPrice } from "./solution";
+
+describe("Cheapest Flights Within K Stops:", () => {
+  it("findCheapestPrice should return 700 for test 1", () => {
+    const n = 4;
+    const flights = [
+      [0, 1, 100],
+      [1, 2, 100],
+      [2, 0, 100],
+      [1, 3, 600],
+      [2, 3, 200],
+    ];
+    const src = 0;
+    const dst = 3;
+    const k = 1;
+    const result = findCheapestPrice(n, flights, src, dst, k);
+    expect(result).toEqual(700);
+  });
+
+  it("findCheapestPrice should return 200 for test 2", () => {
+    const n = 3;
+    const flights = [
+      [0, 1, 100],
+      [1, 2, 100],
+      [0, 2, 500],
+    ];
+    const src = 0;
+    const dst = 2;
+    const k = 1;
+    const result = findCheapestPrice(n, flights, src, dst, k);
+    expect(result).toEqual(200);
+  });
+
+  it("findCheapestPrice should return 500 for test 3", () => {
+    const n = 3;
+    const flights = [
+      [0, 1, 100],
+      [1, 2, 100],
+      [0, 2, 500],
+    ];
+    const src = 0;
+    const dst = 2;
+    const k = 0;
+    const result = findCheapestPrice(n, flights, src, dst, k);
+    expect(result).toEqual(500);
+  });
+});

814ca0f1d720117e21a2f6f655d1767c308b428c
Maxim Kryuk <mkryuk@gmail.com>
2024-02-25 00:33:53 +0200

    Trees and Graphs: Find All People With Secret init

---
+++ src/hard-collection/trees-and-graphs/find-all-people/solution.ts
@@ -0,0 +1,7 @@
+export function findAllPeople(
+  n: number,
+  meetings: number[][],
+  firstPerson: number,
+): number[] {
+  return [];
+}
---
+++ src/hard-collection/trees-and-graphs/find-all-people/test.spec.ts
@@ -0,0 +1,39 @@
+import { findAllPeople } from "./solution";
+
+describe("Find All People With Secret:", () => {
+  it("findAllPeople should return [0,1,2,3,5] for test 1", () => {
+    const n = 6;
+    const meetings = [
+      [1, 2, 5],
+      [2, 3, 8],
+      [1, 5, 10],
+    ];
+    const firstPerson = 1;
+    const result = findAllPeople(n, meetings, firstPerson);
+    expect(result).toEqual([0, 1, 2, 3, 5]);
+  });
+
+  it("findAllPeople should return [0,1,3] for test 2", () => {
+    const n = 4,
+      meetings = [
+        [3, 1, 3],
+        [1, 2, 2],
+        [0, 3, 3],
+      ];
+    const firstPerson = 3;
+    const result = findAllPeople(n, meetings, firstPerson);
+    expect(result).toEqual([0, 1, 3]);
+  });
+
+  it("findAllPeople should return [0,1,2,3,4] for test 3", () => {
+    const n = 5;
+    const meetings = [
+      [3, 4, 2],
+      [1, 2, 1],
+      [2, 3, 1],
+    ];
+    const firstPerson = 1;
+    const result = findAllPeople(n, meetings, firstPerson);
+    expect(result).toEqual([0, 1, 2, 3, 4]);
+  });
+});

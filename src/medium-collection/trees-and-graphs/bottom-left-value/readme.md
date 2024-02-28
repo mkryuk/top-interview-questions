ff532f436e0cf5e6355131e2c4af99f381b5da73
Maxim Kryuk <mkryuk@gmail.com>
2024-02-28 14:28:05 +0200

    Trees and Graphs: Find Bottom Left Tree Value init

---
+++ src/medium-collection/trees-and-graphs/bottom-left-value/readme.md
@@ -0,0 +1,26 @@
+# Find Bottom Left Tree Value
+
+Given the root of a binary tree, return the leftmost value in the last row of the tree.
+
+## Example 1:
+
+![Example 1](./images/ex1.jpeg)
+
+```
+Input: root = [2,1,3]
+Output: 1
+```
+
+## Example 2:
+
+![Example 2](./images/ex2.jpeg)
+
+```
+Input: root = [1,2,3,4,null,5,6,null,null,7]
+Output: 7
+```
+
+## Constraints:
+
+- The number of nodes in the tree is in the range [1, 104].
+- -2^31 <= Node.val <= 2^31 - 1
---
+++ src/medium-collection/trees-and-graphs/bottom-left-value/test.spec.ts
@@ -0,0 +1,18 @@
+import { arrayToTreeNode } from "../common";
+import { findBottomLeftValue } from "./solution";
+
+describe("Find Bottom Left Tree Value:", () => {
+  it("findBottomLeftValue should return 1 for root = [2,1,3]", () => {
+    const nodes = [2, 1, 3];
+    const root = arrayToTreeNode(nodes);
+    const result = findBottomLeftValue(root);
+    expect(result).toEqual(1);
+  });
+
+  it("findBottomLeftValue should return 7 for root = [1,2,3,4,null,5,6,null,null,7]", () => {
+    const nodes = [1, 2, 3, 4, null, 5, 6, null, null, 7];
+    const root = arrayToTreeNode(nodes);
+    const result = findBottomLeftValue(root);
+    expect(result).toEqual(1);
+  });
+});

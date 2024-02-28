63e53d62f7d042a03c537b1f8fca229ae7e37d0e
Maxim Kryuk <mkryuk@gmail.com>
2024-02-28 14:17:03 +0200

    Array and Strings: Reformat Date solution

--- src/easy-collection/strings/reformat-date/solution.ts
+++ src/easy-collection/strings/reformat-date/solution.ts
@@ -1,3 +1,24 @@
 export function reformatDate(date: string): string {
-  return "";
+  const months: { [key: string]: string } = {
+    Jan: "01",
+    Feb: "02",
+    Mar: "03",
+    Apr: "04",
+    May: "05",
+    Jun: "06",
+    Jul: "07",
+    Aug: "08",
+    Sep: "09",
+    Oct: "10",
+    Nov: "11",
+    Dec: "12",
+  };
+  // Split the date string into its components
+  const [day, month, year] = date.split(" ");
+  // Remove the ordinal suffix and ensure day is in 2-digit format
+  const dayFormatted = ("0" + day.replace(/(st|nd|rd|th)/, "")).slice(-2);
+  // Use type assertion to assure TypeScript that `month` is a key of `months`
+  const monthFormatted = months[month as keyof typeof months];
+  // Return the formatted date string
+  return `${year}-${monthFormatted}-${dayFormatted}`;
 }

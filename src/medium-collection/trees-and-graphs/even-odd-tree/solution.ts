8feaeae6b89294836d72761e2e9e2be0d7d00009
Maxim Kryuk <mkryuk@gmail.com>
2024-02-29 20:20:10 +0200

    Trees and Graphs: Even Odd Tree solution

--- src/medium-collection/trees-and-graphs/even-odd-tree/solution.ts
+++ src/medium-collection/trees-and-graphs/even-odd-tree/solution.ts
@@ -1,7 +1,33 @@
 import { TreeNode } from "../common";
 
 export function isEvenOddTree(root: TreeNode | null): boolean {
-  return false;
+  if (root === null) {
+    return false;
+  }
+  let queue: TreeNode[] = [root];
+  let level = 0;
+  while (queue.length > 0) {
+    const size = queue.length;
+    let prevValue = level % 2 === 0 ? -Infinity : Infinity;
+    for (let i = 0; i < size; i++) {
+      let node = queue.shift()!;
+      if (
+        (level % 2 === 0 && (node.val % 2 === 0 || node.val <= prevValue)) ||
+        (level % 2 !== 0 && (node.val % 2 !== 0 || node.val >= prevValue))
+      ) {
+        return false;
+      }
+      prevValue = node.val;
+      if (node.left !== null) {
+        queue.push(node.left);
+      }
+      if (node.right !== null) {
+        queue.push(node.right);
+      }
+    }
+    level++;
+  }
+  return true;
 }
 
 export function arrayToTreeNode(arr: (number | null)[]): TreeNode | null {

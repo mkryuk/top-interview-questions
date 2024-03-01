5fcfd75221475b4996b379463879c954f47e930f
Maxim Kryuk <mkryuk@gmail.com>
2024-03-01 17:48:37 +0200

    Math: Maximum Odd Binary Number solution

--- src/easy-collection/math/maximum-odd-binary-number/solution.ts
+++ src/easy-collection/math/maximum-odd-binary-number/solution.ts
@@ -1,3 +1,6 @@
 export function maximumOddBinaryNumber(s: string): string {
-  return "";
+  const onesCount = s.split("").filter((char) => char === "1").length;
+  let result =
+    "1".repeat(onesCount - 1) + "0".repeat(s.length - onesCount) + "1";
+  return result;
 }

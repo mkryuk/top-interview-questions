7a42698fd4223b5ccadb3537b655cf6a703ca134
Maxim Kryuk <mkryuk@gmail.com>
2024-03-02 12:11:49 +0200

    Math: Squares of a Sorted Array solution

--- src/easy-collection/math/sorted-squares/solution.ts
+++ src/easy-collection/math/sorted-squares/solution.ts
@@ -1,3 +1,18 @@
 export function sortedSquares(nums: number[]): number[] {
-  return [];
+  const n = nums.length;
+  let left = 0;
+  let right = n - 1;
+  let lastIndex = n - 1;
+  let result = new Array(n);
+  while (left <= right) {
+    if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+      result[lastIndex] = Math.pow(nums[left], 2);
+      left++;
+    } else {
+      result[lastIndex] = Math.pow(nums[right], 2);
+      right--;
+    }
+    lastIndex--;
+  }
+  return result;
 }

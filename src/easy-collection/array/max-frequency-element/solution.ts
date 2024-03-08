1196c708024cc7eec4531b02f44cf8b3be09a52b
Maxim Kryuk <mkryuk@gmail.com>
2024-03-08 22:30:47 +0200

    Array and Strings: Count Elements With Maximum Frequency init

---
+++ src/easy-collection/array/max-frequency-element/solution.ts
@@ -0,0 +1,3 @@
+export function maxFrequencyElements(nums: number[]): number {
+  return 0;
+}
---
+++ src/easy-collection/array/max-frequency-element/test.spec.ts
@@ -0,0 +1,15 @@
+import { maxFrequencyElements } from "./solution";
+
+describe("Count Elements With Maximum Frequency:", () => {
+  it("maxFrequencyElements should return 4 for nums = [1,2,2,3,1,4]", () => {
+    const nums = [1, 2, 2, 3, 1, 4];
+    const result = maxFrequencyElements(nums);
+    expect(result).toEqual(4);
+  });
+
+  it("maxFrequencyElements should return 5 for nums = [1,2,3,4,5]", () => {
+    const nums = [1, 2, 3, 4, 5];
+    const result = maxFrequencyElements(nums);
+    expect(result).toEqual(5);
+  });
+});

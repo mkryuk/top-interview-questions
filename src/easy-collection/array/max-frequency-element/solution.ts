33285698e6368563c6dd0a8c5779e3ac0823af0f
Maxim Kryuk <mkryuk@gmail.com>
2024-03-08 22:53:05 +0200

    Array and Strings: Count Elements With Maximum Frequency solution

--- src/easy-collection/array/max-frequency-element/solution.ts
+++ src/easy-collection/array/max-frequency-element/solution.ts
@@ -1,3 +1,22 @@
 export function maxFrequencyElements(nums: number[]): number {
-  return 0;
+  let frequencyMap = new Map<number, number>();
+  for (const num of nums) {
+    frequencyMap.set(num, (frequencyMap.get(num) ?? 0) + 1);
+  }
+
+  let maxCount = 0;
+  for (const [num, count] of frequencyMap) {
+    if (count > maxCount) {
+      maxCount = count;
+    }
+  }
+
+  let result = 0;
+  for (const [num, count] of frequencyMap) {
+    if (count === maxCount) {
+      result += count;
+    }
+  }
+
+  return result;
 }

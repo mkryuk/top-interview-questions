09931e878bbdb60b95913150bc5e982feb140d1c
Maxim Kryuk <mkryuk@gmail.com>
2024-03-09 13:38:16 +0200

    Array and Strings: Minimum Common Value solution

--- src/easy-collection/array/minimum-common-value/solution.ts
+++ src/easy-collection/array/minimum-common-value/solution.ts
@@ -1,3 +1,14 @@
 export function getCommon(nums1: number[], nums2: number[]): number {
-  return 0;
+  let index1 = 0;
+  let index2 = 0;
+  while (index1 < nums1.length && index2 < nums2.length) {
+    if (nums1[index1] < nums2[index2]) {
+      index1++;
+    } else if (nums1[index1] > nums2[index2]) {
+      index2++;
+    } else {
+      return nums1[index1];
+    }
+  }
+  return -1;
 }

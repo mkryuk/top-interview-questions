e1a6ea3f0a2ab2d5a0ecf6a53ca40980df50b40f
Maxim Kryuk <mkryuk@gmail.com>
2024-03-10 23:16:15 +0200

    Array and Strings: Intersection of Two Arrays solution

--- src/easy-collection/array/intersection-of-two-arrays/solution.ts
+++ src/easy-collection/array/intersection-of-two-arrays/solution.ts
@@ -1,3 +1,10 @@
 export function intersection(nums1: number[], nums2: number[]): number[] {
-  return [];
+  const nums1Set = new Set(nums1);
+  const resultSet = new Set<number>();
+  for (const num of nums2) {
+    if (nums1Set.has(num)) {
+      resultSet.add(num);
+    }
+  }
+  return Array.from(resultSet);
 }

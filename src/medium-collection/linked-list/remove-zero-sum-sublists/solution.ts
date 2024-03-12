f53afdb2d899f7f9010a2121b0f5581593e9b10a
Maxim Kryuk <mkryuk@gmail.com>
2024-03-12 23:25:57 +0200

    Linked List: Remove Zero Sum Consecutive Nodes from Linked List solution

--- src/medium-collection/linked-list/remove-zero-sum-sublists/solution.ts
+++ src/medium-collection/linked-list/remove-zero-sum-sublists/solution.ts
@@ -1,5 +1,22 @@
 import { ListNode } from "../common";
 
 export function removeZeroSumSublists(head: ListNode | null): ListNode | null {
-  return null;
+  let dummy: ListNode | null = new ListNode(0, head);
+
+  const prefixSumMap = new Map<number, ListNode>();
+  let prefixVal = 0;
+  for (let node: ListNode | null = dummy; node !== null; node = node.next) {
+    prefixVal += node.val;
+    prefixSumMap.set(prefixVal, node);
+  }
+
+  prefixVal = 0;
+  for (let node: ListNode | null = dummy; node !== null; node = node.next) {
+    prefixVal += node.val;
+    if (prefixSumMap.has(prefixVal)) {
+      node.next = prefixSumMap.get(prefixVal)!.next;
+    }
+  }
+
+  return dummy.next;
 }

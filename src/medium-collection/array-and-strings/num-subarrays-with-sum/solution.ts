eda032b6c4c8b853327928667d61f9ce3ec0854e
Maxim Kryuk <mkryuk@gmail.com>
2024-03-15 15:36:29 +0200

    Array and Strings: Binary Subarrays With Sum solution

--- src/medium-collection/array-and-strings/num-subarrays-with-sum/solution.ts
+++ src/medium-collection/array-and-strings/num-subarrays-with-sum/solution.ts
@@ -1,3 +1,14 @@
 export function numSubarraysWithSum(nums: number[], goal: number): number {
-  return 0;
+  let prefixSumCount = new Map<number, number>();
+  let prefixSum = 0;
+  prefixSumCount.set(0, 1);
+  let result = 0;
+  for (let num of nums) {
+    prefixSum += num;
+    if (prefixSumCount.has(prefixSum - goal)) {
+      result += prefixSumCount.get(prefixSum - goal)!;
+    }
+    prefixSumCount.set(prefixSum, (prefixSumCount.get(prefixSum) ?? 0) + 1);
+  }
+  return result;
 }

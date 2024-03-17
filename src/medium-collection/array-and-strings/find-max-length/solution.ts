6ae870d938b8e5b51300dd3c7482e0c2da11c35c
Maxim Kryuk <mkryuk@gmail.com>
2024-03-17 22:44:48 +0200

    Array and Strings: Contiguous Array solution

--- src/medium-collection/array-and-strings/find-max-length/solution.ts
+++ src/medium-collection/array-and-strings/find-max-length/solution.ts
@@ -1,3 +1,15 @@
 export function findMaxLength(nums: number[]): number {
-  return 0;
+  let result = 0;
+  // initialize with 0 at the index -1
+  let map = new Map<number, number>([[0, -1]]);
+  let prefixSum = 0;
+  for (let index = 0; index < nums.length; index++) {
+    prefixSum += nums[index] === 1 ? 1 : -1;
+    if (map.has(prefixSum)) {
+      result = Math.max(result, index - map.get(prefixSum)!);
+    } else {
+      map.set(prefixSum, index);
+    }
+  }
+  return result;
 }

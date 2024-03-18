24d113686299d7f25224ab7fed2a7aada56dc604
Maxim Kryuk <mkryuk@gmail.com>
2024-03-18 17:54:37 +0200

    Array and Strings: Insert Interval solution

--- src/medium-collection/array-and-strings/insert-interval/solution.ts
+++ src/medium-collection/array-and-strings/insert-interval/solution.ts
@@ -2,5 +2,28 @@ export function insert(
   intervals: number[][],
   newInterval: number[],
 ): number[][] {
-  return [];
+  let result: number[][] = [];
+
+  let i = 0;
+  // Add all intervals ending before newInterval starts
+  while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+    result.push(intervals[i]);
+    i++;
+  }
+
+  // Merge all overlapping intervals to one considering newInterval
+  while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+    newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+    newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+    i++;
+  }
+  result.push(newInterval);
+
+  // Add all the rest
+  while (i < intervals.length) {
+    result.push(intervals[i]);
+    i++;
+  }
+
+  return result;
 }

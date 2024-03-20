5c64e53568119b64bfa2c511083a0c7510c7b96b
Maxim Kryuk <mkryuk@gmail.com>
2024-03-20 18:42:08 +0200

    Linked List: Merge In Between Linked Lists solution

--- src/medium-collection/linked-list/merge-in-between/solution.ts
+++ src/medium-collection/linked-list/merge-in-between/solution.ts
@@ -6,5 +6,26 @@ export function mergeInBetween(
   b: number,
   list2: ListNode | null,
 ): ListNode | null {
-  return null;
+  let nodeA: ListNode | null = null;
+  let nodeB: ListNode | null = null;
+  let head: ListNode | null = list1;
+  let index = 0;
+  while (head !== null && index <= b + 1) {
+    // the node before a position
+    if (index === a - 1) {
+      nodeA = head;
+    }
+    // the node after b position
+    if (index === b) {
+      nodeB = head.next;
+    }
+    head = head.next;
+    index++;
+  }
+  nodeA!.next = list2;
+  while (list2 && list2.next) {
+    list2 = list2.next;
+  }
+  list2!.next = nodeB;
+  return list1;
 }

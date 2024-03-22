b55baa034aaa2d978aba4efe65454da7af66b152
Maxim Kryuk <mkryuk@gmail.com>
2024-03-22 23:28:36 +0200

    Array and Strings: Decrypt String from Alphabet to Integer Mapping solution

--- src/easy-collection/array/decrypt-alphabet/solution.ts
+++ src/easy-collection/array/decrypt-alphabet/solution.ts
@@ -1,3 +1,14 @@
 export function freqAlphabets(s: string): string {
-  return "";
+  let result: string[] = [];
+  for (let i = s.length - 1; i >= 0; i--) {
+    let num;
+    if (s[i] === "#") {
+      num = parseInt(s.substring(i - 2, i));
+      i -= 2;
+    } else {
+      num = parseInt(s[i]);
+    }
+    result.push(String.fromCharCode(96 + num));
+  }
+  return result.reverse().join("");
 }

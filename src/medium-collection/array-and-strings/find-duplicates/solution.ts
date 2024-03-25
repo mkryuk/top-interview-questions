ea8a56cb43ee880482447569f7036098cf9bfac2
Maxim Kryuk <mkryuk@gmail.com>
2024-03-26 00:15:09 +0200

    Array and Strings: Find All Duplicates in an Array solution

--- src/medium-collection/array-and-strings/find-duplicates/solution.ts
+++ src/medium-collection/array-and-strings/find-duplicates/solution.ts
@@ -1,3 +1,12 @@
 export function findDuplicates(nums: number[]): number[] {
-  return [];
+  const result: number[] = [];
+  for (let i = 0; i < nums.length; i++) {
+    let index = Math.abs(nums[i]) - 1;
+    if (nums[index] < 0) {
+      result.push(index + 1);
+    } else {
+      nums[index] = -nums[index];
+    }
+  }
+  return result;
 }

44c2bb6e3eba2b38459ffb4b39e5dfb2f9923f81
Maxim Kryuk <mkryuk@gmail.com>
2024-03-26 22:26:49 +0200

    Array and Strings: Maximum Product of Three Numbers solution

--- src/easy-collection/array/max-product/solution.ts
+++ src/easy-collection/array/max-product/solution.ts
@@ -1,3 +1,10 @@
 export function maximumProduct(nums: number[]): number {
-  return 0;
+  nums.sort((a, b) => a - b);
+  const n = nums.length;
+  // The largest product can either be from the three largest numbers
+  const prod1: number = nums[n - 1] * nums[n - 2] * nums[n - 3];
+  // Or from two smallest (possibly negative) and the largest number
+  const prod2: number = nums[0] * nums[1] * nums[n - 1];
+  const result = Math.max(prod1, prod2);
+  return result;
 }

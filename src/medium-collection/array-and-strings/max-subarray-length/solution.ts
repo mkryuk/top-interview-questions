5cc51b6e50b2596c118960c102712632202957d2
Maxim Kryuk <mkryuk@gmail.com>
2024-03-28 19:59:11 +0200

    Array and Strings: Length of Longest Subarray With at Most K Frequency solution

--- src/medium-collection/array-and-strings/max-subarray-length/solution.ts
+++ src/medium-collection/array-and-strings/max-subarray-length/solution.ts
@@ -1,3 +1,14 @@
 export function maxSubarrayLength(nums: number[], k: number): number {
-  return 0;
+  let result = 0;
+  let left = 0;
+  let frequencyMap = new Map<number, number>();
+  for (let right = 0; right < nums.length; right++) {
+    frequencyMap.set(nums[right], (frequencyMap.get(nums[right]) ?? 0) + 1);
+    while (frequencyMap.get(nums[right])! > k) {
+      frequencyMap.set(nums[left], frequencyMap.get(nums[left])! - 1);
+      left++;
+    }
+    result = Math.max(result, right - left + 1);
+  }
+  return result;
 }

c5f00aee0893eaac907130db8640a5554c52824e
Maxim Kryuk <mkryuk@gmail.com>
2024-03-29 23:22:53 +0200

    Array and Strings: Count Subarrays Where Max Element Appears at Least K Times solution

--- src/medium-collection/array-and-strings/count-subarrays/solution.ts
+++ src/medium-collection/array-and-strings/count-subarrays/solution.ts
@@ -1,3 +1,18 @@
 export function countSubarrays(nums: number[], k: number): number {
-  return 0;
+  let maxElement = Math.max(...nums);
+  let result = 0;
+  let maxElementIndexes: number[] = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === maxElement) {
+      maxElementIndexes.push(i);
+    }
+    const maxElementCount = maxElementIndexes.length;
+    if (maxElementCount >= k) {
+      // Add the number of subarrays ending at the current index
+      // where the maximum element appears at least k times.
+      result += maxElementIndexes[maxElementCount - k] + 1;
+    }
+  }
+
+  return result;
 }

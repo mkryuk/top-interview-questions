fa1e969c299ec9a0a722edc6f6776e268ea1af32
Maxim Kryuk <mkryuk@gmail.com>
2024-04-02 23:37:42 +0300

    Array and Strings: Isomorphic Strings init

---
+++ src/easy-collection/strings/is-isomorphic/test.spec.ts
@@ -0,0 +1,24 @@
+import { isIsomorphic } from "./solution";
+
+describe("Isomorphic Strings", () => {
+  it('isIsomorphic should return true for s = "egg", t = "add"', () => {
+    const s = "egg";
+    const t = "add";
+    const result = isIsomorphic(s, t);
+    expect(result).toBeTrue();
+  });
+
+  it('isIsomorphic should return false for s = "foo", t = "bar"', () => {
+    const s = "foo";
+    const t = "bar";
+    const result = isIsomorphic(s, t);
+    expect(result).toBeFalse();
+  });
+
+  it('isIsomorphic should return true for s = "paper", t = "title"', () => {
+    const s = "paper";
+    const t = "title";
+    const result = isIsomorphic(s, t);
+    expect(result).toBeTrue();
+  });
+});

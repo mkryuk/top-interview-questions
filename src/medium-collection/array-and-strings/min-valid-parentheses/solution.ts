cb2e57fbef4a0d88904b2ec51efc920908d3c804
Maxim Kryuk <mkryuk@gmail.com>
2024-04-07 00:11:58 +0300

    Array and Strings: Minimum Remove to Make Valid Parentheses solution

--- src/medium-collection/array-and-strings/min-valid-parentheses/solution.ts
+++ src/medium-collection/array-and-strings/min-valid-parentheses/solution.ts
@@ -1,3 +1,26 @@
 export function minRemoveToMakeValid(s: string): string {
-  return "";
+  let stack: number[] = [];
+  let indicesToRemove = new Set<number>();
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      stack.push(i);
+    } else if (s[i] === ")") {
+      if (stack.length > 0) {
+        stack.pop();
+      } else {
+        indicesToRemove.add(i);
+      }
+    }
+  }
+  for (const index of stack) {
+    indicesToRemove.add(index);
+  }
+
+  let result: string[] = [];
+  for (let i = 0; i < s.length; i++) {
+    if (!indicesToRemove.has(i)) {
+      result.push(s[i]);
+    }
+  }
+  return result.join("");
 }

882cbb213112a29af848cc25fa09073c42368d99
Maxim Kryuk <mkryuk@gmail.com>
2024-04-09 16:19:41 +0300

    Array and Strings: Time Needed to Buy Tickets solution

--- src/easy-collection/array/time-to-buy-tickets/solution.ts
+++ src/easy-collection/array/time-to-buy-tickets/solution.ts
@@ -1,3 +1,11 @@
 export function timeRequiredToBuy(tickets: number[], k: number): number {
-  return 0;
+  let result = 0;
+  for (let i = 0; i < tickets.length; i++) {
+    if (i <= k) {
+      result += Math.min(tickets[i], tickets[k]);
+    } else {
+      result += Math.min(tickets[i], tickets[k] - 1);
+    }
+  }
+  return result;
 }

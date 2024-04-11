4fa8e3d46cf076c1340200746e543c3cd15a2c0f
Maxim Kryuk <mkryuk@gmail.com>
2024-04-11 18:07:00 +0300

    Array and Strings: Reveal Cards In Increasing Order solution

--- src/medium-collection/array-and-strings/reveal-cards-increasing/solution.ts
+++ src/medium-collection/array-and-strings/reveal-cards-increasing/solution.ts
@@ -1,3 +1,11 @@
 export function deckRevealedIncreasing(deck: number[]): number[] {
-  return [];
+  deck.sort((a, b) => a - b);
+  let result: number[] = [];
+  for (let i = deck.length - 1; i >= 0; i--) {
+    if (result.length > 0) {
+      result.unshift(result.pop()!);
+    }
+    result.unshift(deck[i]);
+  }
+  return result;
 }

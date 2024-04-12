d7a47d36b2cd854c3650688979947da633709f1d
Maxim Kryuk <mkryuk@gmail.com>
2024-04-12 20:28:48 +0300

    Array and Strings: Remove K Digits solution

--- src/medium-collection/array-and-strings/remove-k-digits/solution.ts
+++ src/medium-collection/array-and-strings/remove-k-digits/solution.ts
@@ -1,3 +1,17 @@
 export function removeKdigits(num: string, k: number): string {
-  return "";
+  let stack: string[] = [];
+  for (let i = 0; i < num.length; i++) {
+    while (stack.length > 0 && stack[stack.length - 1] > num[i] && k > 0) {
+      stack.pop();
+      k--;
+    }
+    stack.push(num[i]);
+  }
+  while (k > 0) {
+    stack.pop();
+    k--;
+  }
+
+  const result = stack.join("").replace(/^0+/, "");
+  return result === "" ? "0" : result;
 }

12928462e509e9d93e05e473477d9c3cef0fa028
Maxim Kryuk <mkryuk@gmail.com>
2024-04-13 20:29:31 +0300

    Dynamic Programming: Maximal Rectangle init

---
+++ src/hard-collection/dynamic-programming/maximal-rectangle/test.spec.ts
@@ -0,0 +1,26 @@
+import { maximalRectangle } from "./solution";
+
+describe("Maximal Rectangle", () => {
+  it("maximalRectangle should return 6 for Example 1", () => {
+    const matrix = [
+      ["1", "0", "1", "0", "0"],
+      ["1", "0", "1", "1", "1"],
+      ["1", "1", "1", "1", "1"],
+      ["1", "0", "0", "1", "0"],
+    ];
+    const result = maximalRectangle(matrix);
+    expect(result).toEqual(6);
+  });
+
+  it('maximalRectangle should return 0 for matrix = [["0"]]', () => {
+    const matrix = [["0"]];
+    const result = maximalRectangle(matrix);
+    expect(result).toEqual(0);
+  });
+
+  it('maximalRectangle should return 1 for matrix = [["1"]]', () => {
+    const matrix = [["1"]];
+    const result = maximalRectangle(matrix);
+    expect(result).toEqual(1);
+  });
+});

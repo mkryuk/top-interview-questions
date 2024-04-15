a675e469eb0561722fb4987840c60b620c98e93a
Maxim Kryuk <mkryuk@gmail.com>
2024-04-15 19:04:31 +0300

    Trees and Graphs: Sum Root to Leaf Numbers solution

--- src/medium-collection/trees-and-graphs/sum-root-to-leaf/solution.ts
+++ src/medium-collection/trees-and-graphs/sum-root-to-leaf/solution.ts
@@ -1,5 +1,23 @@
 import { TreeNode } from "../common";
 
 export function sumNumbers(root: TreeNode | null): number {
-  return 0;
+  if (root === null) {
+    return 0;
+  }
+  const stack: [TreeNode, number][] = [[root, root.val]];
+  let result = 0;
+  while (stack.length > 0) {
+    const [node, num] = stack.pop()!;
+    if (node.left === null && node.right === null) {
+      result += num;
+    }
+    if (node.left !== null) {
+      stack.push([node.left, num * 10 + node.left.val]);
+    }
+    if (node.right !== null) {
+      stack.push([node.right, num * 10 + node.right.val]);
+    }
+  }
+
+  return result;
 }

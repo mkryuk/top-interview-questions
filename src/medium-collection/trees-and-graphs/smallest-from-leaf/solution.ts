96f3815ea869f4aed69fd21c86a459656a5175f1
Maxim Kryuk <mkryuk@gmail.com>
2024-04-18 00:22:22 +0300

    Trees and Graphs: Smallest String Starting From Leaf bfs solution

--- src/medium-collection/trees-and-graphs/smallest-from-leaf/solution.ts
+++ src/medium-collection/trees-and-graphs/smallest-from-leaf/solution.ts
@@ -1,5 +1,34 @@
 import { TreeNode } from "../common";
 
 export function smallestFromLeaf(root: TreeNode | null): string {
-  return "";
+  if (root === null) return "";
+
+  let queue: [TreeNode, string[]][] = [
+    [root, [String.fromCharCode("a".charCodeAt(0) + root.val)]],
+  ];
+  // Using "{" to ensure it is lexicographically higher than any valid character.
+  let result = "{";
+
+  while (queue.length > 0) {
+    const [node, path] = queue.shift()!;
+
+    if (node.left === null && node.right === null) {
+      const currentString = path.reverse().join("");
+      if (currentString < result) {
+        result = currentString;
+      }
+    }
+
+    if (node.left !== null) {
+      const char = String.fromCharCode("a".charCodeAt(0) + node.left.val);
+      queue.push([node.left, [...path, char]]);
+    }
+
+    if (node.right !== null) {
+      const char = String.fromCharCode("a".charCodeAt(0) + node.right.val);
+      queue.push([node.right, [...path, char]]);
+    }
+  }
+
+  return result;
 }

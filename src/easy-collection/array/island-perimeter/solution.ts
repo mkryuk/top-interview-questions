52834d0352f8eea96ebe9ec01d62ef50d49efb70
Maxim Kryuk <mkryuk@gmail.com>
2024-04-18 20:06:59 +0300

    Array and Strings: Island Perimeter solution

--- src/easy-collection/array/island-perimeter/solution.ts
+++ src/easy-collection/array/island-perimeter/solution.ts
@@ -1,3 +1,18 @@
 export function islandPerimeter(grid: number[][]): number {
-  return 0;
+  let result = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === 1) {
+        result += 4;
+        // Check for adjacent land cells and subtract shared edges
+        if (i > 0 && grid[i - 1][j] === 1) {
+          result -= 2;
+        }
+        if (j > 0 && grid[i][j - 1] === 1) {
+          result -= 2;
+        }
+      }
+    }
+  }
+  return result;
 }

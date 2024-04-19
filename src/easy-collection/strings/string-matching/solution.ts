4d59282037ab62557d7c5ee72af1caea30f45d7d
Maxim Kryuk <mkryuk@gmail.com>
2024-04-19 21:16:50 +0300

    Array and Strings: String Matching in an Array solution

--- src/easy-collection/strings/string-matching/solution.ts
+++ src/easy-collection/strings/string-matching/solution.ts
@@ -1,3 +1,13 @@
 export function stringMatching(words: string[]): string[] {
-  return [];
+  const result: string[] = [];
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < words.length; j++) {
+      if (i === j) {
+        continue;
+      } else if (words[i].includes(words[j]) && !result.includes(words[j])) {
+        result.push(words[j]);
+      }
+    }
+  }
+  return result;
 }

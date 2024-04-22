54f44528ae6ef7b169cd539f64ece8a3eb72e6cc
Maxim Kryuk <mkryuk@gmail.com>
2024-04-22 21:16:40 +0300

    Array and Strings: Open the Lock solution

--- src/medium-collection/array-and-strings/open-lock/solution.ts
+++ src/medium-collection/array-and-strings/open-lock/solution.ts
@@ -1,3 +1,32 @@
 export function openLock(deadends: string[], target: string): number {
-  return 0;
+  const queue: [string, number][] = [["0000", 0]];
+  const deadSet = new Set<string>(deadends);
+  const visited = new Set<string>();
+  if (deadSet.has("0000")) {
+    return -1;
+  }
+
+  while (queue.length > 0) {
+    const [currStr, turns] = queue.shift()!;
+    if (currStr === target) {
+      return turns;
+    }
+    for (let i = 0; i < 4; i++) {
+      const num = parseInt(currStr[i], 10);
+      const up = (num + 1) % 10;
+      const down = (num + 9) % 10;
+      const nextUp = currStr.slice(0, i) + up + currStr.slice(i + 1);
+      const nextDown = currStr.slice(0, i) + down + currStr.slice(i + 1);
+      if (!visited.has(nextUp) && !deadSet.has(nextUp)) {
+        visited.add(nextUp);
+        queue.push([nextUp, turns + 1]);
+      }
+      if (!visited.has(nextDown) && !deadSet.has(nextDown)) {
+        visited.add(nextDown);
+        queue.push([nextDown, turns + 1]);
+      }
+    }
+  }
+
+  return -1;
 }

a00369eca969f52bc99a175adbda852c0c206eb4
Maxim Kryuk <mkryuk@gmail.com>
2024-04-22 12:20:54 +0300

    Array and Strings: Count the Number of Special Characters I solution

--- src/easy-collection/strings/num-of-special-chars/solution.ts
+++ src/easy-collection/strings/num-of-special-chars/solution.ts
@@ -1,3 +1,20 @@
 export function numberOfSpecialChars(word: string): number {
-  return 0;
+  let smallLettersSet = new Set<string>();
+  let resultSet = new Set<string>();
+  for (const char of word) {
+    if (isSmallLetter(char)) {
+      smallLettersSet.add(char);
+    }
+  }
+  for (const char of word) {
+    if (!isSmallLetter(char) && smallLettersSet.has(char.toLowerCase())) {
+      resultSet.add(char);
+    }
+  }
+  return resultSet.size;
+}
+
+function isSmallLetter(char: string): boolean {
+  const charCode = char.charCodeAt(0);
+  return charCode > 96 && charCode < 123;
 }

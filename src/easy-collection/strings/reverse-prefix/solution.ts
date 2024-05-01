0133b728648e6a345667c9f608490ad3765fac5a
Maxim Kryuk <mkryuk@gmail.com>
2024-05-01 14:27:28 +0300

    Array and Strings: Reverse Prefix of Word solution

--- src/easy-collection/strings/reverse-prefix/solution.ts
+++ src/easy-collection/strings/reverse-prefix/solution.ts
@@ -1,3 +1,10 @@
 export function reversePrefix(word: string, ch: string): string {
-  return "";
+  const index = word.indexOf(ch);
+  const result =
+    word
+      .slice(0, index + 1)
+      .split("")
+      .reverse()
+      .join("") + word.slice(index + 1);
+  return result;
 }

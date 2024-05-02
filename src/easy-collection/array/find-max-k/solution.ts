d2cd6cc7aab0bf0310ed3a39aa5f4f474a5b9695
Maxim Kryuk <mkryuk@gmail.com>
2024-05-02 17:37:45 +0300

    Array and Strings: Largest Positive Integer That Exists With Its Negative solution

--- src/easy-collection/array/find-max-k/solution.ts
+++ src/easy-collection/array/find-max-k/solution.ts
@@ -1,3 +1,19 @@
 export function findMaxK(nums: number[]): number {
-  return 0;
+  nums.sort((a, b) => a - b);
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    if (nums[left] > 0) {
+      break;
+    }
+    const absLeft = Math.abs(nums[left]);
+    if (absLeft === nums[right]) {
+      return nums[right];
+    } else if (absLeft < nums[right]) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+  return -1;
 }

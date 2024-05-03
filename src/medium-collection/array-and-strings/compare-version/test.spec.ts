7135fbba892ce287f81bd763cbfa1823f479b280
Maxim Kryuk <mkryuk@gmail.com>
2024-05-03 14:48:49 +0300

    Array and Strings: Compare Version Numbers init

---
+++ src/medium-collection/array-and-strings/compare-version/test.spec.ts
@@ -0,0 +1,24 @@
+import { compareVersion } from "./solution";
+
+describe("Compare Version Numbers", () => {
+  it('compareVersion should return 0 for version1 = "1.01", version2 = "1.001"', () => {
+    const version1 = "1.01";
+    const version2 = "1.001";
+    const result = compareVersion(version1, version2);
+    expect(result).toEqual(0);
+  });
+
+  it('compareVersion should return 0 for version1 = "1.0", version2 = "1.0.0"', () => {
+    const version1 = "1.0";
+    const version2 = "1.0.0";
+    const result = compareVersion(version1, version2);
+    expect(result).toEqual(0);
+  });
+
+  it('compareVersion should return -1 for version1 = "0.1", version2 = "1.1"', () => {
+    const version1 = "0.1";
+    const version2 = "1.1";
+    const result = compareVersion(version1, version2);
+    expect(result).toEqual(-1);
+  });
+});

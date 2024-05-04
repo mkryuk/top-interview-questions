734f6746d5dc7deb907b61bb9b04071b3e5abf73
Maxim Kryuk <mkryuk@gmail.com>
2024-05-04 23:57:06 +0300

    Math: Number of Steps to Reduce a Number to Zero solution

--- src/easy-collection/math/number-of-steps/solution.ts
+++ src/easy-collection/math/number-of-steps/solution.ts
@@ -1,3 +1,12 @@
 export function numberOfSteps(num: number): number {
-  return 0;
+  let result = 0;
+  while (num !== 0) {
+    if (num % 2 === 0) {
+      num /= 2;
+    } else {
+      num -= 1;
+    }
+    result++;
+  }
+  return result;
 }

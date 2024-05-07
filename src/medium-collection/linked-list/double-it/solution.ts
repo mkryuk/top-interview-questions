774a4dba5a4c1415da66e41a6adbc4dd2e6902a8
Maxim Kryuk <mkryuk@gmail.com>
2024-05-07 18:39:22 +0300

    Linked List: Double a Number Represented as a Linked List solution

--- src/medium-collection/linked-list/double-it/solution.ts
+++ src/medium-collection/linked-list/double-it/solution.ts
@@ -1,5 +1,43 @@
 import { ListNode } from "../common";
 
 export function doubleIt(head: ListNode | null): ListNode | null {
-  return null;
+  if (head === null) {
+    return null;
+  }
+  // Reverse the linked list to handle doubling from the least significant digit.
+  head = reverseList(head);
+
+  let carry = 0;
+  let temp = head;
+
+  // Traverse the reversed list, double each digit, and handle carries.
+  while (temp !== null) {
+    const current = temp.val * 2 + carry;
+    temp.val = current % 10;
+    carry = Math.trunc(current / 10);
+    temp = temp.next;
+  }
+
+  // Reverse the list again to restore the original order.
+  head = reverseList(head);
+
+  // If there's a carry left after the last digit, prepend a new node.
+  if (carry > 0) {
+    head = new ListNode(carry, head);
+  }
+
+  return head;
+}
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let current: ListNode | null = head;
+  let prev: ListNode | null = null;
+  while (current !== null) {
+    const next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+
+  return prev;
 }

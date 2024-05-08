a4a0974f99ee231a188cfb44aefee9a198ad4cd7
Maxim Kryuk <mkryuk@gmail.com>
2024-05-08 23:15:22 +0300

    Array and Strings: Relative Ranks solution

--- src/easy-collection/array/relative-ranks/solution.ts
+++ src/easy-collection/array/relative-ranks/solution.ts
@@ -1,3 +1,19 @@
 export function findRelativeRanks(score: number[]): string[] {
-  return [];
+  const positionMap = new Map(
+    [...score].sort((a, b) => b - a).map((v, i) => [v, i + 1]),
+  );
+  const result: string[] = [];
+  for (const s of score) {
+    const position = positionMap.get(s)!;
+    if (position === 1) {
+      result.push("Gold Medal");
+    } else if (position === 2) {
+      result.push("Silver Medal");
+    } else if (position === 3) {
+      result.push("Bronze Medal");
+    } else {
+      result.push(`${position}`);
+    }
+  }
+  return result;
 }

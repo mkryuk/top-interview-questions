c4321735a2d29f9ddbed2f14f46d6d74bed55def
Maxim Kryuk <mkryuk@gmail.com>
2024-05-11 12:18:29 +0300

    Sorting and Searching: K-th Smallest Prime Fraction solution

--- src/medium-collection/sorting-and-searching/kth-smallest-fraction/solution.ts
+++ src/medium-collection/sorting-and-searching/kth-smallest-fraction/solution.ts
@@ -1,3 +1,25 @@
+import { Heap } from "../../../hard-collection/design/heap/heap";
+
 export function kthSmallestPrimeFraction(arr: number[], k: number): number[] {
-  return [];
+  let heap = new Heap<{ value: number; numIndex: number; denIndex: number }>(
+    (a, b) => a.value - b.value,
+  );
+
+  for (let i = 1; i < arr.length; i++) {
+    heap.push({ value: arr[0] / arr[i], numIndex: 0, denIndex: i });
+  }
+
+  for (let i = 1; i < k; i++) {
+    let { numIndex, denIndex } = heap.pop()!;
+    if (numIndex + 1 < denIndex) {
+      heap.push({
+        value: arr[numIndex + 1] / arr[denIndex],
+        numIndex: numIndex + 1,
+        denIndex: denIndex,
+      });
+    }
+  }
+
+  let { numIndex, denIndex } = heap.pop()!;
+  return [arr[numIndex], arr[denIndex]];
 }

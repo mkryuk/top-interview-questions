8241d0f3c174e6d48dfe11787b1aa70772ad8922
Maxim Kryuk <mkryuk@gmail.com>
2024-05-14 20:39:28 +0300

    Array and Strings: Path with Maximum Gold solution

--- src/medium-collection/array-and-strings/get-maximum-gold/solution.ts
+++ src/medium-collection/array-and-strings/get-maximum-gold/solution.ts
@@ -1,3 +1,31 @@
 export function getMaximumGold(grid: number[][]): number {
-  return 0;
+  let result = 0;
+  for (let col = 0; col < grid.length; col++) {
+    for (let row = 0; row < grid[0].length; row++) {
+      if (grid[col][row] > 0) {
+        result = Math.max(result, dfs(grid, col, row));
+      }
+    }
+  }
+  return result;
+}
+
+function dfs(grid: number[][], col: number, row: number): number {
+  const m = grid.length;
+  const n = grid[0].length;
+  if (col < 0 || col >= m || row < 0 || row >= n || grid[col][row] === 0) {
+    return 0;
+  }
+  const gold = grid[col][row];
+  grid[col][row] = 0;
+  const result =
+    gold +
+    Math.max(
+      dfs(grid, col + 1, row),
+      dfs(grid, col - 1, row),
+      dfs(grid, col, row + 1),
+      dfs(grid, col, row - 1),
+    );
+  grid[col][row] = gold;
+  return result;
 }
--- src/medium-collection/array-and-strings/get-maximum-gold/test.spec.ts
+++ src/medium-collection/array-and-strings/get-maximum-gold/test.spec.ts
@@ -22,4 +22,16 @@ describe("Path with Maximum Gold", () => {
     const result = getMaximumGold(grid);
     expect(result).toEqual(28);
   });
+
+  it("getMaximumGold should return 60 for grid = [[1,0,7,0,0,0],[2,0,6,0,1,0],[3,5,6,7,4,2],[4,3,1,0,2,0],[3,0,5,0,20,0]]", () => {
+    const grid = [
+      [1, 0, 7, 0, 0, 0],
+      [2, 0, 6, 0, 1, 0],
+      [3, 5, 6, 7, 4, 2],
+      [4, 3, 1, 0, 2, 0],
+      [3, 0, 5, 0, 20, 0],
+    ];
+    const result = getMaximumGold(grid);
+    expect(result).toEqual(60);
+  });
 });

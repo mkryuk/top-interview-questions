795b5162afa22afb6c4a8e6223e7f2900fbf6cd0
Maxim Kryuk <mkryuk@gmail.com>
2024-05-16 20:24:16 +0300

    Trees and Graphs: Evaluate Boolean Binary Tree solution

--- src/easy-collection/trees/evaluate-tree/solution.ts
+++ src/easy-collection/trees/evaluate-tree/solution.ts
@@ -1,5 +1,14 @@
 import { TreeNode } from "../common";
 
 export function evaluateTree(root: TreeNode | null): boolean {
-  return false;
+  if (root === null) {
+    return false;
+  }
+  if (root !== null && root.left === null && root.right === null) {
+    return root.val === 1;
+  }
+  const left = evaluateTree(root.left);
+  const right = evaluateTree(root.right);
+  const result = root.val === 2 ? left || right : left && right;
+  return result;
 }

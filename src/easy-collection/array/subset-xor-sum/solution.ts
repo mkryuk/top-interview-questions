fecb77e65863e5b8d132c58b77b33e8712065b74
Maxim Kryuk <mkryuk@gmail.com>
2024-05-20 23:23:02 +0300

    Array and Strings: Sum of All Subset XOR Totals solution

--- src/easy-collection/array/subset-xor-sum/solution.ts
+++ src/easy-collection/array/subset-xor-sum/solution.ts
@@ -1,3 +1,16 @@
 export function subsetXORSum(nums: number[]): number {
-  return 0;
+  let result = 0;
+  const n = nums.length;
+  function dfs(index: number, currentXOR: number) {
+    if (index === n) {
+      result += currentXOR;
+      return;
+    }
+    // Include nums[index] in the subset
+    dfs(index + 1, currentXOR ^ nums[index]);
+    // Exclude nums[index] from the subset
+    dfs(index + 1, currentXOR);
+  }
+  dfs(0, 0);
+  return result;
 }

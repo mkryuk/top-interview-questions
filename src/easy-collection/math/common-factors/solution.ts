223745bcf38fe728644cd34c8991e5d0040c25dd
Maxim Kryuk <mkryuk@gmail.com>
2024-05-22 13:03:21 +0300

    Math: Number of Common Factors solution

--- src/easy-collection/math/common-factors/solution.ts
+++ src/easy-collection/math/common-factors/solution.ts
@@ -1,3 +1,10 @@
 export function commonFactors(a: number, b: number): number {
-  return 0;
+  let result = 0;
+  let minNumber = Math.min(a, b);
+  for (let i = 1; i <= minNumber; i++) {
+    if (a % i === 0 && b % i === 0) {
+      result++;
+    }
+  }
+  return result;
 }

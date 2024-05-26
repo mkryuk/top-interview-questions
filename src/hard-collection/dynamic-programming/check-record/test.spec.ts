20fbb9d4c5e93d34786d7a7d920b5fc87c84c1b1
Maxim Kryuk <mkryuk@gmail.com>
2024-05-26 11:55:29 +0300

    Dynamic Programming: Student Attendance Record II init

---
+++ src/hard-collection/dynamic-programming/check-record/test.spec.ts
@@ -0,0 +1,21 @@
+import { checkRecord } from "./solution";
+
+describe("Student Attendance Record II", () => {
+  it("checkRecord should return 8 n = 2", () => {
+    const n = 2;
+    const result = checkRecord(n);
+    expect(result).toEqual(2);
+  });
+
+  it("checkRecord should return 3 n = 1", () => {
+    const n = 1;
+    const result = checkRecord(n);
+    expect(result).toEqual(3);
+  });
+
+  it("checkRecord should return 183236316 n = 10101", () => {
+    const n = 10101;
+    const result = checkRecord(n);
+    expect(result).toEqual(183236316);
+  });
+});

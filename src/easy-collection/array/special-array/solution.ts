8e4a174ef5eb1ab91045fa287350346617f784b8
Maxim Kryuk <mkryuk@gmail.com>
2024-05-28 00:24:24 +0300

    Array and Strings: Special Array With X Elements Greater Than or Equal X solution

--- src/easy-collection/array/special-array/solution.ts
+++ src/easy-collection/array/special-array/solution.ts
@@ -1,3 +1,28 @@
 export function specialArray(nums: number[]): number {
-  return 0;
+  nums.sort((a, b) => a - b);
+  const n = nums.length;
+  for (let i = 0; i <= nums.length; i++) {
+    const index = getIndex(i, nums);
+    let greaterNumbers = n - index;
+    if (greaterNumbers === i) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+function getIndex(n: number, nums: number[]): number {
+  let left = 0;
+  let right = nums.length;
+
+  while (left < right) {
+    let mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] < n) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  return left;
 }

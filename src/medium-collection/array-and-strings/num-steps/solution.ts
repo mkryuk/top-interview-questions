d23784d1e6d1fe18826db625a8a783a5248123f5
Maxim Kryuk <mkryuk@gmail.com>
2024-05-29 20:28:45 +0300

    Array and Strings: Number of Steps to Reduce a Number in Binary Representation to One solution

--- src/medium-collection/array-and-strings/num-steps/solution.ts
+++ src/medium-collection/array-and-strings/num-steps/solution.ts
@@ -1,3 +1,29 @@
 export function numSteps(s: string): number {
-  return 0;
+  let result = 0;
+  let arr = s.split("");
+  while (arr.length > 1) {
+    let lastIndex = arr.length - 1;
+    // if the last character is '0', it's even
+    // so divide by 2 (remove the last character)
+    if (arr[lastIndex] === "0") {
+      arr.pop();
+    } else {
+      // if the last character is '1', it's odd
+      // so we need to add 1
+      while (lastIndex >= 0 && arr[lastIndex] === "1") {
+        // set all trailing '1's to '0'
+        arr[lastIndex] = "0";
+        lastIndex--;
+      }
+      if (lastIndex >= 0) {
+        // set the first '0' from the end to '1'
+        arr[lastIndex] = "1";
+      } else {
+        // if all bits were '1', we need an extra bit (e.g., 111 + 1 = 1000)
+        arr.unshift("1");
+      }
+    }
+    result++;
+  }
+  return result;
 }

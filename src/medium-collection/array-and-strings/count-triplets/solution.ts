908e98adbaf851d3c5e63d1f9e7b50dcbae1c62e
Maxim Kryuk <mkryuk@gmail.com>
2024-05-31 11:40:04 +0300

    Array and Strings: Count Triplets That Can Form Two Arrays of Equal XOR solution

--- src/medium-collection/array-and-strings/count-triplets/solution.ts
+++ src/medium-collection/array-and-strings/count-triplets/solution.ts
@@ -1,3 +1,16 @@
 export function countTriplets(arr: number[]): number {
-  return 0;
+  const n = arr.length;
+  const prefixXor = new Array(n + 1).fill(0);
+  for (let i = 0; i < n; i++) {
+    prefixXor[i + 1] = prefixXor[i] ^ arr[i];
+  }
+  let result = 0;
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (prefixXor[j] === prefixXor[i + 1]) {
+        result += i - j;
+      }
+    }
+  }
+  return result;
 }

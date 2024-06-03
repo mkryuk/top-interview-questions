3abd7ffcdc5be14f007fec709780dfac2a3a2f00
Maxim Kryuk <mkryuk@gmail.com>
2024-06-03 15:16:26 +0300

    Array and Strings: Smallest Range I solution

--- src/easy-collection/array/smallest-range/solution.ts
+++ src/easy-collection/array/smallest-range/solution.ts
@@ -1,3 +1,15 @@
 export function smallestRangeI(nums: number[], k: number): number {
-  return 0;
+  let min = Infinity;
+  let max = -Infinity;
+  for (const num of nums) {
+    if (num < min) {
+      min = num;
+    }
+    if (num > max) {
+      max = num;
+    }
+  }
+  min += k;
+  max -= k;
+  return Math.max(0, max - min);
 }
--- src/easy-collection/array/smallest-range/test.spec.ts
+++ src/easy-collection/array/smallest-range/test.spec.ts
@@ -21,4 +21,11 @@ describe("Smallest Range I", () => {
     const result = smallestRangeI(nums, k);
     expect(result).toEqual(0);
   });
+
+  it("smallestRangeI should return 0 for nums = [9,9,2,8,7], k = 4", () => {
+    const nums = [9, 9, 2, 8, 7];
+    const k = 4;
+    const result = smallestRangeI(nums, k);
+    expect(result).toEqual(0);
+  });
 });

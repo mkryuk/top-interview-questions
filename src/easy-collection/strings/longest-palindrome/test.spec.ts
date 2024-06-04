e5dfa78585dd2889b79b3880b4425d08c611cb5b
Maxim Kryuk <mkryuk@gmail.com>
2024-06-04 16:59:43 +0300

    Array and Strings: Longest Palindrome init

---
+++ src/easy-collection/strings/longest-palindrome/test.spec.ts
@@ -0,0 +1,15 @@
+import { longestPalindrome } from "./solution";
+
+describe("Longest Palindrome", () => {
+  it('longestPalindrome should return 7 for s = "abccccdd"', () => {
+    const s = "abccccdd";
+    const result = longestPalindrome(s);
+    expect(result).toEqual(7);
+  });
+
+  it('longestPalindrome should return 1 for s = "a"', () => {
+    const s = "a";
+    const result = longestPalindrome(s);
+    expect(result).toEqual(1);
+  });
+});

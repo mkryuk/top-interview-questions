db9e3156685b2ad49888c273067c3eb833eeebca
Maxim Kryuk <mkryuk@gmail.com>
2024-06-07 12:23:05 +0300

    Array and Strings: Replace Words solution

--- src/medium-collection/array-and-strings/replace-words/solution.ts
+++ src/medium-collection/array-and-strings/replace-words/solution.ts
@@ -1,3 +1,13 @@
 export function replaceWords(dictionary: string[], sentence: string): string {
-  return "";
+  dictionary.sort((a, b) => a.length - b.length);
+  const words = sentence.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    for (const root of dictionary) {
+      if (words[i].startsWith(root)) {
+        words[i] = root;
+        break;
+      }
+    }
+  }
+  return words.join(" ");
 }

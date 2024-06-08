62c545f38b15c3495c69dff83afc7256685cb5e5
Maxim Kryuk <mkryuk@gmail.com>
2024-06-08 12:04:21 +0300

    Array and Strings: Continuous Subarray Sum init

---
+++ src/medium-collection/array-and-strings/check-subarray-sum/test.spec.ts
@@ -0,0 +1,24 @@
+import { checkSubarraySum } from "./solution";
+
+describe("Continuous Subarray Sum", () => {
+  it("checkSubarraySum should return true for nums = [23,2,4,6,7], k = 6", () => {
+    const nums = [23, 2, 4, 6, 7];
+    const k = 6;
+    const result = checkSubarraySum(nums, k);
+    expect(result).toBeTrue();
+  });
+
+  it("checkSubarraySum should return true for nums = [23,2,6,4,7], k = 6", () => {
+    const nums = [23, 2, 6, 4, 7];
+    const k = 6;
+    const result = checkSubarraySum(nums, k);
+    expect(result).toBeTrue();
+  });
+
+  it("checkSubarraySum should return false for nums = [23,2,6,4,7], k = 13", () => {
+    const nums = [23, 2, 6, 4, 7];
+    const k = 13;
+    const result = checkSubarraySum(nums, k);
+    expect(result).toBeFalse();
+  });
+});

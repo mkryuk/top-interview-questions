3efe91d1201d11dbfe3dc7e2b6764a912d4f4eba
Maxim Kryuk <mkryuk@gmail.com>
2024-06-10 13:55:17 +0300

    Array and Strings: Height Checker solution

--- src/easy-collection/array/height-checker/solution.ts
+++ src/easy-collection/array/height-checker/solution.ts
@@ -1,3 +1,10 @@
 export function heightChecker(heights: number[]): number {
-  return 0;
+  const expected = [...heights].sort((a, b) => a - b);
+  let result = 0;
+  for (let i = 0; i < heights.length; i++) {
+    if (heights[i] !== expected[i]) {
+      result++;
+    }
+  }
+  return result;
 }

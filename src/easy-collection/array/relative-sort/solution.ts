7532332d273359aa011bb27a02c2194a75e170c0
Maxim Kryuk <mkryuk@gmail.com>
2024-06-11 13:58:27 +0300

    Array and Strings: Relative Sort Array solution

--- src/easy-collection/array/relative-sort/solution.ts
+++ src/easy-collection/array/relative-sort/solution.ts
@@ -1,3 +1,20 @@
 export function relativeSortArray(arr1: number[], arr2: number[]): number[] {
-  return [];
+  const orderMap = new Map<number, number>();
+  for (let i = 0; i < arr2.length; i++) {
+    orderMap.set(arr2[i], i);
+  }
+
+  arr1.sort((a, b) => {
+    if (orderMap.has(a) && orderMap.has(b)) {
+      return orderMap.get(a)! - orderMap.get(b)!;
+    } else if (orderMap.has(a)) {
+      return -1;
+    } else if (orderMap.has(b)) {
+      return 1;
+    } else {
+      return a - b;
+    }
+  });
+
+  return arr1;
 }

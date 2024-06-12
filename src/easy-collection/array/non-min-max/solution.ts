c4ff302220fff1995acc4ec9ede2986140da622d
Maxim Kryuk <mkryuk@gmail.com>
2024-06-12 14:34:02 +0300

    Array and Strings: Neither Minimum nor Maximum solution

--- src/easy-collection/array/non-min-max/solution.ts
+++ src/easy-collection/array/non-min-max/solution.ts
@@ -1,3 +1,14 @@
 export function findNonMinOrMax(nums: number[]): number {
-  return 0;
+  let min = Infinity;
+  let max = -Infinity;
+  for (const num of nums) {
+    min = Math.min(num, min);
+    max = Math.max(num, max);
+  }
+  for (const num of nums) {
+    if (num !== min && num !== max) {
+      return num;
+    }
+  }
+  return -1;
 }

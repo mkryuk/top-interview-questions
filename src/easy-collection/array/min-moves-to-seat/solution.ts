e950e00872783d8fac68a3d515b3e1a8aea3ccb5
Maxim Kryuk <mkryuk@gmail.com>
2024-06-13 12:13:40 +0300

    Array and Strings: Minimum Number of Moves to Seat Everyone solution

--- src/easy-collection/array/min-moves-to-seat/solution.ts
+++ src/easy-collection/array/min-moves-to-seat/solution.ts
@@ -1,3 +1,9 @@
 export function minMovesToSeat(seats: number[], students: number[]): number {
-  return 0;
+  seats.sort((a, b) => a - b);
+  students.sort((a, b) => a - b);
+  let result = 0;
+  for (let i = 0; i < seats.length; i++) {
+    result += Math.abs(seats[i] - students[i]);
+  }
+  return result;
 }

7e7b81a49a470c275360cb0caf8f9d1fe9276f84
Maxim Kryuk <mkryuk@gmail.com>
2024-06-15 00:35:51 +0300

    Array and Strings: Minimum Increment to Make Array Unique solution

--- src/medium-collection/array-and-strings/min-increment/solution.ts
+++ src/medium-collection/array-and-strings/min-increment/solution.ts
@@ -1,3 +1,27 @@
 export function minIncrementForUnique(nums: number[]): number {
-  return 0;
+  const max = Math.max(...nums);
+  const countArray = new Array(max + 1).fill(0);
+  for (const num of nums) {
+    countArray[num]++;
+  }
+  let nextAvailable = 0;
+  let result = 0;
+  for (let num = 0; num < countArray.length; num++) {
+    while (countArray[num] > 1) {
+      // Find the next available number
+      nextAvailable = Math.max(nextAvailable, num);
+      while (nextAvailable < max && countArray[nextAvailable] !== 0) {
+        nextAvailable++;
+      }
+      if (nextAvailable >= max) {
+        nextAvailable++;
+      }
+      // the number of moves is the diff between next and the current num
+      result += nextAvailable - num;
+      countArray[nextAvailable]++;
+      countArray[num]--;
+    }
+  }
+
+  return result;
 }

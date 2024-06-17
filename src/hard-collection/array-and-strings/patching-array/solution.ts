a9ec87aecfabc6259bd71035d9ad8ef6525f93ad
Maxim Kryuk <mkryuk@gmail.com>
2024-06-17 11:14:05 +0300

    Array and Strings: Patching Array solution

--- src/hard-collection/array-and-strings/patching-array/solution.ts
+++ src/hard-collection/array-and-strings/patching-array/solution.ts
@@ -1,3 +1,15 @@
 export function minPatches(nums: number[], n: number): number {
-  return 0;
+  let result = 0;
+  let missing = 1;
+  let index = 0;
+  while (missing <= n) {
+    if (index < nums.length && nums[index] <= missing) {
+      missing += nums[index];
+      index++;
+    } else {
+      missing += missing;
+      result++;
+    }
+  }
+  return result;
 }

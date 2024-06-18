80ae7856d1478a41e712b75d3b1e987ec80848c4
Maxim Kryuk <mkryuk@gmail.com>
2024-06-18 23:25:41 +0300

    Array and Strings: Most Profit Assigning Work solution

--- src/medium-collection/array-and-strings/max-profit-assignment/solution.ts
+++ src/medium-collection/array-and-strings/max-profit-assignment/solution.ts
@@ -3,5 +3,21 @@ export function maxProfitAssignment(
   profit: number[],
   worker: number[],
 ): number {
-  return 0;
+  let difficultyProfit = difficulty
+    .map((diff, index) => [diff, profit[index]])
+    .sort((a, b) => b[1] - a[1]);
+  worker.sort((a, b) => b - a);
+  let wIndex = 0;
+  let dpIndex = 0;
+  let result = 0;
+  while (wIndex < worker.length && dpIndex < difficultyProfit.length) {
+    const [diff, profit] = difficultyProfit[dpIndex];
+    if (worker[wIndex] >= diff) {
+      result += profit;
+      wIndex++;
+    } else {
+      dpIndex++;
+    }
+  }
+  return result;
 }

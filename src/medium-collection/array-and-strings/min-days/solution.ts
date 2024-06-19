bf848b224d02d6d7d617652f9405ee02aeca963e
Maxim Kryuk <mkryuk@gmail.com>
2024-06-20 00:13:51 +0300

    Array and Strings: Minimum Number of Days to Make m Bouquets solution

--- src/medium-collection/array-and-strings/min-days/solution.ts
+++ src/medium-collection/array-and-strings/min-days/solution.ts
@@ -1,3 +1,42 @@
 export function minDays(bloomDay: number[], m: number, k: number): number {
-  return 0;
+  if (bloomDay.length < m * k) {
+    return -1;
+  }
+  let min = Infinity;
+  let max = -Infinity;
+  for (const day of bloomDay) {
+    min = Math.min(day, min);
+    max = Math.max(day, max);
+  }
+  while (min < max) {
+    const mid = Math.floor(min + (max - min) / 2);
+    if (canMakeBouquets(mid, bloomDay, m, k)) {
+      max = mid;
+    } else {
+      min = mid + 1;
+    }
+  }
+  return min;
+}
+
+function canMakeBouquets(
+  days: number,
+  bloomDay: number[],
+  m: number,
+  k: number,
+): boolean {
+  let flowers = 0;
+  let bouquets = 0;
+  for (let i = 0; i < bloomDay.length; i++) {
+    if (bloomDay[i] <= days) {
+      flowers++;
+    } else {
+      flowers = 0;
+    }
+    if (flowers === k) {
+      bouquets++;
+      flowers = 0;
+    }
+  }
+  return bouquets >= m;
 }

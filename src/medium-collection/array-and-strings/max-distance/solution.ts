36b5b9af6c07b85283987a5a3fdc67c2e70e2a69
Maxim Kryuk <mkryuk@gmail.com>
2024-06-21 11:27:50 +0300

    Array and Strings: Magnetic Force Between Two Balls solution

--- src/medium-collection/array-and-strings/max-distance/solution.ts
+++ src/medium-collection/array-and-strings/max-distance/solution.ts
@@ -1,3 +1,33 @@
 export function maxDistance(position: number[], m: number): number {
-  return 0;
+  const n = position.length;
+  position.sort((a, b) => a - b);
+  let left = 1;
+  let right = position[n - 1] - position[0];
+  let result = 0;
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+    if (canPlaceBalls(mid, position, m)) {
+      result = mid;
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return result;
+}
+
+function canPlaceBalls(force: number, position: number[], m: number): boolean {
+  let lastPosition = position[0];
+  let count = 1;
+  for (let i = 1; i < position.length; i++) {
+    const diff = position[i] - lastPosition;
+    if (diff >= force) {
+      lastPosition = position[i];
+      count++;
+    }
+    if (count >= m) {
+      return true;
+    }
+  }
+  return false;
 }

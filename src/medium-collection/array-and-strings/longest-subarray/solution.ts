5dbcfb5b2ab16fe88795b599b0a192ab23aabbcc
Maxim Kryuk <mkryuk@gmail.com>
2024-06-24 00:37:48 +0300

    Array and Strings: Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit solution

--- src/medium-collection/array-and-strings/longest-subarray/solution.ts
+++ src/medium-collection/array-and-strings/longest-subarray/solution.ts
@@ -1,3 +1,36 @@
 export function longestSubarray(nums: number[], limit: number): number {
-  return 0;
+  const minQueue: number[] = [];
+  const maxQueue: number[] = [];
+  let left = 0;
+  let result = 0;
+  for (let right = 0; right < nums.length; right++) {
+    while (
+      maxQueue.length > 0 &&
+      nums[maxQueue[maxQueue.length - 1]] < nums[right]
+    ) {
+      maxQueue.pop();
+    }
+    maxQueue.push(right);
+
+    while (
+      minQueue.length > 0 &&
+      nums[minQueue[minQueue.length - 1]] > nums[right]
+    ) {
+      minQueue.pop();
+    }
+    minQueue.push(right);
+
+    while (nums[maxQueue[0]] - nums[minQueue[0]] > limit) {
+      if (maxQueue[0] === left) {
+        maxQueue.shift();
+      }
+      if (minQueue[0] === left) {
+        minQueue.shift();
+      }
+      left++;
+    }
+
+    result = Math.max(result, right - left + 1);
+  }
+  return result;
 }

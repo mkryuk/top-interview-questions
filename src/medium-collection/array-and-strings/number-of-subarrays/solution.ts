52843c40f58d300205f150cc5cbac83c922f10f9
Maxim Kryuk <mkryuk@gmail.com>
2024-06-23 12:45:18 +0300

    Array and Strings:  Count Number of Nice Subarrays solution

--- src/medium-collection/array-and-strings/number-of-subarrays/solution.ts
+++ src/medium-collection/array-and-strings/number-of-subarrays/solution.ts
@@ -1,3 +1,20 @@
 export function numberOfSubarrays(nums: number[], k: number): number {
-  return 0;
+  const n = nums.length;
+  const prefix = new Array(n + 1).fill(0);
+  prefix[0] = 1;
+
+  let countOdd = 0;
+  let result = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (nums[i] % 2 === 1) {
+      countOdd++;
+    }
+    if (countOdd >= k) {
+      result += prefix[countOdd - k];
+    }
+    prefix[countOdd]++;
+  }
+
+  return result;
 }

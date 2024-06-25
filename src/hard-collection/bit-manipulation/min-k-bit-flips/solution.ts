96a3422c4e62f5ad9b48215cdbb1a35272252006
Maxim Kryuk <mkryuk@gmail.com>
2024-06-25 13:10:09 +0300

    Bit Manipulation: Minimum Number of K Consecutive Bit Flips solution

--- src/hard-collection/bit-manipulation/min-k-bit-flips/solution.ts
+++ src/hard-collection/bit-manipulation/min-k-bit-flips/solution.ts
@@ -1,3 +1,21 @@
 export function minKBitFlips(nums: number[], k: number): number {
-  return 0;
+  const n = nums.length;
+  const flipped = new Array(n).fill(0);
+  let flipCount = 0;
+  let result = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (i >= k) {
+      flipCount ^= flipped[i - k];
+    }
+    if (nums[i] == flipCount) {
+      if (i + k > n) {
+        return -1;
+      }
+      flipped[i] = 1;
+      flipCount ^= 1;
+      result++;
+    }
+  }
+  return result;
 }

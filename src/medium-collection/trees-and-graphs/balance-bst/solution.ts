55962ce0f748c490db0a1e0a3489cfe9e1c53509
Maxim Kryuk <mkryuk@gmail.com>
2024-06-27 00:26:51 +0300

    Trees and Graphs: Balance a Binary Search Tree solution

--- src/medium-collection/trees-and-graphs/balance-bst/solution.ts
+++ src/medium-collection/trees-and-graphs/balance-bst/solution.ts
@@ -1,5 +1,32 @@
 import { TreeNode } from "../common";
 
 export function balanceBST(root: TreeNode | null): TreeNode | null {
-  return null;
+  let sortedArray: number[] = [];
+  bstToArray(root, sortedArray);
+  const result = createBst(sortedArray, 0, sortedArray.length - 1);
+  return result;
+}
+
+function bstToArray(node: TreeNode | null, arr: number[]) {
+  if (node === null) {
+    return;
+  }
+  bstToArray(node.left, arr);
+  arr.push(node.val);
+  bstToArray(node.right, arr);
+}
+
+function createBst(
+  arr: number[],
+  left: number,
+  right: number,
+): TreeNode | null {
+  if (left > right) {
+    return null;
+  }
+  let mid = left + Math.floor((right - left) / 2);
+  const node = new TreeNode(arr[mid]);
+  node.left = createBst(arr, left, mid - 1);
+  node.right = createBst(arr, mid + 1, right);
+  return node;
 }

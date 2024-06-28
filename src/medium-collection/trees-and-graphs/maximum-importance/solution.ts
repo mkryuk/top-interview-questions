b8b1bee8f5ee4c3e24ff5bfcd3499100d77df855
Maxim Kryuk <mkryuk@gmail.com>
2024-06-28 23:44:09 +0300

    Trees and Graphs: Maximum Total Importance of Roads init

---
+++ src/medium-collection/trees-and-graphs/maximum-importance/solution.ts
@@ -0,0 +1,3 @@
+export function maximumImportance(n: number, roads: number[][]): number {
+  return 0;
+}
---
+++ src/medium-collection/trees-and-graphs/maximum-importance/test.spec.ts
@@ -0,0 +1,28 @@
+import { maximumImportance } from "./solution";
+
+describe("Maximum Total Importance of Roads", () => {
+  it("maximumImportance should return 43 for n = 5, roads = [[0,1],[1,2],[2,3],[0,2],[1,3],[2,4]]", () => {
+    const n = 5;
+    const roads = [
+      [0, 1],
+      [1, 2],
+      [2, 3],
+      [0, 2],
+      [1, 3],
+      [2, 4],
+    ];
+    const result = maximumImportance(n, roads);
+    expect(result).toEqual(42);
+  });
+
+  it("maximumImportance should return 20 for n = 5, roads = [[0,3],[2,4],[1,3]]", () => {
+    const n = 5;
+    const roads = [
+      [0, 3],
+      [2, 4],
+      [1, 3],
+    ];
+    const result = maximumImportance(n, roads);
+    expect(result).toEqual(20);
+  });
+});

1e08bca0adb68edf1c9c6597a304cb03a74cb81e
Maxim Kryuk <mkryuk@gmail.com>
2024-06-28 22:25:04 +0300

    Trees and Graphs: Find Center of Star Graph solution

--- src/easy-collection/trees/find-center/solution.ts
+++ src/easy-collection/trees/find-center/solution.ts
@@ -1,3 +1,25 @@
 export function findCenter(edges: number[][]): number {
-  return 0;
+  let numCount = new Map<number, number>();
+
+  for (const [a, b] of edges) {
+    if (!numCount.has(a)) {
+      numCount.set(a, 0);
+    }
+    if (!numCount.has(b)) {
+      numCount.set(b, 0);
+    }
+    numCount.set(a, numCount.get(a)! + 1);
+    numCount.set(b, numCount.get(b)! + 1);
+  }
+
+  let maxCount = 0;
+  let maxNum = 0;
+  for (const [num, count] of numCount) {
+    if (maxCount < count) {
+      maxCount = count;
+      maxNum = num;
+    }
+  }
+
+  return maxNum;
 }

1a7dd4904f8af926a9967a7f069d491115d78bc4
Maxim Kryuk <mkryuk@gmail.com>
2024-06-30 22:05:10 +0300

    Trees and Graphs: All Ancestors of a Node in a Directed Acyclic Graph solution

--- src/medium-collection/trees-and-graphs/get-ancestors/solution.ts
+++ src/medium-collection/trees-and-graphs/get-ancestors/solution.ts
@@ -1,3 +1,38 @@
 export function getAncestors(n: number, edges: number[][]): number[][] {
-  return [];
+  let revEdges = new Map<number, number[]>();
+  for (const [from, to] of edges) {
+    if (!revEdges.has(to)) {
+      revEdges.set(to, []);
+    }
+    revEdges.get(to)!.push(from);
+  }
+
+  const result: number[][] = [];
+  const ancestorsMap = new Map<number, number[]>();
+  for (let i = 0; i < n; i++) {
+    const ancestors = dfs(i, ancestorsMap, revEdges);
+    result.push(ancestors);
+  }
+  return result;
+}
+
+function dfs(
+  node: number,
+  ancestorsMap: Map<number, number[]>,
+  revEdges: Map<number, number[]>,
+): number[] {
+  if (ancestorsMap.has(node)) {
+    return ancestorsMap.get(node)!;
+  }
+
+  const ancestorsSet = new Set<number>();
+  for (const nextNode of revEdges.get(node) ?? []) {
+    ancestorsSet.add(nextNode);
+    const nextAncestors = dfs(nextNode, ancestorsMap, revEdges);
+    nextAncestors.forEach((a) => ancestorsSet.add(a));
+  }
+  const ancestors = Array.from(ancestorsSet);
+  ancestors.sort((a, b) => a - b);
+  ancestorsMap.set(node, ancestors);
+  return ancestors;
 }

e1cdbcd18b6ff5438fbf275709fe7c99f11137c9
Maxim Kryuk <mkryuk@gmail.com>
2024-07-02 00:04:10 +0300

    Array and Strings: Three Consecutive Odds solution

--- src/easy-collection/array/three-consecutive-odds/solution.ts
+++ src/easy-collection/array/three-consecutive-odds/solution.ts
@@ -1,3 +1,14 @@
 export function threeConsecutiveOdds(arr: number[]): boolean {
+  let consecutiveCount = 0;
+  for (const num of arr) {
+    if (num % 2 !== 0) {
+      consecutiveCount++;
+    } else {
+      consecutiveCount = 0;
+    }
+    if (consecutiveCount === 3) {
+      return true;
+    }
+  }
   return false;
 }

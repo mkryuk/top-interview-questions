f6b85f77bc40f865970140c9e055e669d2205f2d
Maxim Kryuk <mkryuk@gmail.com>
2024-07-04 02:02:56 +0300

    Array and Strings: Minimum Difference Between Largest and Smallest Value in Three Moves solution

--- src/medium-collection/array-and-strings/min-difference/solution.ts
+++ src/medium-collection/array-and-strings/min-difference/solution.ts
@@ -1,3 +1,12 @@
 export function minDifference(nums: number[]): number {
-  return 0;
+  if (nums.length < 5) {
+    return 0;
+  }
+  nums.sort((a, b) => a - b);
+  let minDiff = Infinity;
+  const n = nums.length;
+  for (let i = 0; i < 4; i++) {
+    minDiff = Math.min(minDiff, nums[n - 4 + i] - nums[i]);
+  }
+  return minDiff;
 }

98c4c87f9c1c25e8e0f1e1a4546b375280a984d3
Maxim Kryuk <mkryuk@gmail.com>
2024-07-04 14:34:43 +0300

    Linked List: Merge Nodes in Between Zeros init

---
+++ src/medium-collection/linked-list/merge-nodes/solution.ts
@@ -0,0 +1,5 @@
+import { ListNode } from "../common";
+
+export function mergeNodes(head: ListNode | null): ListNode | null {
+  return null;
+}
---
+++ src/medium-collection/linked-list/merge-nodes/test.spec.ts
@@ -0,0 +1,20 @@
+import { arrayToList, listToArray } from "../common";
+import { mergeNodes } from "./solution";
+
+describe("Merge Nodes in Between Zeros", () => {
+  it("mergeNodes should return [4,11] for head = [0,3,1,0,4,5,2,0]", () => {
+    const nodes = [0, 3, 1, 0, 4, 5, 2, 0];
+    const head = arrayToList(nodes);
+    const result = mergeNodes(head);
+    const resultArray = listToArray(result);
+    expect(resultArray).toEqual([4, 1]);
+  });
+
+  it("mergeNodes should return [1,3,4] for head = [0,1,0,3,0,2,2,0]", () => {
+    const nodes = [0, 1, 0, 3, 0, 2, 2, 0];
+    const head = arrayToList(nodes);
+    const result = mergeNodes(head);
+    const resultArray = listToArray(result);
+    expect(resultArray).toEqual([1, 3, 4]);
+  });
+});

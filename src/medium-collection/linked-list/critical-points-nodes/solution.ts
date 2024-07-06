53b269966edfefaf87ed5fda4589d15ef14f3acd
Maxim Kryuk <mkryuk@gmail.com>
2024-07-06 13:07:23 +0300

    Linked List: Find the Minimum and Maximum Number of Nodes Between Critical Points solution

--- src/medium-collection/linked-list/critical-points-nodes/solution.ts
+++ src/medium-collection/linked-list/critical-points-nodes/solution.ts
@@ -1,5 +1,43 @@
 import { ListNode } from "../common";
 
 export function nodesBetweenCriticalPoints(head: ListNode | null): number[] {
-  return [];
+  let prevVal = head!.val;
+  let temp = head!.next;
+  let min = Infinity;
+  let max = -1;
+  let currentIndex = 0;
+  let firstFound = -1;
+  let lastFound = -1;
+  while (temp !== null) {
+    currentIndex++;
+    if (!isLocalMinMax(prevVal, temp)) {
+      prevVal = temp.val;
+      temp = temp.next;
+      continue;
+    }
+    if (firstFound === -1) {
+      firstFound = currentIndex;
+    }
+    if (lastFound !== -1) {
+      min = Math.min(min, currentIndex - lastFound);
+      max = currentIndex - firstFound;
+    }
+    lastFound = currentIndex;
+    prevVal = temp.val;
+    temp = temp.next;
+  }
+  return [min === Infinity ? -1 : min, max];
+}
+
+function isLocalMinMax(prev: number, node: ListNode): boolean {
+  return (
+    (node !== null &&
+      node.next !== null &&
+      node.val < prev &&
+      node.val < node.next.val) ||
+    (node !== null &&
+      node.next !== null &&
+      node.val > prev &&
+      node.val > node.next.val)
+  );
 }

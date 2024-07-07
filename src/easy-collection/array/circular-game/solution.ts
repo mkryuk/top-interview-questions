d3c2e80bf69ed8d160b244b670f49ed64313d2c7
Maxim Kryuk <mkryuk@gmail.com>
2024-07-07 23:55:08 +0300

    Array and Strings: Find the Losers of the Circular Game solution

--- src/easy-collection/array/circular-game/solution.ts
+++ src/easy-collection/array/circular-game/solution.ts
@@ -1,3 +1,19 @@
 export function circularGameLosers(n: number, k: number): number[] {
-  return [];
+  const players = new Array(n).fill(0);
+  let index = 0;
+  let turn = 0;
+  while (players[index] !== 1) {
+    players[index] = 1;
+    turn++;
+    const steps = turn * k;
+    index = (index + steps) % n;
+  }
+
+  const result = [];
+  for (let i = 0; i < players.length; i++) {
+    if (players[i] === 0) {
+      result.push(i + 1);
+    }
+  }
+  return result;
 }

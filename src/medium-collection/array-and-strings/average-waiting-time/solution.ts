1b75e68eb42f6483333c7eccf3e44ff8d66a7297
Maxim Kryuk <mkryuk@gmail.com>
2024-07-10 00:11:47 +0300

    Array and Strings: Average Waiting Time solution

--- src/medium-collection/array-and-strings/average-waiting-time/solution.ts
+++ src/medium-collection/array-and-strings/average-waiting-time/solution.ts
@@ -1,3 +1,13 @@
 export function averageWaitingTime(customers: number[][]): number {
-  return 0;
+  let currentTime = 0;
+  let totalTime = 0;
+  for (const [arrivalTime, cookTime] of customers) {
+    if (currentTime < arrivalTime) {
+      currentTime = arrivalTime;
+    }
+    currentTime += cookTime;
+    const waitTime = currentTime - arrivalTime;
+    totalTime += waitTime;
+  }
+  return totalTime / customers.length;
 }

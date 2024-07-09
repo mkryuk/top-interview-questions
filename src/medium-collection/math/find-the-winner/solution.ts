cc9b064f27ce06baf53507d8e7553cdd7e7f29fc
Maxim Kryuk <mkryuk@gmail.com>
2024-07-09 23:12:36 +0300

    Math: Find the Winner of the Circular Game solution

--- src/medium-collection/math/find-the-winner/solution.ts
+++ src/medium-collection/math/find-the-winner/solution.ts
@@ -1,3 +1,12 @@
 export function findTheWinner(n: number, k: number): number {
-  return 0;
+  // Convert the result from 0-based index to 1-based index
+  return josephus(n, k) + 1;
+}
+
+function josephus(n: number, k: number): number {
+  if (n === 1) {
+    return 0;
+  } else {
+    return (josephus(n - 1, k) + k) % n;
+  }
 }

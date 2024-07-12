eba6430afaa99f9143595e557750c42269ab176f
Maxim Kryuk <mkryuk@gmail.com>
2024-07-12 16:07:12 +0300

    Array and Strings: Maximum Score From Removing Substrings init

---
+++ src/medium-collection/array-and-strings/maximum-gain/solution.ts
@@ -0,0 +1,3 @@
+export function maximumGain(s: string, x: number, y: number): number {
+  return 0;
+}
---
+++ src/medium-collection/array-and-strings/maximum-gain/test.spec.ts
@@ -0,0 +1,19 @@
+import { maximumGain } from "./solution";
+
+describe("Maximum Score From Removing Substrings", () => {
+  it('maximumGain should return 19 for s = "cdbcbbaaabab", x = 4, y = 5', () => {
+    const s = "cdbcbbaaabab";
+    const x = 4;
+    const y = 5;
+    const result = maximumGain(s, x, y);
+    expect(result).toEqual(19);
+  });
+
+  it('maximumGain should return 20 for s = "aabbaaxybbaabb", x = 5, y = 4', () => {
+    const s = "aabbaaxybbaabb";
+    const x = 5;
+    const y = 4;
+    const result = maximumGain(s, x, y);
+    expect(result).toEqual(20);
+  });
+});

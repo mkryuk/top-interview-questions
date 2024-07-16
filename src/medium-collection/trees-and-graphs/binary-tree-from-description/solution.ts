c8e9419401b6f8aa6c9f56a07c386ef50df733a9
Maxim Kryuk <mkryuk@gmail.com>
2024-07-16 22:20:33 +0300

    Trees and Graphs: Create Binary Tree From Descriptions solution

--- src/medium-collection/trees-and-graphs/binary-tree-from-description/solution.ts
+++ src/medium-collection/trees-and-graphs/binary-tree-from-description/solution.ts
@@ -1,5 +1,36 @@
 import { TreeNode } from "../common";
 
 export function createBinaryTree(descriptions: number[][]): TreeNode | null {
-  return null;
+  const nodes: Map<number, TreeNode> = new Map();
+  const children: Set<number> = new Set();
+
+  for (const [parentVal, childVal, isLeft] of descriptions) {
+    if (!nodes.has(parentVal)) {
+      nodes.set(parentVal, new TreeNode(parentVal));
+    }
+    if (!nodes.has(childVal)) {
+      nodes.set(childVal, new TreeNode(childVal));
+    }
+
+    const parentNode = nodes.get(parentVal)!;
+    const childNode = nodes.get(childVal)!;
+
+    if (isLeft === 1) {
+      parentNode.left = childNode;
+    } else {
+      parentNode.right = childNode;
+    }
+
+    children.add(childVal);
+  }
+
+  let root: TreeNode | null = null;
+  for (const [val, node] of nodes) {
+    if (!children.has(val)) {
+      root = node;
+      break;
+    }
+  }
+
+  return root;
 }

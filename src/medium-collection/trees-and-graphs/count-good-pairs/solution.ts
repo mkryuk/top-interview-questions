504b441d79015c62108fb77629dce11ecfa8ae22
Maxim Kryuk <mkryuk@gmail.com>
2024-07-18 23:26:23 +0300

    Trees and Graphs: Number of Good Leaf Nodes Pairs solution

--- src/medium-collection/trees-and-graphs/count-good-pairs/solution.ts
+++ src/medium-collection/trees-and-graphs/count-good-pairs/solution.ts
@@ -1,5 +1,36 @@
 import { TreeNode } from "../common";
 
 export function countPairs(root: TreeNode | null, distance: number): number {
-  return 0;
+  let result = 0;
+
+  function dfs(node: TreeNode | null): number[] {
+    if (!node) {
+      return [];
+    }
+
+    if (!node.left && !node.right) {
+      return [1];
+    }
+
+    const leftDistances = dfs(node.left);
+    const rightDistances = dfs(node.right);
+
+    // count pairs
+    for (const ld of leftDistances) {
+      for (const rd of rightDistances) {
+        if (ld + rd <= distance) {
+          result++;
+        }
+      }
+    }
+
+    // return distances incremented by 1 for the parent node
+    const newDistances = [...leftDistances, ...rightDistances].map(
+      (d) => d + 1,
+    );
+    return newDistances;
+  }
+
+  dfs(root);
+  return result;
 }

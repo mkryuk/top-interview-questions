ca38096479df527fea23b0827b529cd8b04c3986
Maxim Kryuk <mkryuk@gmail.com>
2024-07-20 00:16:29 +0300

    Array and Strings: Lucky Numbers in a Matrix solution

--- src/easy-collection/array/lucky-numbers/solution.ts
+++ src/easy-collection/array/lucky-numbers/solution.ts
@@ -1,3 +1,27 @@
 export function luckyNumbers(matrix: number[][]): number[] {
-  return [];
+  const colsMax = new Set<number>();
+  const rowsMin = new Set<number>();
+
+  // find the minimum in each row and add to rowsMin set
+  for (let row = 0; row < matrix.length; row++) {
+    rowsMin.add(Math.min(...matrix[row]));
+  }
+
+  // find the maximum in each column and add to colsMax set
+  for (let col = 0; col < matrix[0].length; col++) {
+    let max = matrix[0][col];
+    for (let row = 0; row < matrix.length; row++) {
+      max = Math.max(max, matrix[row][col]);
+    }
+    colsMax.add(max);
+  }
+
+  // find the intersection of rowsMin and colsMax
+  const result: number[] = [];
+  for (const item of rowsMin.values()) {
+    if (colsMax.has(item)) {
+      result.push(item);
+    }
+  }
+  return result;
 }

4456b1b5697b27c0d3b156eef5467867c8d40e50
Maxim Kryuk <mkryuk@gmail.com>
2024-07-23 16:15:20 +0300

    Bit Manipulation: Complement of Base 10 Integer solution

--- src/easy-collection/bit-manipulation/bitwise-complement/solution.ts
+++ src/easy-collection/bit-manipulation/bitwise-complement/solution.ts
@@ -1,3 +1,13 @@
 export function bitwiseComplement(n: number): number {
-  return 0;
+  if (n === 0) {
+    return 1;
+  }
+
+  let bitLength = 0;
+  let temp = n;
+  while (temp > 0) {
+    bitLength++;
+    temp >>= 1;
+  }
+  return n ^ (Math.pow(2, bitLength) - 1);
 }

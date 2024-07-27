0b074989b89ffa1a4cf6cab0a76c2e6a8104a198
Maxim Kryuk <mkryuk@gmail.com>
2024-07-28 00:33:40 +0300

    Trees and Graphs: Minimum Cost to Convert String I init

---
+++ src/medium-collection/trees-and-graphs/min-cost/solution.ts
@@ -0,0 +1,9 @@
+export function minimumCost(
+  source: string,
+  target: string,
+  original: string[],
+  changed: string[],
+  cost: number[],
+): number {
+  return 0;
+}
---
+++ src/medium-collection/trees-and-graphs/min-cost/test.spec.ts
@@ -0,0 +1,33 @@
+import { minimumCost } from "./solution";
+
+describe("Minimum Cost to Convert String I", () => {
+  it('minimumCost should return 28 for source = "abcd", target = "acbe", original = ["a","b","c","c","e","d"], changed = ["b","c","b","e","b","e"], cost = [2,5,5,1,2,20]', () => {
+    const source = "abcd";
+    const target = "acbe";
+    const original = ["a", "b", "c", "c", "e", "d"];
+    const changed = ["b", "c", "b", "e", "b", "e"];
+    const cost = [2, 5, 5, 1, 2, 20];
+    const result = minimumCost(source, target, original, changed, cost);
+    expect(result).toEqual(28);
+  });
+
+  it('minimumCost should return 12 for source = "aaaa", target = "bbbb", original = ["a","c"], changed = ["c","b"], cost = [1,2]', () => {
+    const source = "aaaa";
+    const target = "bbbb";
+    const original = ["a", "c"];
+    const changed = ["c", "b"];
+    const cost = [1, 2];
+    const result = minimumCost(source, target, original, changed, cost);
+    expect(result).toEqual(12);
+  });
+
+  it('minimumCost should return -1 for source = "abcd", target = "abce", original = ["a"], changed = ["e"], cost = [10000]', () => {
+    const source = "abcd";
+    const target = "abce";
+    const original = ["a"];
+    const changed = ["e"];
+    const cost = [10000];
+    const result = minimumCost(source, target, original, changed, cost);
+    expect(result).toEqual(-1);
+  });
+});

3591ceb21aaa66eb8967bb2ab56f2841e7b02a70
Maxim Kryuk <mkryuk@gmail.com>
2024-07-30 16:19:29 +0300

    Array and Strings: Count Number of Teams solution

--- src/medium-collection/array-and-strings/num-teams/solution.ts
+++ src/medium-collection/array-and-strings/num-teams/solution.ts
@@ -1,3 +1,31 @@
 export function numTeams(rating: number[]): number {
-  return 0;
+  let count = 0;
+  const n = rating.length;
+
+  for (let j = 0; j < n; j++) {
+    let lessThanLeft = 0;
+    let greaterThanLeft = 0;
+    let lessThanRight = 0;
+    let greaterThanRight = 0;
+
+    for (let i = 0; i < j; i++) {
+      if (rating[i] < rating[j]) {
+        lessThanLeft++;
+      } else if (rating[i] > rating[j]) {
+        greaterThanLeft++;
+      }
+    }
+
+    for (let k = j + 1; k < n; k++) {
+      if (rating[k] < rating[j]) {
+        lessThanRight++;
+      } else if (rating[k] > rating[j]) {
+        greaterThanRight++;
+      }
+    }
+
+    count += lessThanLeft * greaterThanRight + greaterThanLeft * lessThanRight;
+  }
+
+  return count;
 }

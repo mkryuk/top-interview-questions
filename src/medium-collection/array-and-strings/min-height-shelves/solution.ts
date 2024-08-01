769f6ef1776e30864d900ff397a6e3ff22de9105
Maxim Kryuk <mkryuk@gmail.com>
2024-08-01 23:57:53 +0300

    Array and Strings: Filling Bookcase Shelves solution

--- src/medium-collection/array-and-strings/min-height-shelves/solution.ts
+++ src/medium-collection/array-and-strings/min-height-shelves/solution.ts
@@ -2,5 +2,22 @@ export function minHeightShelves(
   books: number[][],
   shelfWidth: number,
 ): number {
-  return 0;
+  const n = books.length;
+  const dp = new Array(n + 1).fill(Infinity);
+  dp[0] = 0;
+
+  for (let i = 1; i <= n; i++) {
+    let width = 0;
+    let height = 0;
+    for (let j = i; j > 0; j--) {
+      width += books[j - 1][0];
+      if (width > shelfWidth) {
+        break;
+      }
+      height = Math.max(height, books[j - 1][1]);
+      dp[i] = Math.min(dp[i], dp[j - 1] + height);
+    }
+  }
+
+  return dp[n];
 }
--- src/medium-collection/array-and-strings/min-height-shelves/test.spec.ts
+++ src/medium-collection/array-and-strings/min-height-shelves/test.spec.ts
@@ -1,7 +1,7 @@
 import { minHeightShelves } from "./solution";
 
 describe("Filling Bookcase Shelves", () => {
-  it("minHeightShelves should return 6 books = [[1,1],[2,3],[2,3],[1,1],[1,1],[1,1],[1,2]], shelfWidth = 4", () => {
+  it("minHeightShelves should return 6 for books = [[1,1],[2,3],[2,3],[1,1],[1,1],[1,1],[1,2]], shelfWidth = 4", () => {
     const books = [
       [1, 1],
       [2, 3],
@@ -16,14 +16,38 @@ describe("Filling Bookcase Shelves", () => {
     expect(result).toEqual(6);
   });
 
-  it("minHeightShelves should return 4 books = [[1,3],[2,4],[3,2]], shelfWidth = 6", () => {
+  it("minHeightShelves should return 4 for books = [[1,3],[2,4],[3,2]], shelfWidth = 6", () => {
     const books = [
-        [1, 3],
-        [2, 4],
-        [3, 2],
-      ],
-      shelfWidth = 6;
+      [1, 3],
+      [2, 4],
+      [3, 2],
+    ];
+    const shelfWidth = 6;
     const result = minHeightShelves(books, shelfWidth);
     expect(result).toEqual(4);
+  });
+
+  it("minHeightShelves should return 10 for books = [[2,1],[2,2],[2,3],[2,4]], shelfWidth = 3", () => {
+    const books = [
+      [2, 1],
+      [2, 2],
+      [2, 3],
+      [2, 4],
+    ];
+    const shelfWidth = 3;
+    const result = minHeightShelves(books, shelfWidth);
+    expect(result).toEqual(10);
+  });
+
+  it("minHeightShelves should return 8 for books = [[2,3],[2,1],[2,3],[1,4]], shelfWidth = 3", () => {
+    const books = [
+      [2, 3],
+      [2, 1],
+      [2, 3],
+      [1, 4],
+    ];
+    const shelfWidth = 3;
+    const result = minHeightShelves(books, shelfWidth);
+    expect(result).toEqual(8);
   });
 });

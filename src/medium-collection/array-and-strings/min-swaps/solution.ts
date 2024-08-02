24bf06df733d51d6d0ee13cb78106abaf4e187eb
Maxim Kryuk <mkryuk@gmail.com>
2024-08-02 22:47:14 +0300

    Array and Strings: Minimum Swaps to Group All 1's Together II init

---
+++ src/medium-collection/array-and-strings/min-swaps/solution.ts
@@ -0,0 +1,3 @@
+export function minSwaps(nums: number[]): number {
+  return 0;
+}
---
+++ src/medium-collection/array-and-strings/min-swaps/test.spec.ts
@@ -0,0 +1,21 @@
+import { minSwaps } from "./solution";
+
+describe("Minimum Swaps to Group All 1's Together II", () => {
+  it("minSwaps should return 1 for nums = [0,1,0,1,1,0,0]", () => {
+    const nums = [0, 1, 0, 1, 1, 0, 0];
+    const result = minSwaps(nums);
+    expect(result).toEqual(1);
+  });
+
+  it("minSwaps should return 2 for nums = [0,1,1,1,0,0,1,1,0]", () => {
+    const nums = [0, 1, 1, 1, 0, 0, 1, 1, 0];
+    const result = minSwaps(nums);
+    expect(result).toEqual(2);
+  });
+
+  it("minSwaps should return 0 for nums = [1,1,0,0,1]", () => {
+    const nums = [1, 1, 0, 0, 1];
+    const result = minSwaps(nums);
+    expect(result).toEqual(0);
+  });
+});

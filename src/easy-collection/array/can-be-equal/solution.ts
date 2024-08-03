bc6987604652496d11fce9347f5e9a268808c807
Maxim Kryuk <mkryuk@gmail.com>
2024-08-04 01:09:53 +0300

    Array and Strings: Make Two Arrays Equal by Reversing Subarrays solution

--- src/easy-collection/array/can-be-equal/solution.ts
+++ src/easy-collection/array/can-be-equal/solution.ts
@@ -1,3 +1,20 @@
 export function canBeEqual(target: number[], arr: number[]): boolean {
-  return false;
+  if (arr.length !== target.length) {
+    return false;
+  }
+  let map = new Map<number, number>();
+  for (const num of arr) {
+    if (!map.has(num)) {
+      map.set(num, 0);
+    }
+    map.set(num, map.get(num)! + 1);
+  }
+
+  for (const num of target) {
+    if (!map.has(num) || map.get(num)! == 0) {
+      return false;
+    }
+    map.set(num, map.get(num)! - 1);
+  }
+  return true;
 }

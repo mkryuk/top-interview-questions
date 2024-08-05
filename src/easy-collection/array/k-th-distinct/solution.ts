5f7c0fcaaacdde4e226a4de720d637f5e52c1c66
Maxim Kryuk <mkryuk@gmail.com>
2024-08-05 16:14:24 +0300

    Array and Strings: Kth Distinct String in an Array solution

--- src/easy-collection/array/k-th-distinct/solution.ts
+++ src/easy-collection/array/k-th-distinct/solution.ts
@@ -1,3 +1,19 @@
 export function kthDistinct(arr: string[], k: number): string {
+  let map = new Map<string, number>();
+  for (const str of arr) {
+    if (!map.has(str)) {
+      map.set(str, 0);
+    }
+    map.set(str, map.get(str)! + 1);
+  }
+
+  for (const [str, count] of map) {
+    if (count === 1) {
+      k--;
+    }
+    if (k === 0) {
+      return str;
+    }
+  }
   return "";
 }

057f9bcd52747624d7d2b0acfd20e08337620acf
Maxim Kryuk <mkryuk@gmail.com>
2024-08-07 23:57:10 +0300

    Array and Strings: Minimum Number of Pushes to Type Word II solution

--- src/medium-collection/array-and-strings/minimum-pushes/solution.ts
+++ src/medium-collection/array-and-strings/minimum-pushes/solution.ts
@@ -1,3 +1,22 @@
 export function minimumPushes(word: string): number {
-  return 0;
+  const countMap = new Map<string, number>();
+  for (const char of word) {
+    if (!countMap.has(char)) {
+      countMap.set(char, 0);
+    }
+    countMap.set(char, countMap.get(char)! + 1);
+  }
+
+  const sortedCounts = Array.from(countMap.values()).sort(
+    (count1, count2) => count2 - count1,
+  );
+
+  let result = 0;
+  let keyIndex = 0;
+  for (const count of sortedCounts) {
+    const position = Math.floor(keyIndex / 8) + 1;
+    result += count * position;
+    keyIndex++;
+  }
+  return result;
 }

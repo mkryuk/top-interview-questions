e10d9dae733d2418549ed25177ad2374c1dc7582
Maxim Kryuk <mkryuk@gmail.com>
2024-08-10 23:45:38 +0300

    Array and Strings: Magic Squares In Grid solution

--- src/medium-collection/array-and-strings/num-magic-squares/solution.ts
+++ src/medium-collection/array-and-strings/num-magic-squares/solution.ts
@@ -1,3 +1,50 @@
 export function numMagicSquaresInside(grid: number[][]): number {
-  return 0;
+  const rows = grid.length;
+  const cols = grid[0].length;
+  let result = 0;
+  for (let col = 0; col <= rows - 3; col++) {
+    for (let row = 0; row <= cols - 3; row++) {
+      if (isMagic(col, row, grid)) {
+        result++;
+      }
+    }
+  }
+  return result;
+}
+
+function isMagic(i: number, j: number, grid: number[][]): boolean {
+  const uniqueValues = new Set<number>();
+  for (let col = i; col < i + 3; col++) {
+    for (let row = j; row < j + 3; row++) {
+      if (
+        grid[col][row] < 0 ||
+        grid[col][row] > 9 ||
+        uniqueValues.has(grid[col][row])
+      ) {
+        return false;
+      }
+      uniqueValues.add(grid[i][j]);
+    }
+  }
+
+  // Check rows, columns, and diagonals
+  const s1 = grid[i][j] + grid[i][j + 1] + grid[i][j + 2];
+  const s2 = grid[i + 1][j] + grid[i + 1][j + 1] + grid[i + 1][j + 2];
+  const s3 = grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2];
+  const s4 = grid[i][j] + grid[i + 1][j] + grid[i + 2][j];
+  const s5 = grid[i][j + 1] + grid[i + 1][j + 1] + grid[i + 2][j + 1];
+  const s6 = grid[i][j + 2] + grid[i + 1][j + 2] + grid[i + 2][j + 2];
+  const s7 = grid[i][j] + grid[i + 1][j + 1] + grid[i + 2][j + 2];
+  const s8 = grid[i][j + 2] + grid[i + 1][j + 1] + grid[i + 2][j];
+
+  return (
+    s1 === 15 &&
+    s2 === 15 &&
+    s3 === 15 &&
+    s4 === 15 &&
+    s5 === 15 &&
+    s6 === 15 &&
+    s7 === 15 &&
+    s8 === 15
+  );
 }
--- src/medium-collection/array-and-strings/num-magic-squares/test.spec.ts
+++ src/medium-collection/array-and-strings/num-magic-squares/test.spec.ts
@@ -10,4 +10,14 @@ describe("Magic Squares In Grid", () => {
     const result = numMagicSquaresInside(grid);
     expect(result).toEqual(1);
   });
+
+  it("numMagicSquaresInside should return 0 for grid = [[5,5,5],[5,5,5],[5,5,5]]", () => {
+    const grid = [
+      [5, 5, 5],
+      [5, 5, 5],
+      [5, 5, 5],
+    ];
+    const result = numMagicSquaresInside(grid);
+    expect(result).toEqual(0);
+  });
 });

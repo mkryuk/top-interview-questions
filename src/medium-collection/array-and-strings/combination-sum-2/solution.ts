304fb2f17cf320726df54a294fff5be3f070bf5f
Maxim Kryuk <mkryuk@gmail.com>
2024-08-13 20:06:07 +0300

    Array and Strings: Combination Sum II solution

--- src/medium-collection/array-and-strings/combination-sum-2/solution.ts
+++ src/medium-collection/array-and-strings/combination-sum-2/solution.ts
@@ -2,5 +2,32 @@ export function combinationSum2(
   candidates: number[],
   target: number,
 ): number[][] {
-  return [];
+  candidates.sort((a, b) => a - b);
+  const result: number[][] = [];
+  backtrack(candidates, 0, target, [], result);
+  return result;
+}
+
+function backtrack(
+  candidates: number[],
+  start: number,
+  target: number,
+  combination: number[],
+  result: number[][],
+) {
+  if (target === 0) {
+    result.push([...combination]);
+    return;
+  }
+  for (let i = start; i < candidates.length; i++) {
+    if (i > start && candidates[i] === candidates[i - 1]) {
+      continue;
+    }
+    if (candidates[i] > target) {
+      break;
+    }
+    combination.push(candidates[i]);
+    backtrack(candidates, i + 1, target - candidates[i], combination, result);
+    combination.pop();
+  }
 }

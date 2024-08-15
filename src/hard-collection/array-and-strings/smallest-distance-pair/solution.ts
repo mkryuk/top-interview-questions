3a7c8077bb402b525a681f2415bd81cb4aedae09
Maxim Kryuk <mkryuk@gmail.com>
2024-08-15 20:06:30 +0300

    Array and Strings: Find K-th Smallest Pair Distance solution

--- src/hard-collection/array-and-strings/smallest-distance-pair/solution.ts
+++ src/hard-collection/array-and-strings/smallest-distance-pair/solution.ts
@@ -1,3 +1,33 @@
+import { Heap } from "../../design/heap/heap";
+
 export function smallestDistancePair(nums: number[], k: number): number {
-  return 0;
+  nums.sort((a, b) => a - b);
+
+  let left = 0;
+  let right = nums[nums.length - 1] - nums[0];
+
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    const count = countPairs(nums, mid);
+
+    if (count < k) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  return left;
+}
+
+function countPairs(nums: number[], mid: number): number {
+  let count = 0;
+  let j = 0;
+  for (let i = 0; i < nums.length; i++) {
+    while (j < nums.length && nums[j] - nums[i] <= mid) {
+      j++;
+    }
+    count += j - i - 1;
+  }
+  return count;
 }

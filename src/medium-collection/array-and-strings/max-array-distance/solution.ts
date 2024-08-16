c3e48028a15967eef2687d5c3ebcd8a9c6efd08d
Maxim Kryuk <mkryuk@gmail.com>
2024-08-16 13:13:21 +0300

    Array and Strings: Maximum Distance in Arrays solution

--- src/medium-collection/array-and-strings/max-array-distance/solution.ts
+++ src/medium-collection/array-and-strings/max-array-distance/solution.ts
@@ -1,3 +1,30 @@
 export function maxDistance(arrays: number[][]): number {
-  return 0;
+  let minArray: [number, number][] = [];
+  let maxArray: [number, number][] = [];
+
+  for (let index = 0; index < arrays.length; index++) {
+    let min = arrays[index][0];
+    let max = arrays[index][arrays[index].length - 1];
+    minArray.push([min, index]);
+    maxArray.push([max, index]);
+  }
+
+  minArray.sort(([a], [b]) => a - b);
+  maxArray.sort(([a], [b]) => b - a);
+
+  let result = 0;
+
+  // check if the min and max values come from the same array
+  if (minArray[0][1] === maxArray[0][1]) {
+    // if they do, compare the second-largest difference
+    result = Math.max(
+      maxArray[1][0] - minArray[0][0], // max of second array - min of first array
+      maxArray[0][0] - minArray[1][0], // max of first array - min of second array
+    );
+  } else {
+    // if they don't, calculate the difference between the largest max and smallest min
+    result = maxArray[0][0] - minArray[0][0];
+  }
+
+  return result;
 }

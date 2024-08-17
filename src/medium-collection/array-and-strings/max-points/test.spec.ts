d356af55bda98c1cb6737ab294f1e55486ae0bb4
Maxim Kryuk <mkryuk@gmail.com>
2024-08-17 19:13:03 +0300

    Array and Strings: Maximum Number of Points with Cost init

---
+++ src/medium-collection/array-and-strings/max-points/test.spec.ts
@@ -0,0 +1,23 @@
+import { maxPoints } from "../../../hard-collection/math/max-points/solution";
+
+describe("Maximum Number of Points with Cost", () => {
+  it("maxPoints should return 9 for points = [[1,2,3],[1,5,1],[3,1,1]]", () => {
+    const points = [
+      [1, 2, 3],
+      [1, 5, 1],
+      [3, 1, 1],
+    ];
+    const result = maxPoints(points);
+    expect(result).toEqual(9);
+  });
+
+  it("maxPoints should return 11 for points = [[1,5],[2,3],[4,2]]", () => {
+    const points = [
+      [1, 5],
+      [2, 3],
+      [4, 2],
+    ];
+    const result = maxPoints(points);
+    expect(result).toEqual(11);
+  });
+});

e5494be42b397f1c87a8a5644e4c24fa8b08ae3f
Maxim Kryuk <mkryuk@gmail.com>
2024-08-29 17:26:47 +0300

    Array and Strings: Most Stones Removed with Same Row or Column init

---
+++ src/medium-collection/array-and-strings/remove-stones/test.spec.ts
@@ -0,0 +1,34 @@
+import { removeStones } from "./solution";
+
+describe("Most Stones Removed with Same Row or Column", () => {
+  it("removeStones should return 5 for stones = [[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]", () => {
+    const stones = [
+      [0, 0],
+      [0, 1],
+      [1, 0],
+      [1, 2],
+      [2, 1],
+      [2, 2],
+    ];
+    const result = removeStones(stones);
+    expect(result).toEqual(5);
+  });
+
+  it("removeStones should return 3 for stones = [[0,0],[0,2],[1,1],[2,0],[2,2]]", () => {
+    const stones = [
+      [0, 0],
+      [0, 2],
+      [1, 1],
+      [2, 0],
+      [2, 2],
+    ];
+    const result = removeStones(stones);
+    expect(result).toEqual(5);
+  });
+
+  it("removeStones should return 0 for stones = [[0,0]]", () => {
+    const stones = [[0, 0]];
+    const result = removeStones(stones);
+    expect(result).toEqual(0);
+  });
+});

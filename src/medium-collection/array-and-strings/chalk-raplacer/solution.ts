3386a16eb080b4a007495228162fac9ac33aa709
Maxim Kryuk <mkryuk@gmail.com>
2024-09-02 17:38:50 +0300

    Array and Strings: Find the Student that Will Replace the Chalk solution

--- src/medium-collection/array-and-strings/chalk-raplacer/solution.ts
+++ src/medium-collection/array-and-strings/chalk-raplacer/solution.ts
@@ -1,3 +1,11 @@
 export function chalkReplacer(chalk: number[], k: number): number {
-  return 0;
+  const total = chalk.reduce((acc, val) => acc + val, 0);
+  let rest = k % total;
+  for (let i = 0; i < chalk.length; i++) {
+    if (rest < chalk[i]) {
+      return i;
+    }
+    rest -= chalk[i];
+  }
+  return -1;
 }

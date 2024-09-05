12ec0d8f4db002a04ab10b0883e9a3e9432a1535
Maxim Kryuk <mkryuk@gmail.com>
2024-09-05 17:55:57 +0300

    Array and Strings: Find Missing Observations solution

--- src/easy-collection/array/missing-rolls/solution.ts
+++ src/easy-collection/array/missing-rolls/solution.ts
@@ -3,5 +3,21 @@ export function missingRolls(
   mean: number,
   n: number,
 ): number[] {
-  return [];
+  const m = rolls.length;
+  const totalSum = mean * (n + m);
+  const rollsSum = rolls.reduce((prev, curr) => prev + curr, 0);
+  let remainingPoints = totalSum - rollsSum;
+
+  if (remainingPoints < n || remainingPoints > 6 * n) {
+    return [];
+  }
+
+  let result = Array(n).fill(1);
+  remainingPoints -= n;
+  for (let i = 0; i < n && remainingPoints > 0; i++) {
+    const current = Math.min(5, remainingPoints);
+    result[i] += current;
+    remainingPoints -= current;
+  }
+  return result;
 }
--- src/easy-collection/array/missing-rolls/test.spec.ts
+++ src/easy-collection/array/missing-rolls/test.spec.ts
@@ -9,18 +9,29 @@ describe("Find Missing Observations", () => {
     expect(result).toEqual([6, 6]);
   });
 
-  it("missingRolls should return [2,3,2,2] for rolls = [1,5,6], mean = 3, n = 4", () => {
+  it("missingRolls should return [6,1,1,1] for rolls = [1,5,6], mean = 3, n = 4", () => {
     const rolls = [1, 5, 6];
     const mean = 3;
     const n = 4;
     const result = missingRolls(rolls, mean, n);
-    expect(result).toEqual([2, 3, 2, 2]);
+    expect(result).toEqual([6, 1, 1, 1]);
   });
 
   it("missingRolls should return [] for rolls = [1,2,3,4], mean = 6, n = 4", () => {
     const rolls = [1, 2, 3, 4];
     const mean = 6;
     const n = 4;
+    const result = missingRolls(rolls, mean, n);
+    expect(result).toEqual([]);
+  });
+
+  it("missingRolls should return [] for rolls = [4,2,2,5,4,5,4,5,3,3,6,1,2,4,2,1,6,5,4,2,3,4,2,3,3,5,4,1,4,4,5,3,6,1,5,2,3,3,6,1,6,4,1,3], mean = 2, n = 53", () => {
+    const rolls = [
+      4, 2, 2, 5, 4, 5, 4, 5, 3, 3, 6, 1, 2, 4, 2, 1, 6, 5, 4, 2, 3, 4, 2, 3, 3,
+      5, 4, 1, 4, 4, 5, 3, 6, 1, 5, 2, 3, 3, 6, 1, 6, 4, 1, 3,
+    ];
+    const mean = 2;
+    const n = 53;
     const result = missingRolls(rolls, mean, n);
     expect(result).toEqual([]);
   });

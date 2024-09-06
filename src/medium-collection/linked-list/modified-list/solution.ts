67dcbb6d5bef7d2ba56ef285c8eaa9068dad014b
Maxim Kryuk <mkryuk@gmail.com>
2024-09-07 00:05:26 +0300

    Linked List: Delete Nodes From Linked List Present in Array solution

--- src/medium-collection/linked-list/modified-list/solution.ts
+++ src/medium-collection/linked-list/modified-list/solution.ts
@@ -4,5 +4,18 @@ export function modifiedList(
   nums: number[],
   head: ListNode | null,
 ): ListNode | null {
-  return null;
+  let numsSet = new Set(nums);
+  let result = new ListNode();
+  result.next = head;
+  let prev = result;
+  let current = head;
+  while (current !== null) {
+    if (numsSet.has(current.val)) {
+      prev.next = current.next;
+    } else {
+      prev = current;
+    }
+    current = current.next;
+  }
+  return result.next;
 }

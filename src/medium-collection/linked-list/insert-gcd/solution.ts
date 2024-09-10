9f27b4ceba11ada25006af734e6cf5b5a77fff61
Maxim Kryuk <mkryuk@gmail.com>
2024-09-10 18:44:06 +0300

    Linked List: Insert Greatest Common Divisors in Linked List solution

--- src/medium-collection/linked-list/insert-gcd/solution.ts
+++ src/medium-collection/linked-list/insert-gcd/solution.ts
@@ -1,5 +1,21 @@
 import { ListNode } from "../common";
 
 export function insertGreatestCommonDivisors(head: ListNode | null): ListNode | null {
-  return null;
+  let current = head;
+  while (current !== null && current.next !== null) {
+    const divisor = gcd(current.val, current.next.val);
+    const newNode = new ListNode(divisor, current.next);
+    current.next = newNode;
+    current = newNode.next;
+  }
+  return head;
+}
+
+function gcd(a: number, b: number): number {
+  while (b !== 0) {
+    const temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
 }

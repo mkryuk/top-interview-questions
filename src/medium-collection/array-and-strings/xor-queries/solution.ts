916cd7d3275d7d0162a911d5c6b4fbe628626d65
Maxim Kryuk <mkryuk@gmail.com>
2024-09-13 14:08:29 +0300

    Array and Strings: XOR Queries of a Subarray solution

--- src/medium-collection/array-and-strings/xor-queries/solution.ts
+++ src/medium-collection/array-and-strings/xor-queries/solution.ts
@@ -1,3 +1,17 @@
 export function xorQueries(arr: number[], queries: number[][]): number[] {
-  return [];
+  const prefixXor = new Array(arr.length).fill(0);
+  prefixXor[0] = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    prefixXor[i] = prefixXor[i - 1] ^ arr[i];
+  }
+
+  let result: number[] = [];
+  for (const [left, right] of queries) {
+    if (left === 0) {
+      result.push(prefixXor[right]);
+    } else {
+      result.push(prefixXor[right] ^ prefixXor[left - 1]);
+    }
+  }
+  return result;
 }

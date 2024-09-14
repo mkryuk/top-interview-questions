20118fd4ec2e784a90b4f13c93f76cb967b6be8f
Maxim Kryuk <mkryuk@gmail.com>
2024-09-14 21:29:49 +0300

    Bit Manipulation: Longest Subarray With Maximum Bitwise AND solution

--- src/medium-collection/bit-manipulation/longest-subarray/solution.ts
+++ src/medium-collection/bit-manipulation/longest-subarray/solution.ts
@@ -1,3 +1,14 @@
 export function longestSubarray(nums: number[]): number {
-  return 0;
+  let max = Math.max(...nums);
+  let currLen = 0;
+  let result = 0;
+  for (const num of nums) {
+    if (num === max) {
+      currLen++;
+      result = Math.max(result, currLen);
+    } else {
+      currLen = 0;
+    }
+  }
+  return result;
 }

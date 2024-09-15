f84641d7d3e2149cad279fe80d4aa486a9dd0308
Maxim Kryuk <mkryuk@gmail.com>
2024-09-15 14:03:56 +0300

    Array and Strings: Find the Longest Substring Containing Vowels in Even Counts solution

--- src/medium-collection/array-and-strings/find-longest-substring/solution.ts
+++ src/medium-collection/array-and-strings/find-longest-substring/solution.ts
@@ -1,3 +1,27 @@
 export function findTheLongestSubstring(s: string): number {
-  return 0;
+  const vowelToBit: { [key: string]: number } = {
+    a: 0,
+    e: 1,
+    i: 2,
+    o: 3,
+    u: 4,
+  };
+  let maskToIndex = new Map<number, number>();
+  maskToIndex.set(0, -1);
+  let result = 0;
+  let mask = 0;
+  for (let i = 0; i < s.length; i++) {
+    const ch = s[i];
+    if (vowelToBit.hasOwnProperty(ch)) {
+      const bit = vowelToBit[ch];
+      mask ^= 1 << bit;
+    }
+    if (maskToIndex.has(mask)) {
+      const prevIndex = maskToIndex.get(mask)!;
+      result = Math.max(result, i - prevIndex);
+    } else {
+      maskToIndex.set(mask, i);
+    }
+  }
+  return result;
 }

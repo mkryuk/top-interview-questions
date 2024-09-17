a3e71ec9f4f1ff79138c06b582a270dcc5a2441a
Maxim Kryuk <mkryuk@gmail.com>
2024-09-17 14:42:33 +0300

    Array and Strings: Minimum Time Difference solution

--- src/medium-collection/array-and-strings/find-min-difference/solution.ts
+++ src/medium-collection/array-and-strings/find-min-difference/solution.ts
@@ -1,3 +1,17 @@
 export function findMinDifference(timePoints: string[]): number {
-  return 0;
+  const minutes = timePoints.map(timeToMinutes);
+  minutes.sort((a, b) => a - b);
+  let result = Number.MAX_SAFE_INTEGER;
+  for (let i = 1; i < minutes.length; i++) {
+    result = Math.min(result, minutes[i] - minutes[i - 1]);
+  }
+
+  // special case: compare the first and last times (wrap-around)
+  result = Math.min(result, 1440 + minutes[0] - minutes[minutes.length - 1]);
+  return result;
+}
+
+function timeToMinutes(time: string): number {
+  let [hour, minute] = time.split(":").map(Number);
+  return hour * 60 + minute;
 }

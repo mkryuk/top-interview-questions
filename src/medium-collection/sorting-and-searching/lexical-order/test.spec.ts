daecb990fa72c8f80d9d76a385e6dd00ad7b7e49
Maxim Kryuk <mkryuk@gmail.com>
2024-09-21 23:39:41 +0300

    Sorting and Searching: Lexicographical Numbers init

---
+++ src/medium-collection/sorting-and-searching/lexical-order/test.spec.ts
@@ -0,0 +1,15 @@
+import { lexicalOrder } from "./solution";
+
+describe("Lexicographical Numbers", () => {
+  it("lexicalOrder should return [1,10,11,12,13,2,3,4,5,6,7,8,9] for n = 13", () => {
+    const n = 13;
+    const result = lexicalOrder(n);
+    expect(result).toEqual([1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9]);
+  });
+
+  it("lexicalOrder should return [1,2] for n = 2", () => {
+    const n = 2;
+    const result = lexicalOrder(n);
+    expect(result).toEqual([1, 2]);
+  });
+});

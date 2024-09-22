f9f292114a799b13d8247b0ba7ba81151b0ae58d
Maxim Kryuk <mkryuk@gmail.com>
2024-09-22 22:26:50 +0300

    Sorting and Searching: Lexicographical Numbers solution

--- src/medium-collection/sorting-and-searching/lexical-order/solution.ts
+++ src/medium-collection/sorting-and-searching/lexical-order/solution.ts
@@ -1,3 +1,16 @@
 export function lexicalOrder(n: number): number[] {
-  return [];
+  const result: number[] = [];
+  let curr = 1;
+  for (let i = 0; i < n; i++) {
+    result.push(curr);
+    if (curr * 10 <= n) {
+      curr *= 10;
+    } else {
+      while (curr % 10 === 9 || curr + 1 > n) {
+        curr = Math.floor(curr / 10);
+      }
+      curr++;
+    }
+  }
+  return result;
 }

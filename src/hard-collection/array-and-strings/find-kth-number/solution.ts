86d7339bb9a18a5ac8feac2c72dee406f94b31f7
Maxim Kryuk <mkryuk@gmail.com>
2024-09-23 12:06:18 +0300

    Array and Strings: K-th Smallest in Lexicographical Order solution

--- src/hard-collection/array-and-strings/find-kth-number/solution.ts
+++ src/hard-collection/array-and-strings/find-kth-number/solution.ts
@@ -1,3 +1,29 @@
 export function findKthNumber(n: number, k: number): number {
-  return 0;
+  let curr = 1;
+  k = k - 1; // we start from number 1
+
+  while (k > 0) {
+    let steps = calcSteps(n, curr, curr + 1);
+    if (steps <= k) {
+      // move to next sibling
+      curr += 1;
+      k -= steps;
+    } else {
+      // move to the first child
+      curr *= 10;
+      k -= 1; // count the current node
+    }
+  }
+
+  return curr;
+}
+
+function calcSteps(n: number, curr: number, next: number): number {
+  let steps = 0;
+  while (curr <= n) {
+    steps += Math.min(n + 1, next) - curr;
+    curr *= 10;
+    next *= 10;
+  }
+  return steps;
 }
--- src/hard-collection/array-and-strings/find-kth-number/test.spec.ts
+++ src/hard-collection/array-and-strings/find-kth-number/test.spec.ts
@@ -14,4 +14,18 @@ describe("K-th Smallest in Lexicographical Order", () => {
     const result = findKthNumber(n, k);
     expect(result).toEqual(1);
   });
+
+  it("findKthNumber should return 2 for n = 10, k = 3", () => {
+    const n = 10;
+    const k = 3;
+    const result = findKthNumber(n, k);
+    expect(result).toEqual(2);
+  });
+
+  it("findKthNumber should return 416126219 for n = 681692778, k = 351251360", () => {
+    const n = 681692778;
+    const k = 351251360;
+    const result = findKthNumber(n, k);
+    expect(result).toEqual(416126219);
+  });
 });

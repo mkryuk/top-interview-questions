e23f246bf38c2b3a52c812fe3a045d181a9fcfe9
Maxim Kryuk <mkryuk@gmail.com>
2024-09-24 15:19:45 +0300

    Array and Strings: Find the Length of the Longest Common Prefix solution

--- src/medium-collection/array-and-strings/longest-common-prefix/solution.ts
+++ src/medium-collection/array-and-strings/longest-common-prefix/solution.ts
@@ -1,3 +1,24 @@
 export function longestCommonPrefix(arr1: number[], arr2: number[]): number {
-  return 0;
+  let arr1Prefixes = new Set<string>();
+  // create all possible prefixes from arr1
+  for (const num of arr1) {
+    const numStr = num.toString();
+    for (let i = 1; i <= numStr.length; i++) {
+      const prefix = numStr.substring(0, i);
+      arr1Prefixes.add(prefix);
+    }
+  }
+
+  let result = 0;
+  // check if any prefix from arr2 matches
+  for (const num of arr2) {
+    const numStr = num.toString();
+    for (let i = 1; i <= numStr.length; i++) {
+      const prefix = numStr.substring(0, i);
+      if (arr1Prefixes.has(prefix)) {
+        result = Math.max(result, i);
+      }
+    }
+  }
+  return result;
 }

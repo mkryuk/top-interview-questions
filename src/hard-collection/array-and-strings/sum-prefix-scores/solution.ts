0013864d7651487a4c802f504391b3ac449559e1
Maxim Kryuk <mkryuk@gmail.com>
2024-09-25 23:32:33 +0300

    Array and Strings: Sum of Prefix Scores of Strings solution

--- src/hard-collection/array-and-strings/sum-prefix-scores/solution.ts
+++ src/hard-collection/array-and-strings/sum-prefix-scores/solution.ts
@@ -1,3 +1,30 @@
 export function sumPrefixScores(words: string[]): number[] {
-  return [];
+  const root: TrieNode = { count: 0, children: {} };
+  for (const word of words) {
+    let node = root;
+    for (const ch of word) {
+      if (!node.children[ch]) {
+        node.children[ch] = { count: 0, children: {} };
+      }
+      node = node.children[ch];
+      node.count += 1;
+    }
+  }
+
+  let result: number[] = [];
+  for (const word of words) {
+    let node = root;
+    let sum = 0;
+    for (const ch of word) {
+      node = node.children[ch];
+      sum += node.count;
+    }
+    result.push(sum);
+  }
+  return result;
+}
+
+interface TrieNode {
+  count: number;
+  children: { [key: string]: TrieNode };
 }

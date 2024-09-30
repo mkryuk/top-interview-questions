795253312d8b08de2516121886122da045e178a4
Maxim Kryuk <mkryuk@gmail.com>
2024-09-30 13:50:15 +0300

    Design: Design a Stack With Increment Operation init

---
+++ src/medium-collection/design/custom-stack/solution.ts
@@ -0,0 +1,11 @@
+export class CustomStack {
+  constructor(maxSize: number) {}
+
+  push(x: number): void {}
+
+  pop(): number {
+    return 0;
+  }
+
+  increment(k: number, val: number): void {}
+}

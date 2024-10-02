36b5913ece9732c303a03af8a130e08435b816aa
Maxim Kryuk <mkryuk@gmail.com>
2024-10-02 14:04:04 +0300

    Array and Strings: Rank Transform of an Array solution

--- src/easy-collection/array/array-rank-transform/solution.ts
+++ src/easy-collection/array/array-rank-transform/solution.ts
@@ -1,3 +1,9 @@
 export function arrayRankTransform(arr: number[]): number[] {
-  return [];
+  let rankMap = new Map<number, number>();
+  let uniqueValues = Array.from(new Set(arr)).sort((a, b) => a - b);
+  let rank = 1;
+  for (let value of uniqueValues) {
+    rankMap.set(value, rank++);
+  }
+  return arr.map((v) => rankMap.get(v)!);
 }

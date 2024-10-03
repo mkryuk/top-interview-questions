474a66f0fdf5e4bc102b12fd96740f28ff39a744
Maxim Kryuk <mkryuk@gmail.com>
2024-10-03 20:59:53 +0300

    Array and Strings: Make Sum Divisible by P solution

--- src/medium-collection/array-and-strings/min-subarray/solution.ts
+++ src/medium-collection/array-and-strings/min-subarray/solution.ts
@@ -1,3 +1,26 @@
 export function minSubarray(nums: number[], p: number): number {
-  return 0;
+  const totalMod = nums.reduce((a, b) => a + b, 0) % p;
+
+  if (totalMod === 0) {
+    return 0;
+  }
+
+  const prefixSumIndex = new Map<number, number>();
+  prefixSumIndex.set(0, -1);
+  let prefixSumMod = 0;
+  let result = nums.length;
+  for (let i = 0; i < nums.length; i++) {
+    prefixSumMod = mod(prefixSumMod + nums[i], p);
+    const targetMod = mod(prefixSumMod - totalMod, p);
+    if (prefixSumIndex.has(targetMod)) {
+      let index = prefixSumIndex.get(targetMod)!;
+      result = Math.min(result, i - index);
+    }
+    prefixSumIndex.set(prefixSumMod, i);
+  }
+  return result === nums.length ? -1 : result;
+}
+
+function mod(a: number, b: number): number {
+  return ((a % b) + b) % b;
 }

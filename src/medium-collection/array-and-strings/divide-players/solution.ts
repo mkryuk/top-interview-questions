6c870a2f4bbefb5fcc1ac3d4798551c118e625bf
Maxim Kryuk <mkryuk@gmail.com>
2024-10-05 00:16:52 +0300

    Array and Strings: Divide Players Into Teams of Equal Skill solution

--- src/medium-collection/array-and-strings/divide-players/solution.ts
+++ src/medium-collection/array-and-strings/divide-players/solution.ts
@@ -1,3 +1,14 @@
 export function dividePlayers(skill: number[]): number {
-  return 0;
+  skill.sort((a, b) => a - b);
+  let left = 0;
+  let right = skill.length - 1;
+  const targetSkill = skill[left] + skill[right];
+  let result = 0;
+  while (left < right) {
+    if (skill[left] + skill[right] !== targetSkill) {
+      return -1;
+    }
+    result += skill[left++] * skill[right--];
+  }
+  return result;
 }

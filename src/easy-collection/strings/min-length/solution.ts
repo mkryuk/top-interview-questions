998288dced4617cdac2f70a96ab60e341932ecf1
Maxim Kryuk <mkryuk@gmail.com>
2024-10-07 20:02:26 +0300

    Array and Strings: Minimum String Length After Removing Substrings solution

--- src/easy-collection/strings/min-length/solution.ts
+++ src/easy-collection/strings/min-length/solution.ts
@@ -1,3 +1,11 @@
 export function minLength(s: string): number {
-  return 0;
+  let stack: string[] = [];
+  for (const ch of s) {
+    if (stack[stack.length - 1] + ch === "AB" || stack[stack.length - 1] + ch === "CD") {
+      stack.pop();
+    } else {
+      stack.push(ch);
+    }
+  }
+  return stack.length;
 }

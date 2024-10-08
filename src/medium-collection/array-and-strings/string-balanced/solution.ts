41312a0fd4eb78d971cf1b1be270f77c9cf709b5
Maxim Kryuk <mkryuk@gmail.com>
2024-10-08 12:44:45 +0300

    Array and Strings: Minimum Number of Swaps to Make the String Balanced init

---
+++ src/medium-collection/array-and-strings/string-balanced/solution.ts
@@ -0,0 +1,3 @@
+export function minSwaps(s: string): number {
+  return 0;
+}
---
+++ src/medium-collection/array-and-strings/string-balanced/test.spec.ts
@@ -0,0 +1,21 @@
+import { minSwaps } from "./solution";
+
+describe("Minimum Number of Swaps to Make the String Balanced", () => {
+  it('minSwaps should return 1 for s = "][]["', () => {
+    const s = "][][";
+    const result = minSwaps(s);
+    expect(result).toEqual(1);
+  });
+
+  it('minSwaps should return 2 for s = "]]][[["', () => {
+    const s = "]]][[[";
+    const result = minSwaps(s);
+    expect(result).toEqual(2);
+  });
+
+  it('minSwaps should return 1 for s = "[]"', () => {
+    const s = "[]";
+    const result = minSwaps(s);
+    expect(result).toEqual(0);
+  });
+});

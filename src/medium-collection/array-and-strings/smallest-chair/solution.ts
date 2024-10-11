0f3d729a57f52e7661fbef09103b09237652a33c
Maxim Kryuk <mkryuk@gmail.com>
2024-10-12 00:14:46 +0300

    Array and Strings: The Number of the Smallest Unoccupied Chair solution

--- src/medium-collection/array-and-strings/smallest-chair/solution.ts
+++ src/medium-collection/array-and-strings/smallest-chair/solution.ts
@@ -1,3 +1,32 @@
+import { Heap } from "../../../hard-collection/design/heap/heap";
+
 export function smallestChair(times: number[][], targetFriend: number): number {
-  return 0;
+  // track seat indexes
+  let available = new Heap<number>((a, b) => a - b);
+  // track [leavingTime, chairNumber] for occupied chairs
+  let occupied = new Heap<[number, number]>((a, b) => a[0] - b[0]);
+  // [arrival, leaving, index];
+  let events = times.map(([arrival, leaving], index) => [arrival, leaving, index]);
+  // sort by arrival
+  events.sort((a, b) => a[0] - b[0]);
+  const n = times.length;
+  for (let i = 0; i < n; i++) {
+    available.push(i);
+  }
+
+  for (const [arrival, leaving, index] of events) {
+    while (occupied.size() > 0 && occupied.root()![0] <= arrival) {
+      const [_, chairNumber] = occupied.pop()!;
+      available.push(chairNumber);
+    }
+
+    const chairNumber = available.pop()!;
+    occupied.push([leaving, chairNumber]);
+
+    if (index === targetFriend) {
+      return chairNumber;
+    }
+  }
+
+  return -1;
 }
--- src/medium-collection/array-and-strings/smallest-chair/test.spec.ts
+++ src/medium-collection/array-and-strings/smallest-chair/test.spec.ts
@@ -22,4 +22,28 @@ describe("The Number of the Smallest Unoccupied Chair", () => {
     const result = smallestChair(times, targetFriend);
     expect(result).toEqual(2);
   });
+
+  it("smallestChair should return 0 for times = [[4,5],[12,13],[5,6],[1,2],[8,9],[9,10],[6,7],[3,4],[7,8],[13,14],[15,16],[14,15],[10,11],[11,12],[2,3],[16,17]], targetFriend = 15", () => {
+    const times = [
+      [4, 5],
+      [12, 13],
+      [5, 6],
+      [1, 2],
+      [8, 9],
+      [9, 10],
+      [6, 7],
+      [3, 4],
+      [7, 8],
+      [13, 14],
+      [15, 16],
+      [14, 15],
+      [10, 11],
+      [11, 12],
+      [2, 3],
+      [16, 17],
+    ];
+    const targetFriend = 15;
+    const result = smallestChair(times, targetFriend);
+    expect(result).toEqual(0);
+  });
 });

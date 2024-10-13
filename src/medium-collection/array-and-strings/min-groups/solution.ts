0cb67dd0f574179159d4c963a24234c14e160938
Maxim Kryuk <mkryuk@gmail.com>
2024-10-13 11:13:23 +0300

    Array and Strings: Divide Intervals Into Minimum Number of Groups solution

--- src/medium-collection/array-and-strings/min-groups/solution.ts
+++ src/medium-collection/array-and-strings/min-groups/solution.ts
@@ -1,3 +1,19 @@
 export function minGroups(intervals: number[][]): number {
-  return 0;
+  let events: [number, number][] = [];
+  for (const [start, end] of intervals) {
+    events.push([start, +1]);
+    events.push([end, -1]);
+  }
+
+  // start events (+1) before end events (-1) at the same time
+  events.sort(([timeA, deltaA], [timeB, deltaB]) => (timeA === timeB ? deltaB - deltaA : timeA - timeB));
+
+  let result = 0;
+  let active = 0;
+  for (const [time, delta] of events) {
+    active += delta;
+    result = Math.max(result, active);
+  }
+
+  return result;
 }

4687e3b9e30c976f9c9bc6e815b636b7ff6839a9
Maxim Kryuk <mkryuk@gmail.com>
2024-10-14 23:26:32 +0300

    Array and Strings: Maximal Score After Applying K Operations solution

--- src/medium-collection/array-and-strings/max-k-elements/solution.ts
+++ src/medium-collection/array-and-strings/max-k-elements/solution.ts
@@ -1,3 +1,17 @@
+import { Heap } from "../../../hard-collection/design/heap/heap";
+
 export function maxKelements(nums: number[], k: number): number {
-  return 0;
+  const heap = new Heap<number>((a, b) => b - a);
+  for (const num of nums) {
+    heap.push(num);
+  }
+
+  let result = 0;
+  for (let i = 0; i < k; i++) {
+    const num = heap.pop()!;
+    result += num;
+    heap.push(Math.ceil(num / 3));
+  }
+
+  return result;
 }

af42f4b4c383cfc0ec55e129290ae406b0a80762
Maxim Kryuk <mkryuk@gmail.com>
2024-10-15 16:56:04 +0300

    Array and Strings: Separate Black and White Balls solution

--- src/medium-collection/array-and-strings/minimum-steps/solution.ts
+++ src/medium-collection/array-and-strings/minimum-steps/solution.ts
@@ -1,3 +1,12 @@
 export function minimumSteps(s: string): number {
-  return 0;
+  let result = 0;
+  let countOnes = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "1") {
+      countOnes++;
+    } else {
+      result += countOnes;
+    }
+  }
+  return result;
 }

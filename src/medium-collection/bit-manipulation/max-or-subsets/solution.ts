a34a61889095c86020f14d848a2d84ba5fe73c85
Maxim Kryuk <mkryuk@gmail.com>
2024-10-18 13:47:06 +0300

    Bit Manipulation: Count Number of Maximum Bitwise-OR Subsets solution

--- src/medium-collection/bit-manipulation/max-or-subsets/solution.ts
+++ src/medium-collection/bit-manipulation/max-or-subsets/solution.ts
@@ -1,3 +1,17 @@
 export function countMaxOrSubsets(nums: number[]): number {
-  return 0;
+  const items: number[] = [];
+  backTrack(0, 0, nums, items);
+  let max = Math.max(...items);
+  let result = items.filter((i) => i === max);
+  return result.length;
+}
+
+function backTrack(leftOr: number, i: number, nums: number[], ors: number[]): void {
+  if (i === nums.length) {
+    ors.push(leftOr);
+    return;
+  }
+
+  backTrack(leftOr | nums[i], i + 1, nums, ors);
+  backTrack(leftOr, i + 1, nums, ors);
 }

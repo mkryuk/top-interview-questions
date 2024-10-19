5e87314a2a1d0e08bf5b7383b3195074ec46c127
Maxim Kryuk <mkryuk@gmail.com>
2024-10-19 23:12:25 +0300

    Bit Manipulation: Find Kth Bit in Nth Binary String solution

--- src/medium-collection/bit-manipulation/k-th-bit/solution.ts
+++ src/medium-collection/bit-manipulation/k-th-bit/solution.ts
@@ -1,3 +1,23 @@
 export function findKthBit(n: number, k: number): string {
-  return "";
+  if (n === 1) {
+    return "0";
+  }
+  // len = 2 ^ n
+  const len = Math.pow(2, n);
+  // mid = len / 2
+  const mid = Math.trunc(len / 2);
+  // the mid bit always 1
+  if (k === mid) {
+    return "1";
+  } else if (k < mid) {
+    // id the k is in the left we can reduce n to n -1
+    return findKthBit(n - 1, k);
+  } else {
+    // if k is in the right side
+    // we should find the k-th bit from the end and invert it
+    const corresponding = len - k;
+    const bit = findKthBit(n - 1, corresponding);
+    // invert the bit
+    return bit === "0" ? "1" : "0";
+  }
 }
--- src/medium-collection/bit-manipulation/k-th-bit/test.spec.ts
+++ src/medium-collection/bit-manipulation/k-th-bit/test.spec.ts
@@ -14,4 +14,11 @@ describe("Find Kth Bit in Nth Binary String", () => {
     const result = findKthBit(n, k);
     expect(result).toEqual("1");
   });
+
+  it('findKthBit should return "1" for n = 3, k = 7', () => {
+    const n = 3;
+    const k = 7;
+    const result = findKthBit(n, k);
+    expect(result).toEqual("1");
+  });
 });

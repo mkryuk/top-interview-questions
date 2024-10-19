12b6270ca2440abc5e2c028f429f901e49352aab
Maxim Kryuk <mkryuk@gmail.com>
2024-10-19 22:37:38 +0300

    Bit Manipulation: Find Kth Bit in Nth Binary String init

---
+++ src/medium-collection/bit-manipulation/k-th-bit/test.spec.ts
@@ -0,0 +1,17 @@
+import { findKthBit } from "./solution";
+
+describe("Find Kth Bit in Nth Binary String", () => {
+  it('findKthBit should return "0" for n = 3, k = 1', () => {
+    const n = 3;
+    const k = 1;
+    const result = findKthBit(n, k);
+    expect(result).toEqual("0");
+  });
+
+  it('findKthBit should return "1" for n = 4, k = 11', () => {
+    const n = 4;
+    const k = 11;
+    const result = findKthBit(n, k);
+    expect(result).toEqual("1");
+  });
+});

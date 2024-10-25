107ade74e321c4f81dfad2e528acada3c8fb4441
Maxim Kryuk <mkryuk@gmail.com>
2024-10-25 15:29:03 +0300

    Trees and Graphs: Remove Sub-Folders from the Filesystem init

---
+++ src/medium-collection/trees-and-graphs/remove-subfolders/test.spec.ts
@@ -0,0 +1,21 @@
+import { removeSubfolders } from "./solution";
+
+describe("Remove Sub-Folders from the Filesystem", () => {
+  it('removeSubfolders should return ["/a","/c/d","/c/f"] for folder = ["/a","/a/b","/c/d","/c/d/e","/c/f"]', () => {
+    const folder = ["/a", "/a/b", "/c/d", "/c/d/e", "/c/f"];
+    const result = removeSubfolders(folder);
+    expect(result).toEqual(["/a", "/c/d", "/c/f"]);
+  });
+
+  it('removeSubfolders should return ["/a"] for folder = ["/a","/a/b/c","/a/b/d"]', () => {
+    const folder = ["/a", "/a/b/c", "/a/b/d"];
+    const result = removeSubfolders(folder);
+    expect(result).toEqual(["/a"]);
+  });
+
+  it('removeSubfolders should return ["/a/b/c","/a/b/ca","/a/b/d"] for folder = ["/a/b/c","/a/b/ca","/a/b/d"]', () => {
+    const folder = ["/a/b/c", "/a/b/ca", "/a/b/d"];
+    const result = removeSubfolders(folder);
+    expect(result).toEqual(["/a/b/c", "/a/b/ca", "/a/b/d"]);
+  });
+});

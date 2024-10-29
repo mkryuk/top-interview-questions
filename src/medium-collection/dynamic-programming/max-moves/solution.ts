7cc114d24a9a8e4b5a7afd37f3be8c34c482e852
Maxim Kryuk <mkryuk@gmail.com>
2024-10-29 21:59:22 +0200

    Dynamic Programming: Maximum Number of Moves in a Grid solution

--- src/medium-collection/dynamic-programming/max-moves/solution.ts
+++ src/medium-collection/dynamic-programming/max-moves/solution.ts
@@ -1,3 +1,31 @@
 export function maxMoves(grid: number[][]): number {
-  return 0;
+  const m = grid.length;
+  const n = grid[0].length;
+  const cache = Array.from({ length: m }, () => new Array(n).fill(-1));
+  let result = 0;
+  for (let i = 0; i < m; i++) {
+    result = Math.max(result, dfs(i, 0, grid, cache));
+  }
+  return result;
+}
+
+function dfs(row: number, col: number, grid: number[][], cache: number[][]): number {
+  const m = grid.length;
+  const n = grid[0].length;
+  if (cache[row][col] !== -1) {
+    return cache[row][col];
+  }
+  let directions = [-1, 0, 1];
+  let maxMoves = 0;
+
+  for (const dir of directions) {
+    const newRow = row + dir;
+    const newCol = col + 1;
+    if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && grid[row][col] < grid[newRow][newCol]) {
+      maxMoves = Math.max(maxMoves, 1 + dfs(newRow, newCol, grid, cache));
+    }
+  }
+
+  cache[row][col] = maxMoves;
+  return maxMoves;
 }

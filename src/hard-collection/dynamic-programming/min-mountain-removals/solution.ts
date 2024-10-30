34b8b2ad16224fdf5632fa6ca3453a7e5e8ab18c
Maxim Kryuk <mkryuk@gmail.com>
2024-10-31 00:15:19 +0200

    Dynamic Programming: Minimum Number of Removals to Make Mountain Array solution

--- src/hard-collection/dynamic-programming/min-mountain-removals/solution.ts
+++ src/hard-collection/dynamic-programming/min-mountain-removals/solution.ts
@@ -1,3 +1,29 @@
 export function minimumMountainRemovals(nums: number[]): number {
-  return 0;
+  const n = nums.length;
+  const lis: number[] = new Array(n).fill(1);
+  const lds: number[] = new Array(n).fill(1);
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j] && lis[j] + 1 > lis[i]) {
+        lis[i] = lis[j] + 1;
+      }
+    }
+  }
+
+  for (let i = n - 1; i >= 0; i--) {
+    for (let j = i + 1; j < n; j++) {
+      if (nums[i] > nums[j] && lds[j] + 1 > lds[i]) {
+        lds[i] = lds[j] + 1;
+      }
+    }
+  }
+
+  let result = 0;
+  for (let i = 0; i < n; i++) {
+    if (lis[i] >= 2 && lds[i] >= 2) {
+      result = Math.max(result, lis[i] + lds[i] - 1);
+    }
+  }
+  return n - result;
 }

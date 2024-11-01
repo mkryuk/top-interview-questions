2a8ef5d154e1af4252aa68015a2430501466a5e0
Maxim Kryuk <mkryuk@gmail.com>
2024-11-01 13:29:15 +0200

    Array and Strings: Delete Characters to Make Fancy String solution

--- src/easy-collection/strings/make-fancy-string/solution.ts
+++ src/easy-collection/strings/make-fancy-string/solution.ts
@@ -1,3 +1,14 @@
 export function makeFancyString(s: string): string {
-  return "";
+  let result: string[] = [];
+  for (const char of s) {
+    if (
+      result.length >= 2 &&
+      result[result.length - 1] === result[result.length - 2] &&
+      result[result.length - 1] === char
+    ) {
+      continue;
+    }
+    result.push(char);
+  }
+  return result.join("");
 }

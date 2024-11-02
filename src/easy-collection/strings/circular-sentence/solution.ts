6887d201d33b03a5907161fe27b00ef6fca42965
Maxim Kryuk <mkryuk@gmail.com>
2024-11-02 20:04:20 +0200

    Array and Strings: Circular Sentence solution

--- src/easy-collection/strings/circular-sentence/solution.ts
+++ src/easy-collection/strings/circular-sentence/solution.ts
@@ -1,3 +1,13 @@
 export function isCircularSentence(sentence: string): boolean {
-  return false;
+  const words = sentence.split(" ");
+  let word = words[0];
+  for (let i = 1; i < words.length; i++) {
+    if (word[word.length - 1] !== words[i][0]) {
+      return false;
+    }
+    word = words[i];
+  }
+  const firstWord = words[0];
+  const lastWord = words[words.length - 1];
+  return firstWord[0] === lastWord[lastWord.length - 1];
 }

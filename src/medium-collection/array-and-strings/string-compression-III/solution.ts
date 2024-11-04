ebda239166c2716489b94660b4b31f911a870ac2
Maxim Kryuk <mkryuk@gmail.com>
2024-11-04 16:11:25 +0200

    Array and Strings: String Compression III solution

--- src/medium-collection/array-and-strings/string-compression-III/solution.ts
+++ src/medium-collection/array-and-strings/string-compression-III/solution.ts
@@ -1,3 +1,12 @@
 export function compressedString(word: string): string {
-  return "";
+  const result: string[] = [];
+  for (let i = 0; i < word.length; ) {
+    let count = 1;
+    while (i < word.length && word[i] === word[i + count] && count < 9) {
+      count++;
+    }
+    result.push(count + word[i]);
+    i += count;
+  }
+  return result.join("");
 }

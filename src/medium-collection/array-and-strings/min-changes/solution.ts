7475c2657591584446a93fe4ef13cf41605f988b
Maxim Kryuk <mkryuk@gmail.com>
2024-11-05 16:28:15 +0200

    Array and Strings: Minimum Number of Changes to Make Binary String Beautiful solution

--- src/medium-collection/array-and-strings/min-changes/solution.ts
+++ src/medium-collection/array-and-strings/min-changes/solution.ts
@@ -1,3 +1,9 @@
 export function minChanges(s: string): number {
-  return 0;
+  let result = 0;
+  for (let i = 0; i < s.length; i += 2) {
+    if (s[i] !== s[i + 1]) {
+      result++;
+    }
+  }
+  return result;
 }

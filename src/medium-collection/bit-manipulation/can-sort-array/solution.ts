7c35b589c14ef5a7c1af09d96acd9018a6deb827
Maxim Kryuk <mkryuk@gmail.com>
2024-11-07 01:21:57 +0200

    Bit Manipulation: Find if Array Can Be Sorted solution

--- src/medium-collection/bit-manipulation/can-sort-array/solution.ts
+++ src/medium-collection/bit-manipulation/can-sort-array/solution.ts
@@ -1,3 +1,30 @@
 export function canSortArray(nums: number[]): boolean {
-  return false;
+  let max = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    let currentMin = nums[i];
+    let currentMax = nums[i];
+    let counter = counterBits(nums[i]);
+    // pass through the whole group with the same bit count
+    while (i < nums.length - 1 && counterBits(nums[i + 1]) === counter) {
+      currentMin = Math.min(currentMin, nums[i + 1]);
+      currentMax = Math.max(currentMax, nums[i + 1]);
+      i++;
+    }
+    // if the group's min less than prev group max
+    // we unable to sort the array
+    if (currentMin < max) {
+      return false;
+    }
+    max = currentMax;
+  }
+  return true;
+}
+
+function counterBits(num: number): number {
+  let counter = 0;
+  while (num > 0) {
+    counter += num & 1;
+    num >>>= 1;
+  }
+  return counter;
 }

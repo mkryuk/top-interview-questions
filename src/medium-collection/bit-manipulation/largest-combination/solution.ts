5e64853f676b2a2b7b09b4f1e28bea2721e0cb83
Maxim Kryuk <mkryuk@gmail.com>
2024-11-07 15:07:06 +0200

    Bit Manipulation: Largest Combination With Bitwise AND Greater Than Zero solution

--- src/medium-collection/bit-manipulation/largest-combination/solution.ts
+++ src/medium-collection/bit-manipulation/largest-combination/solution.ts
@@ -1,3 +1,15 @@
 export function largestCombination(candidates: number[]): number {
-  return 0;
+  // 24 bits for numbers up to 10^7
+  let result = new Array(24).fill(0);
+  for (let candidate of candidates) {
+    let index = 0;
+    while (candidate > 0) {
+      if ((candidate & 1) !== 0) {
+        result[index]++;
+      }
+      candidate >>>= 1;
+      index++;
+    }
+  }
+  return Math.max(...result);
 }

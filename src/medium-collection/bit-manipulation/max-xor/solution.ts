8813ddfcb15d18c627d398349092d2320c9c5877
Maxim Kryuk <mkryuk@gmail.com>
2024-11-09 11:46:47 +0200

    Bit Manipulation: Maximum XOR for Each Query solution

--- src/medium-collection/bit-manipulation/max-xor/solution.ts
+++ src/medium-collection/bit-manipulation/max-xor/solution.ts
@@ -1,3 +1,15 @@
 export function getMaximumXor(nums: number[], maximumBit: number): number[] {
-  return [];
+  const n = nums.length;
+  const maxNum = (1 << maximumBit) - 1;
+  const result: number[] = new Array(n);
+  let xorSum = 0;
+  for (let i = 0; i < n; i++) {
+    xorSum ^= nums[i];
+  }
+
+  for (let i = 0; i < n; i++) {
+    result[i] = xorSum ^ maxNum;
+    xorSum = xorSum ^ nums[n - 1 - i];
+  }
+  return result;
 }

9c17806f77749a53a5b491426a8412397dfdd929
Maxim Kryuk <mkryuk@gmail.com>
2024-11-09 19:28:54 +0200

    Bit Manipulation: Minimum Array End solution

--- src/medium-collection/bit-manipulation/min-array-end/solution.ts
+++ src/medium-collection/bit-manipulation/min-array-end/solution.ts
@@ -1,3 +1,15 @@
 export function minEnd(n: number, x: number): number {
-  return 0;
+  let result: bigint = BigInt(x);
+  let remaining: bigint = BigInt(n - 1);
+  let position: bigint = BigInt(1);
+
+  while (remaining > BigInt(0)) {
+    if ((BigInt(x) & position) === BigInt(0)) {
+      result |= (remaining & BigInt(1)) * position;
+      remaining >>= BigInt(1);
+    }
+    position <<= BigInt(1);
+  }
+
+  return Number(result);
 }

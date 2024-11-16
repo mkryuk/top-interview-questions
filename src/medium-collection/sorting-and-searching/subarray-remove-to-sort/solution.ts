25ce36d09e0c0dc9381a501b344fbf588bc4a7c7
Maxim Kryuk <mkryuk@gmail.com>
2024-11-16 22:39:27 +0200

    Sorting and Searching: Shortest Subarray to be Removed to Make Array Sorted solution

--- src/medium-collection/sorting-and-searching/subarray-remove-to-sort/solution.ts
+++ src/medium-collection/sorting-and-searching/subarray-remove-to-sort/solution.ts
@@ -1,3 +1,31 @@
 export function findLengthOfShortestSubarray(arr: number[]): number {
-  return 0;
+  const n = arr.length;
+  let left = 0;
+  while (left + 1 < n && arr[left] <= arr[left + 1]) {
+    left++;
+  }
+
+  // the entire array is sorted
+  if (left === n - 1) {
+    return 0;
+  }
+
+  let right = n - 1;
+  while (right > 0 && arr[right] >= arr[right - 1]) {
+    right--;
+  }
+
+  let i = 0;
+  let j = right;
+  let result = Math.min(n - left - 1, right);
+  while (i <= left && j < n) {
+    if (arr[i] <= arr[j]) {
+      result = Math.min(result, j - i - 1);
+      i++;
+    } else {
+      j++;
+    }
+  }
+
+  return result;
 }

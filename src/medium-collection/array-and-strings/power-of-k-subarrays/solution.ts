3708fba96407430c30a90becfaeb4fd8b3ea17dc
Maxim Kryuk <mkryuk@gmail.com>
2024-11-17 22:34:20 +0200

    Array and Strings: Find the Power of K-Size Subarrays I solution

--- src/medium-collection/array-and-strings/power-of-k-subarrays/solution.ts
+++ src/medium-collection/array-and-strings/power-of-k-subarrays/solution.ts
@@ -1,3 +1,19 @@
 export function resultsArray(nums: number[], k: number): number[] {
-  return [];
+  if (k === 1) {
+    return nums;
+  }
+  const n = nums.length;
+  const result = new Array(n - k + 1).fill(-1);
+  let count = 1;
+  for (let i = 0; i < n - 1; i++) {
+    if (nums[i] + 1 === nums[i + 1]) {
+      count++;
+    } else {
+      count = 1;
+    }
+    if (count >= k) {
+      result[i - k + 2] = nums[i + 1];
+    }
+  }
+  return result;
 }

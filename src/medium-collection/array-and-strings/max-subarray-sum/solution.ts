7969ef4dcead86cf377d29339e86a51b46c8a991
Maxim Kryuk <mkryuk@gmail.com>
2024-11-19 23:56:02 +0200

    Array and Strings: Maximum Sum of Distinct Subarrays With Length K solution

--- src/medium-collection/array-and-strings/max-subarray-sum/solution.ts
+++ src/medium-collection/array-and-strings/max-subarray-sum/solution.ts
@@ -1,3 +1,24 @@
 export function maximumSubarraySum(nums: number[], k: number): number {
-  return 0;
+  let count = new Map<number, number>();
+  const n = nums.length;
+  let result = 0;
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    count.set(nums[i], (count.get(nums[i]) ?? 0) + 1);
+    sum += nums[i];
+    // decrease the window sum
+    if (i >= k) {
+      count.set(nums[i - k], count.get(nums[i - k])! - 1);
+      sum -= nums[i - k];
+    }
+    // remove num out of the window
+    if (count.get(nums[i - k]) === 0) {
+      count.delete(nums[i - k]);
+    }
+    // check if all elements are distinct
+    if (count.size === k) {
+      result = Math.max(result, sum);
+    }
+  }
+  return result;
 }
--- src/medium-collection/array-and-strings/max-subarray-sum/test.spec.ts
+++ src/medium-collection/array-and-strings/max-subarray-sum/test.spec.ts
@@ -12,6 +12,20 @@ describe("Maximum Sum of Distinct Subarrays With Length K", () => {
     const nums = [4, 4, 4];
     const k = 3;
     const result = maximumSubarraySum(nums, k);
-    expect(result).toEqual(15);
+    expect(result).toEqual(0);
+  });
+
+  it("maximumSubarraySum should return 0 for nums = [1,2,2], k = 2", () => {
+    const nums = [1, 2, 2];
+    const k = 2;
+    const result = maximumSubarraySum(nums, k);
+    expect(result).toEqual(3);
+  });
+
+  it("maximumSubarraySum should return 24 for nums = [1,1,1,7,8,9], k = 3", () => {
+    const nums = [1, 1, 1, 7, 8, 9];
+    const k = 3;
+    const result = maximumSubarraySum(nums, k);
+    expect(result).toEqual(24);
   });
 });

e530dbb0be3890097cc5f3ce0b32c7f10c390c7b
Maxim Kryuk <mkryuk@gmail.com>
2024-11-23 13:11:34 +0200

    Array and Strings: Flip Columns For Maximum Number of Equal Rows solution

--- src/medium-collection/array-and-strings/flip-columns-for-equal-rows/solution.ts
+++ src/medium-collection/array-and-strings/flip-columns-for-equal-rows/solution.ts
@@ -1,3 +1,16 @@
 export function maxEqualRowsAfterFlips(matrix: number[][]): number {
-  return 0;
+  const rowPatterns = new Map<string, number>();
+  for (const row of matrix) {
+    let pattern: number[] = [];
+    for (const ch of row) {
+      pattern.push(ch === row[0] ? 1 : 0);
+    }
+    const patternKey = pattern.join(",");
+    rowPatterns.set(patternKey, (rowPatterns.get(patternKey) ?? 0) + 1);
+  }
+  let result = 0;
+  for (const val of rowPatterns.values()) {
+    result = Math.max(result, val);
+  }
+  return result;
 }

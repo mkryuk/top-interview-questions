541cf5e4d138ce02427e103369664e685ea9381b
Maxim Kryuk <mkryuk@gmail.com>
2024-11-25 17:31:26 +0200

    Array and Strings: Maximum Matrix Sum solution

--- src/medium-collection/array-and-strings/max-matrix-sum/solution.ts
+++ src/medium-collection/array-and-strings/max-matrix-sum/solution.ts
@@ -1,3 +1,18 @@
 export function maxMatrixSum(matrix: number[][]): number {
-  return 0;
+  let result = 0;
+  let minVal = Number.MAX_SAFE_INTEGER;
+  let negativeCount = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] < 0) {
+        negativeCount++;
+      }
+      minVal = Math.min(minVal, Math.abs(matrix[i][j]));
+      result += Math.abs(matrix[i][j]);
+    }
+  }
+  if (negativeCount % 2 !== 0) {
+    result -= 2 * minVal;
+  }
+  return result;
 }

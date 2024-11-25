6120c9de9a809cfb74de3ec64a7d00bcfb248dec
Maxim Kryuk <mkryuk@gmail.com>
2024-11-26 01:11:32 +0200

    Array and Strings: Sliding Puzzle solution

--- src/hard-collection/array-and-strings/sliding-puzzle/solution.ts
+++ src/hard-collection/array-and-strings/sliding-puzzle/solution.ts
@@ -1,3 +1,32 @@
 export function slidingPuzzle(board: number[][]): number {
-  return 0;
+  let initialState = board.flat().join("");
+  let targetState = "123450";
+  let neighbors = [
+    [1, 3],
+    [0, 2, 4],
+    [1, 5],
+    [0, 4],
+    [1, 3, 5],
+    [2, 4],
+  ];
+  let queue: [string, number][] = [[initialState, 0]];
+  let visited = new Set<string>();
+  visited.add(initialState);
+  while (queue.length > 0) {
+    const [state, steps] = queue.shift()!;
+    if (state === targetState) {
+      return steps;
+    }
+    const zeroIndex = state.indexOf("0");
+    for (const neighbor of neighbors[zeroIndex]) {
+      const newBoard = state.split("");
+      [newBoard[zeroIndex], newBoard[neighbor]] = [newBoard[neighbor], newBoard[zeroIndex]];
+      const newState = newBoard.join("");
+      if (!visited.has(newState)) {
+        visited.add(newState);
+        queue.push([newState, steps + 1]);
+      }
+    }
+  }
+  return -1;
 }

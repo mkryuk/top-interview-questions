231510c1158a9f12c8e7057b2fa350b15b6885df
Maxim Kryuk <mkryuk@gmail.com>
2024-11-26 13:39:21 +0200

    Trees and Graphs: Find Champion II solution

--- src/medium-collection/trees-and-graphs/find-champion/solution.ts
+++ src/medium-collection/trees-and-graphs/find-champion/solution.ts
@@ -1,3 +1,8 @@
 export function findChampion(n: number, edges: number[][]): number {
-  return 0;
+  const refCount = new Array(n).fill(0);
+  for (const [from, to] of edges) {
+    refCount[to]++;
+  }
+  const zeroRefs = refCount.map((value, index) => (value === 0 ? index : -1)).filter((index) => index !== -1);
+  return zeroRefs.length === 1 ? zeroRefs[0] : -1;
 }

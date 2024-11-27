a6421c60cc37ea538c6d226d0623f0ff3e82a58a
Maxim Kryuk <mkryuk@gmail.com>
2024-11-27 14:04:17 +0200

    Trees and Graphs: Shortest Distance After Road Addition Queries I init

---
+++ src/medium-collection/trees-and-graphs/shortest-distance-after-query/solution.ts
@@ -0,0 +1,3 @@
+export function shortestDistanceAfterQueries(n: number, queries: number[][]): number[] {
+  return [];
+}
---
+++ src/medium-collection/trees-and-graphs/shortest-distance-after-query/test.spec.ts
@@ -0,0 +1,24 @@
+import { shortestDistanceAfterQueries } from "./solution";
+
+describe("Shortest Distance After Road Addition Queries I", () => {
+  it("shortestDistanceAfterQueries should return [3,2,1] for n = 5, queries = [[2,4],[0,2],[0,4]]", () => {
+    const n = 5;
+    const queries = [
+      [2, 4],
+      [0, 2],
+      [0, 4],
+    ];
+    const result = shortestDistanceAfterQueries(n, queries);
+    expect(result).toEqual([3, 2, 1]);
+  });
+
+  it("shortestDistanceAfterQueries should return [1,1] for n = 4, queries = [[0,3],[0,2]]", () => {
+    const n = 4;
+    const queries = [
+      [0, 3],
+      [0, 2],
+    ];
+    const result = shortestDistanceAfterQueries(n, queries);
+    expect(result).toEqual([1, 1]);
+  });
+});

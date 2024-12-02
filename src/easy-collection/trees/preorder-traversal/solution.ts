db777f447f71add6bcd5b74b9ac6bd1da59f15be
Maxim Kryuk <mkryuk@gmail.com>
2024-12-02 16:27:50 +0200

    Trees and Graphs: Binary Tree Preorder Traversal recursive solution

--- src/easy-collection/trees/preorder-traversal/solution.ts
+++ src/easy-collection/trees/preorder-traversal/solution.ts
@@ -1,5 +1,16 @@
 import { TreeNode } from "../common";
 
 export function preorderTraversal(root: TreeNode | null): number[] {
-  return [];
+  let result: number[] = [];
+  preorder(root, result);
+  return result;
+}
+
+function preorder(node: TreeNode | null, result: number[]) {
+  if (node === null) {
+    return;
+  }
+  result.push(node.val);
+  preorder(node.left, result);
+  preorder(node.right, result);
 }

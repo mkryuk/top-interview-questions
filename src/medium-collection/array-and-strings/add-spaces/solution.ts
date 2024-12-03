f3db4dcad28ee765faa4e194cc6b993ca814546e
Maxim Kryuk <mkryuk@gmail.com>
2024-12-03 12:52:07 +0200

    Array and Strings: Adding Spaces to a String solution

--- src/medium-collection/array-and-strings/add-spaces/solution.ts
+++ src/medium-collection/array-and-strings/add-spaces/solution.ts
@@ -1,3 +1,12 @@
 export function addSpaces(s: string, spaces: number[]): string {
-  return "";
+  const result: string[] = [];
+  let spaceIndex = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (i === spaces[spaceIndex]) {
+      result.push(" ");
+      spaceIndex++;
+    }
+    result.push(s[i]);
+  }
+  return result.join("");
 }

e4ba3136d8a39eba0834695624199ef9585136d4
Maxim Kryuk <mkryuk@gmail.com>
2024-12-04 23:51:48 +0200

    Array and Strings: Make String a Subsequence Using Cyclic Increments solution

--- src/medium-collection/array-and-strings/can-make-sequence/solution.ts
+++ src/medium-collection/array-and-strings/can-make-sequence/solution.ts
@@ -1,3 +1,10 @@
 export function canMakeSubsequence(str1: string, str2: string): boolean {
-  return false;
+  let str2Index = 0;
+  for (let i = 0; i < str1.length && str2Index < str2.length; i++) {
+    const incCharCode = str1.charCodeAt(i) + 1 > 122 ? 97 : str1.charCodeAt(i) + 1;
+    if (str1[i] === str2[str2Index] || incCharCode === str2.charCodeAt(str2Index)) {
+      str2Index++;
+    }
+  }
+  return str2Index === str2.length ? true : false;
 }

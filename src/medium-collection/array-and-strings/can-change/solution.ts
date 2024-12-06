3e493a375e3c03d071352734a4b3c98d2c35ca37
Maxim Kryuk <mkryuk@gmail.com>
2024-12-06 15:21:29 +0200

    Array and Strings: Move Pieces to Obtain a String solution

--- src/medium-collection/array-and-strings/can-change/solution.ts
+++ src/medium-collection/array-and-strings/can-change/solution.ts
@@ -1,3 +1,33 @@
 export function canChange(start: string, target: string): boolean {
-  return false;
+  let n = start.length;
+  let i = 0;
+  let j = 0;
+
+  while (i < n || j < n) {
+    while (i < n && start[i] === "_") {
+      i++;
+    }
+    while (j < n && target[j] === "_") {
+      j++;
+    }
+    if (i === n && j === n) {
+      return true;
+    }
+    if ((i === n) !== (j === n)) {
+      return false;
+    }
+    if (start[i] !== target[j]) {
+      return false;
+    }
+    if (start[i] === "L" && i < j) {
+      return false;
+    }
+    if (start[i] === "R" && i > j) {
+      return false;
+    }
+    i++;
+    j++;
+  }
+
+  return true;
 }

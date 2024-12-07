04644baab1d4f45812da0eb5fa40b06d5cee7d62
Maxim Kryuk <mkryuk@gmail.com>
2024-12-07 15:42:43 +0200

    Array and Strings: Maximum Number of Integers to Choose From a Range I solution

--- src/medium-collection/array-and-strings/max-count/solution.ts
+++ src/medium-collection/array-and-strings/max-count/solution.ts
@@ -1,3 +1,16 @@
 export function maxCount(banned: number[], n: number, maxSum: number): number {
-  return 0;
+  const bannedSet = new Set(banned);
+  let currentSum = 0;
+  let result = 0;
+  for (let i = 1; i <= n; i++) {
+    if (currentSum + i > maxSum) {
+      break;
+    }
+    if (bannedSet.has(i)) {
+      continue;
+    }
+    currentSum += i;
+    result++;
+  }
+  return result;
 }

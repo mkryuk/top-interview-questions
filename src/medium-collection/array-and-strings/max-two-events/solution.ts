a6ccc51b9b68952ff33e0cb48ae55bd718de7f14
Maxim Kryuk <mkryuk@gmail.com>
2024-12-08 10:51:55 +0200

    Array and Strings: Two Best Non-Overlapping Events init

---
+++ src/medium-collection/array-and-strings/max-two-events/solution.ts
@@ -0,0 +1,3 @@
+export function maxTwoEvents(events: number[][]): number {
+  return 0;
+}
---
+++ src/medium-collection/array-and-strings/max-two-events/test.spec.ts
@@ -0,0 +1,33 @@
+import { maxTwoEvents } from "./solution";
+
+describe("Two Best Non-Overlapping Events", () => {
+  it("maxTwoEvents should return 4 for events = [[1,3,2],[4,5,2],[2,4,3]]", () => {
+    const events = [
+      [1, 3, 2],
+      [4, 5, 2],
+      [2, 4, 3],
+    ];
+    const result = maxTwoEvents(events);
+    expect(result).toEqual(4);
+  });
+
+  it("maxTwoEvents should return 5 for events = [[1,3,2],[4,5,2],[1,5,5]]", () => {
+    const events = [
+      [1, 3, 2],
+      [4, 5, 2],
+      [1, 5, 5],
+    ];
+    const result = maxTwoEvents(events);
+    expect(result).toEqual(5);
+  });
+
+  it("maxTwoEvents should return 8 for events = [[1,5,3],[1,5,1],[6,6,5]]", () => {
+    const events = [
+      [1, 5, 3],
+      [1, 5, 1],
+      [6, 6, 5],
+    ];
+    const result = maxTwoEvents(events);
+    expect(result).toEqual(8);
+  });
+});

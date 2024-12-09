f9cc9c3c8d4845ff890a1a9554e42a406d3e757a
Maxim Kryuk <mkryuk@gmail.com>
2024-12-10 00:00:18 +0200

    Array and Strings: Special Array II solution

--- src/medium-collection/array-and-strings/special-array-II/solution.ts
+++ src/medium-collection/array-and-strings/special-array-II/solution.ts
@@ -1,3 +1,17 @@
 export function isArraySpecial(nums: number[], queries: number[][]): boolean[] {
-  return [];
+  const n = nums.length;
+  const sameParity = new Array(n - 1).fill(0);
+  for (let i = 0; i < n; i++) {
+    // if both are even or both are odd, mark 1
+    if (nums[i] % 2 === nums[i + 1] % 2) {
+      sameParity[i] = 1;
+    }
+  }
+
+  const prefixSum = new Array(n).fill(0);
+  for (let i = 1; i < n; i++) {
+    prefixSum[i] = prefixSum[i - 1] + sameParity[i - 1];
+  }
+
+  return queries.map(([from, to]) => (from === to ? true : prefixSum[from] - prefixSum[to] === 0));
 }

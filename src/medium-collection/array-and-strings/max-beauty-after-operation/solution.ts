5b2ea0a99480ddf61759cb7105334056f50b02f3
Maxim Kryuk <mkryuk@gmail.com>
2024-12-12 01:33:02 +0200

    Array and Strings: Maximum Beauty of an Array After Applying Operation solution

--- src/medium-collection/array-and-strings/max-beauty-after-operation/solution.ts
+++ src/medium-collection/array-and-strings/max-beauty-after-operation/solution.ts
@@ -1,3 +1,13 @@
 export function maximumBeauty(nums: number[], k: number): number {
-  return 0;
+  nums.sort((a, b) => a - b);
+
+  let left = 0;
+  let result = 0;
+  for (let right = 0; right < nums.length; right++) {
+    while (nums[right] - nums[left] > 2 * k) {
+      left++;
+    }
+    result = Math.max(result, right - left + 1);
+  }
+  return result;
 }

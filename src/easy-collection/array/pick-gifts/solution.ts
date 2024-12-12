e4e3f5540d0e8d6293d1ef6f24fa76aa48328792
Maxim Kryuk <mkryuk@gmail.com>
2024-12-12 16:44:49 +0200

    Array and Strings: Take Gifts From the Richest Pile solution

--- src/easy-collection/array/pick-gifts/solution.ts
+++ src/easy-collection/array/pick-gifts/solution.ts
@@ -1,3 +1,17 @@
+import { Heap } from "../../../hard-collection/design/heap/heap";
+
 export function pickGifts(gifts: number[], k: number): number {
-  return 0;
+  const maxHeap = new Heap<number>((a, b) => b - a);
+  for (const gift of gifts) {
+    maxHeap.push(gift);
+  }
+
+  for (let i = 0; i < k; i++) {
+    let pile = maxHeap.pop()!;
+    pile = Math.floor(Math.sqrt(pile));
+    maxHeap.push(pile);
+  }
+
+  const result = maxHeap.nodes.reduce((prev, curr) => prev + curr, 0);
+  return result;
 }

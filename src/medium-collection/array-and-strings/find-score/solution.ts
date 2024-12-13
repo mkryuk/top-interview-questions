9a5af7c498bb3509def4278679ebba4bb59c2db3
Maxim Kryuk <mkryuk@gmail.com>
2024-12-14 00:22:17 +0200

    Array and Strings: Find Score of an Array After Marking All Elements solution

--- src/medium-collection/array-and-strings/find-score/solution.ts
+++ src/medium-collection/array-and-strings/find-score/solution.ts
@@ -1,3 +1,25 @@
 export function findScore(nums: number[]): number {
-  return 0;
+  const n = nums.length;
+  const indexNum: [number, number][] = [];
+  for (let index = 0; index < n; index++) {
+    indexNum.push([index, nums[index]]);
+  }
+  // sort by num or index in case nums are equal
+  indexNum.sort(([i1, n1], [i2, n2]) => n1 - n2 || i1 - i2);
+  const marked = new Array(n).fill(false);
+  let result = 0;
+  for (const [index, num] of indexNum) {
+    if (marked[index]) {
+      continue;
+    }
+    result += num;
+    marked[index] = true;
+    if (index > 0) {
+      marked[index - 1] = true;
+    }
+    if (index < n - 1) {
+      marked[index + 1] = true;
+    }
+  }
+  return result;
 }

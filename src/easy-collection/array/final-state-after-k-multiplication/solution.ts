ded45d1a2c390778e860b4a409e02ab75078ce2b
Maxim Kryuk <mkryuk@gmail.com>
2024-12-16 16:28:42 +0200

    Array and Strings: Final Array State After K Multiplication Operations I solution

--- src/easy-collection/array/final-state-after-k-multiplication/solution.ts
+++ src/easy-collection/array/final-state-after-k-multiplication/solution.ts
@@ -1,3 +1,18 @@
+import { Heap } from "../../../hard-collection/design/heap/heap";
+
 export function getFinalState(nums: number[], k: number, multiplier: number): number[] {
-  return [];
+  let minHeap = new Heap<[number, number]>(([ai, a], [bi, b]) => a - b || ai - bi);
+  const n = nums.length;
+  for (let i = 0; i < n; i++) {
+    minHeap.push([i, nums[i]]);
+  }
+
+  for (let i = 0; i < k; i++) {
+    let [index, num] = minHeap.pop()!;
+    num *= multiplier;
+    nums[index] = num;
+    minHeap.push([index, num]);
+  }
+
+  return nums;
 }

9bfe85272aee5cf92d7285a833e754460f1a40bf
Maxim Kryuk <mkryuk@gmail.com>
2024-12-19 00:22:36 +0200

    Array and Strings: Final Prices With a Special Discount in a Shop solution

--- src/easy-collection/array/final-prices/solution.ts
+++ src/easy-collection/array/final-prices/solution.ts
@@ -1,3 +1,14 @@
 export function finalPrices(prices: number[]): number[] {
-  return [];
+  let result: number[] = [];
+  const n = prices.length;
+  for (let i = 0; i < n; i++) {
+    const current = prices[i];
+    let j = i + 1;
+    while (j < n && prices[j] > current) {
+      j++;
+    }
+    const price = j === n ? current : current - prices[j];
+    result.push(price);
+  }
+  return result;
 }

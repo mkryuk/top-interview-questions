2b04b71f869dfdd5aee8eb129103fe1983384921
Maxim Kryuk <mkryuk@gmail.com>
2024-12-20 00:01:35 +0200

    Array and Strings: Max Chunks To Make Sorted solution

--- src/medium-collection/array-and-strings/max-chunks-to-sorted/solution.ts
+++ src/medium-collection/array-and-strings/max-chunks-to-sorted/solution.ts
@@ -1,3 +1,12 @@
 export function maxChunksToSorted(arr: number[]): number {
-  return 0;
+  const n = arr.length;
+  let result = 0;
+  let max = -Infinity;
+  for (let i = 0; i < n; i++) {
+    max = Math.max(max, arr[i]);
+    if (max === i) {
+      result++;
+    }
+  }
+  return result;
 }
--- src/medium-collection/array-and-strings/max-chunks-to-sorted/test.spec.ts
+++ src/medium-collection/array-and-strings/max-chunks-to-sorted/test.spec.ts
@@ -1,15 +1,21 @@
 import { maxChunksToSorted } from "./solution";
 
 describe("Max Chunks To Make Sorted", () => {
-  it("maxChunksToSorted should return 1 for arr = [4,3,2,1,0];", () => {
+  it("maxChunksToSorted should return 1 for arr=[4,3,2,1,0]", () => {
     const arr = [4, 3, 2, 1, 0];
     const result = maxChunksToSorted(arr);
     expect(result).toBe(1);
   });
 
-  it("maxChunksToSorted should return 4 for arr = [1,0,2,3,4];", () => {
+  it("maxChunksToSorted should return 4 for arr=[1,0,2,3,4]", () => {
     const arr = [1, 0, 2, 3, 4];
     const result = maxChunksToSorted(arr);
     expect(result).toBe(4);
+  });
+
+  it("maxChunksToSorted should return 1 for arr=[2,0,3,4,1]", () => {
+    const arr = [2, 0, 3, 4, 1];
+    const result = maxChunksToSorted(arr);
+    expect(result).toBe(1);
   });
 });

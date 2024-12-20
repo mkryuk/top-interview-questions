60ac14119e342ec9c2e8fd79f4552682b1f4b754
Maxim Kryuk <mkryuk@gmail.com>
2024-12-20 16:56:33 +0200

    Trees and Graphs: Reverse Odd Levels of Binary Tree solution

--- src/medium-collection/trees-and-graphs/reverse-odd-levels/solution.ts
+++ src/medium-collection/trees-and-graphs/reverse-odd-levels/solution.ts
@@ -1,5 +1,40 @@
 import { TreeNode } from "../common";
 
 export function reverseOddLevels(root: TreeNode | null): TreeNode | null {
-  return null;
+  if (root == null) {
+    return null;
+  }
+  const queue: TreeNode[] = [root];
+  let start = 0;
+  let level = 0;
+
+  while (start < queue.length) {
+    const levelSize = queue.length - start;
+    const currentLevel: TreeNode[] = [];
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue[start++];
+      currentLevel.push(node);
+      if (node.left !== null) {
+        queue.push(node.left);
+      }
+      if (node.right !== null) {
+        queue.push(node.right);
+      }
+    }
+
+    if (level % 2 === 1) {
+      reverseLevelValues(currentLevel);
+    }
+    level++;
+  }
+  return root;
+}
+function reverseLevelValues(levelNodes: TreeNode[]) {
+  let left = 0;
+  let right = levelNodes.length - 1;
+  while (left < right) {
+    [levelNodes[left].val, levelNodes[right].val] = [levelNodes[right].val, levelNodes[left].val];
+    left++;
+    right--;
+  }
 }

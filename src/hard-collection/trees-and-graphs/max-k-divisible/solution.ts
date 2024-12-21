607165291e8ec930e88afa2068f13bae9eb214be
Maxim Kryuk <mkryuk@gmail.com>
2024-12-21 23:24:27 +0200

    Trees and Graphs: Maximum Number of K-Divisible Components solution

--- src/hard-collection/trees-and-graphs/max-k-divisible/solution.ts
+++ src/hard-collection/trees-and-graphs/max-k-divisible/solution.ts
@@ -1,3 +1,33 @@
 export function maxKDivisibleComponents(n: number, edges: number[][], values: number[], k: number): number {
-  return 0;
+  const tree: number[][] = Array.from({ length: n }, () => []);
+  for (const [a, b] of edges) {
+    tree[a].push(b);
+    tree[b].push(a);
+  }
+
+  let components = 0;
+  dfs(0, -1, tree, values, k, () => components++);
+  return components;
+}
+function dfs(
+  node: number,
+  parent: number,
+  tree: number[][],
+  values: number[],
+  k: number,
+  incrementComponents: () => number,
+): number {
+  let subtreeSum = values[node];
+
+  for (const neighbor of tree[node]) {
+    if (neighbor !== parent) {
+      subtreeSum += dfs(neighbor, node, tree, values, k, incrementComponents);
+    }
+  }
+
+  if (subtreeSum % k === 0) {
+    incrementComponents();
+    return 0;
+  }
+  return subtreeSum;
 }

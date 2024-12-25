c035d34c659e3cf39b911818928e57f34b7fb4c7
Maxim Kryuk <mkryuk@gmail.com>
2024-12-25 14:03:42 +0200

    Trees and Graphs: Find Largest Value in Each Tree Row solution

--- src/medium-collection/trees-and-graphs/largest-values/solution.ts
+++ src/medium-collection/trees-and-graphs/largest-values/solution.ts
@@ -1,5 +1,26 @@
 import { TreeNode } from "../common";
 
 export function largestValues(root: TreeNode | null): number[] {
-  return [];
+  if (root === null) {
+    return [];
+  }
+  let queue: TreeNode[] = [root];
+  let qIdx = 0;
+  const result: number[] = [];
+  while (queue.length - qIdx > 0) {
+    const levelLength = queue.length - qIdx;
+    let maxValue = -Infinity;
+    for (let i = 0; i < levelLength; i++) {
+      const node = queue[qIdx++];
+      maxValue = Math.max(maxValue, node.val);
+      if (node.left !== null) {
+        queue.push(node.left);
+      }
+      if (node.right !== null) {
+        queue.push(node.right);
+      }
+    }
+    result.push(maxValue);
+  }
+  return result;
 }

2c8e4ade70b77be8be0defc1a52530d1403ca040
Maxim Kryuk <mkryuk@gmail.com>
2024-12-28 00:51:02 +0200

    Array and Strings: Best Sightseeing Pair solution

--- src/medium-collection/array-and-strings/best-sightseeing-pair/solution.ts
+++ src/medium-collection/array-and-strings/best-sightseeing-pair/solution.ts
@@ -1,3 +1,10 @@
 export function maxScoreSightseeingPair(values: number[]): number {
-  return 0;
+  let maxLeftScore = values[0];
+  let result = -Infinity;
+  for (let i = 1; i < values.length; i++) {
+    let currentScore = maxLeftScore + values[i] - i;
+    result = Math.max(result, currentScore);
+    maxLeftScore = Math.max(maxLeftScore, values[i] + i);
+  }
+  return result;
 }

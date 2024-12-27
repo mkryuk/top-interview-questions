dafaa772f45ab6fe1a5a4a123978ba4dfb987276
Maxim Kryuk <mkryuk@gmail.com>
2024-12-27 11:29:31 +0200

    Dynamic Programming: Target Sum solution

--- src/medium-collection/dynamic-programming/target-sum/solution.ts
+++ src/medium-collection/dynamic-programming/target-sum/solution.ts
@@ -1,3 +1,20 @@
 export function findTargetSumWays(nums: number[], target: number): number {
-  return 0;
+  const sum = nums.reduce((prev, curr) => prev + curr, 0);
+  if (Math.abs(target) > sum) {
+    return 0;
+  }
+  if ((sum + target) % 2 !== 0) {
+    return 0;
+  }
+  let dpLength = Math.floor((sum + target) / 2);
+  const dp = new Array(dpLength + 1).fill(0);
+  // there's exactly 1 way to reach 0 sum (choose nothing)
+  dp[0] = 1;
+
+  for (const num of nums) {
+    for (let i = dpLength; i >= num; i--) {
+      dp[i] += dp[i - num];
+    }
+  }
+  return dp[dpLength];
 }

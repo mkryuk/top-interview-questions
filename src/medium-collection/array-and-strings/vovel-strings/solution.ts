8455aeecdaaff9c3066efcd06aff1127fe178eb7
Maxim Kryuk <mkryuk@gmail.com>
2025-01-03 13:38:53 +0200

    Array and Strings: Count Vowel Strings in Ranges solution

--- src/medium-collection/array-and-strings/vovel-strings/solution.ts
+++ src/medium-collection/array-and-strings/vovel-strings/solution.ts
@@ -1,3 +1,17 @@
 export function vowelStrings(words: string[], queries: number[][]): number[] {
-  return [];
+  const n = words.length;
+  const prefixSum = new Array(n + 1).fill(0);
+  let vowels = new Set(["a", "e", "i", "o", "u"]);
+  for (let i = 0; i < n; i++) {
+    if (vowels.has(words[i][0]) && vowels.has(words[i][words[i].length - 1])) {
+      prefixSum[i + 1]++;
+    }
+    prefixSum[i + 1] += prefixSum[i] ?? 0;
+  }
+  const result: number[] = [];
+  for (const [start, end] of queries) {
+    const quantity = prefixSum[end + 1] - prefixSum[start];
+    result.push(quantity);
+  }
+  return result;
 }

abcb411a35cb04d738e82aa06682467f126c96ca
Maxim Kryuk <mkryuk@gmail.com>
2025-01-04 19:59:57 +0200

    Array and Strings: Count Substrings That Satisfy K-Constraint I solution

--- src/easy-collection/strings/count-k-substrings/solution.ts
+++ src/easy-collection/strings/count-k-substrings/solution.ts
@@ -1,3 +1,28 @@
 export function countKConstraintSubstrings(s: string, k: number): number {
-  return 0;
+  const n = s.length;
+  let result = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) {
+      if (satisfiesConstraint(s, k, i, j)) {
+        result++;
+      }
+    }
+  }
+  return result;
+}
+
+function satisfiesConstraint(s: string, k: number, start: number, end: number) {
+  let zeroCount = 0;
+  let oneCount = 0;
+  for (let i = start; i <= end; i++) {
+    if (s[i] === "0") {
+      zeroCount++;
+    } else {
+      oneCount++;
+    }
+    if (zeroCount > k && oneCount > k) {
+      return false;
+    }
+  }
+  return true;
 }

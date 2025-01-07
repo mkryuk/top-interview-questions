7c4ec23345c2f9515fc6872d98c6f1ec541a9da5
Maxim Kryuk <mkryuk@gmail.com>
2025-01-07 11:03:10 +0200

    Array and Strings: Minimum Number of Operations to Move All Balls to Each Box solution

--- src/medium-collection/array-and-strings/min-move-operations/solution.ts
+++ src/medium-collection/array-and-strings/min-move-operations/solution.ts
@@ -1,3 +1,25 @@
 export function minOperations(boxes: string): number[] {
-  return [];
+  const n = boxes.length;
+  const result = new Array(n).fill(0);
+  let operations = 0;
+  let count = 0;
+  // count the balls to move from the left
+  for (let i = 0; i < n; i++) {
+    operations += count;
+    if (boxes[i] === "1") {
+      count++;
+    }
+    result[i] += operations;
+  }
+  operations = 0;
+  count = 0;
+  // count the balls to move from the right
+  for (let i = n - 1; i >= 0; i--) {
+    operations += count;
+    if (boxes[i] === "1") {
+      count++;
+    }
+    result[i] += operations;
+  }
+  return result;
 }

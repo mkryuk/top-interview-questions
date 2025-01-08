40c60543dd0416a5ba76fffb5b01113c324bbca9
Maxim Kryuk <mkryuk@gmail.com>
2025-01-08 14:00:13 +0200

    Array and Strings: Count Prefix and Suffix Pairs I solution

--- src/easy-collection/array/prefix-suffix-pairs/solution.ts
+++ src/easy-collection/array/prefix-suffix-pairs/solution.ts
@@ -1,3 +1,23 @@
 export function countPrefixSuffixPairs(words: string[]): number {
-  return 0;
+  let result = 0;
+  for (let i = 0; i < words.length; i++) {
+    for (let j = i + 1; j < words.length; j++) {
+      if (isPrefixAndSuffix(words[i], words[j])) {
+        result++;
+      }
+    }
+  }
+  return result;
+}
+
+function isPrefixAndSuffix(str1: string, str2: string): boolean {
+  if (str1.length > str2.length) {
+    return false;
+  }
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i] !== str2[i] || str1[str1.length - 1 - i] !== str2[str2.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }

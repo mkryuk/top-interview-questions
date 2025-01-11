ee09ebc08b6bdc3997d266dd6965021f853f5c7e
Maxim Kryuk <mkryuk@gmail.com>
2025-01-11 12:39:17 +0200

    Array and Strings: Construct K Palindrome Strings solution

--- src/medium-collection/array-and-strings/can-construct/solution.ts
+++ src/medium-collection/array-and-strings/can-construct/solution.ts
@@ -1,3 +1,16 @@
 export function canConstruct(s: string, k: number): boolean {
-  return false;
+  if (s.length < k) {
+    return false;
+  }
+  const charCount = new Map<string, number>();
+  for (const char of s) {
+    charCount.set(char, (charCount.get(char) ?? 0) + 1);
+  }
+  let oddCount = 0;
+  for (const [char, count] of charCount) {
+    if (count % 2 !== 0) {
+      oddCount++;
+    }
+  }
+  return oddCount <= k;
 }

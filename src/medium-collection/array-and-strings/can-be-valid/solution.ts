f8acc2550c5edd8b8b1d2b24e9cc85f578fcc5cd
Maxim Kryuk <mkryuk@gmail.com>
2025-01-12 20:48:38 +0200

    Array and Strings: Check if a Parentheses String Can Be Valid solution

--- src/medium-collection/array-and-strings/can-be-valid/solution.ts
+++ src/medium-collection/array-and-strings/can-be-valid/solution.ts
@@ -1,3 +1,29 @@
 export function canBeValid(s: string, locked: string): boolean {
-  return false;
+  if (s.length % 2 !== 0) {
+    return false;
+  }
+  // tracks the least number of open brackets possible.
+  let minOpen = 0;
+  // tracks the most number of open brackets possible.
+  let maxOpen = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (locked[i] === "1") {
+      if (s[i] === "(") {
+        minOpen++;
+        maxOpen++;
+      } else {
+        minOpen--;
+        maxOpen--;
+      }
+    } else {
+      maxOpen++;
+      minOpen--;
+    }
+    if (maxOpen < 0) {
+      return false;
+    }
+    // minOpen cannot be negative
+    minOpen = Math.max(minOpen, 0);
+  }
+  return minOpen === 0;
 }

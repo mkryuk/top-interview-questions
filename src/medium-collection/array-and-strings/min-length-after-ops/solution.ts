050b707333ea40d26b11a54e452b30c4efd2a18a
Maxim Kryuk <mkryuk@gmail.com>
2025-01-13 12:35:58 +0200

    Array and Strings: Minimum Length of String After Operations solution

--- src/medium-collection/array-and-strings/min-length-after-ops/solution.ts
+++ src/medium-collection/array-and-strings/min-length-after-ops/solution.ts
@@ -1,3 +1,14 @@
 export function minimumLength(s: string): number {
-  return 0;
+  const charCount = new Array(26).fill(0);
+  for (const char of s) {
+    charCount[char.charCodeAt(0) - 97]++;
+  }
+  let result = 0;
+  for (const count of charCount) {
+    if (count === 0) {
+      continue;
+    }
+    result += count % 2 === 0 ? 2 : 1;
+  }
+  return result;
 }

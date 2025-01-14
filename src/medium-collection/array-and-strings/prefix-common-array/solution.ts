d5ff664332d517bac56542c9905f0631fd451854
Maxim Kryuk <mkryuk@gmail.com>
2025-01-14 16:27:51 +0200

    Array and Strings: Find the Prefix Common Array of Two Arrays solution

--- src/medium-collection/array-and-strings/prefix-common-array/solution.ts
+++ src/medium-collection/array-and-strings/prefix-common-array/solution.ts
@@ -1,3 +1,22 @@
 export function findThePrefixCommonArray(A: number[], B: number[]): number[] {
-  return [];
+  const n = A.length;
+  const aIndexMap = new Map<number, number>();
+  for (let i = 0; i < n; i++) {
+    aIndexMap.set(A[i], i);
+  }
+  const bIndexMap = new Map<number, number>();
+  for (let i = 0; i < n; i++) {
+    bIndexMap.set(B[i], i);
+  }
+  let result = new Array(n).fill(0);
+  for (let i = 0; i < n; i++) {
+    const index = Math.max(aIndexMap.get(A[i])!, bIndexMap.get(A[i])!);
+    result[index] += 1;
+  }
+  let commonCount = 0;
+  for (let i = 0; i < n; i++) {
+    commonCount += result[i];
+    result[i] = commonCount;
+  }
+  return result;
 }

21cf30855f1c35c85cf01ed30fb5f4cc4b5b8a32
Maxim Kryuk <mkryuk@gmail.com>
2025-01-16 12:48:16 +0200

    Bit Manipulation: Minimize XOR solution

--- src/medium-collection/bit-manipulation/minimize-xor/solution.ts
+++ src/medium-collection/bit-manipulation/minimize-xor/solution.ts
@@ -1,3 +1,31 @@
 export function minimizeXor(num1: number, num2: number): number {
-  return 0;
+  let bitCount = countSetBits(num2);
+  let result = 0;
+  // place 1s from the most significant bit of num1 as much as possible
+  for (let i = 31; i >= 0; i--) {
+    if ((num1 & (1 << i)) !== 0 && bitCount > 0) {
+      result |= 1 << i;
+      bitCount--;
+    }
+  }
+
+  // if there are still bits left to place, fill from the least significant bit
+  for (let i = 0; i < 32 && bitCount > 0; i++) {
+    if ((num1 & (1 << i)) === 0) {
+      result |= 1 << i;
+      bitCount--;
+    }
+  }
+  return result;
+}
+
+function countSetBits(num: number): number {
+  let count = 0;
+  while (num > 0) {
+    if ((num & 1) === 1) {
+      count++;
+    }
+    num >>= 1;
+  }
+  return count;
 }

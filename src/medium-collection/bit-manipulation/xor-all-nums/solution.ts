1bb9bd778238bb5e0a19bbc0f633fcb0744007f3
Maxim Kryuk <mkryuk@gmail.com>
2025-01-16 23:16:19 +0200

    Bit Manipulation: Bitwise XOR of All Pairings solution

--- src/medium-collection/bit-manipulation/xor-all-nums/solution.ts
+++ src/medium-collection/bit-manipulation/xor-all-nums/solution.ts
@@ -1,3 +1,16 @@
 export function xorAllNums(nums1: number[], nums2: number[]): number {
-  return 0;
+  let xorNums1 = 0;
+  for (const num of nums1) {
+    xorNums1 ^= num;
+  }
+
+  let xorNums2 = 0;
+  for (const num of nums2) {
+    xorNums2 ^= num;
+  }
+
+  // if nums1.length is odd, xorNums2 contributes
+  // if nums2.length is odd, xorNums1 contributes
+  let result = (nums1.length % 2 === 1 ? xorNums2 : 0) ^ (nums2.length % 2 === 1 ? xorNums1 : 0);
+  return result;
 }

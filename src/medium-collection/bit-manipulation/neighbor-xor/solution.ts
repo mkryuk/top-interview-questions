a3fceeba401d89c3d72aa80092f55c19ab1d6234
Maxim Kryuk <mkryuk@gmail.com>
2025-01-17 13:08:44 +0200

    Bit Manipulation: Neighboring Bitwise XOR solution

--- src/medium-collection/bit-manipulation/neighbor-xor/solution.ts
+++ src/medium-collection/bit-manipulation/neighbor-xor/solution.ts
@@ -1,3 +1,9 @@
 export function doesValidArrayExist(derived: number[]): boolean {
-  return false;
+  let xorSum = 0;
+  // the xor-sum of the derived array should be 0
+  // since there is always a duplicate occurrence of each element.
+  for (const num of derived) {
+    xorSum ^= num;
+  }
+  return xorSum === 0 ? true : false;
 }

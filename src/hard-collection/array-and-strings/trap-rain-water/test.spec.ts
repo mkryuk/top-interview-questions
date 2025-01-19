fad3d4ae6d38599e96b1227245553c76b8a58685
Maxim Kryuk <mkryuk@gmail.com>
2025-01-19 21:56:09 +0200

    Array and Strings: Trapping Rain Water II init

---
+++ src/hard-collection/array-and-strings/trap-rain-water/test.spec.ts
@@ -0,0 +1,25 @@
+import { trapRainWater } from "./solution";
+
+describe("Trapping Rain Water II", () => {
+  it("trapRainWater should return 4 for heightMap = [[1,4,3,1,3,2],[3,2,1,3,2,4],[2,3,3,2,3,1]]", () => {
+    const heightMap = [
+      [1, 4, 3, 1, 3, 2],
+      [3, 2, 1, 3, 2, 4],
+      [2, 3, 3, 2, 3, 1],
+    ];
+    const result = trapRainWater(heightMap);
+    expect(result).toEqual(4);
+  });
+
+  it("trapRainWater should return 10 for heightMap = [[3,3,3,3,3],[3,2,2,2,3],[3,2,1,2,3],[3,2,2,2,3],[3,3,3,3,3]]", () => {
+    const heightMap = [
+      [3, 3, 3, 3, 3],
+      [3, 2, 2, 2, 3],
+      [3, 2, 1, 2, 3],
+      [3, 2, 2, 2, 3],
+      [3, 3, 3, 3, 3],
+    ];
+    const result = trapRainWater(heightMap);
+    expect(result).toEqual(10);
+  });
+});

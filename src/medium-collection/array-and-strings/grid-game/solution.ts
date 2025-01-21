2a0b52ecc9c38e4fb67177e732d5fdad5b24defb
Maxim Kryuk <mkryuk@gmail.com>
2025-01-21 19:11:34 +0200

    Array and Strings: Grid Game solution

--- src/medium-collection/array-and-strings/grid-game/solution.ts
+++ src/medium-collection/array-and-strings/grid-game/solution.ts
@@ -1,3 +1,25 @@
 export function gridGame(grid: number[][]): number {
-  return 0;
+  const m = 2;
+  const n = grid[0].length;
+  let topSum = 0;
+  let topPrefix = new Array(n).fill(0);
+  for (let i = 0; i < n; i++) {
+    topSum += grid[0][i];
+    topPrefix[i] = topSum;
+  }
+  let bottomSum = 0;
+  let bottomPrefix = new Array(n).fill(0);
+  for (let i = 0; i < n; i++) {
+    bottomSum += grid[1][i];
+    bottomPrefix[i] = bottomSum;
+  }
+
+  let result = Infinity;
+  for (let i = 0; i < n; i++) {
+    const topPoints = topSum - topPrefix[i];
+    const bottomPoints = i > 0 ? bottomPrefix[i - 1] : 0;
+    const secondRobotPoints = Math.max(topPoints, bottomPoints);
+    result = Math.min(result, secondRobotPoints);
+  }
+  return result;
 }

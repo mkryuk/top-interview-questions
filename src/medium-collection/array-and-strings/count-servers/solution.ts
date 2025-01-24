9a000d3dc288a36d9cacec49c0146d15dd39705f
Maxim Kryuk <mkryuk@gmail.com>
2025-01-24 14:25:57 +0200

    Array and Strings: Count Servers that Communicate solution

--- src/medium-collection/array-and-strings/count-servers/solution.ts
+++ src/medium-collection/array-and-strings/count-servers/solution.ts
@@ -1,3 +1,24 @@
 export function countServers(grid: number[][]): number {
-  return 0;
+  const m = grid.length;
+  const n = grid[0].length;
+  const rowCount = new Array(m).fill(0);
+  const colCount = new Array(n).fill(0);
+  for (let row = 0; row < m; row++) {
+    for (let col = 0; col < n; col++) {
+      if (grid[row][col] === 1) {
+        rowCount[row]++;
+        colCount[col]++;
+      }
+    }
+  }
+
+  let result = 0;
+  for (let row = 0; row < m; row++) {
+    for (let col = 0; col < n; col++) {
+      if (grid[row][col] === 1 && (rowCount[row] > 1 || colCount[col] > 1)) {
+        result++;
+      }
+    }
+  }
+  return result;
 }

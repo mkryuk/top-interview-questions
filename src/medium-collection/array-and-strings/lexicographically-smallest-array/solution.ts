7d47dd29eda4ebee009da08648fc32683e75b949
Maxim Kryuk <mkryuk@gmail.com>
2025-01-26 12:49:05 +0200

    Array and Strings: Make Lexicographically Smallest Array by Swapping Elements solution

--- src/medium-collection/array-and-strings/lexicographically-smallest-array/solution.ts
+++ src/medium-collection/array-and-strings/lexicographically-smallest-array/solution.ts
@@ -1,3 +1,30 @@
 export function lexicographicallySmallestArray(nums: number[], limit: number): number[] {
-  return [];
+  const indexed = nums.map((num, index) => ({ num, index }));
+  indexed.sort((a, b) => a.num - b.num);
+  const groups: { num: number; index: number }[][] = [];
+  let currentGroup: { num: number; index: number }[] = [indexed[0]];
+  for (let i = 1; i < indexed.length; i++) {
+    const previous = indexed[i - 1];
+    const current = indexed[i];
+    if (current.num - previous.num <= limit) {
+      currentGroup.push(current);
+    } else {
+      groups.push(currentGroup);
+      currentGroup = [current];
+    }
+  }
+
+  // push the last group
+  groups.push(currentGroup);
+
+  const result = new Array(nums.length);
+  for (const group of groups) {
+    group.sort((a, b) => a.index - b.index);
+    const sortedValues = group.map((item) => item.num).sort((a, b) => a - b);
+    for (let i = 0; i < group.length; i++) {
+      result[group[i].index] = sortedValues[i];
+    }
+  }
+
+  return result;
 }

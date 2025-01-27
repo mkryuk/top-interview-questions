8e1e5887bce71bcb1f03835697a1e1bef0731e06
Maxim Kryuk <mkryuk@gmail.com>
2025-01-28 00:38:10 +0200

    Trees and Graphs: Course Schedule IV solution

--- src/medium-collection/trees-and-graphs/course-schedule-IV/solution.ts
+++ src/medium-collection/trees-and-graphs/course-schedule-IV/solution.ts
@@ -1,3 +1,33 @@
 export function checkIfPrerequisite(numCourses: number, prerequisites: number[][], queries: number[][]): boolean[] {
-  return [];
+  const isReachable = new Map<number, Set<number>>();
+  const graph = new Map<number, number[]>();
+  for (const [from, to] of prerequisites) {
+    if (!graph.has(from)) {
+      graph.set(from, []);
+    }
+    graph.get(from)!.push(to);
+  }
+
+  for (let i = 0; i < numCourses; i++) {
+    if (!isReachable.has(i)) {
+      isReachable.set(i, new Set());
+    }
+    const visited = new Set<number>();
+    const prerequisites = [...(graph.get(i) ?? [])];
+    while (prerequisites.length > 0) {
+      const prerequisite = prerequisites.pop()!;
+      if (!visited.has(prerequisite)) {
+        visited.add(prerequisite);
+        isReachable.get(i)!.add(prerequisite);
+        prerequisites.push(...(graph.get(prerequisite) ?? []));
+      }
+    }
+  }
+
+  const result: boolean[] = [];
+  for (const [from, to] of queries) {
+    result.push(isReachable.get(from)?.has(to) ?? false);
+  }
+
+  return result;
 }

844df7ee04a01dd089ac07c25c99796bf260c56b
Maxim Kryuk <mkryuk@gmail.com>
2025-01-27 15:53:21 +0200

    Trees and Graphs: Course Schedule IV init

---
+++ src/medium-collection/trees-and-graphs/course-schedule-IV/solution.ts
@@ -0,0 +1,3 @@
+export function checkIfPrerequisite(numCourses: number, prerequisites: number[][], queries: number[][]): boolean[] {
+  return [];
+}
---
+++ src/medium-collection/trees-and-graphs/course-schedule-IV/test.spec.ts
@@ -0,0 +1,40 @@
+import { checkIfPrerequisite } from "./solution";
+
+describe("Course Schedule IV", () => {
+  it("checkIfPrerequisite should return [false,true] for numCourses = 2, prerequisites = [[1,0]], queries = [[0,1],[1,0]]", () => {
+    const numCourses = 2;
+    const prerequisites = [[1, 0]];
+    const queries = [
+      [0, 1],
+      [1, 0],
+    ];
+    const result = checkIfPrerequisite(numCourses, prerequisites, queries);
+    expect(result).toEqual([false, true]);
+  });
+
+  it("checkIfPrerequisite should return [false,false] for numCourses = 2, prerequisites = [], queries = [[1,0],[0,1]]", () => {
+    const numCourses = 2;
+    const prerequisites: number[][] = [];
+    const queries = [
+      [1, 0],
+      [0, 1],
+    ];
+    const result = checkIfPrerequisite(numCourses, prerequisites, queries);
+    expect(result).toEqual([false, false]);
+  });
+
+  it("checkIfPrerequisite should return [true,true] for numCourses = 3, prerequisites = [[1,2],[1,0],[2,0]], queries = [[1,0],[1,2]]", () => {
+    const numCourses = 3;
+    const prerequisites = [
+      [1, 2],
+      [1, 0],
+      [2, 0],
+    ];
+    const queries = [
+      [1, 0],
+      [1, 2],
+    ];
+    const result = checkIfPrerequisite(numCourses, prerequisites, queries);
+    expect(result).toEqual([true, true]);
+  });
+});

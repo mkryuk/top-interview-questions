a4e649f47122e6e84cb36d78db50a143967683f5
Maxim Kryuk <mkryuk@gmail.com>
2025-02-01 14:47:40 +0200

    Array and Strings: Special Array I solution

--- src/easy-collection/array/special-array-I/solution.ts
+++ src/easy-collection/array/special-array-I/solution.ts
@@ -1,3 +1,8 @@
 export function isArraySpecial(nums: number[]): boolean {
-  return false;
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] % 2 === nums[i + 1] % 2) {
+      return false;
+    }
+  }
+  return true;
 }

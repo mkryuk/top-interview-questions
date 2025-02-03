ac86c7689f579d5c639a367368ee47ab5aeae9c1
Maxim Kryuk <mkryuk@gmail.com>
2025-02-04 00:17:02 +0200

    Array and Strings: Longest Strictly Increasing or Strictly Decreasing Subarray solution

--- src/easy-collection/array/longest-monotonic-subarray/solution.ts
+++ src/easy-collection/array/longest-monotonic-subarray/solution.ts
@@ -1,3 +1,19 @@
 export function longestMonotonicSubarray(nums: number[]): number {
-  return 0;
+  let result = 1;
+  let increasingLength = 1;
+  let decreasingLength = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > nums[i - 1]) {
+      increasingLength++;
+      decreasingLength = 1;
+    } else if (nums[i] < nums[i - 1]) {
+      decreasingLength++;
+      increasingLength = 1;
+    } else {
+      increasingLength = 1;
+      decreasingLength = 1;
+    }
+    result = Math.max(result, decreasingLength, increasingLength);
+  }
+  return result;
 }

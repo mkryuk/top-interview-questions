bbf210d3550e1101bbafbff53041215efebaa26f
Maxim Kryuk <mkryuk@gmail.com>
2025-02-04 13:11:33 +0200

    Array and Strings: Maximum Ascending Subarray Sum solution

--- src/easy-collection/array/max-ascending-sum/solution.ts
+++ src/easy-collection/array/max-ascending-sum/solution.ts
@@ -1,3 +1,14 @@
 export function maxAscendingSum(nums: number[]): number {
-  return 0;
+  const n = nums.length;
+  let ascendingSum = nums[0];
+  let result = ascendingSum;
+  for (let i = 1; i < n; i++) {
+    if (nums[i] > nums[i - 1]) {
+      ascendingSum += nums[i];
+      result = Math.max(result, ascendingSum);
+    } else {
+      ascendingSum = nums[i];
+    }
+  }
+  return result;
 }

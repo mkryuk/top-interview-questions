f25ed55b8fb2c5d55d0f68394207d2edfbdb3e6a
Maxim Kryuk <mkryuk@gmail.com>
2025-02-05 15:34:05 +0200

    Array and Strings: Check if One String Swap Can Make Strings Equal solution

--- src/easy-collection/array/almost-equal/solution.ts
+++ src/easy-collection/array/almost-equal/solution.ts
@@ -1,3 +1,16 @@
 export function areAlmostEqual(s1: string, s2: string): boolean {
-  return false;
+  const n = s1.length;
+  const diffIndices: number[] = [];
+  for (let i = 0; i < n; i++) {
+    if (s1[i] !== s2[i]) {
+      diffIndices.push(i);
+    }
+  }
+
+  if (diffIndices.length > 2) {
+    return false;
+  }
+  const [firstIndex, secondIndex] = diffIndices;
+
+  return diffIndices.length === 0 || (s1[firstIndex] === s2[secondIndex] && s1[secondIndex] === s2[firstIndex]);
 }

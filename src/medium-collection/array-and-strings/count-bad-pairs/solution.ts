b093730b13e716e2c5231e96f96edd8761bbbeca
Maxim Kryuk <mkryuk@gmail.com>
2025-02-09 13:16:30 +0200

    Array and Strings: Count Number of Bad Pairs solution

--- src/medium-collection/array-and-strings/count-bad-pairs/solution.ts
+++ src/medium-collection/array-and-strings/count-bad-pairs/solution.ts
@@ -1,3 +1,17 @@
 export function countBadPairs(nums: number[]): number {
-  return 0;
+  const frequencyMap = new Map<number, number>();
+  const n = nums.length;
+  let goodPairs = 0;
+  for (let i = 0; i < n; i++) {
+    // j - i != nums[j] - nums[i] is the same as nums[i] - i != nums[j] - j.
+    const diff = nums[i] - i;
+    if (frequencyMap.has(diff)) {
+      goodPairs += frequencyMap.get(diff)!;
+      frequencyMap.set(diff, frequencyMap.get(diff)! + 1);
+    } else {
+      frequencyMap.set(diff, 1);
+    }
+  }
+  const totalPairs = (n * (n - 1)) / 2;
+  return totalPairs - goodPairs;
 }

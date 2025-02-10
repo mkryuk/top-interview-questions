6bce53a29c8288f17c4024dc508ad4d7e749c08e
Maxim Kryuk <mkryuk@gmail.com>
2025-02-10 12:39:07 +0200

    Array and Strings: Clear Digits solution

--- src/easy-collection/array/clear-digits/solution.ts
+++ src/easy-collection/array/clear-digits/solution.ts
@@ -1,3 +1,11 @@
 export function clearDigits(s: string): string {
-  return "";
+  const result: string[] = [];
+  for (const ch of s) {
+    if (ch >= "0" && ch <= "9") {
+      result.pop();
+    } else {
+      result.push(ch);
+    }
+  }
+  return result.join("");
 }

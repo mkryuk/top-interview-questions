5656375e79d723112728fb11e42df055f9fe8f0a
Maxim Kryuk <mkryuk@gmail.com>
2025-02-12 18:44:31 +0200

    Array and Strings: Remove All Occurrences of a Substring solution

--- src/medium-collection/array-and-strings/remove-occurrences/solution.ts
+++ src/medium-collection/array-and-strings/remove-occurrences/solution.ts
@@ -1,3 +1,24 @@
 export function removeOccurrences(s: string, part: string): string {
-  return "";
+  const partLength = part.length;
+  const stack: string[] = [];
+  for (let ch of s) {
+    stack.push(ch);
+    if (stack.length < partLength) {
+      continue;
+    }
+    let match = true;
+    const stackLength = stack.length;
+    for (let i = 0; i < partLength; i++) {
+      if (stack[stackLength - partLength + i] !== part[i]) {
+        match = false;
+        break;
+      }
+    }
+    if (match) {
+      for (let i = 0; i < partLength; i++) {
+        stack.pop();
+      }
+    }
+  }
+  return stack.join("");
 }

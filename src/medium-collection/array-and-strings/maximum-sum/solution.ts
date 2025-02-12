7a9f87d4d9396140c32d1e7de38a0b8096c2f169
Maxim Kryuk <mkryuk@gmail.com>
2025-02-12 19:22:35 +0200

    Array and Strings: Max Sum of a Pair With Equal Sum of Digits solution

--- src/medium-collection/array-and-strings/maximum-sum/solution.ts
+++ src/medium-collection/array-and-strings/maximum-sum/solution.ts
@@ -1,3 +1,29 @@
 export function maximumSum(nums: number[]): number {
-  return 0;
+  const map = new Map<number, [number, number]>();
+  let result = -1;
+  for (const num of nums) {
+    const digitSum = sumOfDigits(num);
+    if (!map.has(digitSum)) {
+      map.set(digitSum, [num, -1]);
+      continue;
+    }
+    const pair = map.get(digitSum)!;
+    if (num > pair[0]) {
+      pair[1] = pair[0];
+      pair[0] = num;
+    } else if (num > pair[1]) {
+      pair[1] = num;
+    }
+    result = Math.max(result, pair[0] + pair[1]);
+  }
+  return result;
+}
+
+function sumOfDigits(num: number): number {
+  let result = 0;
+  while (num > 0) {
+    result += num % 10;
+    num = Math.trunc(num / 10);
+  }
+  return result;
 }

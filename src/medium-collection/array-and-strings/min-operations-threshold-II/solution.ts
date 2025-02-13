0309b7552cf64d785951ba3833339af56f0597a9
Maxim Kryuk <mkryuk@gmail.com>
2025-02-14 00:02:03 +0200

    Array and Strings: Minimum Operations to Exceed Threshold Value II solution

--- src/medium-collection/array-and-strings/min-operations-threshold-II/solution.ts
+++ src/medium-collection/array-and-strings/min-operations-threshold-II/solution.ts
@@ -1,3 +1,17 @@
+import { Heap } from "../../../hard-collection/design/heap/heap";
+
 export function minOperations(nums: number[], k: number): number {
-  return 0;
+  let heap = new Heap<number>((a, b) => a - b);
+  for (const num of nums) {
+    heap.push(num);
+  }
+  let result = 0;
+  while (heap.size() >= 2 && heap.top()! < k) {
+    const first = heap.pop()!;
+    const second = heap.pop()!;
+    const num = first * 2 + second;
+    heap.push(num);
+    result++;
+  }
+  return result;
 }

5f0152810c81edb82152749aa445cdc74aa1f053
Maxim Kryuk <mkryuk@gmail.com>
2025-02-14 14:37:01 +0200

    Design: Product of the Last K Numbers solution

--- src/medium-collection/design/product-of-numbers/solution.ts
+++ src/medium-collection/design/product-of-numbers/solution.ts
@@ -1,9 +1,26 @@
 export class ProductOfNumbers {
-  constructor() {}
+  products: number[];
+  constructor() {
+    this.products = [1];
+  }
 
-  add(num: number): void {}
+  add(num: number): void {
+    if (num === 0) {
+      this.products = [1];
+      return;
+    }
+    const n = this.products.length;
+    const prevProduct = this.products[n - 1];
+    this.products.push(num * prevProduct);
+  }
 
   getProduct(k: number): number {
-    return 0;
+    if (this.products.length <= k) {
+      return 0;
+    }
+    const n = this.products.length;
+    const lastProduct = this.products[n - 1];
+    const kthProduct = this.products[n - k - 1];
+    return lastProduct / kthProduct;
   }
 }

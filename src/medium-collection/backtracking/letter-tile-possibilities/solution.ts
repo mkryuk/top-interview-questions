a71bd9b516f46b58fee60dfb786924cb7887bae1
Maxim Kryuk <mkryuk@gmail.com>
2025-02-18 00:22:54 +0200

    Backtracking: Letter Tile Possibilities solution

--- src/medium-collection/backtracking/letter-tile-possibilities/solution.ts
+++ src/medium-collection/backtracking/letter-tile-possibilities/solution.ts
@@ -1,3 +1,21 @@
 export function numTilePossibilities(tiles: string): number {
-  return 0;
+  const freq = new Array(26).fill(0);
+  for (const char of tiles) {
+    freq[char.charCodeAt(0) - 65]++;
+  }
+  const result = backtrack(freq);
+  return result;
+}
+
+function backtrack(freq: number[]): number {
+  let count = 0;
+  for (let i = 0; i < 26; i++) {
+    if (freq[i] > 0) {
+      count++;
+      freq[i]--;
+      count += backtrack(freq);
+      freq[i]++;
+    }
+  }
+  return count;
 }

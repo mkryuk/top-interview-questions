58f864d3f7687ccafecd89e97e746a0d61f4910a
Maxim Kryuk <mkryuk@gmail.com>
2025-02-18 23:42:23 +0200

    Backtracking: Construct Smallest Number From DI String solution

--- src/medium-collection/backtracking/smallest-number/solution.ts
+++ src/medium-collection/backtracking/smallest-number/solution.ts
@@ -1,3 +1,16 @@
 export function smallestNumber(pattern: string): string {
-  return "";
+  const n = pattern.length;
+  const result: string[] = [];
+  const stack: number[] = [];
+  for (let i = 0; i <= n; i++) {
+    stack.push(i + 1);
+    // if we're at the end or the current pattern character is 'I'
+    if (i === n || pattern[i] === "I") {
+      while (stack.length > 0) {
+        // pop from stack to form the correct order for 'D' sequences.
+        result.push(stack.pop()!.toString());
+      }
+    }
+  }
+  return result.join("");
 }

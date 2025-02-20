8deccd2ce84167452abb4cb61c6f1cb4156080ea
Maxim Kryuk <mkryuk@gmail.com>
2025-02-20 13:31:24 +0200

    Backtracking: The k-th Lexicographical String of All Happy Strings of Length n solution

--- src/medium-collection/backtracking/get-happy-string/solution.ts
+++ src/medium-collection/backtracking/get-happy-string/solution.ts
@@ -1,3 +1,18 @@
 export function getHappyString(n: number, k: number): string {
-  return "";
+  const strings: string[] = [];
+  backtrack("", n, strings);
+  return strings.length >= k ? strings[k - 1] : "";
+}
+
+function backtrack(current: string, n: number, strings: string[]) {
+  if (current.length === n) {
+    strings.push(current);
+    return;
+  }
+  const chars = ["a", "b", "c"];
+  for (const ch of chars) {
+    if (current[current.length - 1] !== ch) {
+      backtrack(current + ch, n, strings);
+    }
+  }
 }

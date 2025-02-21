22fde398f9c247c1dcdd0730d4c8821f6fb1664b
Maxim Kryuk <mkryuk@gmail.com>
2025-02-21 14:48:34 +0200

    Trees and Graphs: Find Elements in a Contaminated Binary Tree solution

--- src/medium-collection/trees-and-graphs/find-elements/solution.ts
+++ src/medium-collection/trees-and-graphs/find-elements/solution.ts
@@ -1,9 +1,27 @@
 import { TreeNode } from "../common";
 
 export class FindElements {
-  constructor(root: TreeNode | null) {}
+  treeValues: Set<number>;
+  constructor(root: TreeNode | null) {
+    this.treeValues = new Set();
+    this.recoverValues(root, 0);
+  }
+
+  private recoverValues(node: TreeNode | null, val: number) {
+    if (node === null) {
+      return;
+    }
+    node.val = val;
+    this.treeValues.add(val);
+    if (node.left !== null) {
+      this.recoverValues(node.left, val * 2 + 1);
+    }
+    if (node.right !== null) {
+      this.recoverValues(node.right, val * 2 + 2);
+    }
+  }
 
   find(target: number): boolean {
-    return false;
+    return this.treeValues.has(target);
   }
 }

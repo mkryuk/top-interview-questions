3e0861864a60b477b7035685a5bee5a16a2fe802
Maxim Kryuk <mkryuk@gmail.com>
2025-02-23 00:33:32 +0200

    Trees and Graphs: Recover a Tree From Preorder Traversal solution

--- src/hard-collection/trees-and-graphs/recover-from-preorder/solution.ts
+++ src/hard-collection/trees-and-graphs/recover-from-preorder/solution.ts
@@ -1,5 +1,38 @@
 import { TreeNode } from "../common";
 
 export function recoverFromPreorder(traversal: string): TreeNode | null {
-  return null;
+  let i = 0;
+  let stack: TreeNode[] = [];
+  while (i < traversal.length) {
+    let depth = 0;
+    // find the depth
+    while (i < traversal.length && traversal[i] === "-") {
+      depth++;
+      i++;
+    }
+
+    // get node value
+    let val = 0;
+    while (i < traversal.length && traversal[i] !== "-") {
+      val = val * 10 + parseInt(traversal[i]);
+      i++;
+    }
+    const node = new TreeNode(val);
+
+    // if stack length equal to depth it is the left child
+    if (depth === stack.length) {
+      if (stack.length > 0) {
+        stack[stack.length - 1].left = node;
+      }
+    } else {
+      // otherwise we go up and set the right child
+      while (stack.length > depth) {
+        stack.pop();
+      }
+      stack[stack.length - 1].right = node;
+    }
+
+    stack.push(node);
+  }
+  return stack[0];
 }
--- src/hard-collection/trees-and-graphs/recover-from-preorder/test.spec.ts
+++ src/hard-collection/trees-and-graphs/recover-from-preorder/test.spec.ts
@@ -13,13 +13,13 @@ describe("Recover a Tree From Preorder Traversal", () => {
     const traversal = "1-2--3---4-5--6---7";
     const root = recoverFromPreorder(traversal);
     const result = treeNodeToArray(root);
-    expect(result).toEqual([1, 2, 5, 3, null, 6, null, 4, null, 7]);
+    expect(result).toEqual([1, 2, 5, 3, null, 6, null, 4, null, 7, null]);
   });
 
   it('recoverFromPreorder should return [1,401,null,349,88,90] for traversal = "1-401--349---90--88"', () => {
     const traversal = "1-401--349---90--88";
     const root = recoverFromPreorder(traversal);
     const result = treeNodeToArray(root);
-    expect(result).toEqual([1, 401, null, 349, 88, 90]);
+    expect(result).toEqual([1, 401, null, 349, 88, 90, null]);
   });
 });

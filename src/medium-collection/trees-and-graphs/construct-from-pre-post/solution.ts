74898f060a34324928dd9a376f135fb319dbe5ee
Maxim Kryuk <mkryuk@gmail.com>
2025-02-23 21:22:25 +0200

    Trees and Graphs: Construct Binary Tree from Preorder and Postorder Traversal solution

--- src/medium-collection/trees-and-graphs/construct-from-pre-post/solution.ts
+++ src/medium-collection/trees-and-graphs/construct-from-pre-post/solution.ts
@@ -1,5 +1,17 @@
 import { TreeNode } from "../common";
 
 export function constructFromPrePost(preorder: number[], postorder: number[]): TreeNode | null {
-  return null;
+  if (preorder.length === 0) {
+    return null;
+  }
+  const root = new TreeNode(preorder[0]);
+  if (preorder.length === 1) {
+    return root;
+  }
+
+  const leftSubtreeRootVal = preorder[1];
+  const leftSubtreeSize = postorder.indexOf(leftSubtreeRootVal) + 1;
+  root.left = constructFromPrePost(preorder.slice(1, leftSubtreeSize + 1), postorder.slice(0, leftSubtreeSize));
+  root.right = constructFromPrePost(preorder.slice(leftSubtreeSize + 1), postorder.slice(leftSubtreeSize, -1));
+  return root;
 }

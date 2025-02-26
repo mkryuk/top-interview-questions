c7ac668b69b6929df61e4893465b7d6de12b0b40
Maxim Kryuk <mkryuk@gmail.com>
2025-02-26 14:16:16 +0200

    Array and Strings: Number of Sub-arrays With Odd Sum solution

--- src/medium-collection/array-and-strings/num-of-subarrays/solution.ts
+++ src/medium-collection/array-and-strings/num-of-subarrays/solution.ts
@@ -1,3 +1,20 @@
 export function numOfSubarrays(arr: number[]): number {
-  return 0;
+  let result = 0;
+  let oddCount = 0;
+  let evenCount = 1;
+  let currentParity = 0;
+  let mod = 1e9 + 7;
+  for (const num of arr) {
+    currentParity = (currentParity + num) % 2;
+    // if current prefix sum is odd, then every previous even prefix sum gives an odd subarray sum
+    // if the current prefix sum is even, then every previous odd prefix sum gives an odd subarray sum.
+    if (currentParity === 0) {
+      result = (result + oddCount) % mod;
+      evenCount++;
+    } else {
+      result = (result + evenCount) % mod;
+      oddCount++;
+    }
+  }
+  return result;
 }

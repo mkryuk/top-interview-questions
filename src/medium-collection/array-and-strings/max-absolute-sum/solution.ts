3aa7653937bdcf717c612c74d47ed5501375f1f2
Maxim Kryuk <mkryuk@gmail.com>
2025-02-27 12:41:22 +0200

    Array and Strings: Maximum Absolute Sum of Any Subarray solution

--- src/medium-collection/array-and-strings/max-absolute-sum/solution.ts
+++ src/medium-collection/array-and-strings/max-absolute-sum/solution.ts
@@ -1,3 +1,14 @@
 export function maxAbsoluteSum(nums: number[]): number {
-  return 0;
+  const n = nums.length;
+  let maxNum = 0;
+  let maxEndings = 0;
+  let minNum = 0;
+  let minEndings = 0;
+  for (let num of nums) {
+    maxEndings = Math.max(maxEndings + num, num);
+    maxNum = Math.max(maxNum, maxEndings);
+    minEndings = Math.min(minEndings + num, num);
+    minNum = Math.min(minNum, minEndings);
+  }
+  return Math.max(maxNum, Math.abs(minNum));
 }

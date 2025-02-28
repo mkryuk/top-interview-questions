24e498192f90beb3cb6453caddc2fa3548960261
Maxim Kryuk <mkryuk@gmail.com>
2025-02-28 19:21:38 +0200

    Dynamic Programming: Shortest Common Supersequence init

---
+++ src/hard-collection/dynamic-programming/shortest-common-supersequence/test.spec.ts
@@ -0,0 +1,15 @@
+describe("Shortest Common Supersequence", () => {
+  it('shortestCommonSupersequence should return "cabac" for str1 = "abac", str2 = "cab"', () => {
+    const str1 = "abac";
+    const str2 = "cab";
+    const result = shortestCommonSupersequence(str1, str2);
+    expect(result).toEqual("cabac");
+  });
+
+  it('shortestCommonSupersequence should return "aaaaaaaa" for str1 = "aaaaaaaa", str2 = "aaaaaaaa"', () => {
+    const str1 = "aaaaaaaa";
+    const str2 = "aaaaaaaa";
+    const result = shortestCommonSupersequence(str1, str2);
+    expect(result).toEqual("aaaaaaaa");
+  });
+});

8d191672dc9f77061a3c5e70e55361f646ac49ab
Maxim Kryuk <mkryuk@gmail.com>
2025-03-01 19:21:46 +0200

    Array and Strings: Apply Operations to an Array solution

--- src/easy-collection/array/apply-operations/solution.ts
+++ src/easy-collection/array/apply-operations/solution.ts
@@ -1,3 +1,17 @@
 export function applyOperations(nums: number[]): number[] {
-  return [];
+  const n = nums.length;
+  for (let i = 0; i < n - 1; i++) {
+    if (nums[i] === nums[i + 1]) {
+      nums[i] = nums[i] * 2;
+      nums[i + 1] = 0;
+    }
+  }
+  const result = new Array(n).fill(0);
+  let j = 0;
+  for (let i = 0; i < n; i++) {
+    if (nums[i] !== 0) {
+      result[j++] = nums[i];
+    }
+  }
+  return result;
 }

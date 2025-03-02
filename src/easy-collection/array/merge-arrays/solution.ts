465d9c5964ee89e2768468e1ec962f588c3ed0b1
Maxim Kryuk <mkryuk@gmail.com>
2025-03-02 23:50:29 +0200

    Array and Strings: Merge Two 2D Arrays by Summing Values solution

--- src/easy-collection/array/merge-arrays/solution.ts
+++ src/easy-collection/array/merge-arrays/solution.ts
@@ -1,3 +1,27 @@
 export function mergeArrays(nums1: number[][], nums2: number[][]): number[][] {
-  return [];
+  const result: number[][] = [];
+  let ptr1 = 0;
+  let ptr2 = 0;
+  while (ptr1 < nums1.length && ptr2 < nums2.length) {
+    if (nums1[ptr1][0] === nums2[ptr2][0]) {
+      result.push([nums1[ptr1][0], nums1[ptr1][1] + nums2[ptr2][1]]);
+      ptr1++;
+      ptr2++;
+    } else if (nums1[ptr1][0] < nums2[ptr2][0]) {
+      result.push(nums1[ptr1]);
+      ptr1++;
+    } else {
+      result.push(nums2[ptr2]);
+      ptr2++;
+    }
+  }
+  while (ptr1 < nums1.length) {
+    result.push(nums1[ptr1]);
+    ptr1++;
+  }
+  while (ptr2 < nums2.length) {
+    result.push(nums2[ptr2]);
+    ptr2++;
+  }
+  return result;
 }

b40a80ded46c3968440f406f03e6d44511c2bd74
Maxim Kryuk <mkryuk@gmail.com>
2025-03-03 20:00:55 +0200

    Array and Strings: Partition Array According to Given Pivot solution

--- src/medium-collection/array-and-strings/pivot-array/solution.ts
+++ src/medium-collection/array-and-strings/pivot-array/solution.ts
@@ -1,3 +1,17 @@
 export function pivotArray(nums: number[], pivot: number): number[] {
-  return [];
+  const n = nums.length;
+  const left = new Array();
+  const middle = new Array();
+  const right = new Array();
+  for (let i = 0; i < n; i++) {
+    if (nums[i] < pivot) {
+      left.push(nums[i]);
+    } else if (nums[i] === pivot) {
+      middle.push(nums[i]);
+    } else {
+      right.push(nums[i]);
+    }
+  }
+
+  return [...left, ...middle, ...right];
 }

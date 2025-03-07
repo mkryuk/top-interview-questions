6a0e9ac426f7f341d30fb0d6ba843b093d77ef93
Maxim Kryuk <mkryuk@gmail.com>
2025-03-07 16:10:57 +0200

    Math: Closest Prime Numbers in Range solution

--- src/medium-collection/math/closest-primes/solution.ts
+++ src/medium-collection/math/closest-primes/solution.ts
@@ -1,3 +1,33 @@
 export function closestPrimes(left: number, right: number): number[] {
-  return [];
+  const primeNumbers = getPrimeNumbers(left, right);
+  let result: [number, number] = [-1, -1];
+  let minDiff = Infinity;
+  for (let i = 0; i < primeNumbers.length - 1; i++) {
+    const leftPrime = primeNumbers[i];
+    const rightPrime = primeNumbers[i + 1];
+    if (rightPrime - leftPrime < minDiff) {
+      minDiff = rightPrime - leftPrime;
+      result = [leftPrime, rightPrime];
+    }
+  }
+  return result;
+}
+
+function getPrimeNumbers(left: number, right: number): number[] {
+  let primeNumbers = new Array(right + 1).fill(true);
+  primeNumbers[0] = primeNumbers[1] = false;
+  for (let i = 2; i <= right; i++) {
+    if (primeNumbers[i]) {
+      for (let j = i * i; j <= right; j += i) {
+        primeNumbers[j] = false;
+      }
+    }
+  }
+  let result: number[] = [];
+  for (let i = Math.max(2, left); i <= right; i++) {
+    if (primeNumbers[i]) {
+      result.push(i);
+    }
+  }
+  return result;
 }

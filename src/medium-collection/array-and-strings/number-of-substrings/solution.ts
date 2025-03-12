62db6f3865d99c26246f617bdf60ec502109dc9f
Maxim Kryuk <mkryuk@gmail.com>
2025-03-12 14:45:22 +0200

    Array and Strings: Number of Substrings Containing All Three Characters solution

--- src/medium-collection/array-and-strings/number-of-substrings/solution.ts
+++ src/medium-collection/array-and-strings/number-of-substrings/solution.ts
@@ -1,3 +1,16 @@
 export function numberOfSubstrings(s: string): number {
-  return 0;
+  const frequencyMap: Record<string, number> = { a: 0, b: 0, c: 0 };
+  let result = 0;
+  const n = s.length;
+  let left = 0;
+  for (let right = 0; right < n; right++) {
+    frequencyMap[s[right]]++;
+    while (frequencyMap["a"] > 0 && frequencyMap["b"] > 0 && frequencyMap["c"] > 0) {
+      result += n - right;
+      frequencyMap[s[left]]--;
+      left++;
+    }
+  }
+
+  return result;
 }

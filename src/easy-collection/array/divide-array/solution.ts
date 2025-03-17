a6475b1ec20d76f4e52ac878a58ad9fa9486085f
Maxim Kryuk <mkryuk@gmail.com>
2025-03-17 12:34:38 +0200

    Array and Strings: Divide Array Into Equal Pairs solution

--- src/easy-collection/array/divide-array/solution.ts
+++ src/easy-collection/array/divide-array/solution.ts
@@ -1,3 +1,13 @@
 export function divideArray(nums: number[]): boolean {
-  return false;
+  const max = Math.max(...nums);
+  const hasPair = new Array(max + 1).fill(true);
+  for (const num of nums) {
+    hasPair[num] = !hasPair[num];
+  }
+  for (let i = 0; i < max; i++) {
+    if (hasPair[i] === false) {
+      return false;
+    }
+  }
+  return true;
 }

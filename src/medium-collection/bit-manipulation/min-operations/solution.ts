95f83d9a5b54c296da558efcfb0a27b81937ffff
Maxim Kryuk <mkryuk@gmail.com>
2025-03-19 23:59:57 +0200

    Bit Manipulation: Minimum Operations to Make Binary Array Elements Equal to One I solution

--- src/medium-collection/bit-manipulation/min-operations/solution.ts
+++ src/medium-collection/bit-manipulation/min-operations/solution.ts
@@ -1,3 +1,18 @@
 export function minOperations(nums: number[]): number {
-  return 0;
+  let result = 0;
+  const n = nums.length;
+  for (let i = 0; i <= n - 3; i++) {
+    if (nums[i] === 0) {
+      for (let j = i; j < i + 3; j++) {
+        nums[j] ^= 1;
+      }
+      result++;
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    if (nums[i] === 0) {
+      return -1;
+    }
+  }
+  return result;
 }

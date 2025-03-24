981d85341fd851b05c9ce61722b0aa038c2d071a
Maxim Kryuk <mkryuk@gmail.com>
2025-03-24 23:52:50 +0200

    Array and Strings: Count Days Without Meetings solution

--- src/medium-collection/array-and-strings/count-days/solution.ts
+++ src/medium-collection/array-and-strings/count-days/solution.ts
@@ -1,3 +1,24 @@
 export function countDays(days: number, meetings: number[][]): number {
-  return 0;
+  const startEnd = new Map<number, number>();
+  for (const [start, end] of meetings) {
+    if (!startEnd.has(start)) {
+      startEnd.set(start, 0);
+    }
+    if (!startEnd.has(end + 1)) {
+      startEnd.set(end + 1, 0);
+    }
+    startEnd.set(start, startEnd.get(start)! + 1);
+    startEnd.set(end + 1, startEnd.get(end + 1)! - 1);
+  }
+  let currentMeeting = 0;
+  let result = 0;
+  for (let i = 1; i <= days; i++) {
+    if (startEnd.has(i)) {
+      currentMeeting += startEnd.get(i)!;
+    }
+    if (currentMeeting === 0) {
+      result++;
+    }
+  }
+  return result;
 }

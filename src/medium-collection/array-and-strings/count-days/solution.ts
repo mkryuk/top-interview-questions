8e869bd55d2e04ee02b7c355be2d2e16e4704c35
Maxim Kryuk <mkryuk@gmail.com>
2025-03-24 15:47:21 +0200

    Array and Strings: Count Days Without Meetings init

---
+++ src/medium-collection/array-and-strings/count-days/solution.ts
@@ -0,0 +1,3 @@
+export function countDays(days: number, meetings: number[][]): number {
+  return 0;
+}
---
+++ src/medium-collection/array-and-strings/count-days/test.spec.ts
@@ -0,0 +1,31 @@
+import { countDays } from "./solution";
+
+describe("Count Days Without Meetings", () => {
+  it("countDays should return 2 for days = 10, meetings = [[5,7],[1,3],[9,10]]", () => {
+    const days = 10;
+    const meetings = [
+      [5, 7],
+      [1, 3],
+      [9, 10],
+    ];
+    const result = countDays(days, meetings);
+    expect(result).toEqual(2);
+  });
+
+  it("countDays should return 2 for days = 5, meetings = [[2,4],[1,3]]", () => {
+    const days = 5;
+    const meetings = [
+      [2, 4],
+      [1, 3],
+    ];
+    const result = countDays(days, meetings);
+    expect(result).toEqual(1);
+  });
+
+  it("countDays should return 0 for days = 6, meetings = [[1,6]]", () => {
+    const days = 6;
+    const meetings = [[1, 6]];
+    const result = countDays(days, meetings);
+    expect(result).toEqual(0);
+  });
+});

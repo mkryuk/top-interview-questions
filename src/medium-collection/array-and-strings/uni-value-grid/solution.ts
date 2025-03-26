73daa0a71898e37340259df1b738e3eedcdb74ad
Maxim Kryuk <mkryuk@gmail.com>
2025-03-26 23:33:40 +0200

    Array and Strings: Minimum Operations to Make a Uni-Value Grid solution

--- src/medium-collection/array-and-strings/uni-value-grid/solution.ts
+++ src/medium-collection/array-and-strings/uni-value-grid/solution.ts
@@ -1,3 +1,25 @@
 export function minOperations(grid: number[][], x: number): number {
-  return 0;
+  const nums = new Array();
+  let remainder = grid[0][0] % x;
+  // flatten array
+  // check if it is possible to make the grid uni-value.
+  // all numbers must have the same remainder modulo x.
+  for (const row of grid) {
+    for (const num of row) {
+      if (num % x !== remainder) {
+        return -1;
+      }
+      nums.push(num);
+    }
+  }
+
+  nums.sort((a, b) => a - b);
+  // median is the target to minimize the total number of operations.
+  const mid = Math.floor(nums.length / 2);
+  let result = 0;
+  for (let i = 0; i < nums.length; i++) {
+    result += Math.abs(nums[i] - nums[mid]) / x;
+  }
+
+  return result;
 }

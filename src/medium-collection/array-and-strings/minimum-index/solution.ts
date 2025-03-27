a6c11c40cebed14a9fb4d89453e4d65e10fc2149
Maxim Kryuk <mkryuk@gmail.com>
2025-03-27 19:07:58 +0200

    Array and Strings: Minimum Index of a Valid Split init

---
+++ src/medium-collection/array-and-strings/minimum-index/solution.ts
@@ -0,0 +1,3 @@
+export function minimumIndex(nums: number[]): number {
+  return 0;
+}
---
+++ src/medium-collection/array-and-strings/minimum-index/test.spec.ts
@@ -0,0 +1,21 @@
+import { minimumIndex } from "./solution";
+
+describe("Minimum Index of a Valid Split", () => {
+  it("minimumIndex should return 2 for nums = [1,2,2,2]", () => {
+    const nums = [1, 2, 2, 2];
+    const result = minimumIndex(nums);
+    expect(result).toEqual(2);
+  });
+
+  it("minimumIndex should return 4 for nums = [2,1,3,1,1,1,7,1,2,1]", () => {
+    const nums = [2, 1, 3, 1, 1, 1, 7, 1, 2, 1];
+    const result = minimumIndex(nums);
+    expect(result).toEqual(4);
+  });
+
+  it("minimumIndex should return -1 for nums = [3,3,3,3,7,2,2]", () => {
+    const nums = [3, 3, 3, 3, 7, 2, 2];
+    const result = minimumIndex(nums);
+    expect(result).toEqual(-1);
+  });
+});

9d73b4c881a966113d48872cd6c73534bbc431e9
Maxim Kryuk <mkryuk@gmail.com>
2025-03-30 20:54:51 +0300

    Array and Strings: Partition Labels solution

--- src/hard-collection/array-and-strings/partition-labels/solution.ts
+++ src/hard-collection/array-and-strings/partition-labels/solution.ts
@@ -1,3 +1,17 @@
 export function partitionLabels(s: string): number[] {
-  return [];
+  const charLastIndex = new Map<string, number>();
+  for (let i = 0; i < s.length; i++) {
+    charLastIndex.set(s[i], i);
+  }
+  let result: number[] = [];
+  for (let i = 0; i < s.length; ) {
+    let lastIndex = charLastIndex.get(s[i])!;
+    for (let j = i + 1; j < lastIndex; j++) {
+      lastIndex = Math.max(lastIndex, charLastIndex.get(s[j])!);
+    }
+    result.push(lastIndex - i + 1);
+    i = lastIndex + 1;
+  }
+
+  return result;
 }

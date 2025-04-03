61c90faa1388e080d483bb38d62738a09fb0338d
Maxim Kryuk <mkryuk@gmail.com>
2025-04-03 14:18:26 +0300

    Array and Strings: Maximum Value of an Ordered Triplet II solution

--- src/medium-collection/array-and-strings/maximum-triplet-value/solution.ts
+++ src/medium-collection/array-and-strings/maximum-triplet-value/solution.ts
@@ -1,3 +1,12 @@
 export function maximumTripletValue(nums: number[]): number {
-  return 0;
+  const n = nums.length;
+  let result = 0;
+  let iMax = 0;
+  let diffMax = 0;
+  for (let i = 0; i < n; i++) {
+    result = Math.max(result, diffMax * nums[i]);
+    iMax = Math.max(iMax, nums[i]);
+    diffMax = Math.max(diffMax, iMax - nums[i]);
+  }
+  return result;
 }

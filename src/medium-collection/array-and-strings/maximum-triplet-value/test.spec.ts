da2e2f6050cb886d62ee5d9cc0ba05311006eec0
Maxim Kryuk <mkryuk@gmail.com>
2025-04-03 14:10:50 +0300

    Array and Strings: Maximum Value of an Ordered Triplet II init

---
+++ src/medium-collection/array-and-strings/maximum-triplet-value/test.spec.ts
@@ -0,0 +1,21 @@
+import { maximumTripletValue } from "./solution";
+
+describe("Maximum Value of an Ordered Triplet II", () => {
+  it("maximumTripletValue should return 77 for nums = [12,6,1,2,7]", () => {
+    const nums = [12, 6, 1, 2, 7];
+    const result = maximumTripletValue(nums);
+    expect(result).toEqual(77);
+  });
+
+  it("maximumTripletValue should return 133 for nums = [1,10,3,4,19]", () => {
+    const nums = [1, 10, 3, 4, 19];
+    const result = maximumTripletValue(nums);
+    expect(result).toEqual(133);
+  });
+
+  it("maximumTripletValue should return 0 for nums = [1,2,3]", () => {
+    const nums = [1, 2, 3];
+    const result = maximumTripletValue(nums);
+    expect(result).toEqual(0);
+  });
+});

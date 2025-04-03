7b600490fc649763964d3806afadf68d5172f831
Maxim Kryuk <mkryuk@gmail.com>
2025-04-03 13:42:37 +0300

    Array and Strings: Maximum Value of an Ordered Triplet I solution

--- src/easy-collection/array/maximum-triplet-value/solution.ts
+++ src/easy-collection/array/maximum-triplet-value/solution.ts
@@ -1,3 +1,12 @@
 export function maximumTripletValue(nums: number[]): number {
-  return 0;
+  let result = 0;
+  const n = nums.length;
+  for (let k = 2; k < n; k++) {
+    for (let j = 1; j < k; j++) {
+      for (let i = 0; i < j; i++) {
+        result = Math.max(result, (nums[i] - nums[j]) * nums[k]);
+      }
+    }
+  }
+  return result;
 }

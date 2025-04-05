c4f2b92885005ef4e66ed9a1e398b2268b1b541e
Maxim Kryuk <mkryuk@gmail.com>
2025-04-05 20:32:17 +0300

    Trees and Graphs: Lowest Common Ancestor of Deepest Leaves solution

--- src/medium-collection/trees-and-graphs/lca-deepest-leaves/solution.ts
+++ src/medium-collection/trees-and-graphs/lca-deepest-leaves/solution.ts
@@ -1,5 +1,19 @@
 import { TreeNode } from "../common";
 
 export function lcaDeepestLeaves(root: TreeNode | null): TreeNode | null {
-  return root;
+  return dfs(root)[1];
+}
+
+function dfs(node: TreeNode | null): [number, TreeNode | null] {
+  if (node === null) {
+    return [0, null];
+  }
+  const [leftDepth, leftLca] = dfs(node.left);
+  const [rightDepth, rightLca] = dfs(node.right);
+  if (leftDepth === rightDepth) {
+    return [leftDepth + 1, node];
+  } else if (leftDepth > rightDepth) {
+    return [leftDepth + 1, leftLca];
+  }
+  return [rightDepth + 1, rightLca];
 }

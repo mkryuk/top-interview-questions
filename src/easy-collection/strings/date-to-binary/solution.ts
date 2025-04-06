b3928a1cfadac0519620d537274aa6fcd34656fe
Maxim Kryuk <mkryuk@gmail.com>
2025-04-06 19:26:18 +0300

    Array and Strings: Convert Date to Binary solution

--- src/easy-collection/strings/date-to-binary/solution.ts
+++ src/easy-collection/strings/date-to-binary/solution.ts
@@ -1,3 +1,19 @@
 export function convertDateToBinary(date: string): string {
-  return "";
+  const [yearStr, monthStr, dayStr] = date.split("-");
+  const year = parseInt(yearStr);
+  const month = parseInt(monthStr);
+  const day = parseInt(dayStr);
+  return `${toBinaryString(year)}-${toBinaryString(month)}-${toBinaryString(day)}`;
+}
+
+function toBinaryString(num: number): string {
+  if (num === 0) {
+    return "0";
+  }
+  let result = "";
+  while (num > 0) {
+    result = (num % 2) + result;
+    num = Math.floor(num / 2);
+  }
+  return result;
 }

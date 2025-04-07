005b2dc90b5100cdd9b2d0c2ff9fb5de9f796c19
Maxim Kryuk <mkryuk@gmail.com>
2025-04-07 15:38:00 +0300

    Dynamic Programming: Partition Equal Subset Sum init

---
+++ src/medium-collection/dynamic-programming/can-partition/test.spec.ts
@@ -0,0 +1,15 @@
+import { canPartition } from "./solution";
+
+describe("Partition Equal Subset Sum", () => {
+  it("canPartition should return true for nums = [1,5,11,5]", () => {
+    const nums = [1, 5, 11, 5];
+    const result = canPartition(nums);
+    expect(result).toBeTrue();
+  });
+
+  it("canPartition should return false for nums = [1,2,3,5]", () => {
+    const nums = [1, 2, 3, 5];
+    const result = canPartition(nums);
+    expect(result).toBeFalse();
+  });
+});

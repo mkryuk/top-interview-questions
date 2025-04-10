bc06e1a8f898a17571f987f6e82d5f1940e89f79
Maxim Kryuk <mkryuk@gmail.com>
2025-04-10 21:38:12 +0300

    Array and Strings: Minimum Operations to Make Array Values Equal to K solution

--- src/easy-collection/array/min-operations/solution.ts
+++ src/easy-collection/array/min-operations/solution.ts
@@ -1,3 +1,14 @@
 export function minOperations(nums: number[], k: number): number {
-  return 0;
+  let set = new Set<number>();
+  let result = 0;
+  for (const num of nums) {
+    if (num < k) {
+      return -1;
+    }
+    if (!set.has(num) && num > k) {
+      result++;
+      set.add(num);
+    }
+  }
+  return result;
 }

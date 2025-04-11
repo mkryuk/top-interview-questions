90421454fa2df31dc4eb3478e93c4ba2fb393889
Maxim Kryuk <mkryuk@gmail.com>
2025-04-11 14:33:40 +0300

    Math: Count Symmetric Integers solution

--- src/easy-collection/math/count-symmetric-integers/solution.ts
+++ src/easy-collection/math/count-symmetric-integers/solution.ts
@@ -1,3 +1,26 @@
 export function countSymmetricIntegers(low: number, high: number): number {
-  return 0;
+  let result = 0;
+  for (let i = low; i <= high; i++) {
+    const strNum = i.toString();
+    if (strNum.length % 2 !== 0) {
+      continue;
+    }
+    const n = strNum.length / 2;
+    let left = strNum.slice(0, n);
+    let right = strNum.slice(n);
+    if (sumEqual(left, right)) {
+      result++;
+    }
+  }
+  return result;
+}
+
+function sumEqual(left: string, right: string): boolean {
+  let leftSum = 0;
+  let rightSum = 0;
+  for (let i = 0; i < left.length; i++) {
+    leftSum += parseInt(left[i]);
+    rightSum += parseInt(right[i]);
+  }
+  return leftSum === rightSum;
 }

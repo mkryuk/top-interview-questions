56d62b9a7b407c779c09c93cd5899b216f3455e3
Maxim Kryuk <mkryuk@gmail.com>
2025-04-15 00:13:18 +0300

    Array and Strings: Count Good Triplets solution

--- src/easy-collection/array/count-good-triplets/solution.ts
+++ src/easy-collection/array/count-good-triplets/solution.ts
@@ -1,3 +1,16 @@
 export function countGoodTriplets(arr: number[], a: number, b: number, c: number): number {
-  return 0;
+  let result = 0;
+  for (let k = 2; k < arr.length; k++) {
+    for (let j = 0; j < k; j++) {
+      if (Math.abs(arr[j] - arr[k]) > b) {
+        continue;
+      }
+      for (let i = 0; i < j; i++) {
+        if (Math.abs(arr[i] - arr[j]) <= a && Math.abs(arr[i] - arr[k]) <= c) {
+          result++;
+        }
+      }
+    }
+  }
+  return result;
 }

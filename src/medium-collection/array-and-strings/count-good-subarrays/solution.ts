3273523460efeab249682b8880c631a08951655d
Maxim Kryuk <mkryuk@gmail.com>
2025-04-17 12:30:58 +0300

    Array and Strings: Count the Number of Good Subarrays solution

--- src/medium-collection/array-and-strings/count-good-subarrays/solution.ts
+++ src/medium-collection/array-and-strings/count-good-subarrays/solution.ts
@@ -1,3 +1,25 @@
 export function countGood(nums: number[], k: number): number {
-  return 0;
+  const n = nums.length;
+  // special case: if k is 0, all subarrays are valid.
+  if (k === 0) {
+    return (n * (n + 1)) / 2;
+  }
+  let left = 0;
+  let result = 0;
+  let pairs = 0;
+  const freq = new Map<number, number>();
+  for (let right = 0; right < n; right++) {
+    let count = freq.get(nums[right]) ?? 0;
+    freq.set(nums[right], count + 1);
+    pairs += count;
+    while (pairs >= k && left <= right) {
+      result += n - right;
+      const leftCount = freq.get(nums[left]) ?? 0;
+      pairs -= leftCount - 1;
+      freq.set(nums[left], leftCount - 1);
+      left++;
+    }
+  }
+
+  return result;
 }

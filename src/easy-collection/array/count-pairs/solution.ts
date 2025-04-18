fd5ab67293f18fec91a57b364635944d35742245
Maxim Kryuk <mkryuk@gmail.com>
2025-04-18 14:27:48 +0300

    Array and Strings: 2176. Count Equal and Divisible Pairs in an Array solution

--- src/easy-collection/array/count-pairs/solution.ts
+++ src/easy-collection/array/count-pairs/solution.ts
@@ -1,3 +1,12 @@
 export function countPairs(nums: number[], k: number): number {
-  return 0;
+  const n = nums.length;
+  let result = 0;
+  for (let j = 1; j < n; j++) {
+    for (let i = 0; i < j; i++) {
+      if (nums[i] === nums[j] && (i * j) % k === 0) {
+        result++;
+      }
+    }
+  }
+  return result;
 }

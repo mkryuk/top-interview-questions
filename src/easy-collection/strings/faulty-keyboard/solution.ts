e25df1785341463be9c2ec78e41565ffc8a0db75
Maxim Kryuk <mkryuk@gmail.com>
2025-04-20 00:13:09 +0300

    Array and Strings: Faulty Keyboard solution

--- src/easy-collection/strings/faulty-keyboard/solution.ts
+++ src/easy-collection/strings/faulty-keyboard/solution.ts
@@ -1,3 +1,28 @@
 export function finalString(s: string): string {
-  return "";
+  const n = s.length;
+  let iCount = 0;
+  for (let i = 0; i < n; i++) {
+    if (s[i] === "i") {
+      iCount++;
+    }
+  }
+  let result: string[] = new Array(n - iCount);
+  let start = 0;
+  let end = result.length - 1;
+  let reversed = false;
+  for (let i = n - 1; i >= 0; i--) {
+    if (s[i] === "i") {
+      reversed = !reversed;
+      continue;
+    }
+    if (!reversed) {
+      result[end] = s[i];
+      end--;
+    } else {
+      result[start] = s[i];
+      start++;
+    }
+  }
+
+  return result.join("");
 }

02b041d4355d97bd6e111588cca0b1e50eeef435
Maxim Kryuk <mkryuk@gmail.com>
2025-04-26 12:42:39 +0300

    Array and Strings: 2845. Count of Interesting Subarrays solution

--- src/medium-collection/array-and-strings/count-interesting-subarrays/solution.ts
+++ src/medium-collection/array-and-strings/count-interesting-subarrays/solution.ts
@@ -1,3 +1,16 @@
 export function countInterestingSubarrays(nums: number[], modulo: number, k: number): number {
-  return 0;
+  let prefixSum = 0;
+  let result = 0;
+  const freqMap = new Map<number, number>();
+  freqMap.set(0, 1);
+  for (const num of nums) {
+    if (num % modulo === k) {
+      prefixSum++;
+    }
+    const current = prefixSum % modulo;
+    const need = (current - k + modulo) % modulo;
+    result += freqMap.get(need) ?? 0;
+    freqMap.set(current, (freqMap.get(current) ?? 0) + 1);
+  }
+  return result;
 }

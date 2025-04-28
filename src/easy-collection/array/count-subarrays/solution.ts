925a9f88f54eb1e003cf5d4e076890f7943fb7c9
Maxim Kryuk <mkryuk@gmail.com>
2025-04-28 15:01:12 +0300

    Array and Strings: 3392. Count Subarrays of Length Three With a Condition solution

--- src/easy-collection/array/count-subarrays/solution.ts
+++ src/easy-collection/array/count-subarrays/solution.ts
@@ -1,3 +1,12 @@
 export function countSubarrays(nums: number[]): number {
-  return 0;
+  let result = 0;
+  for (let i = 0; i < nums.length - 2; i++) {
+    const first = nums[i];
+    const second = nums[i + 1];
+    const third = nums[i + 2];
+    if (first + third === second / 2) {
+      result++;
+    }
+  }
+  return result;
 }

4315bda9324fdf92cef6ed6bfabf3beffc6ec2f1
Maxim Kryuk <mkryuk@gmail.com>
2025-04-30 14:48:01 +0300

    Array and Strings: 1295. Find Numbers with Even Number of Digits init

---
+++ src/easy-collection/array/find-numbers/test.spec.ts
@@ -0,0 +1,15 @@
+import { findNumbers } from "./solution";
+
+describe("Find Numbers with Even Number of Digits", () => {
+  it("findNumbers should return 2 for nums = [12,345,2,6,7896]", () => {
+    const nums = [12, 345, 2, 6, 7896];
+    const result = findNumbers(nums);
+    expect(result).toEqual(2);
+  });
+
+  it("findNumbers should return 1 for nums = [555,901,482,1771]", () => {
+    const nums = [555, 901, 482, 1771];
+    const result = findNumbers(nums);
+    expect(result).toEqual(1);
+  });
+});

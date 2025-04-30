814b385aa7f4d8ba196438e95b815f3743678284
Maxim Kryuk <mkryuk@gmail.com>
2025-04-30 15:03:16 +0300

    Array and Strings: 1295. Find Numbers with Even Number of Digits solution

--- src/easy-collection/array/find-numbers/solution.ts
+++ src/easy-collection/array/find-numbers/solution.ts
@@ -1,3 +1,18 @@
 export function findNumbers(nums: number[]): number {
-  return 0;
+  let result = 0;
+  for (const num of nums) {
+    if (countDigits(num) % 2 === 0) {
+      result++;
+    }
+  }
+  return result;
+}
+
+function countDigits(num: number): number {
+  let result = 1;
+  while (num >= 10) {
+    result++;
+    num = Math.trunc(num / 10);
+  }
+  return result;
 }

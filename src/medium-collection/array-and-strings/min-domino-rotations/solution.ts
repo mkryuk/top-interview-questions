0c2e05254a717dd6679371a54d8197de5781f04d
Maxim Kryuk <mkryuk@gmail.com>
2025-05-04 15:46:34 +0300

    Array and Strings: 1007. Minimum Domino Rotations For Equal Row solution

--- src/medium-collection/array-and-strings/min-domino-rotations/solution.ts
+++ src/medium-collection/array-and-strings/min-domino-rotations/solution.ts
@@ -1,3 +1,26 @@
 export function minDominoRotations(tops: number[], bottoms: number[]): number {
-  return 0;
+  const result = getRotations(tops, bottoms, tops[0]);
+  if (result !== -1 || tops[0] === bottoms[0]) {
+    return result;
+  } else {
+    return getRotations(tops, bottoms, bottoms[0]);
+  }
+}
+
+function getRotations(tops: number[], bottoms: number[], num: number): number {
+  let topRotations = 0;
+  let bottomRotations = 0;
+  for (let i = 0; i < tops.length; i++) {
+    // if we can't make row i have num
+    if (num !== tops[i] && num !== bottoms[i]) {
+      return -1;
+    }
+    if (tops[i] !== num) {
+      topRotations++;
+    }
+    if (bottoms[i] !== num) {
+      bottomRotations++;
+    }
+  }
+  return Math.min(topRotations, bottomRotations);
 }
--- src/medium-collection/array-and-strings/min-domino-rotations/test.spec.ts
+++ src/medium-collection/array-and-strings/min-domino-rotations/test.spec.ts
@@ -14,4 +14,25 @@ describe("Minimum Domino Rotations For Equal Row", () => {
     const result = minDominoRotations(tops, bottoms);
     expect(result).toEqual(-1);
   });
+
+  it("minDominoRotations should return 1 for tops = [1,2,1,1,1,2,2,2], bottoms = [2,1,2,2,2,2,2,2]", () => {
+    const tops = [1, 2, 1, 1, 1, 2, 2, 2];
+    const bottoms = [2, 1, 2, 2, 2, 2, 2, 2];
+    const result = minDominoRotations(tops, bottoms);
+    expect(result).toEqual(1);
+  });
+
+  it("minDominoRotations should return 1 for tops = [2,1,2,2,2,2,2,2], bottoms = [1,2,1,1,1,2,2,2]", () => {
+    const tops = [2, 1, 2, 2, 2, 2, 2, 2];
+    const bottoms = [1, 2, 1, 1, 1, 2, 2, 2];
+    const result = minDominoRotations(tops, bottoms);
+    expect(result).toEqual(1);
+  });
+
+  it("minDominoRotations should return 1 for tops = [1,4,1,6,6,1,6,2], bottoms = [2,2,5,3,1,5,6,6]", () => {
+    const tops = [1, 4, 1, 6, 6, 1, 6, 2];
+    const bottoms = [2, 2, 5, 3, 1, 5, 6, 6];
+    const result = minDominoRotations(tops, bottoms);
+    expect(result).toEqual(-1);
+  });
 });

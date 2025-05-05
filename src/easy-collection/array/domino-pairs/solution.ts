933a628aab4ed56aa77ce09710e6cdc558038f39
Maxim Kryuk <mkryuk@gmail.com>
2025-05-05 15:58:10 +0300

    Array and Strings: 1128. Number of Equivalent Domino Pairs solution

--- src/easy-collection/array/domino-pairs/solution.ts
+++ src/easy-collection/array/domino-pairs/solution.ts
@@ -1,3 +1,11 @@
 export function numEquivDominoPairs(dominoes: number[][]): number {
-  return 0;
+  let dominoesMap = new Map<string, number>();
+  let result = 0;
+  for (const [a, b] of dominoes) {
+    const domino = a < b ? `${a}:${b}` : `${b}:${a}`;
+    const count = dominoesMap.get(domino) ?? 0;
+    result += count;
+    dominoesMap.set(domino, count + 1);
+  }
+  return result;
 }

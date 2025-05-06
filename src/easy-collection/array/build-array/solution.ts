2fd71000b8684c180a9effa28c49a7ee536f74e9
Maxim Kryuk <mkryuk@gmail.com>
2025-05-06 13:52:54 +0300

    Array and Strings: 1920. Build Array from Permutation solution

--- src/easy-collection/array/build-array/solution.ts
+++ src/easy-collection/array/build-array/solution.ts
@@ -1,3 +1,9 @@
 export function buildArray(nums: number[]): number[] {
-  return [];
+  const n = nums.length;
+  const result = new Array(n);
+  for (let i = 0; i < n; i++) {
+    const idx = nums[i];
+    result[i] = nums[idx];
+  }
+  return result;
 }

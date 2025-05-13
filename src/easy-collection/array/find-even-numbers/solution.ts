eb1bf59bc9afb73ff6e09326f57004dc08b47036
Maxim Kryuk <mkryuk@gmail.com>
2025-05-13 11:24:52 +0300

    Array and Strings: 2094. Finding 3-Digit Even Numbers solution

--- src/easy-collection/array/find-even-numbers/solution.ts
+++ src/easy-collection/array/find-even-numbers/solution.ts
@@ -1,3 +1,25 @@
 export function findEvenNumbers(digits: number[]): number[] {
-  return [];
+  const n = digits.length;
+  const numSet: Set<number> = new Set();
+  for (let i = 0; i < n; i++) {
+    if (digits[i] === 0) {
+      continue;
+    }
+    for (let j = 0; j < n; j++) {
+      if (j === i) {
+        continue;
+      }
+      for (let k = 0; k < n; k++) {
+        if (k === i || k === j) {
+          continue;
+        }
+        const num = digits[i] * 100 + digits[j] * 10 + digits[k];
+        if (num % 2 === 0) {
+          numSet.add(num);
+        }
+      }
+    }
+  }
+  const result = Array.from(numSet).sort((a, b) => a - b);
+  return result;
 }

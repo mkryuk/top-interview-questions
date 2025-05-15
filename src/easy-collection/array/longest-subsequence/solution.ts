40bea3b09304dc322e8bead14e4d7e8d020559f0
Maxim Kryuk <mkryuk@gmail.com>
2025-05-15 15:30:36 +0300

    Array and Strings: Longest Unequal Adjacent Groups Subsequence I solution

--- src/easy-collection/array/longest-subsequence/solution.ts
+++ src/easy-collection/array/longest-subsequence/solution.ts
@@ -1,3 +1,13 @@
 export function getLongestSubsequence(words: string[], groups: number[]): string[] {
-  return [];
+  const n = words.length;
+  const result: string[] = [];
+  result.push(words[0]);
+  let currentGroup = groups[0];
+  for (let i = 1; i < n; i++) {
+    if (groups[i] !== currentGroup) {
+      result.push(words[i]);
+      currentGroup = groups[i];
+    }
+  }
+  return result;
 }

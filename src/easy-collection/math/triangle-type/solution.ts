8009e363b0b7df47f1a535f488e55645671f20d3
Maxim Kryuk <mkryuk@gmail.com>
2025-05-19 13:57:36 +0300

    Math: 3024. Type of Triangle solution

--- src/easy-collection/math/triangle-type/solution.ts
+++ src/easy-collection/math/triangle-type/solution.ts
@@ -1,3 +1,10 @@
 export function triangleType(nums: number[]): string {
-  return "";
+  if (nums[0] >= nums[1] + nums[2] || nums[1] >= nums[0] + nums[2] || nums[2] >= nums[0] + nums[1]) {
+    return "none";
+  } else if (nums[0] === nums[1] && nums[1] === nums[2]) {
+    return "equilateral";
+  } else if (nums[0] === nums[1] || nums[1] === nums[2] || nums[2] === nums[0]) {
+    return "isosceles";
+  }
+  return "scalene";
 }

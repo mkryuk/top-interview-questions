0ae9ae105aad2fc34ca61b4380569c54cb43e996
Maxim Kryuk <mkryuk@gmail.com>
2025-05-21 15:35:00 +0300

    Array and Strings: 3355. Zero Array Transformation I solution

--- src/medium-collection/array-and-strings/is-zero-array/solution.ts
+++ src/medium-collection/array-and-strings/is-zero-array/solution.ts
@@ -1,3 +1,16 @@
 export function isZeroArray(nums: number[], queries: number[][]): boolean {
-  return false;
+  const n = nums.length;
+  const diff = new Array(n + 1).fill(0);
+  for (const [l, r] of queries) {
+    diff[l]++;
+    diff[r + 1]--;
+  }
+  let count = 0;
+  for (let i = 0; i < n; i++) {
+    count += diff[i];
+    if (count < nums[i]) {
+      return false;
+    }
+  }
+  return true;
 }

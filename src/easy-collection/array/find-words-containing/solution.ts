29c9d11f9965201cdcf5985b1267f6a51f5f087a
Maxim Kryuk <mkryuk@gmail.com>
2025-05-25 20:36:00 +0300

    Array and String: 2942. Find Words Containing Character solution

--- src/easy-collection/array/find-words-containing/solution.ts
+++ src/easy-collection/array/find-words-containing/solution.ts
@@ -1,3 +1,12 @@
 export function findWordsContaining(words: string[], x: string): number[] {
-  return [];
+  const result: number[] = [];
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < words[i].length; j++) {
+      if (words[i][j] === x) {
+        result.push(i);
+        break;
+      }
+    }
+  }
+  return result;
 }

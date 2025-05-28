751d5eef5b7ea5005a065715004fef2480b10ae1
Maxim Kryuk <mkryuk@gmail.com>
2025-05-28 13:25:36 +0300

    Math: 2894. Divisible and Non-divisible Sums Difference solution

--- src/easy-collection/math/diff-of-sums/solution.ts
+++ src/easy-collection/math/diff-of-sums/solution.ts
@@ -1,3 +1,8 @@
 export function differenceOfSums(n: number, m: number): number {
-  return 0;
+  const totalSum = ((1 + n) * n) / 2;
+  const k = Math.floor(n / m);
+  // first (m) + last (m * k) * count (k) / 2
+  const sumDivisible = Math.floor(((m + m * k) * k) / 2);
+  const sumNotDivisible = totalSum - sumDivisible;
+  return sumNotDivisible - sumDivisible;
 }

1165aa1730944536577d8ab3566c6ad523563473
Maxim Kryuk <mkryuk@gmail.com>
2025-06-01 23:40:55 +0300

    Math: 2929. Distribute Candies Among Children II solution

--- src/medium-collection/math/distribute-candies/solution.ts
+++ src/medium-collection/math/distribute-candies/solution.ts
@@ -1,3 +1,13 @@
 export function distributeCandies(n: number, limit: number): number {
-  return 0;
+  let result = 0;
+  for (let i = 0; i <= Math.min(n, limit); i++) {
+    // we can enumerate the number of candies of one particular child
+    // let it be i which means 0 <= i <= min(limit, n).
+    // suppose the 2nd child gets j candies. Then 0 <= j <= limit and i + j <= n.
+    // the 3rd child will hence get n - i - j candies and we should have 0 <= n - i - j <= limit.
+    // for each i, we have max(0, n - i - limit) <= j <= min(limit, n - i)
+    // each j corresponding to a solution
+    result += Math.max(Math.min(limit, n - i) - Math.max(0, n - i - limit) + 1, 0);
+  }
+  return result;
 }

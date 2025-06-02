dee585ecd9f9863a23494a65db370149f3ac4726
Maxim Kryuk <mkryuk@gmail.com>
2025-06-02 23:24:21 +0300

    Array and Strings: 135. Candy solution

--- src/hard-collection/array-and-strings/candy/solution.ts
+++ src/hard-collection/array-and-strings/candy/solution.ts
@@ -1,3 +1,32 @@
 export function candy(ratings: number[]): number {
-  return 0;
+  const n = ratings.length;
+  const minimums: number[] = [];
+
+  // find local minimums
+  for (let i = 0; i < n; i++) {
+    let mid = ratings[i];
+    let right = i === n - 1 ? Infinity : ratings[i + 1];
+    if (mid < right) {
+      minimums.push(i);
+    }
+  }
+
+  const candies = new Array(n).fill(1);
+  for (const minimum of minimums) {
+    // pass from local minimum to the left
+    for (let i = minimum - 1; i >= 0 && ratings[i] >= ratings[i + 1] && candies[i] <= candies[i + 1]; i--) {
+      if (ratings[i] > ratings[i + 1]) {
+        candies[i] = candies[i + 1] + 1;
+      }
+    }
+    // pass from local minimum to the right
+    for (let i = minimum + 1; i < n && ratings[i] >= ratings[i - 1] && candies[i] <= candies[i - 1]; i++) {
+      if (ratings[i] > ratings[i - 1]) {
+        candies[i] = candies[i - 1] + 1;
+      }
+    }
+  }
+  // sum all candies
+  const result = candies.reduce((prev, curr) => prev + curr, 0);
+  return result;
 }
--- src/hard-collection/array-and-strings/candy/test.spec.ts
+++ src/hard-collection/array-and-strings/candy/test.spec.ts
@@ -12,4 +12,34 @@ describe("Candy", () => {
     const result = candy(ratings);
     expect(result).toEqual(4);
   });
+
+  it("candy should return 7 for ratings = [1,2,3,1]", () => {
+    const ratings = [1, 2, 3, 1];
+    const result = candy(ratings);
+    expect(result).toEqual(7);
+  });
+
+  it("candy should return 12 for ratings = [4,3,1,2,3,1]", () => {
+    const ratings = [4, 3, 1, 2, 3, 1];
+    const result = candy(ratings);
+    expect(result).toEqual(12);
+  });
+
+  it("candy should return 18 for ratings = [1,6,10,8,7,3,2]", () => {
+    const ratings = [1, 6, 10, 8, 7, 3, 2];
+    const result = candy(ratings);
+    expect(result).toEqual(18);
+  });
+
+  it("candy should return 47 for ratings = [1,2,3,5,4,3,2,1,4,3,2,1,3,2,1,1,2,3,4]", () => {
+    const ratings = [1, 2, 3, 5, 4, 3, 2, 1, 4, 3, 2, 1, 3, 2, 1, 1, 2, 3, 4];
+    const result = candy(ratings);
+    expect(result).toEqual(47);
+  });
+
+  it("candy should return 13 for ratings = [2,1,1,2,3,4]", () => {
+    const ratings = [2, 1, 1, 2, 3, 4];
+    const result = candy(ratings);
+    expect(result).toEqual(13);
+  });
 });

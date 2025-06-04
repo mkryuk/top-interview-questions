055cad9f6c7d58c504ad1944df5998639e2bd51c
Maxim Kryuk <mkryuk@gmail.com>
2025-06-04 12:21:21 +0300

    Trees and Graphs: 1298. Maximum Candies You Can Get from Boxes solution

--- src/hard-collection/trees-and-graphs/max-candies/solution.ts
+++ src/hard-collection/trees-and-graphs/max-candies/solution.ts
@@ -5,5 +5,47 @@ export function maxCandies(
   containedBoxes: number[][],
   initialBoxes: number[],
 ): number {
-  return 0;
+  const n = status.length;
+
+  const openedBoxes = new Array(n).fill(false);
+  const haveBoxes = new Array(n).fill(false);
+  const haveKeys = new Array(n).fill(false);
+
+  const queue: number[] = [];
+  let result = 0;
+  // initialize with the boxes we're given initially
+  for (const box of initialBoxes) {
+    if (!haveBoxes[box]) {
+      haveBoxes[box] = true;
+      if (status[box] === 1) {
+        queue.push(box);
+      }
+    }
+  }
+  while (queue.length > 0) {
+    const box = queue.pop()!;
+    if (openedBoxes[box]) {
+      continue;
+    }
+    openedBoxes[box] = true;
+    result += candies[box];
+    // collect any new keys from this box
+    for (const key of keys[box]) {
+      haveKeys[key] = true;
+      // if we already have that box and haven't opened it yet, we can now enqueue it
+      if (haveBoxes[key] && !openedBoxes[key]) {
+        queue.push(key);
+      }
+    }
+
+    // collect any new boxes found inside
+    for (const cb of containedBoxes[box]) {
+      haveBoxes[cb] = true;
+      // if that box is open or we already have its key, enqueue it
+      if (status[cb] === 1 || haveKeys[cb]) {
+        queue.push(cb);
+      }
+    }
+  }
+  return result;
 }

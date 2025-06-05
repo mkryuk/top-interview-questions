bfac3cbdd61c9c6957c4412019e3efb879950ce3
Maxim Kryuk <mkryuk@gmail.com>
2025-06-05 13:27:20 +0300

    Array and Strings: 3403. Find the Lexicographically Largest String From the Box I solution

--- src/medium-collection/array-and-strings/answer-string/solution.ts
+++ src/medium-collection/array-and-strings/answer-string/solution.ts
@@ -1,3 +1,25 @@
 export function answerString(word: string, numFriends: number): string {
-  return "";
+  const n = word.length;
+  if (numFriends === 1) {
+    return word;
+  }
+  let result = "";
+  for (let start = 0; start < n; start++) {
+    let end = 0;
+    if (start === 0) {
+      // must leave numFriends-1 chars for other parts
+      end = n - numFriends;
+    } else if (start < numFriends) {
+      // max length is (n - numFriends + 1)
+      end = start + (n - numFriends);
+    } else {
+      // can go till the end
+      end = n - 1;
+    }
+    const candidate = word.substring(start, end + 1);
+    if (candidate > result) {
+      result = candidate;
+    }
+  }
+  return result;
 }

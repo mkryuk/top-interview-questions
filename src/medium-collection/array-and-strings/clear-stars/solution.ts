e2ccbdb70ec30a70ff6237742c84a9d61e6b1edc
Maxim Kryuk <mkryuk@gmail.com>
2025-06-08 23:04:57 +0300

    Array and Strings: 3170. Lexicographically Minimum String After Removing Stars solution

--- src/medium-collection/array-and-strings/clear-stars/solution.ts
+++ src/medium-collection/array-and-strings/clear-stars/solution.ts
@@ -1,3 +1,28 @@
 export function clearStars(s: string): string {
-  return "";
+  const n = s.length;
+  const letterPositions: number[][] = Array.from({ length: 26 }, () => []);
+  const deletedPositions = new Array(n).fill(false);
+  for (let i = 0; i < n; i++) {
+    if (s[i] === "*") {
+      for (let i = 0; i < 26; i++) {
+        // if there are letters to delete
+        if (letterPositions[i].length > 0) {
+          const idx = letterPositions[i].pop()!;
+          deletedPositions[idx] = true;
+          break;
+        }
+      }
+    } else {
+      const idx = s.charCodeAt(i) - 97;
+      letterPositions[idx].push(i);
+    }
+  }
+
+  const result: string[] = [];
+  for (let i = 0; i < n; i++) {
+    if (s[i] !== "*" && !deletedPositions[i]) {
+      result.push(s[i]);
+    }
+  }
+  return result.join("");
 }

5ccbc7fdc50215bf195403431271a69e577fdd30
Maxim Kryuk <mkryuk@gmail.com>
2025-06-11 00:07:31 +0300

    Array and Strings: 3442. Maximum Difference Between Even and Odd Frequency I solution

--- src/easy-collection/array/max-difference/solution.ts
+++ src/easy-collection/array/max-difference/solution.ts
@@ -1,3 +1,22 @@
 export function maxDifference(s: string): number {
-  return 0;
+  const n = s.length;
+  const freq = new Array(26).fill(0);
+  let maxOdd = 1;
+  let minEven = n;
+  for (let i = 0; i < n; i++) {
+    const idx = s.charCodeAt(i) - 97;
+    freq[idx]++;
+  }
+  for (const f of freq) {
+    if (f === 0) {
+      continue;
+    }
+    if (f % 2 === 0) {
+      minEven = Math.min(minEven, f);
+    } else {
+      maxOdd = Math.max(maxOdd, f);
+    }
+  }
+  const result = maxOdd - minEven;
+  return result;
 }
--- src/easy-collection/array/max-difference/test.spec.ts
+++ src/easy-collection/array/max-difference/test.spec.ts
@@ -12,4 +12,22 @@ describe("3442. Maximum Difference Between Even and Odd Frequency I", () => {
     const result = maxDifference(s);
     expect(result).toEqual(1);
   });
+
+  it('maxDifference should return -3 for  s = "yzyyys"', () => {
+    const s = "yzyyys";
+    const result = maxDifference(s);
+    expect(result).toEqual(-3);
+  });
+
+  it('maxDifference should return 3 for  s = "aaaaabb"', () => {
+    const s = "aaaaabb";
+    const result = maxDifference(s);
+    expect(result).toEqual(3);
+  });
+
+  it('maxDifference should return -3 for  s = "aaaab"', () => {
+    const s = "aaaab";
+    const result = maxDifference(s);
+    expect(result).toEqual(-3);
+  });
 });

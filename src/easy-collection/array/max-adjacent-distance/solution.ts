e7680b13f4f3d4d1a009bec96b497adda00e0160
Maxim Kryuk <mkryuk@gmail.com>
2025-06-12 14:12:55 +0300

    Array and Strings: 3423. Maximum Difference Between Adjacent Elements in a Circular Array solution

--- src/easy-collection/array/max-adjacent-distance/solution.ts
+++ src/easy-collection/array/max-adjacent-distance/solution.ts
@@ -1,3 +1,9 @@
 export function maxAdjacentDistance(nums: number[]): number {
-  return 0;
+  const n = nums.length;
+  // we add first - last distance to check
+  let result = Math.abs(nums[0] - nums[n - 1]);
+  for (let i = 1; i < n; i++) {
+    result = Math.max(result, Math.abs(nums[i] - nums[i - 1]));
+  }
+  return result;
 }

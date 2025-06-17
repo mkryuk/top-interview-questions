e2a82572da0ef8659bab4a5eddeeb7626c8578d2
Maxim Kryuk <mkryuk@gmail.com>
2025-06-17 14:19:47 +0300

    Array and Strings: 2016. Maximum Difference Between Increasing Elements solution

--- src/easy-collection/array/maximum-difference/solution.ts
+++ src/easy-collection/array/maximum-difference/solution.ts
@@ -1,3 +1,18 @@
 export function maximumDifference(nums: number[]): number {
-  return 0;
+  const n = nums.length;
+  let min = nums[0];
+
+  let result = -1;
+  for (let i = 1; i < n; i++) {
+    const diff = nums[i] - min;
+    if (diff > 0) {
+      // we have a valid increase; update result if it's bigger
+      result = Math.max(result, diff);
+    } else {
+      // nums[i] <= min, make nums[i] the new candidate for the minimum
+      min = nums[i];
+    }
+  }
+
+  return result;
 }

a8b06a919e3efca9343852dfb00c9d31ad616857
Maxim Kryuk <mkryuk@gmail.com>
2025-06-20 15:09:46 +0300

    Array and Strings: 2294. Partition Array Such That Maximum Difference Is K solution

--- src/medium-collection/array-and-strings/partition-array/solution.ts
+++ src/medium-collection/array-and-strings/partition-array/solution.ts
@@ -1,3 +1,14 @@
 export function partitionArray(nums: number[], k: number): number {
-  return 0;
+  const n = nums.length;
+  nums.sort((a, b) => a - b);
+  // initially we have at least 1 subsequence
+  let result = 1;
+  let subseqMin = nums[0];
+  for (let i = 1; i < n; i++) {
+    if (nums[i] - subseqMin > k) {
+      result++;
+      subseqMin = nums[i];
+    }
+  }
+  return result;
 }

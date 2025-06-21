08cb86b4e06fd60b3f268541f4cfdf0acf53b825
Maxim Kryuk <mkryuk@gmail.com>
2025-06-21 23:06:53 +0300

    Array and Strings: 3443. Maximum Manhattan Distance After K Changes solution

--- src/medium-collection/array-and-strings/max-manhattan-distance/solution.ts
+++ src/medium-collection/array-and-strings/max-manhattan-distance/solution.ts
@@ -1,3 +1,31 @@
 export function maxDistance(s: string, k: number): number {
-  return 0;
+  let latitude = 0;
+  let longitude = 0;
+  let result = 0;
+  const n = s.length;
+  // the optimal strategy is to modify the less frequent letters in both directions whenever possible.
+  // if the total number of such letters is greater than k,
+  // then modifying any k of them increases the Manhattan distance by 2×k.
+  // if the total number is less than or equal to k,
+  // then all the less frequent letters in both directions will be modified,
+  // and no further modifications are necessary.
+  // In this case, the Manhattan distance becomes equal to the length of the string.
+  for (let i = 0; i < n; i++) {
+    switch (s[i]) {
+      case "N":
+        latitude++;
+        break;
+      case "S":
+        latitude--;
+        break;
+      case "E":
+        longitude++;
+        break;
+      case "W":
+        longitude--;
+        break;
+    }
+    result = Math.max(result, Math.min(Math.abs(latitude) + Math.abs(longitude) + k * 2, i + 1));
+  }
+  return result;
 }

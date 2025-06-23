70a2cd11225580865deef4ca4a09046475a731d9
Maxim Kryuk <mkryuk@gmail.com>
2025-06-23 15:01:56 +0300

    Array and Strings: 2138. Divide a String Into Groups of Size k solution

--- src/easy-collection/strings/divide-string/solution.ts
+++ src/easy-collection/strings/divide-string/solution.ts
@@ -1,3 +1,13 @@
 export function divideString(s: string, k: number, fill: string): string[] {
-  return [];
+  const result: string[] = [];
+  const groupCount = Math.ceil(s.length / k);
+  for (let i = 0; i < groupCount; i++) {
+    const chunk = s.substring(i * k, i * k + k);
+    if (chunk.length < k) {
+      result.push(chunk + fill.repeat(k - chunk.length));
+    } else {
+      result.push(chunk);
+    }
+  }
+  return result;
 }

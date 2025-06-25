011eee4a30a5f301d09d4ee00eb076722984136d
Maxim Kryuk <mkryuk@gmail.com>
2025-06-25 15:49:08 +0300

    Array and Strings: 2200. Find All K-Distant Indices in an Array solution

--- src/easy-collection/array/find-k-distant-indices/solution.ts
+++ src/easy-collection/array/find-k-distant-indices/solution.ts
@@ -1,3 +1,17 @@
 export function findKDistantIndices(nums: number[], key: number, k: number): number[] {
-  return [];
+  const n = nums.length;
+  const result: number[] = [];
+  let lastEnd = -1;
+  for (let i = 0; i < n; i++) {
+    if (nums[i] === key) {
+      const start = Math.max(i - k, lastEnd + 1);
+      const end = Math.min(i + k, n - 1);
+      for (let index = start; index <= end; index++) {
+        result.push(index);
+      }
+      lastEnd = end;
+    }
+  }
+
+  return result;
 }

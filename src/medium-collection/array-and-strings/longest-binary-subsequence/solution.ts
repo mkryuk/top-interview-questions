38770a2b6d6b92406b67c5ad0d194d9403517836
Maxim Kryuk <mkryuk@gmail.com>
2025-06-27 13:59:17 +0300

    Array and Strings: 2311. Longest Binary Subsequence Less Than or Equal to K solution

--- src/medium-collection/array-and-strings/longest-binary-subsequence/solution.ts
+++ src/medium-collection/array-and-strings/longest-binary-subsequence/solution.ts
@@ -1,3 +1,17 @@
 export function longestSubsequence(s: string, k: number): number {
-  return 0;
+  let sum = 0;
+  let bits = Math.log2(k) + 1;
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+    const ch = s[s.length - 1 - i];
+    if (ch === "0") {
+      result++;
+    } else {
+      if (i < bits && sum + (1 << i) <= k) {
+        sum += 1 << i;
+        result++;
+      }
+    }
+  }
+  return result;
 }

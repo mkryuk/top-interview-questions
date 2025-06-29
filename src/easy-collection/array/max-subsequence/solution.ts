c840a6201874fef1f925a7a5bbdcca709b210068
Maxim Kryuk <mkryuk@gmail.com>
2025-06-29 22:33:36 +0300

    Array and Strings: 2099. Find Subsequence of Length K With the Largest Sum solution

--- src/easy-collection/array/max-subsequence/solution.ts
+++ src/easy-collection/array/max-subsequence/solution.ts
@@ -1,3 +1,15 @@
 export function maxSubsequence(nums: number[], k: number): number[] {
-  return [];
+  return (
+    nums
+      // map to value with index
+      .map((val, idx) => ({ val, idx }))
+      // sort by val
+      .sort((a, b) => b.val - a.val)
+      // take first k
+      .slice(0, k)
+      // sort by index
+      .sort((a, b) => a.idx - b.idx)
+      // map to value array
+      .map((a) => a.val)
+  );
 }

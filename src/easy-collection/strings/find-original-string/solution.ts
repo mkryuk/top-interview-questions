141ef043864c81143b6381878970a370da7696b3
Maxim Kryuk <mkryuk@gmail.com>
2025-07-02 13:45:59 +0300

    Array and Strings: 3330. Find the Original Typed String I solution

--- src/easy-collection/strings/find-original-string/solution.ts
+++ src/easy-collection/strings/find-original-string/solution.ts
@@ -1,3 +1,9 @@
 export function possibleStringCount(word: string): number {
-  return 0;
+  let result = 1;
+  for (let i = 1; i < word.length; i++) {
+    if (word[i] === word[i - 1]) {
+      result++;
+    }
+  }
+  return result;
 }

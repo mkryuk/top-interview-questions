224e6b74a42ac98d6e11196e4f3a8069bd51765a
Maxim Kryuk <mkryuk@gmail.com>
2025-07-04 13:09:30 +0300

    Array and Strings: 3304. Find the K-th Character in String Game I solution

--- src/easy-collection/strings/find-k-th-character/solution.ts
+++ src/easy-collection/strings/find-k-th-character/solution.ts
@@ -1,3 +1,12 @@
 export function kthCharacter(k: number): string {
-  return "";
+  let result = ["a"];
+  while (result.length < k) {
+    const len = result.length;
+    for (let i = 0; i < len; i++) {
+      const code = result[i].charCodeAt(0);
+      const nextCh = code === 122 ? "a" : String.fromCharCode(code + 1);
+      result.push(nextCh);
+    }
+  }
+  return result[k - 1];
 }

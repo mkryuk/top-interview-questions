e5c67deab459d4ca570401bf4d2c023500c5d4c4
Maxim Kryuk <mkryuk@gmail.com>
2025-07-05 13:15:25 +0300

    Array and Strings: 1394. Find Lucky Integer in an Array solution

--- src/easy-collection/array/find-lucky/solution.ts
+++ src/easy-collection/array/find-lucky/solution.ts
@@ -1,3 +1,14 @@
 export function findLucky(arr: number[]): number {
-  return 0;
+  const numCount = new Map<number, number>();
+  for (const num of arr) {
+    numCount.set(num, (numCount.get(num) ?? 0) + 1);
+  }
+
+  let result = -1;
+  for (const [num, count] of numCount) {
+    if (num === count && num > result) {
+      result = num;
+    }
+  }
+  return result;
 }

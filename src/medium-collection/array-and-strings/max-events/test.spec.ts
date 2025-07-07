232d50b65d3508be0c603384aa72ea1dc63d4e2f
Maxim Kryuk <mkryuk@gmail.com>
2025-07-07 15:15:56 +0300

    Array and Strings: 1353. Maximum Number of Events That Can Be Attended init

---
+++ src/medium-collection/array-and-strings/max-events/test.spec.ts
@@ -0,0 +1,24 @@
+import { maxEvents } from "./solution";
+
+describe("1353. Maximum Number of Events That Can Be Attended", () => {
+  it("maxEvents should return 3 for events = [[1,2],[2,3],[3,4]]", () => {
+    const events = [
+      [1, 2],
+      [2, 3],
+      [3, 4],
+    ];
+    const result = maxEvents(events);
+    expect(result).toEqual(3);
+  });
+
+  it("maxEvents should return 4 for events= [[1,2],[2,3],[3,4],[1,2]]", () => {
+    const events = [
+      [1, 2],
+      [2, 3],
+      [3, 4],
+      [1, 2],
+    ];
+    const result = maxEvents(events);
+    expect(result).toEqual(4);
+  });
+});

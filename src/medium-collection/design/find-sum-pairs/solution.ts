58d33e4040dcb28f283082589342dbcdc3a4c88b
Maxim Kryuk <mkryuk@gmail.com>
2025-07-07 15:09:30 +0300

    Design: 1865. Finding Pairs With a Certain Sum solution

--- src/medium-collection/design/find-sum-pairs/solution.ts
+++ src/medium-collection/design/find-sum-pairs/solution.ts
@@ -1,9 +1,37 @@
 export class FindSumPairs {
-  constructor(nums1: number[], nums2: number[]) {}
+  private readonly nums1: number[];
+  private nums2: number[];
+  private freq: Map<number, number>;
 
-  add(index: number, val: number): void {}
+  constructor(nums1: number[], nums2: number[]) {
+    this.nums1 = nums1;
+    this.nums2 = nums2;
+    this.freq = new Map<number, number>();
+
+    for (const n of nums2) {
+      this.freq.set(n, (this.freq.get(n) ?? 0) + 1);
+    }
+  }
+
+  add(index: number, val: number): void {
+    const oldVal = this.nums2[index];
+    const oldCnt = (this.freq.get(oldVal) ?? 0) - 1;
+    if (oldCnt === 0) {
+      this.freq.delete(oldVal);
+    } else {
+      this.freq.set(oldVal, oldCnt);
+    }
+    const newVal = oldVal + val;
+    this.nums2[index] = newVal;
+    this.freq.set(newVal, (this.freq.get(newVal) ?? 0) + 1);
+  }
 
   count(tot: number): number {
-    return 0;
+    let res = 0;
+    for (const num of this.nums1) {
+      const need = tot - num;
+      res += this.freq.get(need) ?? 0;
+    }
+    return res;
   }
 }

fbc83a68327573dfe66674337d815f57bfc051bb
Maxim Kryuk <mkryuk@gmail.com>
2025-07-14 22:57:21 +0300

    Array and Strings: 2410. Maximum Matching of Players With Trainers solution

--- src/medium-collection/array-and-strings/players-and-trainers/solution.ts
+++ src/medium-collection/array-and-strings/players-and-trainers/solution.ts
@@ -1,3 +1,17 @@
 export function matchPlayersAndTrainers(players: number[], trainers: number[]): number {
-  return 0;
+  players.sort((a, b) => a - b);
+  trainers.sort((a, b) => a - b);
+  let result = 0;
+  let i = 0;
+  let j = 0;
+  while (i < players.length && j < trainers.length) {
+    if (players[i] <= trainers[j]) {
+      result++;
+      i++;
+      j++;
+    } else {
+      j++;
+    }
+  }
+  return result;
 }

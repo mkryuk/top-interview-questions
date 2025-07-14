adaea1360bd0bcf2a2f24f57c6286853b202d9a9
Maxim Kryuk <mkryuk@gmail.com>
2025-07-14 23:13:09 +0300

    Linked List: 1290. Convert Binary Number in a Linked List to Integer solution

--- src/easy-collection/linked-list/get-decimal-value/solution.ts
+++ src/easy-collection/linked-list/get-decimal-value/solution.ts
@@ -1,3 +1,16 @@
+import { ListNode } from "../common";
+
 export function getDecimalValue(head: ListNode | null): number {
-  return 0;
+  let bits: number[] = [];
+  let current = head;
+  while (current !== null) {
+    bits.push(current.val);
+    current = current.next;
+  }
+  let result = 0;
+  for (let i = bits.length - 1; i >= 0; i--) {
+    result += bits[i] * Math.pow(2, bits.length - i - 1);
+  }
+
+  return result;
 }
--- src/easy-collection/linked-list/get-decimal-value/test.spec.ts
+++ src/easy-collection/linked-list/get-decimal-value/test.spec.ts
@@ -15,4 +15,11 @@ describe("1290. Convert Binary Number in a Linked List to Integer", () => {
     const result = getDecimalValue(head);
     expect(result).toEqual(0);
   });
+
+  it("getDecimalValue should return 18880 for head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]", () => {
+    const nodes = [1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0];
+    const head = arrayToList(nodes);
+    const result = getDecimalValue(head);
+    expect(result).toEqual(18880);
+  });
 });

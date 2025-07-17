d187e40ed103e1f207c91bbbe0e2ac7755a2d975
Maxim Kryuk <mkryuk@gmail.com>
2025-07-17 11:55:42 +0300

    Array and Strings: 3201. Find the Maximum Length of Valid Subsequence I solution

--- src/medium-collection/array-and-strings/maximum-subsequence-I/solution.ts
+++ src/medium-collection/array-and-strings/maximum-subsequence-I/solution.ts
@@ -1,3 +1,19 @@
 export function maximumLength(nums: number[]): number {
-  return 0;
+  let oddCount = 0;
+  let evenCount = 0;
+  let altCount = 0;
+  let lastParity = -1;
+  for (const num of nums) {
+    const parity = num % 2;
+    if (parity === 0) {
+      evenCount++;
+    } else {
+      oddCount++;
+    }
+    if (altCount === 0 || parity !== lastParity) {
+      altCount++;
+      lastParity = parity;
+    }
+  }
+  return Math.max(oddCount, evenCount, altCount);
 }

89df3ae680629c0c938d0af20a58c464530d6b72
Maxim Kryuk <mkryuk@gmail.com>
2025-07-28 12:04:20 +0300

    Array and Strings: 2210. Count Hills and Valleys in an Array solution

--- src/easy-collection/array/count-hill-valley/solution.ts
+++ src/easy-collection/array/count-hill-valley/solution.ts
@@ -1,3 +1,23 @@
 export function countHillValley(nums: number[]): number {
-  return 0;
+  let minimums = 0;
+  let wasDown = false;
+  let maximums = 0;
+  let wasUp = false;
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] > nums[i + 1]) {
+      wasDown = true;
+    }
+    if (wasDown && nums[i] < nums[i + 1]) {
+      minimums++;
+      wasDown = false;
+    }
+    if (nums[i] < nums[i + 1]) {
+      wasUp = true;
+    }
+    if (wasUp && nums[i] > nums[i + 1]) {
+      maximums++;
+      wasUp = false;
+    }
+  }
+  return minimums + maximums;
 }

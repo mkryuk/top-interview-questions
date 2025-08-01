71b9c8334cc2dcd743b83a4713022ac2b8e3184b
Maxim Kryuk <mkryuk@gmail.com>
2025-08-01 13:09:55 +0300

    Bit Manipulation: 898. Bitwise ORs of Subarrays solution

--- src/medium-collection/bit-manipulation/subarrays-bitwise-ors/solution.ts
+++ src/medium-collection/bit-manipulation/subarrays-bitwise-ors/solution.ts
@@ -1,3 +1,19 @@
 export function subarrayBitwiseORs(arr: number[]): number {
-  return 0;
+  const result = new Set<number>();
+  // ORs of subarrays ending at previous index
+  let prev = new Set<number>();
+
+  for (const num of arr) {
+    const curr = new Set<number>();
+    curr.add(num);
+    for (const p of prev) {
+      curr.add(p | num);
+    }
+    for (const c of curr) {
+      result.add(c);
+    }
+    prev = curr;
+  }
+
+  return result.size;
 }

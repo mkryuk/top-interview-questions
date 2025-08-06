168a38b27686da4dd223577599ccdec28a3ea3e0
Maxim Kryuk <mkryuk@gmail.com>
2025-08-06 14:12:20 +0300

    Array and Strings: 3477. Fruits Into Baskets II solution

--- src/easy-collection/array/fruits-into-baskets-II/solution.ts
+++ src/easy-collection/array/fruits-into-baskets-II/solution.ts
@@ -1,3 +1,18 @@
 export function numOfUnplacedFruits(fruits: number[], baskets: number[]): number {
-  return 0;
+  let result = 0;
+  for (const fruit of fruits) {
+    let placed = false;
+    for (let i = 0; i < baskets.length; i++) {
+      if (fruit <= baskets[i]) {
+        baskets[i] = 0;
+        placed = true;
+        break;
+      }
+    }
+    if (placed === false) {
+      result++;
+    }
+    placed = false;
+  }
+  return result;
 }

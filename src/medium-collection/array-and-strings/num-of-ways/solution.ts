9ad04dda72dce32267d54bfc86a92634f2e2b274
Maxim Kryuk <mkryuk@gmail.com>
2025-08-13 16:04:20 +0300

    Array and Strings: 2787. Ways to Express an Integer as Sum of Powers solution

--- src/medium-collection/array-and-strings/num-of-ways/solution.ts
+++ src/medium-collection/array-and-strings/num-of-ways/solution.ts
@@ -1,3 +1,23 @@
 export function numberOfWays(n: number, x: number): number {
-  return 0;
+  const MOD = 1_000_000_007;
+
+  // precompute all i^x <= n
+  const powers: number[] = [];
+  for (let i = 1; ; i++) {
+    const p = Math.pow(i, x);
+    if (p > n) break;
+    powers.push(p);
+  }
+
+  // dp[s] = number of ways to reach sum s using unique powers
+  const dp = new Array<number>(n + 1).fill(0);
+  dp[0] = 1;
+
+  for (const p of powers) {
+    for (let s = n; s >= p; s--) {
+      dp[s] = (dp[s] + dp[s - p]) % MOD;
+    }
+  }
+
+  return dp[n];
 }

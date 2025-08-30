fe979a9a1a8d61fbe36139850ba3d5a07b90478c
Maxim Kryuk <mkryuk@gmail.com>
2025-08-30 11:15:08 +0300

    3021. Alice and Bob Playing Flower Game solution

--- src/medium-collection/math/flower-game/solution.ts
+++ src/medium-collection/math/flower-game/solution.ts
@@ -1,3 +1,13 @@
 export function flowerGame(n: number, m: number): number {
-  return 0;
+  // idea: alice wins iff x + y is odd, x and y have different parity
+  // count = (odd x * even y) + (even x * odd y)
+  // count odds in [1..k] is ⌈k/2⌉, evens is ⌊k/2⌋
+  const oddN: number = Math.floor((n + 1) / 2);
+  const evenN: number = Math.floor(n / 2);
+  const oddM: number = Math.floor((m + 1) / 2);
+  const evenM: number = Math.floor(m / 2);
+
+  // pairs with different parity
+  const result: number = oddN * evenM + evenN * oddM;
+  return result;
 }

c4878ff7e45d708ece85085ee780015e59b3c8fd
Maxim Kryuk <mkryuk@gmail.com>
2025-09-05 09:49:34 +0300

    Math: 3516. Find Closest Person solution

--- src/easy-collection/math/closest-person/solution.ts
+++ src/easy-collection/math/closest-person/solution.ts
@@ -1,3 +1,10 @@
 export function findClosest(x: number, y: number, z: number): number {
+  const first = Math.abs(x - z);
+  const second = Math.abs(y - z);
+  if (first < second) {
+    return 1;
+  } else if (second < first) {
+    return 2;
+  }
   return 0;
 }

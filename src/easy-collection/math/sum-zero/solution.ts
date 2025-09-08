b1b5828673e2ac8688cdafb836f659626b786316
Maxim Kryuk <mkryuk@gmail.com>
2025-09-08 23:55:25 +0300

    Math: 1304. Find N Unique Integers Sum up to Zero solution

--- src/easy-collection/math/sum-zero/solution.ts
+++ src/easy-collection/math/sum-zero/solution.ts
@@ -1,3 +1,9 @@
 export function sumZero(n: number): number[] {
-  return [];
+  const result = new Array(n);
+  // negative sum of arithmetic progression 1..(n-1)
+  result[0] = -((n * (n - 1)) / 2);
+  for (let i = 1; i < n; i++) {
+    result[i] = i;
+  }
+  return result;
 }

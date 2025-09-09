ddb22284f3ac9b17a184bcd5505ff6a6bdc66111
Maxim Kryuk <mkryuk@gmail.com>
2025-09-09 17:38:47 +0300

    Math: 1317. Convert Integer to the Sum of Two No-Zero Integers solution

--- src/easy-collection/math/no-zero-integers/solution.ts
+++ src/easy-collection/math/no-zero-integers/solution.ts
@@ -1,3 +1,10 @@
 export function getNoZeroIntegers(n: number): number[] {
+  for (let i = 1; i < n; i++) {
+    const a = i;
+    const b = n - i;
+    if (!a.toString().includes("0") && !b.toString().includes("0")) {
+      return [a, b];
+    }
+  }
   return [];
 }

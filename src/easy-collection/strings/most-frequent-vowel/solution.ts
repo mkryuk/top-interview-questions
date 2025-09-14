ecde7eba44555abe0008f73d0bfd8abf7b7039ac
Maxim Kryuk <mkryuk@gmail.com>
2025-09-14 11:57:39 +0300

    Array and Strings: 3541. Find Most Frequent Vowel and Consonant solution

--- src/easy-collection/strings/most-frequent-vowel/solution.ts
+++ src/easy-collection/strings/most-frequent-vowel/solution.ts
@@ -1,3 +1,18 @@
 export function maxFreqSum(s: string): number {
-  return 0;
+  const vowel = new Set();
+  const map = new Map<string, number>();
+  let maxVowel = 0;
+  let maxConsonant = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (!map.has(s[i])) {
+      map.set(s[i], 0);
+    }
+    map.set(s[i], map.get(s[i])! + 1);
+    if (s[i] === "a" || s[i] === "e" || s[i] === "i" || s[i] === "o" || s[i] === "u") {
+      maxVowel = Math.max(maxVowel, map.get(s[i])!);
+    } else {
+      maxConsonant = Math.max(maxConsonant, map.get(s[i])!);
+    }
+  }
+  return maxVowel + maxConsonant;
 }

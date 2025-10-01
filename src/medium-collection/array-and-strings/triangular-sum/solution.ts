8505d771cd5e02d56760e28c973da7905ee45bc1
Maxim Kryuk <mkryuk@gmail.com>
2025-10-01 10:00:41 +0200

    Array and Strings: 2221. Find Triangular Sum of an Array solution

--- src/medium-collection/array-and-strings/triangular-sum/solution.ts
+++ src/medium-collection/array-and-strings/triangular-sum/solution.ts
@@ -1,3 +1,10 @@
 export function triangularSum(nums: number[]): number {
-  return 0;
+  while (nums.length > 1) {
+    const newNums = [];
+    for (let i = 0; i < nums.length - 1; i++) {
+      newNums.push((nums[i] + nums[i + 1]) % 10);
+    }
+    nums = newNums;
+  }
+  return nums[0];
 }

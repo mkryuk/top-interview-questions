e165648f5193c9ebcdd70be70be7d2a5063a6a03
Maxim Kryuk <mkryuk@gmail.com>
2025-10-24 19:43:17 +0300

    Array and Strings: 3461. Check If Digits Are Equal in String After Operations I solution

--- src/easy-collection/strings/has-same-digits/solution.ts
+++ src/easy-collection/strings/has-same-digits/solution.ts
@@ -1,3 +1,12 @@
 export function hasSameDigits(s: string): boolean {
-  return false;
+  let numArr = s.split("").map(Number);
+  const n = numArr.length;
+  while (numArr.length > 2) {
+    const tempNumArr: number[] = [];
+    for (let i = 1; i < numArr.length; i++) {
+      tempNumArr.push((numArr[i - 1] + numArr[i]) % 10);
+    }
+    numArr = tempNumArr;
+  }
+  return numArr[0] === numArr[1];
 }

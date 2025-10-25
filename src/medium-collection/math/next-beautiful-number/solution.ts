7aae01d1c37b6c3c3ad9cf9ac17d072aaa0461ce
Maxim Kryuk <mkryuk@gmail.com>
2025-10-25 10:26:04 +0300

    Math: 2048. Next Greater Numerically Balanced Number solution

--- src/medium-collection/math/next-beautiful-number/solution.ts
+++ src/medium-collection/math/next-beautiful-number/solution.ts
@@ -1,3 +1,29 @@
 export function nextBeautifulNumber(n: number): number {
-  return 0;
+  for (let i = n + 1; i <= 1224444; i += 1) {
+    if (isBalance(i)) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+function isBalance(num: number): boolean {
+  // count occurrences of each digit 0–9
+  const count: number[] = new Array(10).fill(0);
+
+  // extract digits
+  while (num > 0) {
+    const digit: number = num % 10;
+    count[digit] += 1;
+    num = Math.floor(num / 10);
+  }
+
+  // verify each digit obeys its rule
+  for (let d = 0; d < 10; d += 1) {
+    if (count[d] > 0 && count[d] !== d) {
+      return false;
+    }
+  }
+
+  return true;
 }

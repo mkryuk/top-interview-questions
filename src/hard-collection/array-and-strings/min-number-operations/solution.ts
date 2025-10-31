f42034773c23c3a81c7717334565ab0c6639b0ec
Maxim Kryuk <mkryuk@gmail.com>
2025-10-31 23:47:37 +0200

    Array and Strings: 1526. Minimum Number of Increments on Subarrays to Form a Target Array solution

--- src/hard-collection/array-and-strings/min-number-operations/solution.ts
+++ src/hard-collection/array-and-strings/min-number-operations/solution.ts
@@ -1,3 +1,16 @@
 export function minNumberOperations(target: number[]): number {
+  let result = 0;
+  let prev = 0;
+  for (let i = 0; i < target.length; i++) {
+    result += diff(prev, target[i]);
+    prev = target[i];
+  }
+  return result;
+}
+
+function diff(prev: number, curr: number): number {
+  if (curr > prev) {
+    return curr - prev;
+  }
   return 0;
 }

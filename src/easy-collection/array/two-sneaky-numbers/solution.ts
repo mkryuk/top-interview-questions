a8d11ae4ec67eb230942c351d03d549c5f32f5a0
Maxim Kryuk <mkryuk@gmail.com>
2025-11-01 00:20:17 +0200

    Array and Strings: 3289. The Two Sneaky Numbers of Digitville solution

--- src/easy-collection/array/two-sneaky-numbers/solution.ts
+++ src/easy-collection/array/two-sneaky-numbers/solution.ts
@@ -1,3 +1,11 @@
 export function getSneakyNumbers(nums: number[]): number[] {
-  return [];
+  const numCount = new Array(nums.length).fill(0);
+  const result: number[] = [];
+  for (const num of nums) {
+    numCount[num]++;
+    if (numCount[num] > 1) {
+      result.push(num);
+    }
+  }
+  return result;
 }

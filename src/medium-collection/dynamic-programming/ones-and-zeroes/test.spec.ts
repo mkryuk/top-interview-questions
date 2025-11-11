f9b7981123417b452163654fa68fb645790e5fa0
Maxim Kryuk <mkryuk@gmail.com>
2025-11-11 12:39:58 +0200

    Dynamic Programming: 474. Ones and Zeroes init

---
+++ src/medium-collection/dynamic-programming/ones-and-zeroes/test.spec.ts
@@ -0,0 +1,19 @@
+import { findMaxForm } from "./solution";
+
+describe("474. Ones and Zeroes", () => {
+  it('findMaxForm should return 4 for strs = ["10","0001","111001","1","0"], m = 5, n = 3', () => {
+    const strs = ["10", "0001", "111001", "1", "0"];
+    const m = 5;
+    const n = 3;
+    const result = findMaxForm(strs, m, n);
+    expect(result).toEqual(4);
+  });
+
+  it('findMaxForm should return 2 for strs = ["10","0","1"], m = 1, n = 1', () => {
+    const strs = ["10", "0", "1"];
+    const m = 1;
+    const n = 1;
+    const result = findMaxForm(strs, m, n);
+    expect(result).toEqual(4);
+  });
+});

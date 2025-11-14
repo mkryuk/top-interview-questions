de64be025a890ccf5bd2f49d60a8ce9ff2a1ca61
Maxim Kryuk <mkryuk@gmail.com>
2025-11-14 16:15:40 +0200

    Array and Strings: 3228. Maximum Number of Operations to Move Ones to the End solution

--- src/medium-collection/array-and-strings/move-ones-to-end/solution.ts
+++ src/medium-collection/array-and-strings/move-ones-to-end/solution.ts
@@ -1,3 +1,22 @@
 export function maxOperations(s: string): number {
-  return 0;
+  let result: number = 0;
+
+  // ones stores how many '1's we have seen so far while scanning
+  let ones: number = 0;
+
+  for (let i: number = 0; i < s.length; i++) {
+    if (s[i] === "1") {
+      // count each '1' we pass
+      ones++;
+    } else {
+      // we are at a '0'
+      // if this zero is the last in its block (next char is '1' or end of string)
+      // then every '1' seen so far can be moved across this zero block once
+      if (i + 1 === s.length || s[i + 1] === "1") {
+        result += ones;
+      }
+    }
+  }
+
+  return result;
 }

3779cf38d636d8f88f3d444ef653f4f70b7d3bbb
Maxim Kryuk <mkryuk@gmail.com>
2025-11-17 13:05:09 +0200

    Array and Strings: 1513. Number of Substrings With Only 1s solution

--- src/medium-collection/array-and-strings/number-of-substrings-with-1s/solution.ts
+++ src/medium-collection/array-and-strings/number-of-substrings-with-1s/solution.ts
@@ -1,3 +1,28 @@
 export function numSub(s: string): number {
-  return 0;
+  const MOD: number = 1e9 + 7;
+
+  let result: number = 0;
+  let currentRun: number = 0;
+
+  for (let i: number = 0; i < s.length; i++) {
+    if (s[i] === "1") {
+      currentRun++;
+    } else {
+      if (currentRun > 0) {
+        result = (result + countOnesSubstrings(currentRun, MOD)) % MOD;
+        currentRun = 0;
+      }
+    }
+  }
+
+  // add contribution from the last run if string ends with '1'
+  if (currentRun > 0) {
+    result = (result + countOnesSubstrings(currentRun, MOD)) % MOD;
+  }
+
+  return result;
+}
+
+function countOnesSubstrings(runLength: number, MOD: number): number {
+  return ((runLength * (runLength + 1)) / 2) % MOD;
 }

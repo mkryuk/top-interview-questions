5ec61f5e40f0c4c489e9c66e425c953eeaf9b60c
Maxim Kryuk <mkryuk@gmail.com>
2025-11-18 13:43:36 +0200

    Array and Strings: 1437. Check If All 1's Are at Least Length K Places Away solution

--- src/easy-collection/array/k-length-apart/solution.ts
+++ src/easy-collection/array/k-length-apart/solution.ts
@@ -1,3 +1,17 @@
 export function kLengthApart(nums: number[], k: number): boolean {
-  return false;
+  let prevOneIndex = -1;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) {
+      continue;
+    }
+    if (prevOneIndex === -1) {
+      prevOneIndex = i;
+      continue;
+    }
+    if (i - prevOneIndex - 1 < k) {
+      return false;
+    }
+    prevOneIndex = i;
+  }
+  return true;
 }

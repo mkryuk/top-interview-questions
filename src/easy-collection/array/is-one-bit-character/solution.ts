0d24a36e5687c2f35308576682fd1ea19ddccbed
Maxim Kryuk <mkryuk@gmail.com>
2025-11-19 11:50:24 +0200

    Array and Strings: 717. 1-bit and 2-bit Characters solution

--- src/easy-collection/array/is-one-bit-character/solution.ts
+++ src/easy-collection/array/is-one-bit-character/solution.ts
@@ -1,3 +1,11 @@
 export function isOneBitCharacter(bits: number[]): boolean {
-  return false;
+  let i = 0;
+  while (i < bits.length - 1) {
+    if (bits[i] === 0) {
+      i++;
+    } else {
+      i += 2;
+    }
+  }
+  return i === bits.length - 1;
 }

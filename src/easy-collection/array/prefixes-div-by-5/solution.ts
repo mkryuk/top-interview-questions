8feb8bd1bd8afa32894adbf5708a666dd7453287
Maxim Kryuk <mkryuk@gmail.com>
2025-11-25 12:38:48 +0200

    Array and Strings: 1018. Binary Prefix Divisible By 5 solution

--- src/easy-collection/array/prefixes-div-by-5/solution.ts
+++ src/easy-collection/array/prefixes-div-by-5/solution.ts
@@ -1,3 +1,9 @@
 export function prefixesDivBy5(nums: number[]): boolean[] {
-  return [];
+  let num = 0;
+  let result: boolean[] = [];
+  for (let i = 0; i < nums.length; i++) {
+    num = (num * 2 + nums[i]) % 5;
+    result.push(num === 0);
+  }
+  return result;
 }

180547ce48cf707b7d98d5138445a7ca14c7ac88
Maxim Kryuk <mkryuk@gmail.com>
2025-11-26 16:27:24 +0200

    Math: 1015. Smallest Integer Divisible by K solution

--- src/medium-collection/math/smallest-div-by-k/solution.ts
+++ src/medium-collection/math/smallest-div-by-k/solution.ts
@@ -1,3 +1,11 @@
 export function smallestRepunitDivByK(k: number): number {
-  return 0;
+  let remainder = 0;
+  // we only need to check at most k different remainders
+  for (let length = 1; length <= k; length++) {
+    remainder = (remainder * 10 + 1) % k;
+    if (remainder === 0) {
+      return length;
+    }
+  }
+  return -1;
 }

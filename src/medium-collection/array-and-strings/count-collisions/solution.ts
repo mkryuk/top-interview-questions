415008d6857e83f3663a25db00096b7c554556ad
Maxim Kryuk <mkryuk@gmail.com>
2025-12-05 12:46:01 +0200

    Array and Strings: 2211. Count Collisions on a Road solution

--- src/medium-collection/array-and-strings/count-collisions/solution.ts
+++ src/medium-collection/array-and-strings/count-collisions/solution.ts
@@ -1,3 +1,47 @@
 export function countCollisions(directions: string): number {
-  return 0;
+  // find first index that is not 'L'
+  const start: number = findFirstNonL(directions);
+
+  // find last index that is not 'R'
+  const end: number = findLastNonR(directions);
+
+  // if everything is trimmed away, no collisions
+  if (start > end) {
+    return 0;
+  }
+
+  let collisions: number = 0;
+
+  // count all moving cars ('L' or 'R') in the middle segment
+  for (let i: number = start; i <= end; i++) {
+    const ch: string = directions[i];
+
+    if (ch !== "S") {
+      collisions++;
+    }
+  }
+
+  return collisions;
+}
+
+// find index of first character that is not 'L'
+function findFirstNonL(directions: string): number {
+  let i: number = 0;
+
+  while (i < directions.length && directions[i] === "L") {
+    i++;
+  }
+
+  return i;
+}
+
+// find index of last character that is not 'R'
+function findLastNonR(directions: string): number {
+  let i: number = directions.length - 1;
+
+  while (i >= 0 && directions[i] === "R") {
+    i--;
+  }
+
+  return i;
 }

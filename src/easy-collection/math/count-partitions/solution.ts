ccab7780ad29355661e9526e30344a9eec22fb65
Maxim Kryuk <mkryuk@gmail.com>
2025-12-06 15:55:19 +0200

    Math: 3432. Count Partitions with Even Sum Difference solution

--- src/easy-collection/math/count-partitions/solution.ts
+++ src/easy-collection/math/count-partitions/solution.ts
@@ -1,3 +1,16 @@
 export function countPartitions(nums: number[]): number {
-  return 0;
+  const n = nums.length;
+  const prefixSum = new Array(n + 1).fill(0);
+  const totalSum = nums.reduce((a, b) => a + b, 0);
+  let leftSum = 0;
+  let result = 0;
+  for (let i = 0; i < n - 1; i++) {
+    leftSum += nums[i];
+    const rightSum = totalSum - leftSum;
+    if ((rightSum - leftSum) % 2 === 0) {
+      result++;
+    }
+  }
+
+  return result;
 }

72e929500da80fb93831bc4d09d2bd01360c0118
Maxim Kryuk <mkryuk@gmail.com>
2025-12-11 12:22:10 +0100

    Array and Strings: 3577. Count the Number of Computer Unlocking Permutations solution

--- src/medium-collection/array-and-strings/count-permutations/solution.ts
+++ src/medium-collection/array-and-strings/count-permutations/solution.ts
@@ -1,3 +1,15 @@
 export function countPermutations(complexity: number[]): number {
-  return 0;
+  const MOD: number = 1e9 + 7;
+  let result: number = 1;
+
+  // check that every other computer has strictly higher complexity than computer 0
+  for (let i: number = 1; i < complexity.length; i++) {
+    if (complexity[i] <= complexity[0]) {
+      return 0;
+    }
+
+    result = (result * i) % MOD;
+  }
+
+  return result;
 }

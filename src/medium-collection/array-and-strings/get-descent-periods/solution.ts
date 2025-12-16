f7d839e2e922186a7736671d8579ed8704371b15
Maxim Kryuk <mkryuk@gmail.com>
2025-12-16 14:05:06 +0200

    Array and Strings: 2110. Number of Smooth Descent Periods of a Stock solution

--- src/medium-collection/array-and-strings/get-descent-periods/solution.ts
+++ src/medium-collection/array-and-strings/get-descent-periods/solution.ts
@@ -1,3 +1,13 @@
 export function getDescentPeriods(prices: number[]): number {
-  return 0;
+  let result = 0;
+  let runLen = 1;
+  for (let i = 0; i < prices.length; i++) {
+    if (i > 0 && prices[i - 1] - prices[i] === 1) {
+      runLen += 1;
+    } else {
+      runLen = 1;
+    }
+    result += runLen;
+  }
+  return result;
 }

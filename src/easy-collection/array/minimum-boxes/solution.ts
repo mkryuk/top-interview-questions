3a3b632359351805a7b6f1f761b5bbd78a62a057
Maxim Kryuk <mkryuk@gmail.com>
2025-12-25 12:18:15 +0200

    Array and Strings: 3074. Apple Redistribution into Boxes solution

--- src/easy-collection/array/minimum-boxes/solution.ts
+++ src/easy-collection/array/minimum-boxes/solution.ts
@@ -1,3 +1,11 @@
 export function minimumBoxes(apple: number[], capacity: number[]): number {
-  return 0;
+  let apples = apple.reduce((p, c) => p + c, 0);
+  capacity.sort((a, b) => b - a);
+  let result = 0;
+  let boxIndex = 0;
+  while (apples > 0 && boxIndex < capacity.length) {
+    apples -= capacity[boxIndex++];
+    result++;
+  }
+  return result;
 }

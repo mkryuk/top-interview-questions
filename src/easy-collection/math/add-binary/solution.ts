884e0a80f2ce1896238c5745e8956ce7e8bc511c
Maxim Kryuk <mkryuk@gmail.com>
2025-12-28 22:21:22 +0200

    Math: 67. Add Binary solution

--- src/easy-collection/math/add-binary/solution.ts
+++ src/easy-collection/math/add-binary/solution.ts
@@ -1,3 +1,26 @@
 export function addBinary(a: string, b: string): string {
-  return "";
+  let i: number = a.length - 1;
+  let j: number = b.length - 1;
+  let carry: number = 0;
+  const result: string[] = [];
+
+  while (i >= 0 || j >= 0 || carry !== 0) {
+    let sum: number = carry;
+
+    if (i >= 0) {
+      sum += a.charCodeAt(i) - 48; // '0' -> 48, '1' -> 49
+      i--;
+    }
+
+    if (j >= 0) {
+      sum += b.charCodeAt(j) - 48;
+      j--;
+    }
+
+    result.push((sum & 1) === 1 ? "1" : "0");
+    carry = sum >> 1;
+  }
+
+  result.reverse();
+  return result.join("");
 }

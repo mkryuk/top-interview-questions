bdc7ecaa6bb1c4d43a37e6f5384898bcba1db53d
Maxim Kryuk <mkryuk@gmail.com>
2026-01-10 23:23:52 +0200

    Trees and Graphs: 865. Smallest Subtree with all the Deepest Nodes solution

--- src/medium-collection/trees-and-graphs/subtree-with-all-depest/solution.ts
+++ src/medium-collection/trees-and-graphs/subtree-with-all-depest/solution.ts
@@ -1,5 +1,19 @@
 import { TreeNode } from "../common";
 
 export function subtreeWithAllDeepest(root: TreeNode | null): TreeNode | null {
-  return null;
+  return dfs(root)[1];
+}
+
+function dfs(node: TreeNode | null): [number, TreeNode | null] {
+  if (node === null) {
+    return [0, null];
+  }
+  const [leftDeep, leftNode] = dfs(node.left);
+  const [rightDeep, rightNode] = dfs(node.right);
+  if (leftDeep === rightDeep) {
+    return [leftDeep + 1, node];
+  } else if (leftDeep > rightDeep) {
+    return [leftDeep + 1, leftNode];
+  }
+  return [rightDeep + 1, rightNode];
 }

e1ce852ec4d4f1b74accb2535b75a6297ba36c6f
Maxim Kryuk <mkryuk@gmail.com>
2026-01-20 13:23:58 +0200

    Bit Manipulation: 3314. Construct the Minimum Bitwise Array I init

---
+++ src/easy-collection/bit-manipulation/min-bitwise-array/test.spec.ts
@@ -0,0 +1,15 @@
+import { minBitwiseArray } from "./solution";
+
+describe("3314. Construct the Minimum Bitwise Array I", () => {
+  it("minBitwiseArray should return [-1,1,4,3] for nums = [2,3,5,7]", () => {
+    const nums = [2, 3, 5, 7];
+    const result = minBitwiseArray(nums);
+    expect(result).toEqual([-1, 1, 4, 3]);
+  });
+
+  it("minBitwiseArray should return [9,12,15] for nums = [11,13,31]", () => {
+    const nums = [11, 13, 31];
+    const result = minBitwiseArray(nums);
+    expect(result).toEqual([9, 12, 15]);
+  });
+});

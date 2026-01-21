4589a039dd63fb84855d147ee920f9aaad8b48ce
Maxim Kryuk <mkryuk@gmail.com>
2026-01-21 12:58:40 +0200

    Bit Manipulation: 3314. Construct the Minimum Bitwise Array I solution

--- src/easy-collection/bit-manipulation/min-bitwise-array/solution.ts
+++ src/easy-collection/bit-manipulation/min-bitwise-array/solution.ts
@@ -1,3 +1,32 @@
 export function minBitwiseArray(nums: number[]): number[] {
-  return [];
+  const result: number[] = [];
+
+  for (let i: number = 0; i < nums.length; i++) {
+    const p: number = nums[i];
+
+    // only even prime is 2, and it has no solution
+    if (p === 2) {
+      result.push(-1);
+      continue;
+    }
+
+    const t: number = trailingOnesCount(p);
+    const k: number = t - 1;
+    const sub: number = 1 << k;
+
+    result.push(p - sub);
+  }
+
+  return result;
+}
+
+function trailingOnesCount(n: number): number {
+  let count: number = 0;
+
+  while ((n & 1) === 1) {
+    count++;
+    n = n >>> 1;
+  }
+
+  return count;
 }

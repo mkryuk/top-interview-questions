c6bea11e9377fd12f352cd70568ba50158065bf7
Maxim Kryuk <mkryuk@gmail.com>
2026-02-06 23:29:54 +0200

    Array and Strings: 3379. Transformed Array solution

--- src/easy-collection/array/construct-transformed-array/solution.ts
+++ src/easy-collection/array/construct-transformed-array/solution.ts
@@ -1,3 +1,9 @@
 export function constructTransformedArray(nums: number[]): number[] {
-  return [];
+  const n = nums.length;
+  const result = new Array(n);
+  for (let i = 0; i < n; i++) {
+    const j = (((nums[i] + i) % n) + n) % n;
+    result[i] = nums[j];
+  }
+  return result;
 }

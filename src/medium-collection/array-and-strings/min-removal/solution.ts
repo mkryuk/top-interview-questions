396d2f842cfb553b264d4a404988679860bb13ca
Maxim Kryuk <mkryuk@gmail.com>
2026-02-07 11:58:40 +0200

    Array and Strings: 3634. Minimum Removals to Balance Array solution

--- src/medium-collection/array-and-strings/min-removal/solution.ts
+++ src/medium-collection/array-and-strings/min-removal/solution.ts
@@ -1,3 +1,28 @@
 export function minRemoval(nums: number[], k: number): number {
-  return 0;
+  const arr: number[] = nums.slice();
+  arr.sort((a, b) => a - b);
+
+  const n: number = arr.length;
+  let best: number = 1;
+
+  let l: number = 0;
+  for (let r: number = 0; r < n; r += 1) {
+    while (l <= r && !isBalanced(arr[l], arr[r], k)) {
+      l += 1;
+    }
+
+    const len: number = r - l + 1;
+    if (len > best) {
+      best = len;
+    }
+  }
+
+  return n - best;
+}
+
+function isBalanced(minVal: number, maxVal: number, k: number): boolean {
+  // use bigint to avoid overflow: min can be 1e9, k can be 1e5 => product 1e14
+  const left: bigint = BigInt(maxVal);
+  const right: bigint = BigInt(minVal) * BigInt(k);
+  return left <= right;
 }

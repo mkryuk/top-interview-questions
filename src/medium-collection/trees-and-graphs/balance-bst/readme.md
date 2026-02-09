2267f7e3e69a0eaecc35fb9527307ddea7a7af2d
Maxim Kryuk <mkryuk@gmail.com>
2026-02-09 11:35:36 +0200

    Updated: 1382. Balance a Binary Search Tree

--- src/medium-collection/trees-and-graphs/balance-bst/readme.md
+++ src/medium-collection/trees-and-graphs/balance-bst/readme.md
@@ -1,4 +1,4 @@
-# Balance a Binary Search Tree
+# 1382. Balance a Binary Search Tree
 
 Given the root of a binary search tree, return a balanced binary search tree with the same node values. If there is more than one answer, return any of them.
 
--- src/medium-collection/trees-and-graphs/balance-bst/test.spec.ts
+++ src/medium-collection/trees-and-graphs/balance-bst/test.spec.ts
@@ -1,7 +1,7 @@
 import { arrayToTreeNode, treeNodeToArray } from "../common";
 import { balanceBST } from "./solution";
 
-describe("Balance a Binary Search Tree", () => {
+describe("1382. Balance a Binary Search Tree:", () => {
   it("balanceBST should return [2,1,3,null,null,null,4] for root = [1,null,2,null,3,null,4,null,null]", () => {
     const nodes = [1, null, 2, null, 3, null, 4, null, null];
     const root = arrayToTreeNode(nodes);

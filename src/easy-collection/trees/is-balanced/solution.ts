4c04ef134ddbd6d5989bb7579d0c4c43d7a7de80
Maxim Kryuk <mkryuk@gmail.com>
2026-02-09 11:34:07 +0200

    Trees and Graphs: 110. Balanced Binary Tree solution

--- src/easy-collection/trees/is-balanced/solution.ts
+++ src/easy-collection/trees/is-balanced/solution.ts
@@ -1,5 +1,24 @@
 import { TreeNode } from "../common";
 
 export function isBalanced(root: TreeNode | null): boolean {
-  return false;
+  return checkDepth(root) !== -1;
+}
+
+function checkDepth(node: TreeNode | null): number {
+  if (node === null) {
+    return 0;
+  }
+  const leftDepth = checkDepth(node.left);
+  if (leftDepth === -1) {
+    return -1;
+  }
+  const rightDepth = checkDepth(node.right);
+  if (rightDepth === -1) {
+    return -1;
+  }
+  const diff = Math.abs(leftDepth - rightDepth);
+  if (diff > 1) {
+    return -1;
+  }
+  return Math.max(leftDepth, rightDepth) + 1;
 }

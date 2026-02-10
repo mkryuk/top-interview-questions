83d3a583520bfe812da19a1111528f297321afe2
Maxim Kryuk <mkryuk@gmail.com>
2026-02-10 22:33:13 +0200

    Array and Strings: 3719. Longest Balanced Subarray I init

---
+++ src/easy-collection/array/longest-balanced-subarray-I/test.spec.ts
@@ -0,0 +1,21 @@
+import { longestBalanced } from "./solution";
+
+describe("3719. Longest Balanced Subarray I", () => {
+  it("longestBalanced should return 4 for nums = [2,5,4,3]", () => {
+    const nums = [2, 5, 4, 3];
+    const result = longestBalanced(nums);
+    expect(result).toEqual(4);
+  });
+
+  it("longestBalanced should return 5 for nums = [3,2,2,5,4]", () => {
+    const nums = [3, 2, 2, 5, 4];
+    const result = longestBalanced(nums);
+    expect(result).toEqual(5);
+  });
+
+  it("longestBalanced should return 3 for nums = [1,2,3,2]", () => {
+    const nums = [1, 2, 3, 2];
+    const result = longestBalanced(nums);
+    expect(result).toEqual(3);
+  });
+});

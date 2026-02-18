c466b66c04091f18a6721382d2830a2170ac8338
Maxim Kryuk <mkryuk@gmail.com>
2026-02-18 23:37:13 +0200

    Bit Manipulation: 401. Binary Watch solution

--- src/easy-collection/bit-manipulation/binary-watch/solution.ts
+++ src/easy-collection/bit-manipulation/binary-watch/solution.ts
@@ -1,3 +1,27 @@
 export function readBinaryWatch(turnedOn: number): string[] {
-  return [];
+  const result: string[] = [];
+  for (let h = 0; h < 12; h++) {
+    const hourBits = countBits(h);
+    for (let m = 0; m < 60; m++) {
+      const minuteBits = countBits(m);
+      if (hourBits + minuteBits === turnedOn) {
+        result.push(formatTime(h, m));
+      }
+    }
+  }
+  return result;
+}
+
+function countBits(num: number) {
+  let count = 0;
+  while (num > 0) {
+    num &= num - 1;
+    count++;
+  }
+  return count;
+}
+
+function formatTime(h: number, m: number) {
+  const mm = m < 10 ? `0${m}` : m;
+  return `${h}:${mm}`;
 }

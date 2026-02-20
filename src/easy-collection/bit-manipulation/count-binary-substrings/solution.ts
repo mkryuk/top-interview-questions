173a89e0ed66e4ddc99339ec7a081e5f59f29fb1
Maxim Kryuk <mkryuk@gmail.com>
2026-02-21 00:17:00 +0200

    Bit Manipulation: 696. Count Binary Substrings solution

--- src/easy-collection/bit-manipulation/count-binary-substrings/solution.ts
+++ src/easy-collection/bit-manipulation/count-binary-substrings/solution.ts
@@ -1,3 +1,19 @@
 export function countBinarySubstrings(s: string): number {
-  return 0;
+  let previousGroupLength = 0;
+  let currentGroupLength = 1;
+  let total = 0;
+
+  for (let i = 1; i < s.length; i++) {
+    if (s[i] === s[i - 1]) {
+      currentGroupLength++;
+    } else {
+      total += Math.min(previousGroupLength, currentGroupLength);
+      previousGroupLength = currentGroupLength;
+      currentGroupLength = 1;
+    }
+  }
+
+  total += Math.min(previousGroupLength, currentGroupLength);
+
+  return total;
 }

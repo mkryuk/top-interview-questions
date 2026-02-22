641949c9649942668c6e9eadb3077995061d88cd
Maxim Kryuk <mkryuk@gmail.com>
2026-02-22 23:58:18 +0200

    Bit Manipulation: 762. Prime Number of Set Bits in Binary Representation solution

--- src/easy-collection/bit-manipulation/prime-set-bits/solution.ts
+++ src/easy-collection/bit-manipulation/prime-set-bits/solution.ts
@@ -1,3 +1,20 @@
 export function countPrimeSetBits(left: number, right: number): number {
-  return 0;
+  const primes = new Set<number>([2, 3, 5, 7, 11, 13, 17, 19]);
+  let result = 0;
+  for (let num = left; num <= right; num++) {
+    const bits = countBits(num);
+    if (primes.has(bits)) {
+      result++;
+    }
+  }
+  return result;
+}
+
+function countBits(num: number): number {
+  let count = 0;
+  while (num > 0) {
+    num &= num - 1;
+    count++;
+  }
+  return count;
 }

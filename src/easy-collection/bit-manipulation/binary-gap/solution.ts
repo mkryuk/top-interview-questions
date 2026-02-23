940885f4f1286f0abeaa8cbeaf668b42b493c35d
Maxim Kryuk <mkryuk@gmail.com>
2026-02-23 13:53:54 +0200

    Bit Manipulation: 868. Binary Gap solution

--- src/easy-collection/bit-manipulation/binary-gap/solution.ts
+++ src/easy-collection/bit-manipulation/binary-gap/solution.ts
@@ -1,3 +1,17 @@
 export function binaryGap(n: number): number {
-  return 0;
+  let result: number = 0;
+  let lastOnePos: number = -1;
+  let index: number = 0;
+  while (n > 0) {
+    if ((n & 1) === 1) {
+      if (lastOnePos !== -1) {
+        result = Math.max(result, index - lastOnePos);
+      }
+      lastOnePos = index;
+    }
+    n = n >> 1;
+    index++;
+  }
+
+  return result;
 }

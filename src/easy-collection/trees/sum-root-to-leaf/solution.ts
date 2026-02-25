20a0fa965bff1dbfead144997d9c202b77f2e57e
Maxim Kryuk <mkryuk@gmail.com>
2026-02-25 22:46:56 +0200

    Trees and Graphs: 1022. Sum of Root To Leaf Binary Numbers solution

--- src/easy-collection/trees/sum-root-to-leaf/solution.ts
+++ src/easy-collection/trees/sum-root-to-leaf/solution.ts
@@ -1,5 +1,17 @@
 import { TreeNode } from "../common";
 
 export function sumRootToLeaf(root: TreeNode | null): number {
-  return 0;
+  return dfs(root, 0);
+}
+
+function dfs(node: TreeNode | null, current: number): number {
+  if (node === null) {
+    return 0;
+  }
+
+  const sum = (current << 1) | node.val;
+  if (node.left === null && node.right === null) {
+    return sum;
+  }
+  return dfs(node.left, sum) + dfs(node.right, sum);
 }

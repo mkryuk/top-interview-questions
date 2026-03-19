257d6ce5e678d268239074b7ced4a84dfeefcdf0
Maxim Kryuk <mkryuk@gmail.com>
2026-03-19 15:54:30 +0200

    Array and Strings: 3070. Count Submatrices with Top-Left Element and Sum Less Than k solution

--- src/medium-collection/array-and-strings/count-submatrices/solution.ts
+++ src/medium-collection/array-and-strings/count-submatrices/solution.ts
@@ -1,3 +1,28 @@
 export function countSubmatrices(grid: number[][], k: number): number {
-  return 0;
+  const rows: number = grid.length;
+  const cols: number = grid[0].length;
+  const prefix: number[][] = buildPrefixSum(grid, rows, cols);
+
+  let count: number = 0;
+  for (let r = 1; r <= rows; r++) {
+    for (let c = 1; c <= cols; c++) {
+      const sum: number = prefix[r][c];
+      if (sum <= k) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
+
+function buildPrefixSum(grid: number[][], rows: number, cols: number): number[][] {
+  const prefix: number[][] = Array.from({ length: rows + 1 }, () => Array(cols + 1).fill(0));
+  for (let r = 1; r <= rows; r++) {
+    for (let c = 1; c <= cols; c++) {
+      prefix[r][c] = grid[r - 1][c - 1] + prefix[r - 1][c] + prefix[r][c - 1] - prefix[r - 1][c - 1];
+    }
+  }
+
+  return prefix;
 }

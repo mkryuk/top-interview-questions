88f575346fb595ea2a413b46c54be41fe44c68c4
Maxim Kryuk <mkryuk@gmail.com>
2026-03-28 13:03:04 +0200

    Array and Strings: 2946. Matrix Similarity After Cyclic Shifts solution

--- src/easy-collection/array/are-similar/solution.ts
+++ src/easy-collection/array/are-similar/solution.ts
@@ -1,3 +1,13 @@
 export function areSimilar(mat: number[][], k: number): boolean {
-  return false;
+  const m: number = mat.length;
+  const n: number = mat[0].length;
+  k %= n;
+  for (let i: number = 0; i < m; i++) {
+    for (let j: number = 0; j < n; j++) {
+      if (mat[i][j] !== mat[i][(j + k) % n]) {
+        return false;
+      }
+    }
+  }
+  return true;
 }

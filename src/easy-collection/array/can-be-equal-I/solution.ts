f34f404bc2b57ca873cd9ab5831d68565b382e7e
Maxim Kryuk <mkryuk@gmail.com>
2026-03-30 16:09:23 +0300

    Array and Strings: 2839. Check if Strings Can be Made Equal With Operations I solution

--- src/easy-collection/array/can-be-equal-I/solution.ts
+++ src/easy-collection/array/can-be-equal-I/solution.ts
@@ -1,3 +1,13 @@
 export function canBeEqual(s1: string, s2: string): boolean {
-  return false;
+  const s1Even = [s1[0], s1[2]].sort().join("");
+  const s2Even = [s2[0], s2[2]].sort().join("");
+  const s1Odd = [s1[1], s1[3]].sort().join("");
+  const s2Odd = [s2[1], s2[3]].sort().join("");
+  if (s1Even !== s2Even) {
+    return false;
+  }
+  if (s1Odd !== s2Odd) {
+    return false;
+  }
+  return true;
 }

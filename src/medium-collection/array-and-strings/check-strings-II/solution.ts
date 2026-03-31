e5be50cf407605c12310fabb9def659107108b44
Maxim Kryuk <mkryuk@gmail.com>
2026-03-31 12:19:48 +0300

    Array and Strings: 2840. Check if Strings Can be Made Equal With Operations II solution

--- src/medium-collection/array-and-strings/check-strings-II/solution.ts
+++ src/medium-collection/array-and-strings/check-strings-II/solution.ts
@@ -1,3 +1,25 @@
 export function checkStrings(s1: string, s2: string): boolean {
-  return false;
+  const evenCount: number[] = new Array(26).fill(0);
+  const oddCount: number[] = new Array(26).fill(0);
+
+  for (let i: number = 0; i < s1.length; i++) {
+    const idx1: number = s1.charCodeAt(i) - 97;
+    const idx2: number = s2.charCodeAt(i) - 97;
+
+    if (i % 2 === 0) {
+      evenCount[idx1]++;
+      evenCount[idx2]--;
+    } else {
+      oddCount[idx1]++;
+      oddCount[idx2]--;
+    }
+  }
+
+  for (let i: number = 0; i < 26; i++) {
+    if (evenCount[i] !== 0 || oddCount[i] !== 0) {
+      return false;
+    }
+  }
+
+  return true;
 }

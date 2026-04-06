67dc4ed64ca83ed42cf5064d96f3e57b76b75d32
Maxim Kryuk <mkryuk@gmail.com>
2026-04-06 10:34:32 +0300

    Array and Strings: 657. Robot Return to Origin solution

--- src/easy-collection/strings/judge-circle/solution.ts
+++ src/easy-collection/strings/judge-circle/solution.ts
@@ -1,3 +1,17 @@
 export function judgeCircle(moves: string): boolean {
-  return false;
+  let x = 0;
+  let y = 0;
+  for (let i = 0; i < moves.length; i++) {
+    const move = moves[i];
+    if (move === "U") {
+      y++;
+    } else if (move === "D") {
+      y--;
+    } else if (move === "L") {
+      x--;
+    } else if (move === "R") {
+      x++;
+    }
+  }
+  return x === 0 && y === 0;
 }

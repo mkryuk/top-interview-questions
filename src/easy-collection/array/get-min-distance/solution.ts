8d1ec4f57fb8a7a4d2aeb3aeaf40c44d688534e7
Maxim Kryuk <mkryuk@gmail.com>
2026-04-14 12:48:29 +0300

    Array and Strings: 1848. Minimum Distance to the Target Element solution

--- src/easy-collection/array/get-min-distance/solution.ts
+++ src/easy-collection/array/get-min-distance/solution.ts
@@ -1,3 +1,10 @@
 export function getMinDistance(nums: number[], target: number, start: number): number {
-  return 0;
+  let result = Number.MAX_SAFE_INTEGER;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target) {
+      const distance = Math.abs(i - start);
+      result = Math.min(result, distance);
+    }
+  }
+  return result;
 }

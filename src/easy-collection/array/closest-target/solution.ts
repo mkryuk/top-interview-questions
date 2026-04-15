f70e0b895b470022a994d91184c10264b8148996
Maxim Kryuk <mkryuk@gmail.com>
2026-04-16 00:24:42 +0300

    Array and Strings: 2515. Shortest Distance to Target String in a Circular Array solution

--- src/easy-collection/array/closest-target/solution.ts
+++ src/easy-collection/array/closest-target/solution.ts
@@ -1,3 +1,20 @@
 export function closestTarget(words: string[], target: string, startIndex: number): number {
-  return 0;
+  const n = words.length;
+  let result = Number.MAX_SAFE_INTEGER;
+
+  for (let i = 0; i < n; i++) {
+    if (words[i] === target) {
+      const direct = Math.abs(i - startIndex);
+      const circular = n - direct;
+      const distance = Math.min(direct, circular);
+
+      result = Math.min(result, distance);
+    }
+  }
+
+  if (result === Number.MAX_SAFE_INTEGER) {
+    return -1;
+  }
+
+  return result;
 }

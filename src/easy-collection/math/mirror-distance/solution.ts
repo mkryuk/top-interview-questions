c4f67c3996162687e9b8753ea1950226ff899730
Maxim Kryuk <mkryuk@gmail.com>
2026-04-19 23:24:02 +0300

    Math: 3783. Mirror Distance of an Integer solution

--- src/easy-collection/math/mirror-distance/solution.ts
+++ src/easy-collection/math/mirror-distance/solution.ts
@@ -1,3 +1,13 @@
 export function mirrorDistance(n: number): number {
-  return 0;
+  const reversed = reverseNumber(n);
+  return Math.abs(n - reversed);
+}
+
+function reverseNumber(n: number): number {
+  let num = 0;
+  while (n > 0) {
+    num = num * 10 + (n % 10);
+    n = Math.floor(n / 10);
+  }
+  return num;
 }

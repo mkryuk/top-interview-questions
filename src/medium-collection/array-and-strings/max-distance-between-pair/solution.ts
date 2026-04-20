8c9ac24d0896786ac94397b52fc3535288d23313
Maxim Kryuk <mkryuk@gmail.com>
2026-04-20 09:31:44 +0200

    Array and Strings: 1855. Maximum Distance Between a Pair of Values solution

--- src/medium-collection/array-and-strings/max-distance-between-pair/solution.ts
+++ src/medium-collection/array-and-strings/max-distance-between-pair/solution.ts
@@ -1,3 +1,21 @@
 export function maxDistance(nums1: number[], nums2: number[]): number {
-  return 0;
+  let i = 0;
+  let j = 0;
+  let result = 0;
+
+  while (i < nums1.length && j < nums2.length) {
+    if (i > j) {
+      j++;
+      continue;
+    }
+
+    if (nums1[i] <= nums2[j]) {
+      result = Math.max(result, j - i);
+      j++;
+    } else {
+      i++;
+    }
+  }
+
+  return result;
 }

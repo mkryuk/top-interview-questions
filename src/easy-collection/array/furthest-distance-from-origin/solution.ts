405bf8ce1f8bc66f8e4cdd33646a89f7ff0c6a44
Maxim Kryuk <mkryuk@gmail.com>
2026-04-25 22:11:55 +0300

    Array and Strings: # 2833. Furthest Point From Origin solution

--- src/easy-collection/array/furthest-distance-from-origin/solution.ts
+++ src/easy-collection/array/furthest-distance-from-origin/solution.ts
@@ -1,3 +1,16 @@
 export function furthestDistanceFromOrigin(moves: string): number {
-  return 0;
+  let L = 0;
+  let R = 0;
+  let B = 0;
+  for (let i = 0; i < moves.length; i++) {
+    const move = moves[i];
+    if (move === "L") {
+      L++;
+    } else if (move === "R") {
+      R++;
+    } else {
+      B++;
+    }
+  }
+  return Math.abs(L - R) + B;
 }
